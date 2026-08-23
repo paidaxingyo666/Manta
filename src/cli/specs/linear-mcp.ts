0xnxxh/orca

fix(linear): unbound list-issues by default, surface truncation, bind cursor workspace (#15824)
Fixes STA-5076.

list-issues capped at 50 by default and hard-clamped at 250, with hasMore buried
under result.meta and no stderr warning for --json, so a page that stopped early
read as a complete answer. Omitting --limit now walks Linear's pages until they
run out (meta.limit is null), and --limit <n> is the only cap, paging past
Linear's 250-per-request maximum to reach it. result.truncated sits next to
result.issues and is set only when a cap actually held results back; human output
prints "truncated: showing N".

The read still has to fit the CLI's 60s RPC budget, so a 20s wall-clock deadline
and a 200-page ceiling stop the walk early and report truncated with a
continuation cursor rather than failing the command.

Also:
- issued --cursor values bind the resolved workspace, so call -> nextCursor ->
  call works without --workspace; raw Linear cursors still need one and now carry
  nextSteps
- issued cursors whose payload smuggles back `all` or an empty workspace are
  rejected at decode, since either would widen the read past the bound workspace
- JSON issue rows carry priorityLabel (none/urgent/high/medium/low), matching
  orca linear priority set
- truncated and priorityLabel are optional on the wire, so a host that predates
  either is not read as "complete"; readers fall back to meta.hasMore
- the truncation line prints the rows actually rendered, so a remote result with
  no meta.returned cannot print "showing undefined"

src/cli/specs/linear-mcp.ts
@@ -67,8 +67,15 @@ export const LINEAR_MCP_COMMAND_SPECS: CommandSpec[] = [
     ],
     examples: [
       'orca linear list-issues --team ENG --state started --assignee me --json',
-      'orca linear list-issues --query auth --updated-at -P7D --limit 100 --json',
-      'orca linear list-issues --cursor <cursor> --workspace <id> --json'
+      'orca linear list-issues --query auth --updated-at -P7D --json',
+      'orca linear list-issues --assignee me --priority 1 --limit 100 --json',
+      'orca linear list-issues --cursor <cursor> --json'
+    ],
+    notes: [
+      'Omitting --limit returns every match (result.meta.limit is null); --limit <n> caps the read.',
+      'JSON sets result.truncated when a cap held results back; text prints truncated: showing N.',
+      'Reuse --cursor from the previous page. Issued cursors bind the workspace; raw Linear cursors still need --workspace.',
+      '--priority is 0=none, 1=urgent, 2=high, 3=medium, 4=low. JSON includes priorityLabel on each issue.'
     ]
   },
   {

paidaxingyo666/Manta

(cherry picked from commit 3fca1d1648289eede0d69ffc13b80e466f9f50a8)

src/cli/specs/linear-mcp.ts
@@ -67,8 +67,15 @@ export const LINEAR_MCP_COMMAND_SPECS: CommandSpec[] = [
     ],
     examples: [
       'manta linear list-issues --team ENG --state started --assignee me --json',
-      'manta linear list-issues --query auth --updated-at -P7D --limit 100 --json',
-      'manta linear list-issues --cursor <cursor> --workspace <id> --json'
+      'manta linear list-issues --query auth --updated-at -P7D --json',
+      'manta linear list-issues --assignee me --priority 1 --limit 100 --json',
+      'manta linear list-issues --cursor <cursor> --json'
+    ],
+    notes: [
+      'Omitting --limit returns every match (result.meta.limit is null); --limit <n> caps the read.',
+      'JSON sets result.truncated when a cap held results back; text prints truncated: showing N.',
+      'Reuse --cursor from the previous page. Issued cursors bind the workspace; raw Linear cursors still need --workspace.',
+      '--priority is 0=none, 1=urgent, 2=high, 3=medium, 4=low. JSON includes priorityLabel on each issue.'
     ]
   },
   {