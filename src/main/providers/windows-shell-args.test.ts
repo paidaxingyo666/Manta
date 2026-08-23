2022zhang125/orca-yunxiao

fix(pty): wrap codex without expanding or destroying a user alias (#15873)
Co-authored-by: terry-li-hm <12233004+terry-li-hm@users.noreply.github.com>

src/main/providers/windows-shell-args.test.ts
@@ -390,7 +390,7 @@ describe('resolveWindowsShellLaunchArgs', () => {
     const zshEnv = readFileSync(join(getShellReadyWrapperRoot(), 'zsh', '.zshenv'), 'utf8')
     for (const wrapperFile of [bashRcfile, zshEnv]) {
       expect(wrapperFile).toContain('command omp --extension "${ORCA_OMP_STATUS_EXTENSION}" "$@"')
-      expect(wrapperFile).toContain('omp() { __orca_omp "$@"; }')
+      expect(wrapperFile).toContain('function omp { __orca_omp "$@"; }')
       expect(wrapperFile).not.toContain('prime-agent()')
       expect(wrapperFile).not.toContain('__orca_prime_agent')
       expect(wrapperFile).not.toContain('ORCA_PRIME_AGENT_STATUS_EXTENSION')

src/main/pty/codex-preflight-profile-path-rewrite.test.ts
@@ -1,5 +1,13 @@
 import { execFileSync } from 'node:child_process'
-import { chmodSync, existsSync, mkdirSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  chmodSync,
+  existsSync,
+  mkdirSync,
+  mkdtempSync,
+  readFileSync,
+  rmSync,
+  writeFileSync
+} from 'node:fs'
 import { tmpdir } from 'node:os'
 import { delimiter, join } from 'node:path'
 import { afterEach, describe, expect, it } from 'vitest'
@@ -21,6 +29,7 @@ type Fixture = {
   intendedMarker: string
   hijackMarker: string
   codexMarker: string
+  postSnippetMarker: string
   homePath: string
 }
 
@@ -30,7 +39,7 @@ function writeStub(path: string, markerPath: string): void {
   chmodSync(path, 0o755)
 }
 
-function buildFixture(): Fixture {
+function buildFixture(options: { aliasCodex?: boolean } = {}): Fixture {
   const root = mkdtempSync(join(tmpdir(), 'orca-codex-profile-path-'))
   roots.push(root)
   const resourcesPath = join(root, 'resources')
@@ -40,6 +49,7 @@ function buildFixture(): Fixture {
   const intendedMarker = join(root, 'intended-ran')
   const hijackMarker = join(root, 'hijack-ran')
   const codexMarker = join(root, 'codex-ran')
+  const postSnippetMarker = join(root, 'rcfile-continued')
   mkdirSync(hijackDir, { recursive: true })
   mkdirSync(codexDir, { recursive: true })
   mkdirSync(homePath, { recursive: true })
@@ -54,17 +64,33 @@ function buildFixture(): Fixture {
 
   // Why: /etc/profile runs first and macOS path_helper rebuilds PATH from /etc/paths,
   // so the profile must re-prepend both dirs to model a real user's dotfile winning.
+  const aliasLine = options.aliasCodex
+    ? `alias codex='GIT_AUTHOR_NAME=Codex codex --alias-flag'\n`
+    : ''
   writeFileSync(
     join(homePath, '.bash_profile'),
-    `export PATH=${JSON.stringify(codexDir)}:"$PATH"\nexport PATH=${JSON.stringify(hijackDir)}:"$PATH"\n`
+    `export PATH=${JSON.stringify(codexDir)}:"$PATH"\nexport PATH=${JSON.stringify(hijackDir)}:"$PATH"\n${aliasLine}`
   )
-  return { root, resourcesPath, intendedMarker, hijackMarker, codexMarker, homePath }
+  return {
+    root,
+    resourcesPath,
+    intendedMarker,
+    hijackMarker,
+    codexMarker,
+    postSnippetMarker,
+    homePath
+  }
 }
 
 function launchCodexThroughRcfile(fixture: Fixture, preflightValue: string): void {
   const rcfilePath = join(fixture.root, 'rcfile')
   writeFileSync(rcfilePath, getDaemonBashShellReadyRcfileContent(), 'utf8')
-  execFileSync('/bin/bash', ['--rcfile', rcfilePath, '-i', '-c', 'codex --version'], {
+  // Why the second command: the rcfile defines __orca_osc133_preexec *below* the
+  // codex wrapper, so its presence proves bash parsed past the snippet.
+  // Why the trailing `:` — the markers are this file's oracle, so a failed probe
+  // must surface as a missing marker, not an opaque non-zero exit from bash.
+  const command = `codex --version; declare -F __orca_osc133_preexec >/dev/null && printf ran > ${JSON.stringify(fixture.postSnippetMarker)}; :`
+  execFileSync('/bin/bash', ['--rcfile', rcfilePath, '-i', '-c', command], {
     stdio: 'ignore',
     env: {
       HOME: fixture.homePath,
@@ -112,6 +138,28 @@ describe.skipIf(!bashAvailable)('Codex preflight under a profile-rewritten PATH'
     expect(existsSync(fixture.hijackMarker)).toBe(false)
     // Guard against a vacuous pass: the wrapper must still have reached `command codex`.
     expect(existsSync(fixture.codexMarker)).toBe(true)
+  })
+
+  // Why: a user alias named codex used to expand into the wrapper's own `codex()`
+  // header, and bash abandons the rest of the rcfile at that syntax error — taking
+  // the OSC 133 hooks and the shell-ready marker defined below it with it.
+  it('finishes the rcfile and preserves the alias when the user aliased the name codex', () => {
+    const fixture = buildFixture({ aliasCodex: true })
+    const preflightCommand = resolveCodexShellLaunchPreflightCommand({
+      hooksEnabled: true,
+      isPackaged: true,
+      managedHomePath: join(fixture.root, 'codex-home'),
+      userDataPath: join(fixture.root, 'user-data'),
+      resourcesPath: fixture.resourcesPath
+    })
+
+    launchCodexThroughRcfile(fixture, preflightCommand as string)
+
+    expect(existsSync(fixture.postSnippetMarker)).toBe(true)
+    expect(existsSync(fixture.intendedMarker)).toBe(true)
+    expect(existsSync(fixture.hijackMarker)).toBe(false)
+    // The user's own alias must survive and still reach the binary.
+    expect(readFileSync(fixture.codexMarker, 'utf-8').trim()).toBe('--alias-flag --version')
   })
 
   // Why: pins the defect itself, so a regression back to an unqualified name fails here.

src/main/pty/codex-shell-launch-preflight.test.ts
@@ -1,4 +1,12 @@
-import { chmodSync, existsSync, mkdirSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  chmodSync,
+  existsSync,
+  mkdirSync,
+  mkdtempSync,
+  readFileSync,
+  rmSync,
+  writeFileSync
+} from 'node:fs'
 import { tmpdir } from 'node:os'
 import { delimiter, isAbsolute, join } from 'node:path'
 import { execFileSync, spawnSync } from 'node:child_process'
@@ -11,6 +19,8 @@ import {
 } from './codex-shell-launch-preflight'
 
 const roots: string[] = []
+const zshAvailable = existsSync('/bin/zsh')
+const bashAvailable = existsSync('/bin/bash')
 const fishAvailable = spawnSync('fish', ['--version']).status === 0
 const pwshAvailable =
   spawnSync('pwsh', ['-NoLogo', '-NoProfile', '-Command', 'exit 0']).status === 0
@@ -69,6 +79,57 @@ function runAliasLaunch(
   ).trim()
 }
 
+/** Launches a startup file that aliases the very name Orca wraps, then asserts the
+ *  wrapper installed, the preflight ran, and the user's alias still applies. */
+function expectNamedAliasSurvives(shell: string, enableAliases: string): void {
+  const root = mkdtempSync(join(tmpdir(), 'orca-codex-named-alias-'))
+  roots.push(root)
+  const bin = join(root, 'bin')
+  mkdirSync(bin)
+  const preflightMarker = join(root, 'preflight-ran')
+  writeExecutable(
+    join(bin, 'codex'),
+    '#!/bin/sh\nprintf "launched args=[%s] author=[%s]\\n" "$*" "$GIT_AUTHOR_NAME"\n'
+  )
+  writeExecutable(
+    join(bin, 'orca-test'),
+    `#!/bin/sh\nprintf '%s' "$*" > ${JSON.stringify(preflightMarker)}\n`
+  )
+  // Why nested in `if true`: the shell parses a whole compound command before
+  // running any of it, so the alias expands into the wrapper's own header.
+  const startup = join(root, 'startup.sh')
+  writeFileSync(
+    startup,
+    [
+      enableAliases,
+      "alias codex='GIT_AUTHOR_NAME=Codex codex --alias-flag'",
+      'if true; then',
+      getPosixCodexShellLaunchPreflight(),
+      'fi',
+      'printf "parsed\\n"',
+      'codex'
+    ].join('\n')
+  )
+
+  const result = spawnSync(
+    shell,
+    shell.endsWith('/zsh') ? ['-f', startup] : ['--noprofile', '--norc', startup],
+    {
+      encoding: 'utf-8',
+      env: {
+        ...process.env,
+        PATH: `${bin}${delimiter}${process.env.PATH ?? ''}`,
+        ORCA_CODEX_LAUNCH_PREFLIGHT: join(bin, 'orca-test')
+      }
+    }
+  )
+
+  expect(result.status, result.stderr).toBe(0)
+  // The alias's own flags and env prefix must still reach the binary.
+  expect(result.stdout).toBe('parsed\nlaunched args=[--alias-flag] author=[Codex]\n')
+  expect(readFileSync(preflightMarker, 'utf-8')).toBe('agent hooks prepare-codex')
+}
+
 afterEach(() => {
   for (const root of roots.splice(0)) {
     rmSync(root, { recursive: true, force: true })
@@ -92,6 +153,14 @@ describe.skipIf(process.platform === 'win32')('Codex shell launch preflight', ()
     expect(runAliasLaunch(root, getPosixCodexShellLaunchPreflight(), '/bin/zsh')).toBe('normal')
   })
 
+  it.skipIf(!zshAvailable)('keeps a user alias named codex working in zsh', () => {
+    expectNamedAliasSurvives('/bin/zsh', 'setopt aliases')
+  })
+
+  it.skipIf(!bashAvailable)('keeps a user alias named codex working in bash', () => {
+    expectNamedAliasSurvives('/bin/bash', 'shopt -s expand_aliases')
+  })
+
   it('still launches Codex when the best-effort preflight fails', () => {
     const root = mkdtempSync(join(tmpdir(), 'orca-codex-preflight-failure-'))
     roots.push(root)
@@ -101,31 +170,35 @@ describe.skipIf(process.platform === 'win32')('Codex shell launch preflight', ()
     )
   })
 
-  it.each([
+  // Why a loop over it.each: an early `return` reported green on hosts without the
+  // shell, so the zsh half silently never ran on Linux. skipIf reports it as a skip.
+  for (const [shell, strict] of [
     ['/bin/bash', 'set -e'],
     ['/bin/zsh', 'setopt ERR_EXIT']
-  ])('keeps %s startup alive under strict error handling when Codex is absent', (shell, strict) => {
-    if (!existsSync(shell)) {
-      return
-    }
-    const root = mkdtempSync(join(tmpdir(), 'orca-codex-strict-startup-'))
-    roots.push(root)
+  ]) {
+    it.skipIf(!existsSync(shell))(
+      `keeps ${shell} startup alive under strict error handling when Codex is absent`,
+      () => {
+        const root = mkdtempSync(join(tmpdir(), 'orca-codex-strict-startup-'))
+        roots.push(root)
 
-    const output = execFileSync(
-      shell,
-      [
-        shell.endsWith('/zsh') ? '-f' : '--noprofile',
-        '-c',
-        `${strict}\n${getPosixCodexShellLaunchPreflight()}\nprintf alive`
-      ],
-      {
-        encoding: 'utf-8',
-        env: { ...process.env, PATH: root, ORCA_CODEX_LAUNCH_PREFLIGHT: 'orca-test' }
+        const output = execFileSync(
+          shell,
+          [
+            shell.endsWith('/zsh') ? '-f' : '--noprofile',
+            '-c',
+            `${strict}\n${getPosixCodexShellLaunchPreflight()}\nprintf alive`
+          ],
+          {
+            encoding: 'utf-8',
+            env: { ...process.env, PATH: root, ORCA_CODEX_LAUNCH_PREFLIGHT: 'orca-test' }
+          }
+        )
+
+        expect(output).toBe('alive')
       }
     )
-
-    expect(output).toBe('alive')
-  })
+  }
 
   it.skipIf(!fishAvailable)('preserves a user-defined fish function', () => {
     const output = execFileSync(

src/main/pty/codex-shell-launch-preflight.ts
@@ -60,9 +60,15 @@ function isExecutableFileOnDisk(path: string, platform: NodeJS.Platform): boolea
 
 export function getPosixCodexShellLaunchPreflight(): string {
   return `# Why: a typed alias expands inside the shell, after pane launch prep.
-__orca_codex_binary="$(command -v codex 2>/dev/null || :)"
+# Why unalias inside the substitution: an alias named codex makes command -v
+# report the alias text, and the subshell leaves the user's own alias intact.
+# Why || : twice — zsh alone aborts inside the substitution, but every shell's
+# assignment adopts its exit status, so an absent codex trips set -e in bash too.
+__orca_codex_binary="$(unalias codex 2>/dev/null || :; command -v codex 2>/dev/null || :)"
 if [[ -n "\${ORCA_CODEX_LAUNCH_PREFLIGHT:-}" && -n "\${__orca_codex_binary:-}" && -x "\${__orca_codex_binary}" ]]; then
-  codex() {
+  # Why the function reserved word: it suppresses alias expansion of the name,
+  # which otherwise rewrites this header at parse time and aborts the whole file.
+  function codex {
     "\${ORCA_CODEX_LAUNCH_PREFLIGHT}" agent hooks prepare-codex >/dev/null 2>&1 || :
     command codex "$@"
   }

src/main/pty/omp-shell-wrapper.ts
@@ -66,7 +66,9 @@ __orca_omp() {
   fi
 }
 if [[ -n "\${ORCA_OMP_STATUS_EXTENSION:-}" ]]; then
-  omp() { __orca_omp "$@"; }
+  # Why the function reserved word: it suppresses alias expansion of the name, which
+  # an \`alias omp\` otherwise rewrites at parse time, aborting the rest of the file.
+  function omp { __orca_omp "$@"; }
 fi
 `
 }

paidaxingyo666/Manta

(cherry picked from commit 5f2fbd862e422509dcf516d1d8b3a487084bc540)

src/main/providers/windows-shell-args.test.ts
@@ -390,7 +390,7 @@ describe('resolveWindowsShellLaunchArgs', () => {
     const zshEnv = readFileSync(join(getShellReadyWrapperRoot(), 'zsh', '.zshenv'), 'utf8')
     for (const wrapperFile of [bashRcfile, zshEnv]) {
       expect(wrapperFile).toContain('command omp --extension "${MANTA_OMP_STATUS_EXTENSION}" "$@"')
-      expect(wrapperFile).toContain('omp() { __manta_omp "$@"; }')
+      expect(wrapperFile).toContain('function omp { __manta_omp "$@"; }')
       expect(wrapperFile).not.toContain('prime-agent()')
       expect(wrapperFile).not.toContain('__manta_prime_agent')
       expect(wrapperFile).not.toContain('MANTA_PRIME_AGENT_STATUS_EXTENSION')

src/main/pty/codex-preflight-profile-path-rewrite.test.ts
@@ -1,5 +1,13 @@
 import { execFileSync } from 'node:child_process'
-import { chmodSync, existsSync, mkdirSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  chmodSync,
+  existsSync,
+  mkdirSync,
+  mkdtempSync,
+  readFileSync,
+  rmSync,
+  writeFileSync
+} from 'node:fs'
 import { tmpdir } from 'node:os'
 import { delimiter, join } from 'node:path'
 import { afterEach, describe, expect, it } from 'vitest'
@@ -21,6 +29,7 @@ type Fixture = {
   intendedMarker: string
   hijackMarker: string
   codexMarker: string
+  postSnippetMarker: string
   homePath: string
 }
 
@@ -30,7 +39,7 @@ function writeStub(path: string, markerPath: string): void {
   chmodSync(path, 0o755)
 }
 
-function buildFixture(): Fixture {
+function buildFixture(options: { aliasCodex?: boolean } = {}): Fixture {
   const root = mkdtempSync(join(tmpdir(), 'manta-codex-profile-path-'))
   roots.push(root)
   const resourcesPath = join(root, 'resources')
@@ -40,6 +49,7 @@ function buildFixture(): Fixture {
   const intendedMarker = join(root, 'intended-ran')
   const hijackMarker = join(root, 'hijack-ran')
   const codexMarker = join(root, 'codex-ran')
+  const postSnippetMarker = join(root, 'rcfile-continued')
   mkdirSync(hijackDir, { recursive: true })
   mkdirSync(codexDir, { recursive: true })
   mkdirSync(homePath, { recursive: true })
@@ -54,17 +64,33 @@ function buildFixture(): Fixture {
 
   // Why: /etc/profile runs first and macOS path_helper rebuilds PATH from /etc/paths,
   // so the profile must re-prepend both dirs to model a real user's dotfile winning.
+  const aliasLine = options.aliasCodex
+    ? `alias codex='GIT_AUTHOR_NAME=Codex codex --alias-flag'\n`
+    : ''
   writeFileSync(
     join(homePath, '.bash_profile'),
-    `export PATH=${JSON.stringify(codexDir)}:"$PATH"\nexport PATH=${JSON.stringify(hijackDir)}:"$PATH"\n`
+    `export PATH=${JSON.stringify(codexDir)}:"$PATH"\nexport PATH=${JSON.stringify(hijackDir)}:"$PATH"\n${aliasLine}`
   )
-  return { root, resourcesPath, intendedMarker, hijackMarker, codexMarker, homePath }
+  return {
+    root,
+    resourcesPath,
+    intendedMarker,
+    hijackMarker,
+    codexMarker,
+    postSnippetMarker,
+    homePath
+  }
 }
 
 function launchCodexThroughRcfile(fixture: Fixture, preflightValue: string): void {
   const rcfilePath = join(fixture.root, 'rcfile')
   writeFileSync(rcfilePath, getDaemonBashShellReadyRcfileContent(), 'utf8')
-  execFileSync('/bin/bash', ['--rcfile', rcfilePath, '-i', '-c', 'codex --version'], {
+  // Why the second command: the rcfile defines __manta_osc133_preexec *below* the
+  // codex wrapper, so its presence proves bash parsed past the snippet.
+  // Why the trailing `:` — the markers are this file's oracle, so a failed probe
+  // must surface as a missing marker, not an opaque non-zero exit from bash.
+  const command = `codex --version; declare -F __manta_osc133_preexec >/dev/null && printf ran > ${JSON.stringify(fixture.postSnippetMarker)}; :`
+  execFileSync('/bin/bash', ['--rcfile', rcfilePath, '-i', '-c', command], {
     stdio: 'ignore',
     env: {
       HOME: fixture.homePath,
@@ -112,6 +138,28 @@ describe.skipIf(!bashAvailable)('Codex preflight under a profile-rewritten PATH'
     expect(existsSync(fixture.hijackMarker)).toBe(false)
     // Guard against a vacuous pass: the wrapper must still have reached `command codex`.
     expect(existsSync(fixture.codexMarker)).toBe(true)
+  })
+
+  // Why: a user alias named codex used to expand into the wrapper's own `codex()`
+  // header, and bash abandons the rest of the rcfile at that syntax error — taking
+  // the OSC 133 hooks and the shell-ready marker defined below it with it.
+  it('finishes the rcfile and preserves the alias when the user aliased the name codex', () => {
+    const fixture = buildFixture({ aliasCodex: true })
+    const preflightCommand = resolveCodexShellLaunchPreflightCommand({
+      hooksEnabled: true,
+      isPackaged: true,
+      managedHomePath: join(fixture.root, 'codex-home'),
+      userDataPath: join(fixture.root, 'user-data'),
+      resourcesPath: fixture.resourcesPath
+    })
+
+    launchCodexThroughRcfile(fixture, preflightCommand as string)
+
+    expect(existsSync(fixture.postSnippetMarker)).toBe(true)
+    expect(existsSync(fixture.intendedMarker)).toBe(true)
+    expect(existsSync(fixture.hijackMarker)).toBe(false)
+    // The user's own alias must survive and still reach the binary.
+    expect(readFileSync(fixture.codexMarker, 'utf-8').trim()).toBe('--alias-flag --version')
   })
 
   // Why: pins the defect itself, so a regression back to an unqualified name fails here.

src/main/pty/codex-shell-launch-preflight.test.ts
@@ -1,4 +1,12 @@
-import { chmodSync, existsSync, mkdirSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  chmodSync,
+  existsSync,
+  mkdirSync,
+  mkdtempSync,
+  readFileSync,
+  rmSync,
+  writeFileSync
+} from 'node:fs'
 import { tmpdir } from 'node:os'
 import { delimiter, isAbsolute, join } from 'node:path'
 import { execFileSync, spawnSync } from 'node:child_process'
@@ -11,6 +19,8 @@ import {
 } from './codex-shell-launch-preflight'
 
 const roots: string[] = []
+const zshAvailable = existsSync('/bin/zsh')
+const bashAvailable = existsSync('/bin/bash')
 const fishAvailable = spawnSync('fish', ['--version']).status === 0
 const pwshAvailable =
   spawnSync('pwsh', ['-NoLogo', '-NoProfile', '-Command', 'exit 0']).status === 0
@@ -69,6 +79,57 @@ function runAliasLaunch(
   ).trim()
 }
 
+/** Launches a startup file that aliases the very name Manta wraps, then asserts the
+ *  wrapper installed, the preflight ran, and the user's alias still applies. */
+function expectNamedAliasSurvives(shell: string, enableAliases: string): void {
+  const root = mkdtempSync(join(tmpdir(), 'manta-codex-named-alias-'))
+  roots.push(root)
+  const bin = join(root, 'bin')
+  mkdirSync(bin)
+  const preflightMarker = join(root, 'preflight-ran')
+  writeExecutable(
+    join(bin, 'codex'),
+    '#!/bin/sh\nprintf "launched args=[%s] author=[%s]\\n" "$*" "$GIT_AUTHOR_NAME"\n'
+  )
+  writeExecutable(
+    join(bin, 'manta-test'),
+    `#!/bin/sh\nprintf '%s' "$*" > ${JSON.stringify(preflightMarker)}\n`
+  )
+  // Why nested in `if true`: the shell parses a whole compound command before
+  // running any of it, so the alias expands into the wrapper's own header.
+  const startup = join(root, 'startup.sh')
+  writeFileSync(
+    startup,
+    [
+      enableAliases,
+      "alias codex='GIT_AUTHOR_NAME=Codex codex --alias-flag'",
+      'if true; then',
+      getPosixCodexShellLaunchPreflight(),
+      'fi',
+      'printf "parsed\\n"',
+      'codex'
+    ].join('\n')
+  )
+
+  const result = spawnSync(
+    shell,
+    shell.endsWith('/zsh') ? ['-f', startup] : ['--noprofile', '--norc', startup],
+    {
+      encoding: 'utf-8',
+      env: {
+        ...process.env,
+        PATH: `${bin}${delimiter}${process.env.PATH ?? ''}`,
+        MANTA_CODEX_LAUNCH_PREFLIGHT: join(bin, 'manta-test')
+      }
+    }
+  )
+
+  expect(result.status, result.stderr).toBe(0)
+  // The alias's own flags and env prefix must still reach the binary.
+  expect(result.stdout).toBe('parsed\nlaunched args=[--alias-flag] author=[Codex]\n')
+  expect(readFileSync(preflightMarker, 'utf-8')).toBe('agent hooks prepare-codex')
+}
+
 afterEach(() => {
   for (const root of roots.splice(0)) {
     rmSync(root, { recursive: true, force: true })
@@ -92,6 +153,14 @@ describe.skipIf(process.platform === 'win32')('Codex shell launch preflight', ()
     expect(runAliasLaunch(root, getPosixCodexShellLaunchPreflight(), '/bin/zsh')).toBe('normal')
   })
 
+  it.skipIf(!zshAvailable)('keeps a user alias named codex working in zsh', () => {
+    expectNamedAliasSurvives('/bin/zsh', 'setopt aliases')
+  })
+
+  it.skipIf(!bashAvailable)('keeps a user alias named codex working in bash', () => {
+    expectNamedAliasSurvives('/bin/bash', 'shopt -s expand_aliases')
+  })
+
   it('still launches Codex when the best-effort preflight fails', () => {
     const root = mkdtempSync(join(tmpdir(), 'manta-codex-preflight-failure-'))
     roots.push(root)
@@ -101,31 +170,35 @@ describe.skipIf(process.platform === 'win32')('Codex shell launch preflight', ()
     )
   })
 
-  it.each([
+  // Why a loop over it.each: an early `return` reported green on hosts without the
+  // shell, so the zsh half silently never ran on Linux. skipIf reports it as a skip.
+  for (const [shell, strict] of [
     ['/bin/bash', 'set -e'],
     ['/bin/zsh', 'setopt ERR_EXIT']
-  ])('keeps %s startup alive under strict error handling when Codex is absent', (shell, strict) => {
-    if (!existsSync(shell)) {
-      return
-    }
-    const root = mkdtempSync(join(tmpdir(), 'manta-codex-strict-startup-'))
-    roots.push(root)
+  ]) {
+    it.skipIf(!existsSync(shell))(
+      `keeps ${shell} startup alive under strict error handling when Codex is absent`,
+      () => {
+        const root = mkdtempSync(join(tmpdir(), 'manta-codex-strict-startup-'))
+        roots.push(root)
 
-    const output = execFileSync(
-      shell,
-      [
-        shell.endsWith('/zsh') ? '-f' : '--noprofile',
-        '-c',
-        `${strict}\n${getPosixCodexShellLaunchPreflight()}\nprintf alive`
-      ],
-      {
-        encoding: 'utf-8',
-        env: { ...process.env, PATH: root, MANTA_CODEX_LAUNCH_PREFLIGHT: 'manta-test' }
+        const output = execFileSync(
+          shell,
+          [
+            shell.endsWith('/zsh') ? '-f' : '--noprofile',
+            '-c',
+            `${strict}\n${getPosixCodexShellLaunchPreflight()}\nprintf alive`
+          ],
+          {
+            encoding: 'utf-8',
+            env: { ...process.env, PATH: root, MANTA_CODEX_LAUNCH_PREFLIGHT: 'manta-test' }
+          }
+        )
+
+        expect(output).toBe('alive')
       }
     )
-
-    expect(output).toBe('alive')
-  })
+  }
 
   it.skipIf(!fishAvailable)('preserves a user-defined fish function', () => {
     const output = execFileSync(

src/main/pty/codex-shell-launch-preflight.ts
@@ -60,9 +60,15 @@ function isExecutableFileOnDisk(path: string, platform: NodeJS.Platform): boolea
 
 export function getPosixCodexShellLaunchPreflight(): string {
   return `# Why: a typed alias expands inside the shell, after pane launch prep.
-__manta_codex_binary="$(command -v codex 2>/dev/null || :)"
+# Why unalias inside the substitution: an alias named codex makes command -v
+# report the alias text, and the subshell leaves the user's own alias intact.
+# Why || : twice — zsh alone aborts inside the substitution, but every shell's
+# assignment adopts its exit status, so an absent codex trips set -e in bash too.
+__manta_codex_binary="$(unalias codex 2>/dev/null || :; command -v codex 2>/dev/null || :)"
 if [[ -n "\${MANTA_CODEX_LAUNCH_PREFLIGHT:-}" && -n "\${__manta_codex_binary:-}" && -x "\${__manta_codex_binary}" ]]; then
-  codex() {
+  # Why the function reserved word: it suppresses alias expansion of the name,
+  # which otherwise rewrites this header at parse time and aborts the whole file.
+  function codex {
     "\${MANTA_CODEX_LAUNCH_PREFLIGHT}" agent hooks prepare-codex >/dev/null 2>&1 || :
     command codex "$@"
   }

src/main/pty/omp-shell-wrapper.ts
@@ -66,7 +66,9 @@ __manta_omp() {
   fi
 }
 if [[ -n "\${MANTA_OMP_STATUS_EXTENSION:-}" ]]; then
-  omp() { __manta_omp "$@"; }
+  # Why the function reserved word: it suppresses alias expansion of the name, which
+  # an \`alias omp\` otherwise rewrites at parse time, aborting the rest of the file.
+  function omp { __manta_omp "$@"; }
 fi
 `
 }