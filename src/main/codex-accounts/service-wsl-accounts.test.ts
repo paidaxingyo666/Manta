0xnxxh/orca

fix(wsl): migrate 21 call sites onto the WSL runner (#15923)
* fix(wsl): migrate 21 call sites onto the runner, after five review rounds

Rebased onto main now that the runner (#15903) has landed.

21 sites across 15 files move off ad-hoc `execFile('wsl.exe', ...)`. Allowlist
23 -> 16 on the WSL guard; 163 -> 152 on the W1 child_process guard, which moved
as a consequence.

Five review rounds, each finding real defects -- several introduced by the
previous round's fixes:

1. Hooks ran user orca.yaml scripts under dash; probe failure fell back to the
   login shell, reintroducing the ~/.profile stall the runner exists to remove.
2. An unparseable probe was cached permanently, disabling every WSL feature on
   the distro; hooks regressed from "runs degraded" to "fails".
3. Exit 127 had no expiry; a starved 5s probe hard-failed the 10s scan behind
   it; a joiner burned its budget on someone else's probe.
4. The comment stripper blanked live code, so the windowsHide guard walked past
   a real unguarded spawn and reported the file clean; an ownership-probe
   timeout silently deselected the user's Claude account.
5. Verification of the guards themselves.

The recurring finding -- a call answering "is this installed?" on a degraded
PATH -- was eventually fixed structurally rather than per-caller: the runner
refuses an unresolved guest PATH unless the caller opts in. Per-site vigilance
was demonstrably not holding; 3 of 8 sites had already forgotten the analogous
exit-code check.

Remaining 16 files need a runner mode that does not exist: a long-lived
streaming child (OAuth logins, hook relay), a synchronous caller, or a
host-level flag like --status that the guest-command API cannot express.

* fix(wsl): close round 5's P1s -- degrade where PATH was never needed

Round 5 measured the guards by re-executing their algorithms standalone rather
than reading them, and found four things.

P1 -- four skill/plugin paths gained a hard dependency on the login-shell probe
that the

src/main/codex-accounts/service-wsl-accounts.test.ts
@@ -3,7 +3,8 @@ import { EventEmitter } from 'node:events'
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { PassThrough } from 'node:stream'
-import { buildWslCodexAvailabilityArgs, buildWslCodexLoginArgs } from './wsl-codex-command'
+import { buildWslCodexAvailabilityScript, buildWslCodexLoginArgs } from './wsl-codex-command'
+import type { WslResult, WslSpec } from '../wsl/wsl-runner'
 import {
   createCodexAuthJson,
   createRateLimits,
@@ -33,6 +34,14 @@ function decodeEncodedWslBashCommand(command: string): string {
   return encoded ? Buffer.from(encoded, 'base64').toString('utf8') : command
 }
 
+function wslOk(stdout = ''): WslResult {
+  return { environmentResolved: true, code: 0, stdout, stderr: '', timedOut: false }
+}
+
+function wslFailed(code: number, stderr = ''): WslResult {
+  return { environmentResolved: true, code, stdout: '', stderr, timedOut: false }
+}
+
 describe('CodexAccountService config sync', () => {
   registerCodexAccountsTestHomes()
 
@@ -56,22 +65,24 @@ describe('CodexAccountService config sync', () => {
     const execFileSyncMock = vi.fn((_command: string, args: string[]) => {
       const script = decodeEncodedWslBashCommand(String(args.at(-1)))
       expect(args.slice(0, 2)).toEqual(['-d', 'Debian'])
+      expect(script).toContain('readlink -f')
+      return `${wslLinuxHomePath}\n`
+    })
+    const runWslProcessMock = vi.fn(async (spec: WslSpec) => {
+      const script = String(spec.script)
+      expect(spec.distro).toBe('Debian')
+      expect(spec.lane).toBe('probe')
       if (script.includes('WSL_DISTRO_NAME')) {
-        return 'Debian\n/home/alice\n'
+        return wslOk('Debian\n/home/alice\n')
       }
-      if (script.includes('readlink -f')) {
-        return `${wslLinuxHomePath}\n`
+      if (script.includes('_orca_lookup_command=')) {
+        expect(script).toBe(buildWslCodexAvailabilityScript())
+        return wslOk()
       }
-      if (script.includes('command -v codex')) {
-        throw new Error('bash -ic does not inherit the distro login-shell PATH')
-      }
-      if (args.slice(2, 5).join(' ') === '--exec sh -c') {
-        expect(args).toEqual(buildWslCodexAvailabilityArgs('Debian'))
-        return ''
-      }
+      expect(script).toContain('mkdir -p ')
       mkdirSync(wslManagedHomePath, { recursive: true })
       writeFileSync(join(wslManagedHomePath, '.orca-managed-home'), 'account-id-for-test\n')
-      return ''
+      return wslOk()
     })
     const spawnMock = vi.fn((command: string, args: string[]) => {
       expect(command).toBe('wsl.exe')
@@ -110,6 +121,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Debian', linuxPath: wslLinuxHomePath } : null
@@ -172,19 +184,22 @@ describe('CodexAccountService config sync', () => {
     const execFileSyncMock = vi.fn((_command: string, args: string[]) => {
       const script = decodeEncodedWslBashCommand(String(args.at(-1)))
       expect(args.slice(0, 2)).toEqual(['-d', 'Debian'])
+      expect(script).toContain('readlink -f')
+      return `${wslLinuxHomePath}\n`
+    })
+    const runWslProcessMock = vi.fn(async (spec: WslSpec) => {
+      const script = String(spec.script)
+      expect(spec.distro).toBe('Debian')
       if (script.includes('WSL_DISTRO_NAME')) {
-        return 'Debian\n/home/alice\n'
+        return wslOk('Debian\n/home/alice\n')
       }
-      if (script.includes('readlink -f')) {
-        return `${wslLinuxHomePath}\n`
-      }
-      if (args.slice(2, 5).join(' ') === '--exec sh -c') {
-        expect(args).toEqual(buildWslCodexAvailabilityArgs('Debian'))
-        throw new Error('codex missing')
+      if (script.includes('_orca_lookup_command=')) {
+        expect(script).toBe(buildWslCodexAvailabilityScript())
+        return wslFailed(1, 'codex missing')
       }
       mkdirSync(wslManagedHomePath, { recursive: true })
       writeFileSync(join(wslManagedHomePath, '.orca-managed-home'), 'account-id-for-test\n')
-      return ''
+      return wslOk()
     })
     const spawnMock = vi.fn()
 
@@ -195,6 +210,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Debian', linuxPath: wslLinuxHomePath } : null
@@ -260,6 +276,7 @@ describe('CodexAccountService config sync', () => {
       }
       return ''
     })
+    const runWslProcessMock = vi.fn(async () => wslOk())
     let clearSelectionDuringLogin = (): void => {}
     const spawnMock = vi.fn((command: string, args: string[]) => {
       expect(command).toBe('wsl.exe')
@@ -292,6 +309,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Ubuntu', linuxPath: wslLinuxHomePath } : null
@@ -386,15 +404,19 @@ describe('CodexAccountService config sync', () => {
 
     const execFileSyncMock = vi.fn((_command: string, args: string[]) => {
       const script = decodeEncodedWslBashCommand(String(args.at(-1)))
-      if (script.includes('mkdir -p -- "$candidate"')) {
-        mkdirSync(wslManagedHomePath, { recursive: true })
-        writeFileSync(join(wslManagedHomePath, '.orca-managed-home'), 'account-1\n', 'utf-8')
-        return ''
-      }
       if (script.includes('readlink -f')) {
         return `${wslLinuxHomePath}\n`
       }
       return ''
+    })
+    const runWslProcessMock = vi.fn(async (spec: WslSpec) => {
+      const script = String(spec.script)
+      if (script.includes('mkdir -p -- "$candidate"')) {
+        expect(spec.shell).toBe('bash')
+        mkdirSync(wslManagedHomePath, { recursive: true })
+        writeFileSync(join(wslManagedHomePath, '.orca-managed-home'), 'account-1\n', 'utf-8')
+      }
+      return wslOk()
     })
     const spawnMock = vi.fn((command: string, args: string[]) => {
       expect(command).toBe('wsl.exe')
@@ -423,6 +445,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Ubuntu', linuxPath: wslLinuxHomePath } : null
@@ -512,6 +535,7 @@ describe('CodexAccountService config sync', () => {
       }),
       spawn: vi.fn()
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: vi.fn(async () => wslOk()) }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Ubuntu', linuxPath: wslLinuxHomePath } : null

src/main/ipc/preflight-agent-refresh.test.ts
@@ -30,6 +30,11 @@ const {
   mergePersistedWindowsPathMock: vi.fn()
 }))
 
+const runWslProcessMock = vi.hoisted(() => vi.fn())
+// Why the runner and not child_process: WSL agent detection goes through
+// runWslProcess now, so a child_process mock never sees it.
+vi.mock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
+
 vi.mock('electron', () => ({
   ipcMain: {
     handle: handleMock
@@ -91,6 +96,7 @@ describe('preflight', () => {
   const handlers: HandlerMap = {}
 
   beforeEach(() => {
+    runWslProcessMock.mockReset()
     resetPreflightMocks(
       {
         handleMock,
@@ -122,13 +128,9 @@ describe('preflight', () => {
       configurable: true,
       value: 'win32'
     })
-    execFileAsyncMock.mockImplementation(async (command, args) => {
-      if (command !== 'wsl.exe') {
-        throw new Error(`unexpected command ${String(command)}`)
-      }
-      const script = String(args[5])
+    runWslProcessMock.mockImplementation(async ({ script }: { script: string }) => {
       if (script.includes("'claude'")) {
-        return { stdout: '__ORCA_AGENT_PATH__claude\t/home/test/.local/bin/claude\n' }
+        return { environmentResolved: true, code: 0, stdout: '__ORCA_AGENT_PATH__claude\t/home/test/.local/bin/claude\n', stderr: '', timedOut: false }
       }
       throw new Error('not found')
     })
@@ -181,7 +183,7 @@ describe('preflight', () => {
         throw new Error(`unexpected command ${String(command)}`)
       }
       if (String(args[0]) === 'opencode') {
-        return { stdout: '/Users/test/.opencode/bin/opencode\n' }
+        return { environmentResolved: true, code: 0, stdout: '/Users/test/.opencode/bin/opencode\n', stderr: '', timedOut: false }
       }
       throw new Error('not found')
     })
@@ -217,7 +219,7 @@ describe('preflight', () => {
         throw new Error(`unexpected command ${String(command)}`)
       }
       if (String(args[0]) === 'claude') {
-        return { stdout: '/Users/test/.local/bin/claude\n' }
+        return { environmentResolved: true, code: 0, stdout: '/Users/test/.local/bin/claude\n', stderr: '', timedOut: false }
       }
       throw new Error('not found')
     })

src/main/wsl-fish-history-cleanup.test.ts
@@ -5,6 +5,8 @@ import {
   flushWslFishHistoryCleanups
 } from './wsl-fish-history-cleanup'
 
+const okResult = { environmentResolved: true, code: 0, stdout: '', stderr: '', timedOut: false }
+
 // Why reset on BOTH sides: the cleanup queue is module state, so one test's
 // pending work would otherwise serialize behind — or deadlock — the next.
 beforeEach(() => {
@@ -18,22 +20,18 @@ afterEach(async () => {
 
 describe('deleteWslFishHistoryFile', () => {
   it('uses direct argv and bounds a distro cleanup subprocess', async () => {
-    const run = vi.fn().mockResolvedValue({ stdout: '', stderr: '' })
+    const run = vi.fn().mockResolvedValue(okResult)
 
     await deleteWslFishHistoryFile('Ubuntu Test', 'orca_0123456789abcdef', run)
 
-    expect(run).toHaveBeenCalledWith(
-      'wsl.exe',
-      [
-        '--distribution',
-        'Ubuntu Test',
-        '--exec',
-        'fish',
-        '--command',
-        expect.stringContaining('orca_0123456789abcdef_history')
-      ],
-      { timeout: 5_000, windowsHide: true }
-    )
+    expect(run).toHaveBeenCalledWith({
+      distro: 'Ubuntu Test',
+      lane: 'probe',
+      allowDegradedEnvironment: true,
+      program: 'fish',
+      args: ['--command', expect.stringContaining('orca_0123456789abcdef_history')],
+      timeoutMs: 5_000
+    })
   })
 
   it('rejects an unsafe session before spawning', async () => {
@@ -50,7 +48,7 @@ describe('deleteWslFishHistoryFile', () => {
     let settle!: () => void
     const run = vi.fn().mockReturnValue(
       new Promise((resolve) => {
-        settle = () => resolve({ stdout: '', stderr: '' })
+        settle = () => resolve(okResult)
       })
     )
     const session = 'orca_0123456789abcdef'
@@ -65,7 +63,7 @@ describe('deleteWslFishHistoryFile', () => {
   })
 
   it('keeps distinct distros independent of each other', async () => {
-    const run = vi.fn().mockResolvedValue({ stdout: '', stderr: '' })
+    const run = vi.fn().mockResolvedValue(okResult)
 
     await Promise.all([
       deleteWslFishHistoryFile('Ubuntu', 'orca_0123456789abcdef', run),
@@ -76,14 +74,27 @@ describe('deleteWslFishHistoryFile', () => {
   })
 
   it('permits a retry once a failed cleanup settles', async () => {
-    const run = vi.fn().mockRejectedValueOnce(new Error('distro offline')).mockResolvedValue({
-      stdout: '',
-      stderr: ''
-    })
+    const run = vi.fn().mockRejectedValueOnce(new Error('distro offline')).mockResolvedValue(okResult)
     const session = 'orca_0123456789abcdef'
 
     await expect(deleteWslFishHistoryFile('Ubuntu', session, run)).rejects.toThrow('distro offline')
     await expect(deleteWslFishHistoryFile('Ubuntu', session, run)).resolves.toBeUndefined()
     expect(run).toHaveBeenCalledTimes(2)
+  })
+
+  it('treats a non-zero exit as a failed cleanup even though the runner resolved', async () => {
+    const run = vi.fn().mockResolvedValue({ ...okResult, code: 1 })
+
+    await expect(
+      deleteWslFishHistoryFile('Ubuntu', 'orca_0123456789abcdef', run)
+    ).rejects.toThrow('wsl fish history cleanup failed')
+  })
+
+  it('treats a runner timeout as a failed cleanup', async () => {
+    const run = vi.fn().mockResolvedValue({ ...okResult, code: null, timedOut: true })
+
+    await expect(
+      deleteWslFishHistoryFile('Ubuntu', 'orca_0123456789abcdef', run)
+    ).rejects.toThrow('wsl fish history cleanup failed')
   })
 })

paidaxingyo666/Manta

src/main/codex-accounts/service-wsl-accounts.test.ts
@@ -3,7 +3,8 @@ import { EventEmitter } from 'node:events'
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { PassThrough } from 'node:stream'
-import { buildWslCodexAvailabilityArgs, buildWslCodexLoginArgs } from './wsl-codex-command'
+import { buildWslCodexAvailabilityScript, buildWslCodexLoginArgs } from './wsl-codex-command'
+import type { WslResult, WslSpec } from '../wsl/wsl-runner'
 import {
   createCodexAuthJson,
   createRateLimits,
@@ -33,6 +34,14 @@ function decodeEncodedWslBashCommand(command: string): string {
   return encoded ? Buffer.from(encoded, 'base64').toString('utf8') : command
 }
 
+function wslOk(stdout = ''): WslResult {
+  return { environmentResolved: true, code: 0, stdout, stderr: '', timedOut: false }
+}
+
+function wslFailed(code: number, stderr = ''): WslResult {
+  return { environmentResolved: true, code, stdout: '', stderr, timedOut: false }
+}
+
 describe('CodexAccountService config sync', () => {
   registerCodexAccountsTestHomes()
 
@@ -57,22 +66,24 @@ describe('CodexAccountService config sync', () => {
     const execFileSyncMock = vi.fn((_command: string, args: string[]) => {
       const script = decodeEncodedWslBashCommand(String(args.at(-1)))
       expect(args.slice(0, 2)).toEqual(['-d', 'Debian'])
+      expect(script).toContain('readlink -f')
+      return `${wslLinuxHomePath}\n`
+    })
+    const runWslProcessMock = vi.fn(async (spec: WslSpec) => {
+      const script = String(spec.script)
+      expect(spec.distro).toBe('Debian')
+      expect(spec.lane).toBe('probe')
       if (script.includes('WSL_DISTRO_NAME')) {
-        return 'Debian\n/home/alice\n'
+        return wslOk('Debian\n/home/alice\n')
       }
-      if (script.includes('readlink -f')) {
-        return `${wslLinuxHomePath}\n`
+      if (script.includes('_manta_lookup_command=')) {
+        expect(script).toBe(buildWslCodexAvailabilityScript())
+        return wslOk()
       }
-      if (script.includes('command -v codex')) {
-        throw new Error('bash -ic does not inherit the distro login-shell PATH')
-      }
-      if (args.slice(2, 5).join(' ') === '--exec sh -c') {
-        expect(args).toEqual(buildWslCodexAvailabilityArgs('Debian'))
-        return ''
-      }
+      expect(script).toContain('mkdir -p ')
       mkdirSync(wslManagedHomePath, { recursive: true })
       writeFileSync(join(wslManagedHomePath, '.manta-managed-home'), 'account-id-for-test\n')
-      return ''
+      return wslOk()
     })
     const spawnMock = vi.fn((command: string, args: string[]) => {
       expect(command).toBe('wsl.exe')
@@ -111,6 +122,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Debian', linuxPath: wslLinuxHomePath } : null
@@ -174,19 +186,22 @@ describe('CodexAccountService config sync', () => {
     const execFileSyncMock = vi.fn((_command: string, args: string[]) => {
       const script = decodeEncodedWslBashCommand(String(args.at(-1)))
       expect(args.slice(0, 2)).toEqual(['-d', 'Debian'])
+      expect(script).toContain('readlink -f')
+      return `${wslLinuxHomePath}\n`
+    })
+    const runWslProcessMock = vi.fn(async (spec: WslSpec) => {
+      const script = String(spec.script)
+      expect(spec.distro).toBe('Debian')
       if (script.includes('WSL_DISTRO_NAME')) {
-        return 'Debian\n/home/alice\n'
+        return wslOk('Debian\n/home/alice\n')
       }
-      if (script.includes('readlink -f')) {
-        return `${wslLinuxHomePath}\n`
-      }
-      if (args.slice(2, 5).join(' ') === '--exec sh -c') {
-        expect(args).toEqual(buildWslCodexAvailabilityArgs('Debian'))
-        throw new Error('codex missing')
+      if (script.includes('_manta_lookup_command=')) {
+        expect(script).toBe(buildWslCodexAvailabilityScript())
+        return wslFailed(1, 'codex missing')
       }
       mkdirSync(wslManagedHomePath, { recursive: true })
       writeFileSync(join(wslManagedHomePath, '.manta-managed-home'), 'account-id-for-test\n')
-      return ''
+      return wslOk()
     })
     const spawnMock = vi.fn()
 
@@ -197,6 +212,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Debian', linuxPath: wslLinuxHomePath } : null
@@ -262,6 +278,7 @@ describe('CodexAccountService config sync', () => {
       }
       return ''
     })
+    const runWslProcessMock = vi.fn(async () => wslOk())
     let clearSelectionDuringLogin = (): void => {}
     const spawnMock = vi.fn((command: string, args: string[]) => {
       expect(command).toBe('wsl.exe')
@@ -294,6 +311,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Ubuntu', linuxPath: wslLinuxHomePath } : null
@@ -388,15 +406,19 @@ describe('CodexAccountService config sync', () => {
 
     const execFileSyncMock = vi.fn((_command: string, args: string[]) => {
       const script = decodeEncodedWslBashCommand(String(args.at(-1)))
-      if (script.includes('mkdir -p -- "$candidate"')) {
-        mkdirSync(wslManagedHomePath, { recursive: true })
-        writeFileSync(join(wslManagedHomePath, '.manta-managed-home'), 'account-1\n', 'utf-8')
-        return ''
-      }
       if (script.includes('readlink -f')) {
         return `${wslLinuxHomePath}\n`
       }
       return ''
+    })
+    const runWslProcessMock = vi.fn(async (spec: WslSpec) => {
+      const script = String(spec.script)
+      if (script.includes('mkdir -p -- "$candidate"')) {
+        expect(spec.shell).toBe('bash')
+        mkdirSync(wslManagedHomePath, { recursive: true })
+        writeFileSync(join(wslManagedHomePath, '.manta-managed-home'), 'account-1\n', 'utf-8')
+      }
+      return wslOk()
     })
     const spawnMock = vi.fn((command: string, args: string[]) => {
       expect(command).toBe('wsl.exe')
@@ -425,6 +447,7 @@ describe('CodexAccountService config sync', () => {
       execFileSync: execFileSyncMock,
       spawn: spawnMock
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Ubuntu', linuxPath: wslLinuxHomePath } : null
@@ -514,6 +537,7 @@ describe('CodexAccountService config sync', () => {
       }),
       spawn: vi.fn()
     }))
+    vi.doMock('../wsl/wsl-runner', () => ({ runWslProcess: vi.fn(async () => wslOk()) }))
     vi.doMock('../../shared/wsl-paths', () => ({
       parseWslUncPath: (path: string) =>
         path === wslManagedHomePath ? { distro: 'Ubuntu', linuxPath: wslLinuxHomePath } : null

src/main/ipc/preflight-agent-refresh.test.ts
@@ -30,6 +30,11 @@ const {
   mergePersistedWindowsPathMock: vi.fn()
 }))
 
+const runWslProcessMock = vi.hoisted(() => vi.fn())
+// Why the runner and not child_process: WSL agent detection goes through
+// runWslProcess now, so a child_process mock never sees it.
+vi.mock('../wsl/wsl-runner', () => ({ runWslProcess: runWslProcessMock }))
+
 vi.mock('electron', () => ({
   ipcMain: {
     handle: handleMock
@@ -91,6 +96,7 @@ describe('preflight', () => {
   const handlers: HandlerMap = {}
 
   beforeEach(() => {
+    runWslProcessMock.mockReset()
     resetPreflightMocks(
       {
         handleMock,
@@ -122,13 +128,9 @@ describe('preflight', () => {
       configurable: true,
       value: 'win32'
     })
-    execFileAsyncMock.mockImplementation(async (command, args) => {
-      if (command !== 'wsl.exe') {
-        throw new Error(`unexpected command ${String(command)}`)
-      }
-      const script = String(args[5])
+    runWslProcessMock.mockImplementation(async ({ script }: { script: string }) => {
       if (script.includes("'claude'")) {
-        return { stdout: '__MANTA_AGENT_PATH__claude\t/home/test/.local/bin/claude\n' }
+        return { environmentResolved: true, code: 0, stdout: '__MANTA_AGENT_PATH__claude\t/home/test/.local/bin/claude\n', stderr: '', timedOut: false }
       }
       throw new Error('not found')
     })
@@ -181,7 +183,7 @@ describe('preflight', () => {
         throw new Error(`unexpected command ${String(command)}`)
       }
       if (String(args[0]) === 'opencode') {
-        return { stdout: '/Users/test/.opencode/bin/opencode\n' }
+        return { environmentResolved: true, code: 0, stdout: '/Users/test/.opencode/bin/opencode\n', stderr: '', timedOut: false }
       }
       throw new Error('not found')
     })
@@ -217,7 +219,7 @@ describe('preflight', () => {
         throw new Error(`unexpected command ${String(command)}`)
       }
       if (String(args[0]) === 'claude') {
-        return { stdout: '/Users/test/.local/bin/claude\n' }
+        return { environmentResolved: true, code: 0, stdout: '/Users/test/.local/bin/claude\n', stderr: '', timedOut: false }
       }
       throw new Error('not found')
     })

src/main/wsl-fish-history-cleanup.test.ts
@@ -5,6 +5,8 @@ import {
   flushWslFishHistoryCleanups
 } from './wsl-fish-history-cleanup'
 
+const okResult = { environmentResolved: true, code: 0, stdout: '', stderr: '', timedOut: false }
+
 // Why reset on BOTH sides: the cleanup queue is module state, so one test's
 // pending work would otherwise serialize behind — or deadlock — the next.
 beforeEach(() => {
@@ -18,22 +20,18 @@ afterEach(async () => {
 
 describe('deleteWslFishHistoryFile', () => {
   it('uses direct argv and bounds a distro cleanup subprocess', async () => {
-    const run = vi.fn().mockResolvedValue({ stdout: '', stderr: '' })
+    const run = vi.fn().mockResolvedValue(okResult)
 
     await deleteWslFishHistoryFile('Ubuntu Test', 'manta_0123456789abcdef', run)
 
-    expect(run).toHaveBeenCalledWith(
-      'wsl.exe',
-      [
-        '--distribution',
-        'Ubuntu Test',
-        '--exec',
-        'fish',
-        '--command',
-        expect.stringContaining('manta_0123456789abcdef_history')
-      ],
-      { timeout: 5_000, windowsHide: true }
-    )
+    expect(run).toHaveBeenCalledWith({
+      distro: 'Ubuntu Test',
+      lane: 'probe',
+      allowDegradedEnvironment: true,
+      program: 'fish',
+      args: ['--command', expect.stringContaining('manta_0123456789abcdef_history')],
+      timeoutMs: 5_000
+    })
   })
 
   it('rejects an unsafe session before spawning', async () => {
@@ -50,7 +48,7 @@ describe('deleteWslFishHistoryFile', () => {
     let settle!: () => void
     const run = vi.fn().mockReturnValue(
       new Promise((resolve) => {
-        settle = () => resolve({ stdout: '', stderr: '' })
+        settle = () => resolve(okResult)
       })
     )
     const session = 'manta_0123456789abcdef'
@@ -65,7 +63,7 @@ describe('deleteWslFishHistoryFile', () => {
   })
 
   it('keeps distinct distros independent of each other', async () => {
-    const run = vi.fn().mockResolvedValue({ stdout: '', stderr: '' })
+    const run = vi.fn().mockResolvedValue(okResult)
 
     await Promise.all([
       deleteWslFishHistoryFile('Ubuntu', 'manta_0123456789abcdef', run),
@@ -76,14 +74,27 @@ describe('deleteWslFishHistoryFile', () => {
   })
 
   it('permits a retry once a failed cleanup settles', async () => {
-    const run = vi.fn().mockRejectedValueOnce(new Error('distro offline')).mockResolvedValue({
-      stdout: '',
-      stderr: ''
-    })
+    const run = vi.fn().mockRejectedValueOnce(new Error('distro offline')).mockResolvedValue(okResult)
     const session = 'manta_0123456789abcdef'
 
     await expect(deleteWslFishHistoryFile('Ubuntu', session, run)).rejects.toThrow('distro offline')
     await expect(deleteWslFishHistoryFile('Ubuntu', session, run)).resolves.toBeUndefined()
     expect(run).toHaveBeenCalledTimes(2)
+  })
+
+  it('treats a non-zero exit as a failed cleanup even though the runner resolved', async () => {
+    const run = vi.fn().mockResolvedValue({ ...okResult, code: 1 })
+
+    await expect(
+      deleteWslFishHistoryFile('Ubuntu', 'manta_0123456789abcdef', run)
+    ).rejects.toThrow('wsl fish history cleanup failed')
+  })
+
+  it('treats a runner timeout as a failed cleanup', async () => {
+    const run = vi.fn().mockResolvedValue({ ...okResult, code: null, timedOut: true })
+
+    await expect(
+      deleteWslFishHistoryFile('Ubuntu', 'manta_0123456789abcdef', run)
+    ).rejects.toThrow('wsl fish history cleanup failed')
   })
 })