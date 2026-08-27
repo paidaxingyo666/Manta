2022zhang125/orca-yunxiao

fix(orchestration): enforce nested worker depth instead of an accidental fence (#16668)
* fix(orchestration): enforce nested worker depth instead of an accidental fence

Orca documented that "dispatched workers cannot spawn their own sub-workers
(worker-start is coordinator-fenced)". No such check existed. What existed was a
single Run-binding check in the workerStart RPC: a worker's terminal is not bound
to a Run, so worker-start happened to fail. The rule was emergent, asserted by no
test, and written in no doc — and it leaked. A worker could run-create its own
Run, task-create, and worker-start: now bound, the check passed.

Replace it with a real, configurable depth cap.

Depth is derived from the caller's own active Dispatch rather than from Run
binding, which is what dissolves the run-create bypass: creating a Run does not
stop you being a worker. Enforcement lives in a single dispatch-row writer that
owns all three INSERTs that mint a live worker — the generic claim, the supervised
worker-start path (including every retry), and the remote attachment. Two of those
were missed by earlier drafts of this change, so `creator` and `maxDepth` are
required parameters: a new spawn path cannot compile without deciding, and a
boundary test refuses the SQL anywhere else.

Schema v30 adds depth to dispatch_contexts and remote_dispatch_attachments,
NOT NULL DEFAULT 1 and backfilled to 1 so an unstamped or pre-upgrade row fails
closed rather than reading as a root coordinator. The attachment pane indexes
widen to the five states in which a remote worker may still be running:
loss of contact is not evidence of process death, so an unverifiable worker still
counts as a nesting parent.

Also adds the caller-evidence assertion that workerStart was the only Run-scoped
verb to skip, so a declared --from cannot name another terminal's pane and inherit
its depth.

Default is 1, so behaviour is unchanged unless the new setting is raised. Two
limitations are deliberate and documented

src/main/runtime/rpc/methods/orchestration-run-scope.ts
@@ -2,7 +2,10 @@ import type { OrchestrationCompatibilityEvidence } from '../../../../shared/orch
 import { orchestrationSkillRecoveryData } from '../../../../shared/orchestration-rpc-contract'
 import { OrchestrationError } from '../../orchestration/orchestration-error'
 import type { RunRow } from '../../orchestration/types'
-import type { OrcaRuntimeService } from '../../orca-runtime'
+import type {
+  OrcaRuntimeService,
+  OrchestrationCompatibilityCallerAuthority
+} from '../../orca-runtime'
 
 export type RunScopeParams = {
   runId?: string
@@ -31,6 +34,49 @@ export function assertCallerHandleMatchesEvidence(
       { effectsApplied: false }
     )
   }
+}
+
+export type OrchestrationCallerParams = {
+  callerTerminalHandle: string
+  callerEvidence?: OrchestrationCompatibilityEvidence
+  callerAuthority?: OrchestrationCompatibilityCallerAuthority
+  /** Preserve legacy callers that treated a missing pane as an ordinary fence. */
+  requireStablePane?: boolean
+  /**
+   * Skip attestation here because the caller performs it itself — run-use must run
+   * its legacy-takeover check between pane resolution and attestation. Setting this
+   * without asserting elsewhere reopens the hole this helper exists to close.
+   */
+  evidenceAssertedByCaller?: boolean
+}
+
+/** Resolve the caller's runtime pane and, by default, attest its declared handle. */
+export function resolveOrchestrationCaller(
+  runtime: OrcaRuntimeService,
+  params: OrchestrationCallerParams & { requireStablePane: true }
+): string
+export function resolveOrchestrationCaller(
+  runtime: OrcaRuntimeService,
+  params: OrchestrationCallerParams
+): string | null
+export function resolveOrchestrationCaller(
+  runtime: OrcaRuntimeService,
+  params: OrchestrationCallerParams
+): string | null {
+  if (!params.evidenceAssertedByCaller) {
+    assertCallerHandleMatchesEvidence(runtime, params.callerTerminalHandle, params.callerEvidence)
+  }
+  const paneKey =
+    params.callerAuthority?.terminalHandle === params.callerTerminalHandle
+      ? params.callerAuthority.paneKey
+      : runtime.getTerminalPaneKey(params.callerTerminalHandle)
+  if (!paneKey && params.requireStablePane) {
+    throw new OrchestrationError(
+      'stable_pane_required',
+      'The coordinator terminal has no stable pane identity. Run this command inside a live Orca terminal.'
+    )
+  }
+  return paneKey ?? null
 }
 
 // Why: task and gate mutations must share one Run-binding rule.

paidaxingyo666/Manta

(cherry picked from commit 8a07bbd8cf58dcaca62868101627eddc6feeb0ac)

src/main/runtime/rpc/methods/orchestration-run-scope.ts
@@ -2,7 +2,10 @@ import type { OrchestrationCompatibilityEvidence } from '../../../../shared/orch
 import { orchestrationSkillRecoveryData } from '../../../../shared/orchestration-rpc-contract'
 import { OrchestrationError } from '../../orchestration/orchestration-error'
 import type { RunRow } from '../../orchestration/types'
-import type { MantaRuntimeService } from '../../manta-runtime'
+import type {
+  MantaRuntimeService,
+  OrchestrationCompatibilityCallerAuthority
+} from '../../manta-runtime'
 
 export type RunScopeParams = {
   runId?: string
@@ -31,6 +34,49 @@ export function assertCallerHandleMatchesEvidence(
       { effectsApplied: false }
     )
   }
+}
+
+export type OrchestrationCallerParams = {
+  callerTerminalHandle: string
+  callerEvidence?: OrchestrationCompatibilityEvidence
+  callerAuthority?: OrchestrationCompatibilityCallerAuthority
+  /** Preserve legacy callers that treated a missing pane as an ordinary fence. */
+  requireStablePane?: boolean
+  /**
+   * Skip attestation here because the caller performs it itself — run-use must run
+   * its legacy-takeover check between pane resolution and attestation. Setting this
+   * without asserting elsewhere reopens the hole this helper exists to close.
+   */
+  evidenceAssertedByCaller?: boolean
+}
+
+/** Resolve the caller's runtime pane and, by default, attest its declared handle. */
+export function resolveOrchestrationCaller(
+  runtime: MantaRuntimeService,
+  params: OrchestrationCallerParams & { requireStablePane: true }
+): string
+export function resolveOrchestrationCaller(
+  runtime: MantaRuntimeService,
+  params: OrchestrationCallerParams
+): string | null
+export function resolveOrchestrationCaller(
+  runtime: MantaRuntimeService,
+  params: OrchestrationCallerParams
+): string | null {
+  if (!params.evidenceAssertedByCaller) {
+    assertCallerHandleMatchesEvidence(runtime, params.callerTerminalHandle, params.callerEvidence)
+  }
+  const paneKey =
+    params.callerAuthority?.terminalHandle === params.callerTerminalHandle
+      ? params.callerAuthority.paneKey
+      : runtime.getTerminalPaneKey(params.callerTerminalHandle)
+  if (!paneKey && params.requireStablePane) {
+    throw new OrchestrationError(
+      'stable_pane_required',
+      'The coordinator terminal has no stable pane identity. Run this command inside a live Manta terminal.'
+    )
+  }
+  return paneKey ?? null
 }
 
 // Why: task and gate mutations must share one Run-binding rule.