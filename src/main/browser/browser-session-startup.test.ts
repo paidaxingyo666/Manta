2022zhang125/orca-yunxiao

STA-4150: client-hosted remote browser (consolidated) (#15448)
Co-authored-by: Jinwoo-H <jinwoo@stably.ai>

src/main/browser/browser-session-startup.test.ts
@@ -2,10 +2,16 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 
 function installRegistryMock(): {
   configureForOrcaProfileMock: ReturnType<typeof vi.fn>
+  configureRouteSessionsForOrcaProfileMock: ReturnType<typeof vi.fn>
+  configurePairedRuntimeBrowserClientHostsForOrcaProfileMock: ReturnType<typeof vi.fn>
+  collectOrphanedBrowserRoutePartitionStorageMock: ReturnType<typeof vi.fn>
   applyPendingCookieImportMock: ReturnType<typeof vi.fn>
   initializeBrowserSessionsFromPersistedStateMock: ReturnType<typeof vi.fn>
 } {
   const configureForOrcaProfileMock = vi.fn()
+  const configureRouteSessionsForOrcaProfileMock = vi.fn()
+  const configurePairedRuntimeBrowserClientHostsForOrcaProfileMock = vi.fn()
+  const collectOrphanedBrowserRoutePartitionStorageMock = vi.fn(async () => [])
   const applyPendingCookieImportMock = vi.fn()
   const initializeBrowserSessionsFromPersistedStateMock = vi.fn()
 
@@ -16,9 +22,22 @@ function installRegistryMock(): {
       initializeBrowserSessionsFromPersistedState: initializeBrowserSessionsFromPersistedStateMock
     }
   }))
+  vi.doMock('./browser-route-session-runtime', () => ({
+    configureRouteSessionsForOrcaProfile: configureRouteSessionsForOrcaProfileMock
+  }))
+  vi.doMock('./browser-route-partition-storage-runtime', () => ({
+    collectOrphanedBrowserRoutePartitionStorage: collectOrphanedBrowserRoutePartitionStorageMock
+  }))
+  vi.doMock('./paired-runtime-browser-client-host-runtime', () => ({
+    configurePairedRuntimeBrowserClientHostsForOrcaProfile:
+      configurePairedRuntimeBrowserClientHostsForOrcaProfileMock
+  }))
 
   return {
     configureForOrcaProfileMock,
+    configureRouteSessionsForOrcaProfileMock,
+    configurePairedRuntimeBrowserClientHostsForOrcaProfileMock,
+    collectOrphanedBrowserRoutePartitionStorageMock,
     applyPendingCookieImportMock,
     initializeBrowserSessionsFromPersistedStateMock
   }
@@ -47,6 +66,8 @@ describe('initializeBrowserSessionsForApp', () => {
   it('configures the active Orca profile before replaying browser sessions', async () => {
     const {
       configureForOrcaProfileMock,
+      configureRouteSessionsForOrcaProfileMock,
+      configurePairedRuntimeBrowserClientHostsForOrcaProfileMock,
       applyPendingCookieImportMock,
       initializeBrowserSessionsFromPersistedStateMock
     } = installRegistryMock()
@@ -61,12 +82,53 @@ describe('initializeBrowserSessionsForApp', () => {
       orcaProfileId: 'local-work',
       profileDirectory: '/profiles/local-work'
     })
+    expect(configureRouteSessionsForOrcaProfileMock).toHaveBeenCalledWith({
+      orcaProfileId: 'local-work',
+      profileDirectory: '/profiles/local-work'
+    })
+    expect(configurePairedRuntimeBrowserClientHostsForOrcaProfileMock).toHaveBeenCalledWith({
+      orcaProfileId: 'local-work'
+    })
     expect(configureForOrcaProfileMock.mock.invocationCallOrder[0]).toBeLessThan(
       applyPendingCookieImportMock.mock.invocationCallOrder[0]
     )
+    expect(configureRouteSessionsForOrcaProfileMock.mock.invocationCallOrder[0]).toBeLessThan(
+      applyPendingCookieImportMock.mock.invocationCallOrder[0]
+    )
+    expect(
+      configurePairedRuntimeBrowserClientHostsForOrcaProfileMock.mock.invocationCallOrder[0]
+    ).toBeLessThan(applyPendingCookieImportMock.mock.invocationCallOrder[0])
     expect(applyPendingCookieImportMock.mock.invocationCallOrder[0]).toBeLessThan(
       initializeBrowserSessionsFromPersistedStateMock.mock.invocationCallOrder[0]
     )
+  })
+
+  it('sweeps orphaned route partitions once the profile binding runtime is configured', async () => {
+    const {
+      configureRouteSessionsForOrcaProfileMock,
+      collectOrphanedBrowserRoutePartitionStorageMock
+    } = installRegistryMock()
+    const { initializeBrowserSessionsForApp } = await import('./browser-session-startup')
+
+    initializeBrowserSessionsForApp({
+      orcaProfileId: 'local-work',
+      profileDirectory: '/profiles/local-work'
+    })
+
+    expect(collectOrphanedBrowserRoutePartitionStorageMock).toHaveBeenCalledOnce()
+    // Hoisting the sweep above the binding runtime leaves it with no active profile and it collects nothing.
+    expect(configureRouteSessionsForOrcaProfileMock.mock.invocationCallOrder[0]).toBeLessThan(
+      collectOrphanedBrowserRoutePartitionStorageMock.mock.invocationCallOrder[0]
+    )
+  })
+
+  it('does not sweep route partitions when no profile is active', async () => {
+    const { collectOrphanedBrowserRoutePartitionStorageMock } = installRegistryMock()
+    const { initializeBrowserSessionsForApp } = await import('./browser-session-startup')
+
+    initializeBrowserSessionsForApp()
+
+    expect(collectOrphanedBrowserRoutePartitionStorageMock).not.toHaveBeenCalled()
   })
 
   it('initializes browser sessions once per app process', async () => {

paidaxingyo666/Manta

(cherry picked from commit a9781a4118ba507ed30486eff03b2de5b6ad1936)

src/main/browser/browser-session-startup.test.ts
@@ -2,10 +2,16 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 
 function installRegistryMock(): {
   configureForMantaProfileMock: ReturnType<typeof vi.fn>
+  configureRouteSessionsForMantaProfileMock: ReturnType<typeof vi.fn>
+  configurePairedRuntimeBrowserClientHostsForMantaProfileMock: ReturnType<typeof vi.fn>
+  collectOrphanedBrowserRoutePartitionStorageMock: ReturnType<typeof vi.fn>
   applyPendingCookieImportMock: ReturnType<typeof vi.fn>
   initializeBrowserSessionsFromPersistedStateMock: ReturnType<typeof vi.fn>
 } {
   const configureForMantaProfileMock = vi.fn()
+  const configureRouteSessionsForMantaProfileMock = vi.fn()
+  const configurePairedRuntimeBrowserClientHostsForMantaProfileMock = vi.fn()
+  const collectOrphanedBrowserRoutePartitionStorageMock = vi.fn(async () => [])
   const applyPendingCookieImportMock = vi.fn()
   const initializeBrowserSessionsFromPersistedStateMock = vi.fn()
 
@@ -16,9 +22,22 @@ function installRegistryMock(): {
       initializeBrowserSessionsFromPersistedState: initializeBrowserSessionsFromPersistedStateMock
     }
   }))
+  vi.doMock('./browser-route-session-runtime', () => ({
+    configureRouteSessionsForMantaProfile: configureRouteSessionsForMantaProfileMock
+  }))
+  vi.doMock('./browser-route-partition-storage-runtime', () => ({
+    collectOrphanedBrowserRoutePartitionStorage: collectOrphanedBrowserRoutePartitionStorageMock
+  }))
+  vi.doMock('./paired-runtime-browser-client-host-runtime', () => ({
+    configurePairedRuntimeBrowserClientHostsForMantaProfile:
+      configurePairedRuntimeBrowserClientHostsForMantaProfileMock
+  }))
 
   return {
     configureForMantaProfileMock,
+    configureRouteSessionsForMantaProfileMock,
+    configurePairedRuntimeBrowserClientHostsForMantaProfileMock,
+    collectOrphanedBrowserRoutePartitionStorageMock,
     applyPendingCookieImportMock,
     initializeBrowserSessionsFromPersistedStateMock
   }
@@ -47,6 +66,8 @@ describe('initializeBrowserSessionsForApp', () => {
   it('configures the active Manta profile before replaying browser sessions', async () => {
     const {
       configureForMantaProfileMock,
+      configureRouteSessionsForMantaProfileMock,
+      configurePairedRuntimeBrowserClientHostsForMantaProfileMock,
       applyPendingCookieImportMock,
       initializeBrowserSessionsFromPersistedStateMock
     } = installRegistryMock()
@@ -61,12 +82,53 @@ describe('initializeBrowserSessionsForApp', () => {
       mantaProfileId: 'local-work',
       profileDirectory: '/profiles/local-work'
     })
+    expect(configureRouteSessionsForMantaProfileMock).toHaveBeenCalledWith({
+      mantaProfileId: 'local-work',
+      profileDirectory: '/profiles/local-work'
+    })
+    expect(configurePairedRuntimeBrowserClientHostsForMantaProfileMock).toHaveBeenCalledWith({
+      mantaProfileId: 'local-work'
+    })
     expect(configureForMantaProfileMock.mock.invocationCallOrder[0]).toBeLessThan(
       applyPendingCookieImportMock.mock.invocationCallOrder[0]
     )
+    expect(configureRouteSessionsForMantaProfileMock.mock.invocationCallOrder[0]).toBeLessThan(
+      applyPendingCookieImportMock.mock.invocationCallOrder[0]
+    )
+    expect(
+      configurePairedRuntimeBrowserClientHostsForMantaProfileMock.mock.invocationCallOrder[0]
+    ).toBeLessThan(applyPendingCookieImportMock.mock.invocationCallOrder[0])
     expect(applyPendingCookieImportMock.mock.invocationCallOrder[0]).toBeLessThan(
       initializeBrowserSessionsFromPersistedStateMock.mock.invocationCallOrder[0]
     )
+  })
+
+  it('sweeps orphaned route partitions once the profile binding runtime is configured', async () => {
+    const {
+      configureRouteSessionsForMantaProfileMock,
+      collectOrphanedBrowserRoutePartitionStorageMock
+    } = installRegistryMock()
+    const { initializeBrowserSessionsForApp } = await import('./browser-session-startup')
+
+    initializeBrowserSessionsForApp({
+      mantaProfileId: 'local-work',
+      profileDirectory: '/profiles/local-work'
+    })
+
+    expect(collectOrphanedBrowserRoutePartitionStorageMock).toHaveBeenCalledOnce()
+    // Hoisting the sweep above the binding runtime leaves it with no active profile and it collects nothing.
+    expect(configureRouteSessionsForMantaProfileMock.mock.invocationCallOrder[0]).toBeLessThan(
+      collectOrphanedBrowserRoutePartitionStorageMock.mock.invocationCallOrder[0]
+    )
+  })
+
+  it('does not sweep route partitions when no profile is active', async () => {
+    const { collectOrphanedBrowserRoutePartitionStorageMock } = installRegistryMock()
+    const { initializeBrowserSessionsForApp } = await import('./browser-session-startup')
+
+    initializeBrowserSessionsForApp()
+
+    expect(collectOrphanedBrowserRoutePartitionStorageMock).not.toHaveBeenCalled()
   })
 
   it('initializes browser sessions once per app process', async () => {