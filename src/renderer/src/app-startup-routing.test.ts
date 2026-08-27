2022zhang125/orca-yunxiao

fix(updater): surface and degrade renderer shutdown checkpoint failures (STA-5505) (#16497)
* fix(updater): surface and degrade renderer shutdown checkpoint failures

The in-app updater could refuse to install with 'Renderer shutdown
checkpoint was not completed.' while the actual persist() error was
swallowed unlogged, leaving users stranded on old builds (STA-5505).

- report the swallowed persist error: console, crash breadcrumb, and a
  cross-world DOM attribute so the thrown error (and the Update Error
  dialog) names the underlying cause
- stop failing the checkpoint on sleeping-agent quit-capture errors; the
  periodic capture bounds the loss to one minute
- extend the existing durable-session degradation to full-session staging
  failures during an intentional restart, preserving the dirty-draft guard

* fix(quit): degrade and surface checkpoint-vetoed app quits (#15352)

Cmd+Q walked the same shutdown checkpoint as the updater: a persist()
throw preventDefault()ed the synthetic beforeunload and
confirmNativeWindowClose returned silently — quit accepted, nothing
logged, SIGKILL the only exit.

- run the quit checkpoint inside a window-close scope so full-session
  staging failures degrade to the durable tier for app-level closes too
  (dirty editor drafts still hard-block)
- when the checkpoint still vetoes the quit, toast the published failure
  reason instead of dying silently

* fix(updater): retry-then-degrade staging and honest capture-loss accounting

Review findings on the first pass:
- a first full-session staging failure now stays a visible, retryable
  error; only a repeat failure degrades to durable-only staging, so a
  transient IPC failure keeps its retry instead of silently dropping
  just-captured scrollback
- the sleeping-capture comment no longer overstates periodic coverage
  (periodic mode skips done panes and never stamps quit origin); the
  swallowed failure records a crash breadcrumb
- pin the exact degradable-shutdown gate expression i

src/renderer/src/app-startup-routing.test.ts
@@ -488,7 +488,7 @@ describe('renderer startup runtime routing', () => {
   it('checkpoints activeView and all session snapshots through one beforeunload handler (#9002)', () => {
     const source = readSource(SESSION_PERSISTENCE_PATH)
     const checkpointStart = source.indexOf(
-      'const shutdownCheckpoint = createShutdownCheckpointGuard('
+      'const shutdownCheckpointPersist = createShutdownCheckpointPersist({'
     )
     const checkpointEnd = source.indexOf(
       'const persistBeforeUnload = createShutdownCheckpointBeforeUnloadHandler(shutdownCheckpoint)',
@@ -499,25 +499,33 @@ describe('renderer startup runtime routing', () => {
     const checkpointBlock = source.slice(checkpointStart, checkpointEnd)
 
     expect(checkpointBlock).toContain(
-      'let sessionSnapshots: ReturnType<typeof buildWorkspaceSessionHostSnapshots> = []'
+      'const shutdownCheckpointPersist = createShutdownCheckpointPersist({'
     )
     expect(checkpointBlock).toContain(
-      'buildWorkspaceSessionHostSnapshots(buildWorkspaceSessionPayload(freshState), freshState)'
+      'buildWorkspaceSessionHostSnapshots(\n          buildWorkspaceSessionPayload(freshState),\n          freshState\n        )'
     )
-    expect(checkpointBlock).toContain('window.api.app.stageBeforeUnloadSync({')
-    expect(checkpointBlock).toContain('sessions: sessionSnapshots')
-    expect(checkpointBlock).toContain('ui: buildActiveViewUnloadPatch(freshState)')
-    expect(checkpointBlock).toContain('!isIntentionalAppRestartInProgress()')
-    expect(checkpointBlock).toContain('freshState.openFiles.some((file) => file.isDirty)')
-    expect(checkpointBlock).toContain('sessions: []')
+    expect(checkpointBlock).toContain('buildUiPatch: () => buildActiveViewUnloadPatch(')
+    // Why pin the exact gate: the degrade tiers must arm only for intentional
+    // restarts and app-level closes, never for arbitrary unloads.
     expect(checkpointBlock).toContain(
-      'return\n      }\n      window.api.app.stageBeforeUnloadSync({\n        sessions: sessionSnapshots'
+      'isIntentionalAppRestartInProgress() || isWindowCloseCheckpointInProgress()'
+    )
+    expect(checkpointBlock).toContain(
+      'useAppStore.getState().openFiles.some((file) => file.isDirty)'
+    )
+    expect(checkpointBlock).toContain(
+      'stageBeforeUnloadSync: (args) => window.api.app.stageBeforeUnloadSync(args)'
+    )
+    expect(checkpointBlock).toContain('shutdownCheckpointPersist.run')
+    expect(checkpointBlock).toContain('shutdownCheckpointPersist.abandonAttempt')
+    expect(source).toContain(
+      'window.addEventListener(ORCA_APP_RESTART_ABORTED_EVENT, shutdownCheckpoint.abandonAttempt)'
     )
     expect(source).toContain(
-      'window.addEventListener(ORCA_APP_RESTART_ABORTED_EVENT, shutdownCheckpoint.reset)'
+      'ORCA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT,\n      shutdownCheckpoint.abortAfterCheckpointFailure'
     )
     expect(source).toContain(
-      'window.addEventListener(ORCA_RENDERER_UNLOAD_PREVENTED_EVENT, shutdownCheckpoint.reset)'
+      'window.addEventListener(ORCA_RENDERER_UNLOAD_PREVENTED_EVENT, shutdownCheckpoint.abandonAttempt)'
     )
     expect(source).toContain("window.addEventListener('beforeunload', persistBeforeUnload)")
     expect(source.match(/window\.addEventListener\('beforeunload'/g) ?? []).toHaveLength(1)

src/renderer/src/lib/updater-beforeunload.ts
@@ -4,6 +4,7 @@ import {
   ORCA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT,
   ORCA_UPDATER_QUIT_AND_INSTALL_STARTED_EVENT
 } from '../../../shared/updater-renderer-events'
+import { ORCA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT } from '../../../shared/renderer-shutdown-events'
 
 let intentionalAppRestartInProgress = false
 
@@ -27,12 +28,14 @@ export function registerUpdaterBeforeUnloadBypass(): () => void {
   window.addEventListener(ORCA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT, clearInProgress)
   window.addEventListener(ORCA_APP_RESTART_STARTED_EVENT, markInProgress)
   window.addEventListener(ORCA_APP_RESTART_ABORTED_EVENT, clearInProgress)
+  window.addEventListener(ORCA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT, clearInProgress)
 
   return () => {
     window.removeEventListener(ORCA_UPDATER_QUIT_AND_INSTALL_STARTED_EVENT, markInProgress)
     window.removeEventListener(ORCA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT, clearInProgress)
     window.removeEventListener(ORCA_APP_RESTART_STARTED_EVENT, markInProgress)
     window.removeEventListener(ORCA_APP_RESTART_ABORTED_EVENT, clearInProgress)
+    window.removeEventListener(ORCA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT, clearInProgress)
     // Why: hot reloads can re-register this listener inside the same renderer.
     // Reset the module flag on cleanup so a failed earlier restart attempt
     // cannot silently suppress future unsaved-change prompts.

src/shared/renderer-restart-preparation.test.ts
@@ -1,6 +1,11 @@
+// @vitest-environment happy-dom
 import { describe, expect, it, vi } from 'vitest'
 import type { UpdateStatus } from './update-status-types'
-import { ORCA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT } from './renderer-shutdown-events'
+import {
+  ORCA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT,
+  ORCA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT,
+  publishShutdownCheckpointFailureReason
+} from './renderer-shutdown-events'
 import {
   createUpdaterQuitAbortRelay,
   prepareRendererForAppRestart
@@ -11,12 +16,14 @@ describe('prepareRendererForAppRestart', () => {
     const eventTarget = new EventTarget()
     const started = vi.fn()
     const aborted = vi.fn()
+    const independentlyAborted = vi.fn()
     const checkpoint = vi.fn((event: Event) => {
       event.currentTarget?.dispatchEvent(new Event(ORCA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT))
       event.preventDefault()
     })
     eventTarget.addEventListener('restart-started', started)
-    eventTarget.addEventListener('restart-aborted', aborted)
+    eventTarget.addEventListener(ORCA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT, aborted)
+    eventTarget.addEventListener('restart-aborted', independentlyAborted)
     eventTarget.addEventListener('beforeunload', checkpoint)
 
     await expect(
@@ -30,6 +37,25 @@ describe('prepareRendererForAppRestart', () => {
     expect(started).toHaveBeenCalledTimes(1)
     expect(checkpoint).toHaveBeenCalledTimes(1)
     expect(aborted).toHaveBeenCalledTimes(1)
+    expect(independentlyAborted).not.toHaveBeenCalled()
+  })
+
+  it('names the published failure cause in the thrown checkpoint error (STA-5505)', async () => {
+    const eventTarget = new EventTarget()
+    eventTarget.addEventListener('beforeunload', (event) => {
+      // Mirrors the checkpoint guard: publish the cause, then fail the checkpoint.
+      publishShutdownCheckpointFailureReason('sendSync payload rejected')
+      event.currentTarget?.dispatchEvent(new Event(ORCA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT))
+      event.preventDefault()
+    })
+
+    await expect(
+      prepareRendererForAppRestart(eventTarget, {
+        startedEventName: 'restart-started',
+        abortedEventName: 'restart-aborted',
+        awaitCheckpoint: () => Promise.resolve()
+      })
+    ).rejects.toThrow('Renderer shutdown checkpoint was not completed: sendSync payload rejected')
   })
 
   it('does not mistake an unrelated unload veto for checkpoint failure', async () => {

src/shared/renderer-shutdown-events.ts
@@ -1,3 +1,56 @@
 export const ORCA_RENDERER_UNLOAD_PREVENTED_EVENT = 'orca:renderer-unload-prevented'
 export const ORCA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT =
   'orca:renderer-shutdown-checkpoint-failed'
+export const ORCA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT =
+  'orca:renderer-shutdown-checkpoint-aborted'
+
+// Why a DOM attribute: the checkpoint guard runs in the renderer's main world while
+// prepareRendererForAppRestart runs in the context-isolated preload world. Events
+// cross worlds but their JS payloads don't; document attributes are shared platform
+// state, so this is the one channel that carries the failure reason to the thrower.
+export const ORCA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE =
+  'data-orca-shutdown-checkpoint-failure'
+
+export function formatShutdownCheckpointFailureReason(error: unknown): string {
+  try {
+    const reason = String(error instanceof Error ? error.message : error)
+    return reason || 'Unknown shutdown checkpoint failure'
+  } catch {
+    return 'Unknown shutdown checkpoint failure'
+  }
+}
+
+export function publishShutdownCheckpointFailureReason(reason: string): void {
+  try {
+    globalThis.document?.documentElement?.setAttribute(
+      ORCA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE,
+      reason
+    )
+  } catch {
+    // Best-effort diagnostics; the checkpoint verdict itself is carried by the event.
+  }
+}
+
+export function clearShutdownCheckpointFailureReason(): void {
+  try {
+    globalThis.document?.documentElement?.removeAttribute(
+      ORCA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE
+    )
+  } catch {
+    // Best-effort diagnostics only.
+  }
+}
+
+/** Read and clear the published reason so a stale one can't label a later failure. */
+export function consumeShutdownCheckpointFailureReason(): string | null {
+  try {
+    const root = globalThis.document?.documentElement
+    const reason = root?.getAttribute(ORCA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE)
+    if (reason) {
+      root?.removeAttribute(ORCA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE)
+    }
+    return reason || null
+  } catch {
+    return null
+  }
+}

paidaxingyo666/Manta

(cherry picked from commit 290f192d84f6422355db11d73a92ad872bf4d963)

src/renderer/src/app-startup-routing.test.ts
@@ -488,7 +488,7 @@ describe('renderer startup runtime routing', () => {
   it('checkpoints activeView and all session snapshots through one beforeunload handler (#9002)', () => {
     const source = readSource(SESSION_PERSISTENCE_PATH)
     const checkpointStart = source.indexOf(
-      'const shutdownCheckpoint = createShutdownCheckpointGuard('
+      'const shutdownCheckpointPersist = createShutdownCheckpointPersist({'
     )
     const checkpointEnd = source.indexOf(
       'const persistBeforeUnload = createShutdownCheckpointBeforeUnloadHandler(shutdownCheckpoint)',
@@ -499,25 +499,33 @@ describe('renderer startup runtime routing', () => {
     const checkpointBlock = source.slice(checkpointStart, checkpointEnd)
 
     expect(checkpointBlock).toContain(
-      'let sessionSnapshots: ReturnType<typeof buildWorkspaceSessionHostSnapshots> = []'
+      'const shutdownCheckpointPersist = createShutdownCheckpointPersist({'
     )
     expect(checkpointBlock).toContain(
-      'buildWorkspaceSessionHostSnapshots(buildWorkspaceSessionPayload(freshState), freshState)'
+      'buildWorkspaceSessionHostSnapshots(\n          buildWorkspaceSessionPayload(freshState),\n          freshState\n        )'
     )
-    expect(checkpointBlock).toContain('window.api.app.stageBeforeUnloadSync({')
-    expect(checkpointBlock).toContain('sessions: sessionSnapshots')
-    expect(checkpointBlock).toContain('ui: buildActiveViewUnloadPatch(freshState)')
-    expect(checkpointBlock).toContain('!isIntentionalAppRestartInProgress()')
-    expect(checkpointBlock).toContain('freshState.openFiles.some((file) => file.isDirty)')
-    expect(checkpointBlock).toContain('sessions: []')
+    expect(checkpointBlock).toContain('buildUiPatch: () => buildActiveViewUnloadPatch(')
+    // Why pin the exact gate: the degrade tiers must arm only for intentional
+    // restarts and app-level closes, never for arbitrary unloads.
     expect(checkpointBlock).toContain(
-      'return\n      }\n      window.api.app.stageBeforeUnloadSync({\n        sessions: sessionSnapshots'
+      'isIntentionalAppRestartInProgress() || isWindowCloseCheckpointInProgress()'
+    )
+    expect(checkpointBlock).toContain(
+      'useAppStore.getState().openFiles.some((file) => file.isDirty)'
+    )
+    expect(checkpointBlock).toContain(
+      'stageBeforeUnloadSync: (args) => window.api.app.stageBeforeUnloadSync(args)'
+    )
+    expect(checkpointBlock).toContain('shutdownCheckpointPersist.run')
+    expect(checkpointBlock).toContain('shutdownCheckpointPersist.abandonAttempt')
+    expect(source).toContain(
+      'window.addEventListener(MANTA_APP_RESTART_ABORTED_EVENT, shutdownCheckpoint.abandonAttempt)'
     )
     expect(source).toContain(
-      'window.addEventListener(MANTA_APP_RESTART_ABORTED_EVENT, shutdownCheckpoint.reset)'
+      'MANTA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT,\n      shutdownCheckpoint.abortAfterCheckpointFailure'
     )
     expect(source).toContain(
-      'window.addEventListener(MANTA_RENDERER_UNLOAD_PREVENTED_EVENT, shutdownCheckpoint.reset)'
+      'window.addEventListener(MANTA_RENDERER_UNLOAD_PREVENTED_EVENT, shutdownCheckpoint.abandonAttempt)'
     )
     expect(source).toContain("window.addEventListener('beforeunload', persistBeforeUnload)")
     expect(source.match(/window\.addEventListener\('beforeunload'/g) ?? []).toHaveLength(1)

src/renderer/src/lib/updater-beforeunload.ts
@@ -4,6 +4,7 @@ import {
   MANTA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT,
   MANTA_UPDATER_QUIT_AND_INSTALL_STARTED_EVENT
 } from '../../../shared/updater-renderer-events'
+import { MANTA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT } from '../../../shared/renderer-shutdown-events'
 
 let intentionalAppRestartInProgress = false
 
@@ -27,12 +28,14 @@ export function registerUpdaterBeforeUnloadBypass(): () => void {
   window.addEventListener(MANTA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT, clearInProgress)
   window.addEventListener(MANTA_APP_RESTART_STARTED_EVENT, markInProgress)
   window.addEventListener(MANTA_APP_RESTART_ABORTED_EVENT, clearInProgress)
+  window.addEventListener(MANTA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT, clearInProgress)
 
   return () => {
     window.removeEventListener(MANTA_UPDATER_QUIT_AND_INSTALL_STARTED_EVENT, markInProgress)
     window.removeEventListener(MANTA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT, clearInProgress)
     window.removeEventListener(MANTA_APP_RESTART_STARTED_EVENT, markInProgress)
     window.removeEventListener(MANTA_APP_RESTART_ABORTED_EVENT, clearInProgress)
+    window.removeEventListener(MANTA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT, clearInProgress)
     // Why: hot reloads can re-register this listener inside the same renderer.
     // Reset the module flag on cleanup so a failed earlier restart attempt
     // cannot silently suppress future unsaved-change prompts.

src/shared/renderer-restart-preparation.test.ts
@@ -1,6 +1,11 @@
+// @vitest-environment happy-dom
 import { describe, expect, it, vi } from 'vitest'
 import type { UpdateStatus } from './update-status-types'
-import { MANTA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT } from './renderer-shutdown-events'
+import {
+  MANTA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT,
+  MANTA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT,
+  publishShutdownCheckpointFailureReason
+} from './renderer-shutdown-events'
 import {
   createUpdaterQuitAbortRelay,
   prepareRendererForAppRestart
@@ -11,12 +16,14 @@ describe('prepareRendererForAppRestart', () => {
     const eventTarget = new EventTarget()
     const started = vi.fn()
     const aborted = vi.fn()
+    const independentlyAborted = vi.fn()
     const checkpoint = vi.fn((event: Event) => {
       event.currentTarget?.dispatchEvent(new Event(MANTA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT))
       event.preventDefault()
     })
     eventTarget.addEventListener('restart-started', started)
-    eventTarget.addEventListener('restart-aborted', aborted)
+    eventTarget.addEventListener(MANTA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT, aborted)
+    eventTarget.addEventListener('restart-aborted', independentlyAborted)
     eventTarget.addEventListener('beforeunload', checkpoint)
 
     await expect(
@@ -30,6 +37,25 @@ describe('prepareRendererForAppRestart', () => {
     expect(started).toHaveBeenCalledTimes(1)
     expect(checkpoint).toHaveBeenCalledTimes(1)
     expect(aborted).toHaveBeenCalledTimes(1)
+    expect(independentlyAborted).not.toHaveBeenCalled()
+  })
+
+  it('names the published failure cause in the thrown checkpoint error (STA-5505)', async () => {
+    const eventTarget = new EventTarget()
+    eventTarget.addEventListener('beforeunload', (event) => {
+      // Mirrors the checkpoint guard: publish the cause, then fail the checkpoint.
+      publishShutdownCheckpointFailureReason('sendSync payload rejected')
+      event.currentTarget?.dispatchEvent(new Event(MANTA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT))
+      event.preventDefault()
+    })
+
+    await expect(
+      prepareRendererForAppRestart(eventTarget, {
+        startedEventName: 'restart-started',
+        abortedEventName: 'restart-aborted',
+        awaitCheckpoint: () => Promise.resolve()
+      })
+    ).rejects.toThrow('Renderer shutdown checkpoint was not completed: sendSync payload rejected')
   })
 
   it('does not mistake an unrelated unload veto for checkpoint failure', async () => {

src/shared/renderer-shutdown-events.ts
@@ -1,3 +1,56 @@
 export const MANTA_RENDERER_UNLOAD_PREVENTED_EVENT = 'manta:renderer-unload-prevented'
 export const MANTA_RENDERER_SHUTDOWN_CHECKPOINT_FAILED_EVENT =
   'manta:renderer-shutdown-checkpoint-failed'
+export const MANTA_RENDERER_SHUTDOWN_CHECKPOINT_ABORTED_EVENT =
+  'manta:renderer-shutdown-checkpoint-aborted'
+
+// Why a DOM attribute: the checkpoint guard runs in the renderer's main world while
+// prepareRendererForAppRestart runs in the context-isolated preload world. Events
+// cross worlds but their JS payloads don't; document attributes are shared platform
+// state, so this is the one channel that carries the failure reason to the thrower.
+export const MANTA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE =
+  'data-manta-shutdown-checkpoint-failure'
+
+export function formatShutdownCheckpointFailureReason(error: unknown): string {
+  try {
+    const reason = String(error instanceof Error ? error.message : error)
+    return reason || 'Unknown shutdown checkpoint failure'
+  } catch {
+    return 'Unknown shutdown checkpoint failure'
+  }
+}
+
+export function publishShutdownCheckpointFailureReason(reason: string): void {
+  try {
+    globalThis.document?.documentElement?.setAttribute(
+      MANTA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE,
+      reason
+    )
+  } catch {
+    // Best-effort diagnostics; the checkpoint verdict itself is carried by the event.
+  }
+}
+
+export function clearShutdownCheckpointFailureReason(): void {
+  try {
+    globalThis.document?.documentElement?.removeAttribute(
+      MANTA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE
+    )
+  } catch {
+    // Best-effort diagnostics only.
+  }
+}
+
+/** Read and clear the published reason so a stale one can't label a later failure. */
+export function consumeShutdownCheckpointFailureReason(): string | null {
+  try {
+    const root = globalThis.document?.documentElement
+    const reason = root?.getAttribute(MANTA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE)
+    if (reason) {
+      root?.removeAttribute(MANTA_SHUTDOWN_CHECKPOINT_FAILURE_REASON_ATTRIBUTE)
+    }
+    return reason || null
+  } catch {
+    return null
+  }
+}