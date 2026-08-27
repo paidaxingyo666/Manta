2022zhang125/orca-yunxiao

fix(browser): retire helpers with page owners (#16564)

src/cli/runtime/serve-signal-exit-diagnostic.test.ts
@@ -1,8 +1,19 @@
 import { EventEmitter } from 'node:events'
+import { mkdtemp, rm } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
 import { afterEach, describe, expect, it, vi } from 'vitest'
 import { serveSignalExitError } from './serve-signal-exit-diagnostic'
-import { superviseForegroundServe } from './serve-update-supervisor'
+import {
+  SERVE_CHILD_FORCE_KILL_GRACE_MS,
+  SERVE_CHILD_FORCE_KILL_SCHEDULING_MARGIN_MS,
+  superviseForegroundServe
+} from './serve-update-supervisor'
 import { RuntimeClientError } from './types'
+import {
+  QUIT_RENDERER_ACK_TIMEOUT_MS,
+  WILL_QUIT_TEARDOWN_DEADLINE_MS
+} from '../../shared/quit-teardown-deadline'
 
 class FakeChildProcess extends EventEmitter {
   kill = vi.fn()
@@ -17,7 +28,13 @@ function setPlatform(platform: NodeJS.Platform): void {
 
 function superviseUntilExit(code: number | null, signal: NodeJS.Signals | null): Promise<number> {
   const child = new FakeChildProcess()
-  const supervised = superviseForegroundServe({
+  const supervised = superviseChild(child)
+  child.emit('exit', code, signal)
+  return supervised
+}
+
+function superviseChild(child: FakeChildProcess): Promise<number> {
+  return superviseForegroundServe({
     executable: '/Applications/Orca.app/Contents/MacOS/Orca',
     childArgs: ['--serve'],
     spawnOptions: {},
@@ -26,12 +43,12 @@ function superviseUntilExit(code: number | null, signal: NodeJS.Signals | null):
     child: child as never,
     expectedHandoff: null
   })
-  child.emit('exit', code, signal)
-  return supervised
 }
 
 afterEach(() => {
   Object.defineProperty(process, 'platform', originalPlatform)
+  vi.restoreAllMocks()
+  vi.useRealTimers()
 })
 
 describe('serveSignalExitError', () => {
@@ -74,6 +91,83 @@ describe('serveSignalExitError', () => {
 })
 
 describe('superviseForegroundServe signal exits', () => {
+  it('lets pre-commit and committed Electron quit deadlines finish before force-killing serve', async () => {
+    setPlatform('linux')
+    vi.useFakeTimers()
+    const child = new FakeChildProcess()
+    const supervised = superviseChild(child)
+
+    expect(SERVE_CHILD_FORCE_KILL_GRACE_MS).toBe(
+      QUIT_RENDERER_ACK_TIMEOUT_MS +
+        WILL_QUIT_TEARDOWN_DEADLINE_MS +
+        SERVE_CHILD_FORCE_KILL_SCHEDULING_MARGIN_MS
+    )
+    expect(SERVE_CHILD_FORCE_KILL_GRACE_MS).toBeLessThanOrEqual(35_000)
+
+    process.emit('SIGTERM', 'SIGTERM')
+    expect(child.kill).toHaveBeenCalledOnce()
+    expect(child.kill).toHaveBeenLastCalledWith('SIGTERM')
+
+    await vi.advanceTimersByTimeAsync(QUIT_RENDERER_ACK_TIMEOUT_MS + WILL_QUIT_TEARDOWN_DEADLINE_MS)
+    expect(child.kill).toHaveBeenCalledOnce()
+
+    await vi.advanceTimersByTimeAsync(SERVE_CHILD_FORCE_KILL_SCHEDULING_MARGIN_MS)
+    expect(child.kill).toHaveBeenLastCalledWith('SIGKILL')
+    expect(child.kill).toHaveBeenCalledTimes(2)
+
+    child.emit('exit', null, 'SIGKILL')
+    await expect(supervised).rejects.toThrow('Orca serve exited via SIGKILL.')
+  })
+
+  it('lets a shared-console Windows child handle Ctrl-C gracefully', async () => {
+    setPlatform('win32')
+    vi.useFakeTimers()
+    const child = new FakeChildProcess()
+    const supervised = superviseChild(child)
+
+    process.emit('SIGINT', 'SIGINT')
+    expect(child.kill).not.toHaveBeenCalled()
+    expect(vi.getTimerCount()).toBe(1)
+
+    child.emit('exit', 0, null)
+    await expect(supervised).resolves.toBe(0)
+    expect(vi.getTimerCount()).toBe(0)
+  })
+
+  it('does not terminate an exited child when update handoff completion fails late', async () => {
+    vi.useFakeTimers()
+    const missingParent = await mkdtemp(join(tmpdir(), 'orca-serve-missing-handoff-'))
+    await rm(missingParent, { recursive: true })
+    const child = new FakeChildProcess()
+    const supervised = superviseForegroundServe({
+      executable: '/Applications/Orca.app/Contents/MacOS/Orca',
+      childArgs: ['--serve'],
+      spawnOptions: {},
+      spawnChild: vi.fn() as never,
+      handoffPath: join(missingParent, 'handoff.json'),
+      child: child as never,
+      expectedHandoff: {
+        schemaVersion: 1,
+        phase: 'install-requested',
+        fromVersion: '1.0.51',
+        targetVersion: '1.0.61',
+        servingPid: child.pid
+      }
+    })
+    vi.spyOn(process.stderr, 'write').mockImplementation(() => true)
+
+    child.emit('message', {
+      type: 'orca:serve-ready',
+      version: '1.0.61',
+      runtimeId: 'runtime-new'
+    })
+    child.emit('exit', 0, null)
+
+    await expect(supervised).resolves.toBe(1)
+    expect(child.kill).not.toHaveBeenCalled()
+    expect(vi.getTimerCount()).toBe(0)
+  })
+
   it('throws the macOS diagnostic when the child aborts on darwin', async () => {
     setPlatform('darwin')
 

src/main/browser/agent-browser-bridge.ts
@@ -49,6 +49,7 @@ import type {
 } from '../../shared/runtime-types'
 import { assertClipboardTextWriteWithinLimitWithYield } from '../../shared/clipboard-text'
 import { normalizeBrowserNavigationUrl } from '../../shared/browser-url'
+import { mapSettledWithConcurrency } from '../../shared/map-with-concurrency'
 import { iterateBrowserTextInsertionChunks } from './browser-text-insertion'
 import { createAgentBrowserProcessEnvironment } from './agent-browser-process-environment'
 
@@ -57,6 +58,8 @@ const EXEC_TIMEOUT_MS = 90_000
 const CONSECUTIVE_TIMEOUT_LIMIT = 3
 const WAIT_PROCESS_TIMEOUT_GRACE_MS = 1_000
 const STALE_SESSION_CLOSE_TIMEOUT_MS = 3_000
+const AGENT_BROWSER_CLEANUP_TIMEOUT_MS = 5_000
+const AGENT_BROWSER_CLEANUP_CONCURRENCY = 4
 const EMBEDDED_NAVIGATION_TIMEOUT_MS = 30_000
 export const AGENT_BROWSER_TEXT_ARGUMENT_MAX_BYTES = 8 * 1024
 export const AGENT_BROWSER_CLIPBOARD_WRITE_MAX_BYTES = AGENT_BROWSER_TEXT_ARGUMENT_MAX_BYTES
@@ -83,6 +86,10 @@ type QueuedCommand = {
 type ResolvedBrowserCommandTarget = {
   browserPageId: string
   webContentsId: number
+}
+
+type AgentBrowserCleanupOptions = {
+  closeTimeoutMs?: number
 }
 
 export type BrowserMouseModifier = 'cmd' | 'ctrl' | 'alt' | 'shift'
@@ -587,6 +594,7 @@ export class AgentBrowserBridge {
   // Why: `agent-browser close` is async, keyed by session name — recreating before it finishes lets the old teardown close the new session.
   private readonly pendingSessionDestruction = new Map<string, Promise<void>>()
   private readonly cancelledProcesses = new WeakSet<ChildProcess>()
+  private shutdownStarted = false
 
   constructor(
     private readonly browserManager: BrowserManager,
@@ -678,11 +686,16 @@ export class AgentBrowserBridge {
       this.activeWebContentsId = nextWorktreeActiveWebContentsId
     }
     if (browserPageId) {
-      const sessionName = `orca-tab-${browserPageId}`
-      await this.destroySession(sessionName)
-      this.pendingInterceptRestore.delete(sessionName)
+      await this.onPageClosed(browserPageId)
     }
     this.options.onTabsChanged?.(owningWorktreeId)
+  }
+
+  /** Retire a helper by its stable page identity when WebContents mapping is gone. */
+  async onPageClosed(browserPageId: string): Promise<void> {
+    const sessionName = `orca-tab-${browserPageId}`
+    await this.destroySession(sessionName)
+    this.pendingInterceptRestore.delete(sessionName)
   }
 
   async onProcessSwap(
@@ -2044,12 +2057,18 @@ export class AgentBrowserBridge {
 
   // ── Session lifecycle ──
 
-  async destroyAllSessions(): Promise<void> {
-    const promises: Promise<void>[] = []
-    for (const sessionName of this.sessions.keys()) {
-      promises.push(this.destroySession(sessionName))
-    }
-    await Promise.allSettled(promises)
+  async destroyAllSessions(options?: AgentBrowserCleanupOptions): Promise<void> {
+    this.shutdownStarted = true
+    const sessionNames = new Set([
+      ...this.sessions.keys(),
+      ...this.pendingSessionCreation.keys(),
+      ...this.pendingSessionDestruction.keys()
+    ])
+    await mapSettledWithConcurrency(
+      [...sessionNames],
+      AGENT_BROWSER_CLEANUP_CONCURRENCY,
+      (sessionName) => this.destroySession(sessionName, options)
+    )
     this.pendingInterceptRestore.clear()
   }
 
@@ -2073,12 +2092,14 @@ export class AgentBrowserBridge {
     execute: (sessionName: string, target: ResolvedBrowserCommandTarget) => Promise<T>,
     options: EnqueueTargetedCommandOptions = {}
   ): Promise<T> {
+    this.assertCommandAdmission()
     const target = this.resolveCommandTarget(worktreeId, browserPageId, options.requireScopedTarget)
     const sessionName = `orca-tab-${target.browserPageId}`
 
     if (options.ensureSession !== false) {
       await this.ensureSession(sessionName, target.browserPageId, target.webContentsId)
     }
+    this.assertCommandAdmission()
 
     return new Promise<T>((resolve, reject) => {
       let queue = this.commandQueues.get(sessionName)
@@ -2302,6 +2323,7 @@ export class AgentBrowserBridge {
     if (pendingDestruction) {
       await pendingDestruction
     }
+    this.assertCommandAdmission()
 
     if (this.sessions.has(sessionName)) {
       return
@@ -2311,6 +2333,7 @@ export class AgentBrowserBridge {
     const pending = this.pendingSessionCreation.get(sessionName)
     if (pending) {
       await pending
+      this.assertCommandAdmission()
       return
     }
 
@@ -2381,7 +2404,9 @@ export class AgentBrowserBridge {
 
       const destroy = (async (): Promise<void> => {
         try {
-          await this.runAgentBrowserRaw(sessionName, ['--session', sessionName, 'close'])
+          await this.runAgentBrowserRaw(sessionName, ['--session', sessionName, 'close'], {
+            timeoutMs: AGENT_BROWSER_CLEANUP_TIMEOUT_MS
+          })
         } catch {
           // Session may already be dead.
         }
@@ -2400,7 +2425,10 @@ export class AgentBrowserBridge {
     }
   }
 
-  private async destroySession(sessionName: string): Promise<void> {
+  private async destroySession(
+    sessionName: string,
+    options: AgentBrowserCleanupOptions = { closeTimeoutMs: AGENT_BROWSER_CLEANUP_TIMEOUT_MS }
+  ): Promise<void> {
     const pendingDestruction = this.pendingSessionDestruction.get(sessionName)
     if (pendingDestruction) {
       await pendingDestruction
@@ -2443,7 +2471,11 @@ export class AgentBrowserBridge {
     const destroy = (async (): Promise<void> => {
       try {
         // Why: each tab has its own named session — close without --session leaves this tab's daemon running.
-        await this.runAgentBrowserRaw(sessionName, ['--session', sessionName, 'close'])
+        await this.runAgentBrowserRaw(
+          sessionName,
+          ['--session', sessionName, 'close'],
+          options.closeTimeoutMs === undefined ? undefined : { timeoutMs: options.closeTimeoutMs }
+        )
       } catch {
         // Session may already be dead
       }
@@ -2471,6 +2503,12 @@ export class AgentBrowserBridge {
         cmd.reject(err)
       }
       queue.length = 0
+    }
+  }
+
+  private assertCommandAdmission(): void {
+    if (this.shutdownStarted) {
+      throw new BrowserError('browser_owner_unavailable', 'Browser runtime is shutting down')
     }
   }
 

paidaxingyo666/Manta

(cherry picked from commit 0e10fc59258aa3e07e31f33d1dc0024f42389647)

src/cli/runtime/serve-signal-exit-diagnostic.test.ts
@@ -1,8 +1,19 @@
 import { EventEmitter } from 'node:events'
+import { mkdtemp, rm } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
 import { afterEach, describe, expect, it, vi } from 'vitest'
 import { serveSignalExitError } from './serve-signal-exit-diagnostic'
-import { superviseForegroundServe } from './serve-update-supervisor'
+import {
+  SERVE_CHILD_FORCE_KILL_GRACE_MS,
+  SERVE_CHILD_FORCE_KILL_SCHEDULING_MARGIN_MS,
+  superviseForegroundServe
+} from './serve-update-supervisor'
 import { RuntimeClientError } from './types'
+import {
+  QUIT_RENDERER_ACK_TIMEOUT_MS,
+  WILL_QUIT_TEARDOWN_DEADLINE_MS
+} from '../../shared/quit-teardown-deadline'
 
 class FakeChildProcess extends EventEmitter {
   kill = vi.fn()
@@ -17,7 +28,13 @@ function setPlatform(platform: NodeJS.Platform): void {
 
 function superviseUntilExit(code: number | null, signal: NodeJS.Signals | null): Promise<number> {
   const child = new FakeChildProcess()
-  const supervised = superviseForegroundServe({
+  const supervised = superviseChild(child)
+  child.emit('exit', code, signal)
+  return supervised
+}
+
+function superviseChild(child: FakeChildProcess): Promise<number> {
+  return superviseForegroundServe({
     executable: '/Applications/Manta.app/Contents/MacOS/Manta',
     childArgs: ['--serve'],
     spawnOptions: {},
@@ -26,12 +43,12 @@ function superviseUntilExit(code: number | null, signal: NodeJS.Signals | null):
     child: child as never,
     expectedHandoff: null
   })
-  child.emit('exit', code, signal)
-  return supervised
 }
 
 afterEach(() => {
   Object.defineProperty(process, 'platform', originalPlatform)
+  vi.restoreAllMocks()
+  vi.useRealTimers()
 })
 
 describe('serveSignalExitError', () => {
@@ -74,6 +91,83 @@ describe('serveSignalExitError', () => {
 })
 
 describe('superviseForegroundServe signal exits', () => {
+  it('lets pre-commit and committed Electron quit deadlines finish before force-killing serve', async () => {
+    setPlatform('linux')
+    vi.useFakeTimers()
+    const child = new FakeChildProcess()
+    const supervised = superviseChild(child)
+
+    expect(SERVE_CHILD_FORCE_KILL_GRACE_MS).toBe(
+      QUIT_RENDERER_ACK_TIMEOUT_MS +
+        WILL_QUIT_TEARDOWN_DEADLINE_MS +
+        SERVE_CHILD_FORCE_KILL_SCHEDULING_MARGIN_MS
+    )
+    expect(SERVE_CHILD_FORCE_KILL_GRACE_MS).toBeLessThanOrEqual(35_000)
+
+    process.emit('SIGTERM', 'SIGTERM')
+    expect(child.kill).toHaveBeenCalledOnce()
+    expect(child.kill).toHaveBeenLastCalledWith('SIGTERM')
+
+    await vi.advanceTimersByTimeAsync(QUIT_RENDERER_ACK_TIMEOUT_MS + WILL_QUIT_TEARDOWN_DEADLINE_MS)
+    expect(child.kill).toHaveBeenCalledOnce()
+
+    await vi.advanceTimersByTimeAsync(SERVE_CHILD_FORCE_KILL_SCHEDULING_MARGIN_MS)
+    expect(child.kill).toHaveBeenLastCalledWith('SIGKILL')
+    expect(child.kill).toHaveBeenCalledTimes(2)
+
+    child.emit('exit', null, 'SIGKILL')
+    await expect(supervised).rejects.toThrow('Manta serve exited via SIGKILL.')
+  })
+
+  it('lets a shared-console Windows child handle Ctrl-C gracefully', async () => {
+    setPlatform('win32')
+    vi.useFakeTimers()
+    const child = new FakeChildProcess()
+    const supervised = superviseChild(child)
+
+    process.emit('SIGINT', 'SIGINT')
+    expect(child.kill).not.toHaveBeenCalled()
+    expect(vi.getTimerCount()).toBe(1)
+
+    child.emit('exit', 0, null)
+    await expect(supervised).resolves.toBe(0)
+    expect(vi.getTimerCount()).toBe(0)
+  })
+
+  it('does not terminate an exited child when update handoff completion fails late', async () => {
+    vi.useFakeTimers()
+    const missingParent = await mkdtemp(join(tmpdir(), 'manta-serve-missing-handoff-'))
+    await rm(missingParent, { recursive: true })
+    const child = new FakeChildProcess()
+    const supervised = superviseForegroundServe({
+      executable: '/Applications/Manta.app/Contents/MacOS/Manta',
+      childArgs: ['--serve'],
+      spawnOptions: {},
+      spawnChild: vi.fn() as never,
+      handoffPath: join(missingParent, 'handoff.json'),
+      child: child as never,
+      expectedHandoff: {
+        schemaVersion: 1,
+        phase: 'install-requested',
+        fromVersion: '1.0.51',
+        targetVersion: '1.0.61',
+        servingPid: child.pid
+      }
+    })
+    vi.spyOn(process.stderr, 'write').mockImplementation(() => true)
+
+    child.emit('message', {
+      type: 'manta:serve-ready',
+      version: '1.0.61',
+      runtimeId: 'runtime-new'
+    })
+    child.emit('exit', 0, null)
+
+    await expect(supervised).resolves.toBe(1)
+    expect(child.kill).not.toHaveBeenCalled()
+    expect(vi.getTimerCount()).toBe(0)
+  })
+
   it('throws the macOS diagnostic when the child aborts on darwin', async () => {
     setPlatform('darwin')
 

src/main/browser/agent-browser-bridge.ts
@@ -49,6 +49,7 @@ import type {
 } from '../../shared/runtime-types'
 import { assertClipboardTextWriteWithinLimitWithYield } from '../../shared/clipboard-text'
 import { normalizeBrowserNavigationUrl } from '../../shared/browser-url'
+import { mapSettledWithConcurrency } from '../../shared/map-with-concurrency'
 import { iterateBrowserTextInsertionChunks } from './browser-text-insertion'
 import { createAgentBrowserProcessEnvironment } from './agent-browser-process-environment'
 
@@ -57,6 +58,8 @@ const EXEC_TIMEOUT_MS = 90_000
 const CONSECUTIVE_TIMEOUT_LIMIT = 3
 const WAIT_PROCESS_TIMEOUT_GRACE_MS = 1_000
 const STALE_SESSION_CLOSE_TIMEOUT_MS = 3_000
+const AGENT_BROWSER_CLEANUP_TIMEOUT_MS = 5_000
+const AGENT_BROWSER_CLEANUP_CONCURRENCY = 4
 const EMBEDDED_NAVIGATION_TIMEOUT_MS = 30_000
 export const AGENT_BROWSER_TEXT_ARGUMENT_MAX_BYTES = 8 * 1024
 export const AGENT_BROWSER_CLIPBOARD_WRITE_MAX_BYTES = AGENT_BROWSER_TEXT_ARGUMENT_MAX_BYTES
@@ -83,6 +86,10 @@ type QueuedCommand = {
 type ResolvedBrowserCommandTarget = {
   browserPageId: string
   webContentsId: number
+}
+
+type AgentBrowserCleanupOptions = {
+  closeTimeoutMs?: number
 }
 
 export type BrowserMouseModifier = 'cmd' | 'ctrl' | 'alt' | 'shift'
@@ -587,6 +594,7 @@ export class AgentBrowserBridge {
   // Why: `agent-browser close` is async, keyed by session name — recreating before it finishes lets the old teardown close the new session.
   private readonly pendingSessionDestruction = new Map<string, Promise<void>>()
   private readonly cancelledProcesses = new WeakSet<ChildProcess>()
+  private shutdownStarted = false
 
   constructor(
     private readonly browserManager: BrowserManager,
@@ -678,11 +686,16 @@ export class AgentBrowserBridge {
       this.activeWebContentsId = nextWorktreeActiveWebContentsId
     }
     if (browserPageId) {
-      const sessionName = `manta-tab-${browserPageId}`
-      await this.destroySession(sessionName)
-      this.pendingInterceptRestore.delete(sessionName)
+      await this.onPageClosed(browserPageId)
     }
     this.options.onTabsChanged?.(owningWorktreeId)
+  }
+
+  /** Retire a helper by its stable page identity when WebContents mapping is gone. */
+  async onPageClosed(browserPageId: string): Promise<void> {
+    const sessionName = `manta-tab-${browserPageId}`
+    await this.destroySession(sessionName)
+    this.pendingInterceptRestore.delete(sessionName)
   }
 
   async onProcessSwap(
@@ -2044,12 +2057,18 @@ export class AgentBrowserBridge {
 
   // ── Session lifecycle ──
 
-  async destroyAllSessions(): Promise<void> {
-    const promises: Promise<void>[] = []
-    for (const sessionName of this.sessions.keys()) {
-      promises.push(this.destroySession(sessionName))
-    }
-    await Promise.allSettled(promises)
+  async destroyAllSessions(options?: AgentBrowserCleanupOptions): Promise<void> {
+    this.shutdownStarted = true
+    const sessionNames = new Set([
+      ...this.sessions.keys(),
+      ...this.pendingSessionCreation.keys(),
+      ...this.pendingSessionDestruction.keys()
+    ])
+    await mapSettledWithConcurrency(
+      [...sessionNames],
+      AGENT_BROWSER_CLEANUP_CONCURRENCY,
+      (sessionName) => this.destroySession(sessionName, options)
+    )
     this.pendingInterceptRestore.clear()
   }
 
@@ -2073,12 +2092,14 @@ export class AgentBrowserBridge {
     execute: (sessionName: string, target: ResolvedBrowserCommandTarget) => Promise<T>,
     options: EnqueueTargetedCommandOptions = {}
   ): Promise<T> {
+    this.assertCommandAdmission()
     const target = this.resolveCommandTarget(worktreeId, browserPageId, options.requireScopedTarget)
     const sessionName = `manta-tab-${target.browserPageId}`
 
     if (options.ensureSession !== false) {
       await this.ensureSession(sessionName, target.browserPageId, target.webContentsId)
     }
+    this.assertCommandAdmission()
 
     return new Promise<T>((resolve, reject) => {
       let queue = this.commandQueues.get(sessionName)
@@ -2302,6 +2323,7 @@ export class AgentBrowserBridge {
     if (pendingDestruction) {
       await pendingDestruction
     }
+    this.assertCommandAdmission()
 
     if (this.sessions.has(sessionName)) {
       return
@@ -2311,6 +2333,7 @@ export class AgentBrowserBridge {
     const pending = this.pendingSessionCreation.get(sessionName)
     if (pending) {
       await pending
+      this.assertCommandAdmission()
       return
     }
 
@@ -2381,7 +2404,9 @@ export class AgentBrowserBridge {
 
       const destroy = (async (): Promise<void> => {
         try {
-          await this.runAgentBrowserRaw(sessionName, ['--session', sessionName, 'close'])
+          await this.runAgentBrowserRaw(sessionName, ['--session', sessionName, 'close'], {
+            timeoutMs: AGENT_BROWSER_CLEANUP_TIMEOUT_MS
+          })
         } catch {
           // Session may already be dead.
         }
@@ -2400,7 +2425,10 @@ export class AgentBrowserBridge {
     }
   }
 
-  private async destroySession(sessionName: string): Promise<void> {
+  private async destroySession(
+    sessionName: string,
+    options: AgentBrowserCleanupOptions = { closeTimeoutMs: AGENT_BROWSER_CLEANUP_TIMEOUT_MS }
+  ): Promise<void> {
     const pendingDestruction = this.pendingSessionDestruction.get(sessionName)
     if (pendingDestruction) {
       await pendingDestruction
@@ -2443,7 +2471,11 @@ export class AgentBrowserBridge {
     const destroy = (async (): Promise<void> => {
       try {
         // Why: each tab has its own named session — close without --session leaves this tab's daemon running.
-        await this.runAgentBrowserRaw(sessionName, ['--session', sessionName, 'close'])
+        await this.runAgentBrowserRaw(
+          sessionName,
+          ['--session', sessionName, 'close'],
+          options.closeTimeoutMs === undefined ? undefined : { timeoutMs: options.closeTimeoutMs }
+        )
       } catch {
         // Session may already be dead
       }
@@ -2471,6 +2503,12 @@ export class AgentBrowserBridge {
         cmd.reject(err)
       }
       queue.length = 0
+    }
+  }
+
+  private assertCommandAdmission(): void {
+    if (this.shutdownStarted) {
+      throw new BrowserError('browser_owner_unavailable', 'Browser runtime is shutting down')
     }
   }
 