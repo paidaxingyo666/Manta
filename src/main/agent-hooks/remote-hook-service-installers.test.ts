2022zhang125/orca-yunxiao

fix(grok): stop Orca's Grok hooks from costing anything outside Orca (#16666)
* fix(grok): stop Orca's Grok hooks from costing anything outside Orca

Orca registers Grok agent-status hooks in the global $GROK_HOME/hooks. Grok
loads that directory on every session, so a Grok run that Orca did not launch
still paid for the hook on every event, and Orca rewrote the file even after a
user had emptied it to opt out (#15518).

The registered POSIX command now guards on ORCA_PANE_KEY before doing anything.
That variable is part of the pane identity Orca injects into terminals it
launches, and unlike the port and token it never comes from the endpoint file,
so it is present exactly when the session belongs to Orca. A standalone session
short-circuits without spawning a shell for the managed script at all. The same
guard is applied to the remote install, because a remote host runs standalone
Grok sessions too.

PreToolUse is no longer registered. It is a blocking hook, so Orca sat on the
critical path of every tool call and doubled the per-tool spawns, for a
transition PostToolUse already reports.

Windows cannot use the guard: the command there must be a single spawnable
token, so it is a bare script path with no shell to evaluate a test. For that
case the hooks are removed when Orca quits -- locally, on WSL guests, and on
connected SSH hosts -- and reinstalled on the next launch. A config the user has
emptied is left alone on startup; turning the setting back on in Settings is an
explicit and later choice, so that path reinstalls.

Removal is careful about what it is deleting. It strips only Orca's own entries,
keeps user-authored ones, and deletes the file only when no hook entries remain
-- keying that off the whole object would leave a stray non-hook key behind, and
the emptied-config check would then read that remnant as a deliberate opt-out
and never reinstall. A config the user has symlinked into a dotfiles repo is
written through rather than unlinked, and is exempt 

src/main/agent-hooks/remote-hook-service-installers.test.ts
@@ -415,7 +415,7 @@ describe('remote hook service installers', () => {
       const definition = grokConfig.hooks[eventName]?.[0]
       const command = definition?.hooks?.[0]?.command
       expect(command).toContain('/home/dev/.orca/agent-hooks/grok-hook.sh')
-      expect(command).toMatch(/^if \[ -f /)
+      expect(command).toMatch(/^if \[ -n "\$ORCA_PANE_KEY" \] && /)
     }
     // Why: Grok tool matchers are real regexes; bare `*` is invalid match-all.
     expect(grokConfig.hooks.PreToolUse?.[0]?.matcher).toBe('.*')

src/main/grok/hook-service.test.ts
@@ -1,6 +1,16 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { spawn } from 'node:child_process'
-import { mkdirSync, mkdtempSync, readFileSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  existsSync,
+  lstatSync,
+  mkdirSync,
+  mkdtempSync,
+  readFileSync,
+  renameSync,
+  rmSync,
+  symlinkSync,
+  writeFileSync
+} from 'node:fs'
 import { createServer } from 'node:http'
 import { tmpdir } from 'node:os'
 import { dirname, join } from 'node:path'
@@ -235,9 +245,9 @@ describe('GrokHookService', () => {
     expect(Object.keys(config.hooks).sort()).toEqual(
       [
         'Notification',
+        'PreToolUse',
         'PostToolUse',
         'PostToolUseFailure',
-        'PreToolUse',
         'SessionEnd',
         'SessionStart',
         'Stop',
@@ -245,8 +255,11 @@ describe('GrokHookService', () => {
         'UserPromptSubmit'
       ].sort()
     )
-    // Why: Grok matchers are real regexes; bare `*` does not match-all.
+    // Why: PreToolUse drives in-flight tool state and ask_user_question waits. The pane guard below
+    // keeps it inert outside Orca; PostToolUse cannot replace a state that has already ended.
     expect(config.hooks.PreToolUse[0].matcher).toBe('.*')
+    expect(registersManagedGrokScript(config.hooks.PreToolUse[0].hooks[0].command)).toBe(true)
+    // Why: Grok matchers are real regexes; bare `*` does not match-all.
     expect(config.hooks.PostToolUseFailure[0].matcher).toBe('.*')
     expect(config.hooks.PostToolUse[0].matcher).toBe('.*')
     // Why: StopFailure must not carry a tool matcher — lifecycle-only event.
@@ -260,9 +273,13 @@ describe('GrokHookService', () => {
     // Why: build the invalid pattern at runtime so static lint does not flag it.
     const bareStar = ['*', ''].join('')
     expect(() => new RegExp(bareStar)).toThrow()
-    expect(registersManagedGrokScript(config.hooks.PreToolUse[0].hooks[0].command)).toBe(true)
+    expect(registersManagedGrokScript(config.hooks.PostToolUse[0].hooks[0].command)).toBe(true)
     if (process.platform !== 'win32') {
-      expect(config.hooks.PreToolUse[0].hooks[0].command).toContain(join(homeDir, '.orca'))
+      const command = config.hooks.PostToolUse[0].hooks[0].command
+      expect(command).toContain(join(homeDir, '.orca'))
+      // Why: with no Orca pane in the environment the guard short-circuits, so a standalone Grok
+      // session never spawns a shell for the managed script at all.
+      expect(command).toMatch(/^if \[ -n "\$ORCA_PANE_KEY" \] && /)
     }
 
     const script = readFileSync(
@@ -338,6 +355,162 @@ describe('GrokHookService', () => {
       }
       rmSync(grokHome, { recursive: true, force: true })
     }
+  })
+
+  it('does not restore a user-cleared global hook config on the next launch', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const userClearedConfig = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userClearedConfig, 'utf8')
+
+    service.install()
+
+    expect(readFileSync(configPath, 'utf8')).toBe(userClearedConfig)
+  })
+
+  it('preserves a user-cleared config during session cleanup', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const userClearedConfig = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userClearedConfig, 'utf8')
+
+    expect(service.remove().state).toBe('not_installed')
+    expect(readFileSync(configPath, 'utf8')).toBe(userClearedConfig)
+  })
+
+  it('removes the empty managed config when hooks are disabled', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+
+    service.install()
+    expect(service.remove().state).toBe('not_installed')
+    expect(() => readFileSync(configPath, 'utf8')).toThrow()
+  })
+
+  // Why: remove() used to unlink only when the whole config object was empty, so any non-hook key
+  // left a remnant behind — and install()'s user-cleared guard then read that remnant as a
+  // deliberate opt-out and never reinstalled again.
+  it('removes the managed config even when a non-hook key remains', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+
+    expect(service.install().state).toBe('installed')
+    const withSchema = JSON.parse(readFileSync(configPath, 'utf8')) as Record<string, unknown>
+    withSchema.$schema = 'https://grok.example/hooks.schema.json'
+    writeFileSync(configPath, `${JSON.stringify(withSchema, null, 2)}\n`, 'utf8')
+
+    expect(service.remove().state).toBe('not_installed')
+
+    expect(existsSync(configPath)).toBe(false)
+    expect(service.install().state).toBe('installed')
+  })
+
+  // Why: the reported workaround for #15518 was emptying this file by hand, so Orca must not
+  // rewrite it on startup. But then turning the setting back on has to work, or the toggle
+  // silently lies and the only way back is deleting a file in a hidden directory.
+  it('leaves a user-cleared config alone on a startup install', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const userCleared = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userCleared, 'utf8')
+
+    service.install()
+
+    expect(readFileSync(configPath, 'utf8')).toBe(userCleared)
+  })
+
+  it('restores a user-cleared config when the user turns hooks back on', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const userCleared = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userCleared, 'utf8')
+
+    expect(service.install({ userInitiated: true }).state).toBe('installed')
+
+    expect(readFileSync(configPath, 'utf8')).not.toBe(userCleared)
+    expect(service.getStatus().managedHooksPresent).toBe(true)
+  })
+
+  // Why: hook-config-write-path.ts exists because users symlink these configs into dotfiles repos.
+  // Unlinking or renaming onto the link path destroys that link and silently detaches the file they
+  // version-control. Their file is theirs -- strip our entries and write through it.
+  it('writes through a symlinked config instead of destroying the link', async () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'orca-status.json')
+
+    expect(service.install().state).toBe('installed')
+    mkdirSync(dirname(realConfig), { recursive: true })
+    renameSync(configPath, realConfig)
+    symlinkSync(realConfig, configPath)
+
+    await service.removeAsync()
+
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+    expect(existsSync(realConfig)).toBe(true)
+    expect(service.getStatus().managedHooksPresent).toBe(false)
+  })
+
+  it('respects an empty user-managed symlink as an opt-out', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'orca-status.json')
+    const userCleared = '{"hooks":{}}\n'
+    mkdirSync(dirname(configPath), { recursive: true })
+    mkdirSync(dirname(realConfig), { recursive: true })
+    writeFileSync(realConfig, userCleared)
+    symlinkSync(realConfig, configPath)
+
+    expect(service.install().state).toBe('not_installed')
+    expect(readFileSync(realConfig, 'utf8')).toBe(userCleared)
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+  })
+
+  it('writes through a symlinked config on the sync remove path too', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'orca-status.json')
+
+    expect(service.install().state).toBe('installed')
+    mkdirSync(dirname(realConfig), { recursive: true })
+    renameSync(configPath, realConfig)
+    symlinkSync(realConfig, configPath)
+
+    expect(service.remove().state).toBe('not_installed')
+
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+    expect(existsSync(realConfig)).toBe(true)
+    expect(service.getStatus().managedHooksPresent).toBe(false)
+  })
+
+  // Why: a symlinked config is written THROUGH on removal rather than unlinked, so after a quit it
+  // is an empty file WE emptied -- byte-identical to one a user cleared. Letting the user-cleared
+  // heuristic see it meant a symlinked config was silently never reinstalled after the first quit.
+  it('reinstalls into a symlinked config on the launch after a quit', async () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'orca-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'orca-status.json')
+
+    expect(service.install().state).toBe('installed')
+    mkdirSync(dirname(realConfig), { recursive: true })
+    renameSync(configPath, realConfig)
+    symlinkSync(realConfig, configPath)
+    await service.removeAsync()
+    expect(service.getStatus().managedHooksPresent).toBe(false)
+
+    expect(new GrokHookService().install().state).toBe('installed')
+
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+    expect(existsSync(realConfig)).toBe(true)
+    expect(service.getStatus().managedHooksPresent).toBe(true)
   })
 
   it('preserves user-authored hook entries in the Orca Grok config file', () => {

paidaxingyo666/Manta

(cherry picked from commit 5a59bc5bc48720e516dec17cd71784c5c63d1775)

src/main/agent-hooks/remote-hook-service-installers.test.ts
@@ -417,7 +417,7 @@ describe('remote hook service installers', () => {
       const definition = grokConfig.hooks[eventName]?.[0]
       const command = definition?.hooks?.[0]?.command
       expect(command).toContain('/home/dev/.manta/agent-hooks/grok-hook.sh')
-      expect(command).toMatch(/^if \[ -f /)
+      expect(command).toMatch(/^if \[ -n "\$MANTA_PANE_KEY" \] && /)
     }
     // Why: Grok tool matchers are real regexes; bare `*` is invalid match-all.
     expect(grokConfig.hooks.PreToolUse?.[0]?.matcher).toBe('.*')

src/main/grok/hook-service.test.ts
@@ -1,6 +1,16 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { spawn } from 'node:child_process'
-import { mkdirSync, mkdtempSync, readFileSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  existsSync,
+  lstatSync,
+  mkdirSync,
+  mkdtempSync,
+  readFileSync,
+  renameSync,
+  rmSync,
+  symlinkSync,
+  writeFileSync
+} from 'node:fs'
 import { createServer } from 'node:http'
 import { tmpdir } from 'node:os'
 import { dirname, join } from 'node:path'
@@ -235,9 +245,9 @@ describe('GrokHookService', () => {
     expect(Object.keys(config.hooks).sort()).toEqual(
       [
         'Notification',
+        'PreToolUse',
         'PostToolUse',
         'PostToolUseFailure',
-        'PreToolUse',
         'SessionEnd',
         'SessionStart',
         'Stop',
@@ -245,8 +255,11 @@ describe('GrokHookService', () => {
         'UserPromptSubmit'
       ].sort()
     )
-    // Why: Grok matchers are real regexes; bare `*` does not match-all.
+    // Why: PreToolUse drives in-flight tool state and ask_user_question waits. The pane guard below
+    // keeps it inert outside Manta; PostToolUse cannot replace a state that has already ended.
     expect(config.hooks.PreToolUse[0].matcher).toBe('.*')
+    expect(registersManagedGrokScript(config.hooks.PreToolUse[0].hooks[0].command)).toBe(true)
+    // Why: Grok matchers are real regexes; bare `*` does not match-all.
     expect(config.hooks.PostToolUseFailure[0].matcher).toBe('.*')
     expect(config.hooks.PostToolUse[0].matcher).toBe('.*')
     // Why: StopFailure must not carry a tool matcher — lifecycle-only event.
@@ -260,9 +273,13 @@ describe('GrokHookService', () => {
     // Why: build the invalid pattern at runtime so static lint does not flag it.
     const bareStar = ['*', ''].join('')
     expect(() => new RegExp(bareStar)).toThrow()
-    expect(registersManagedGrokScript(config.hooks.PreToolUse[0].hooks[0].command)).toBe(true)
+    expect(registersManagedGrokScript(config.hooks.PostToolUse[0].hooks[0].command)).toBe(true)
     if (process.platform !== 'win32') {
-      expect(config.hooks.PreToolUse[0].hooks[0].command).toContain(join(homeDir, '.manta'))
+      const command = config.hooks.PostToolUse[0].hooks[0].command
+      expect(command).toContain(join(homeDir, '.manta'))
+      // Why: with no Manta pane in the environment the guard short-circuits, so a standalone Grok
+      // session never spawns a shell for the managed script at all.
+      expect(command).toMatch(/^if \[ -n "\$MANTA_PANE_KEY" \] && /)
     }
 
     const script = readFileSync(
@@ -338,6 +355,162 @@ describe('GrokHookService', () => {
       }
       rmSync(grokHome, { recursive: true, force: true })
     }
+  })
+
+  it('does not restore a user-cleared global hook config on the next launch', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const userClearedConfig = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userClearedConfig, 'utf8')
+
+    service.install()
+
+    expect(readFileSync(configPath, 'utf8')).toBe(userClearedConfig)
+  })
+
+  it('preserves a user-cleared config during session cleanup', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const userClearedConfig = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userClearedConfig, 'utf8')
+
+    expect(service.remove().state).toBe('not_installed')
+    expect(readFileSync(configPath, 'utf8')).toBe(userClearedConfig)
+  })
+
+  it('removes the empty managed config when hooks are disabled', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+
+    service.install()
+    expect(service.remove().state).toBe('not_installed')
+    expect(() => readFileSync(configPath, 'utf8')).toThrow()
+  })
+
+  // Why: remove() used to unlink only when the whole config object was empty, so any non-hook key
+  // left a remnant behind — and install()'s user-cleared guard then read that remnant as a
+  // deliberate opt-out and never reinstalled again.
+  it('removes the managed config even when a non-hook key remains', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+
+    expect(service.install().state).toBe('installed')
+    const withSchema = JSON.parse(readFileSync(configPath, 'utf8')) as Record<string, unknown>
+    withSchema.$schema = 'https://grok.example/hooks.schema.json'
+    writeFileSync(configPath, `${JSON.stringify(withSchema, null, 2)}\n`, 'utf8')
+
+    expect(service.remove().state).toBe('not_installed')
+
+    expect(existsSync(configPath)).toBe(false)
+    expect(service.install().state).toBe('installed')
+  })
+
+  // Why: the reported workaround for #15518 was emptying this file by hand, so Manta must not
+  // rewrite it on startup. But then turning the setting back on has to work, or the toggle
+  // silently lies and the only way back is deleting a file in a hidden directory.
+  it('leaves a user-cleared config alone on a startup install', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const userCleared = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userCleared, 'utf8')
+
+    service.install()
+
+    expect(readFileSync(configPath, 'utf8')).toBe(userCleared)
+  })
+
+  it('restores a user-cleared config when the user turns hooks back on', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const userCleared = '{  "hooks" : {}  }\n'
+
+    expect(service.install().state).toBe('installed')
+    writeFileSync(configPath, userCleared, 'utf8')
+
+    expect(service.install({ userInitiated: true }).state).toBe('installed')
+
+    expect(readFileSync(configPath, 'utf8')).not.toBe(userCleared)
+    expect(service.getStatus().managedHooksPresent).toBe(true)
+  })
+
+  // Why: hook-config-write-path.ts exists because users symlink these configs into dotfiles repos.
+  // Unlinking or renaming onto the link path destroys that link and silently detaches the file they
+  // version-control. Their file is theirs -- strip our entries and write through it.
+  it('writes through a symlinked config instead of destroying the link', async () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'manta-status.json')
+
+    expect(service.install().state).toBe('installed')
+    mkdirSync(dirname(realConfig), { recursive: true })
+    renameSync(configPath, realConfig)
+    symlinkSync(realConfig, configPath)
+
+    await service.removeAsync()
+
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+    expect(existsSync(realConfig)).toBe(true)
+    expect(service.getStatus().managedHooksPresent).toBe(false)
+  })
+
+  it('respects an empty user-managed symlink as an opt-out', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'manta-status.json')
+    const userCleared = '{"hooks":{}}\n'
+    mkdirSync(dirname(configPath), { recursive: true })
+    mkdirSync(dirname(realConfig), { recursive: true })
+    writeFileSync(realConfig, userCleared)
+    symlinkSync(realConfig, configPath)
+
+    expect(service.install().state).toBe('not_installed')
+    expect(readFileSync(realConfig, 'utf8')).toBe(userCleared)
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+  })
+
+  it('writes through a symlinked config on the sync remove path too', () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'manta-status.json')
+
+    expect(service.install().state).toBe('installed')
+    mkdirSync(dirname(realConfig), { recursive: true })
+    renameSync(configPath, realConfig)
+    symlinkSync(realConfig, configPath)
+
+    expect(service.remove().state).toBe('not_installed')
+
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+    expect(existsSync(realConfig)).toBe(true)
+    expect(service.getStatus().managedHooksPresent).toBe(false)
+  })
+
+  // Why: a symlinked config is written THROUGH on removal rather than unlinked, so after a quit it
+  // is an empty file WE emptied -- byte-identical to one a user cleared. Letting the user-cleared
+  // heuristic see it meant a symlinked config was silently never reinstalled after the first quit.
+  it('reinstalls into a symlinked config on the launch after a quit', async () => {
+    const service = new GrokHookService()
+    const configPath = join(homeDir, '.grok', 'hooks', 'manta-status.json')
+    const realConfig = join(homeDir, 'dotfiles', 'manta-status.json')
+
+    expect(service.install().state).toBe('installed')
+    mkdirSync(dirname(realConfig), { recursive: true })
+    renameSync(configPath, realConfig)
+    symlinkSync(realConfig, configPath)
+    await service.removeAsync()
+    expect(service.getStatus().managedHooksPresent).toBe(false)
+
+    expect(new GrokHookService().install().state).toBe('installed')
+
+    expect(lstatSync(configPath).isSymbolicLink()).toBe(true)
+    expect(existsSync(realConfig)).toBe(true)
+    expect(service.getStatus().managedHooksPresent).toBe(true)
   })
 
   it('preserves user-authored hook entries in the Manta Grok config file', () => {