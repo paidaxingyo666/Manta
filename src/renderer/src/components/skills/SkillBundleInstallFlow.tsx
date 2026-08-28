2022zhang125/orca-yunxiao

i18n: Make skill install dialogs and errors translatable (#16682)
Extract hardcoded error messages and status labels from skill
installation components into the i18n system. Supports localized
UI for install flows in English, Spanish, Japanese, Korean, Chinese.

Co-authored-by: OrcaWin <293788423+OrcaWin@users.noreply.github.com>

src/renderer/src/components/skills/SkillBundleInstallFlow.tsx
@@ -126,7 +126,11 @@ export function SkillBundleInstallFlow(props: {
   ): Promise<void> => {
     const target = destination()
     if (!target || requestedIds.size === 0) {
-      setError(target ? 'Select at least one skill.' : 'Choose a workspace.')
+      setError(
+        target
+          ? translate('auto.components.skills.install.selectSkill', 'Select at least one skill.')
+          : translate('auto.components.skills.install.chooseWorkspace', 'Choose a workspace.')
+      )
       return
     }
     setBusy(true)
@@ -189,7 +193,7 @@ export function SkillBundleInstallFlow(props: {
       } else if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Orca account before installing.'
+            ? translate('auto.components.skills.install.reconnectBeforeInstalling', 'Reconnect your Orca account before installing.')
             : operation.message
         )
       } else {
@@ -204,7 +208,7 @@ export function SkillBundleInstallFlow(props: {
       }
     } catch (cause) {
       console.warn('[skills] bundle install failed:', cause)
-      setError('Installation failed before Orca could verify the requested bundle.')
+      setError(translate('auto.components.skills.install.bundleVerificationFailed', 'Installation failed before Orca could verify the requested bundle.'))
     } finally {
       installProgress.finish()
       setBusy(false)
@@ -221,7 +225,7 @@ export function SkillBundleInstallFlow(props: {
       ...(environmentId === 'local' || environmentId.startsWith('ssh:') ? {} : { environmentId })
     })
     if (!cancelled.cancelled) {
-      setError('The destination had already finished this installation.')
+      setError(translate('auto.components.skills.install.destinationAlreadyFinished', 'The destination had already finished this installation.'))
     }
   }
 

src/renderer/src/components/skills/SkillInstallDialog.tsx
@@ -91,7 +91,7 @@ export function SkillInstallDialog({
   const resolveLink = useCallback(async (value: string): Promise<void> => {
     const shareId = parseSkillShareId(value)
     if (!shareId) {
-      setError('Enter an Orca skill share link.')
+      setError(translate('auto.components.skills.install.enterShareLink', 'Enter an Orca skill share link.'))
       return
     }
     setBusy(true)
@@ -103,14 +103,14 @@ export function SkillInstallDialog({
         setError(
           operation.status === 'unconfigured'
             ? operation.message
-            : 'This share is unavailable. The link may be invalid, expired, or revoked.'
+            : translate('auto.components.skills.install.shareUnavailable', 'This share is unavailable. The link may be invalid, expired, or revoked.')
         )
         return
       }
       setPreview({ shareId, version: operation.value.version })
     } catch (cause) {
       console.warn('[skills] share resolution failed:', cause)
-      setError('This share is unavailable. The link may be invalid, expired, or revoked.')
+      setError(translate('auto.components.skills.install.shareUnavailable', 'This share is unavailable. The link may be invalid, expired, or revoked.'))
     } finally {
       setBusy(false)
     }
@@ -138,7 +138,7 @@ export function SkillInstallDialog({
     }
     const choice = workspaceChoices.find((candidate) => candidate.id === workspace)
     if (scope === 'workspace' && !choice) {
-      setError('Choose a workspace.')
+      setError(translate('auto.components.skills.install.chooseWorkspace', 'Choose a workspace.'))
       return
     }
     setBusy(true)
@@ -204,7 +204,7 @@ export function SkillInstallDialog({
       if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Orca account before installing.'
+            ? translate('auto.components.skills.install.reconnectBeforeInstalling', 'Reconnect your Orca account before installing.')
             : operation.message
         )
         return
@@ -215,7 +215,12 @@ export function SkillInstallDialog({
       }
     } catch (cause) {
       console.warn('[skills] install failed:', cause)
-      setError('Installation failed before Orca could verify the requested version.')
+      setError(
+        translate(
+          'auto.components.skills.install.requestedVersionVerificationFailed',
+          'Installation failed before Orca could verify the requested version.'
+        )
+      )
     } finally {
       installProgress.finish()
       setBusy(false)
@@ -231,7 +236,7 @@ export function SkillInstallDialog({
       ...(environmentId === 'local' || environmentId.startsWith('ssh:') ? {} : { environmentId })
     })
     if (!cancelled.cancelled) {
-      setError('The destination had already finished this installation.')
+      setError(translate('auto.components.skills.install.destinationAlreadyFinished', 'The destination had already finished this installation.'))
     }
   }
 

src/renderer/src/components/skills/SkillInstallManagementDialog.tsx
@@ -28,6 +28,7 @@ import {
 } from './skill-managed-install-groups'
 import { SkillInstallMachineSelect } from './SkillInstallMachineSelect'
 import { SkillInstallManagementStatus } from './SkillInstallManagementStatus'
+import { translate } from '@/i18n/i18n'
 
 export function SkillInstallManagementDialog({
   open,
@@ -90,7 +91,7 @@ export function SkillInstallManagementDialog({
         return
       }
       console.warn('[skills] managed install listing failed:', cause)
-      setError('Orca could not inspect managed installs on this machine.')
+      setError(translate('auto.components.skills.install.inspectManagedFailed', 'Orca could not inspect managed installs on this machine.'))
     } finally {
       if (generation === loadGeneration.current) {
         setBusy(false)
@@ -120,7 +121,7 @@ export function SkillInstallManagementDialog({
       if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Orca account to load version history.'
+            ? translate('auto.components.skills.install.reconnectForVersionHistory', 'Reconnect your Orca account to load version history.')
             : operation.message
         )
         return
@@ -132,7 +133,7 @@ export function SkillInstallManagementDialog({
         return
       }
       console.warn('[skills] package history failed:', cause)
-      setError('Version history is unavailable for this skill.')
+      setError(translate('auto.components.skills.install.versionHistoryUnavailable', 'Version history is unavailable for this skill.'))
     } finally {
       if (generation === detailGeneration.current) {
         setBusy(false)
@@ -168,7 +169,7 @@ export function SkillInstallManagementDialog({
           installedNames.has(skill.name)
         )
         if (selectedSkills.length === 0) {
-          setError('This version does not contain any of the installed bundle skills.')
+          setError(translate('auto.components.skills.install.bundleSkillsMissing', 'This version does not contain any of the installed bundle skills.'))
           return
         }
         const operation = await window.api.skills.installBundlePackageVersion({
@@ -193,7 +194,7 @@ export function SkillInstallManagementDialog({
         if (operation.status !== 'ok') {
           setError(
             operation.status === 'reconnect-required'
-              ? 'Reconnect your Orca account before changing versions.'
+              ? translate('auto.components.skills.install.reconnectBeforeVersionChange', 'Reconnect your Orca account before changing versions.')
               : operation.message
           )
           return
@@ -219,7 +220,7 @@ export function SkillInstallManagementDialog({
       if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Orca account before changing versions.'
+            ? translate('auto.components.skills.install.reconnectBeforeVersionChange', 'Reconnect your Orca account before changing versions.')
             : operation.message
         )
         return
@@ -233,7 +234,7 @@ export function SkillInstallManagementDialog({
       }
     } catch (cause) {
       console.warn('[skills] version installation failed:', cause)
-      setError('Orca could not verify the requested version.')
+      setError(translate('auto.components.skills.install.versionVerificationFailed', 'Orca could not verify the requested version.'))
     } finally {
       installProgress.finish()
       setBusy(false)
@@ -249,7 +250,7 @@ export function SkillInstallManagementDialog({
       ...(environmentId === 'local' || environmentId.startsWith('ssh:') ? {} : { environmentId })
     })
     if (!cancelled.cancelled) {
-      setError('The destination had already finished this installation.')
+      setError(translate('auto.components.skills.install.destinationAlreadyFinished', 'The destination had already finished this installation.'))
     }
   }
 
@@ -294,7 +295,7 @@ export function SkillInstallManagementDialog({
       }
     } catch (cause) {
       console.warn('[skills] managed removal failed:', cause)
-      setError('Orca could not safely remove this skill.')
+      setError(translate('auto.components.skills.install.removeFailed', 'Orca could not safely remove this skill.'))
     } finally {
       setBusy(false)
     }

src/renderer/src/components/skills/SkillShareDialog.tsx
@@ -231,7 +231,12 @@ export function SkillShareDialog({
     } catch {
       cancellationRequested.current = false
       setCancelling(false)
-      setError('Orca could not send the cancellation request. The upload may still finish.')
+      setError(
+        translate(
+          'auto.components.skills.SkillShareDialog.cancelRequestFailed',
+          'Orca could not send the cancellation request. The upload may still finish.'
+        )
+      )
     }
   }
 

paidaxingyo666/Manta

(cherry picked from commit 7d5c7aa9c3533ec3d880feb058c5e068e36df55a)

src/renderer/src/components/skills/SkillBundleInstallFlow.tsx
@@ -126,7 +126,11 @@ export function SkillBundleInstallFlow(props: {
   ): Promise<void> => {
     const target = destination()
     if (!target || requestedIds.size === 0) {
-      setError(target ? 'Select at least one skill.' : 'Choose a workspace.')
+      setError(
+        target
+          ? translate('auto.components.skills.install.selectSkill', 'Select at least one skill.')
+          : translate('auto.components.skills.install.chooseWorkspace', 'Choose a workspace.')
+      )
       return
     }
     setBusy(true)
@@ -189,7 +193,7 @@ export function SkillBundleInstallFlow(props: {
       } else if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Manta account before installing.'
+            ? translate('auto.components.skills.install.reconnectBeforeInstalling', 'Reconnect your Manta account before installing.')
             : operation.message
         )
       } else {
@@ -204,7 +208,7 @@ export function SkillBundleInstallFlow(props: {
       }
     } catch (cause) {
       console.warn('[skills] bundle install failed:', cause)
-      setError('Installation failed before Manta could verify the requested bundle.')
+      setError(translate('auto.components.skills.install.bundleVerificationFailed', 'Installation failed before Manta could verify the requested bundle.'))
     } finally {
       installProgress.finish()
       setBusy(false)
@@ -221,7 +225,7 @@ export function SkillBundleInstallFlow(props: {
       ...(environmentId === 'local' || environmentId.startsWith('ssh:') ? {} : { environmentId })
     })
     if (!cancelled.cancelled) {
-      setError('The destination had already finished this installation.')
+      setError(translate('auto.components.skills.install.destinationAlreadyFinished', 'The destination had already finished this installation.'))
     }
   }
 

src/renderer/src/components/skills/SkillInstallDialog.tsx
@@ -91,7 +91,7 @@ export function SkillInstallDialog({
   const resolveLink = useCallback(async (value: string): Promise<void> => {
     const shareId = parseSkillShareId(value)
     if (!shareId) {
-      setError('Enter a Manta skill share link.')
+      setError(translate('auto.components.skills.install.enterShareLink', 'Enter a Manta skill share link.'))
       return
     }
     setBusy(true)
@@ -103,14 +103,14 @@ export function SkillInstallDialog({
         setError(
           operation.status === 'unconfigured'
             ? operation.message
-            : 'This share is unavailable. The link may be invalid, expired, or revoked.'
+            : translate('auto.components.skills.install.shareUnavailable', 'This share is unavailable. The link may be invalid, expired, or revoked.')
         )
         return
       }
       setPreview({ shareId, version: operation.value.version })
     } catch (cause) {
       console.warn('[skills] share resolution failed:', cause)
-      setError('This share is unavailable. The link may be invalid, expired, or revoked.')
+      setError(translate('auto.components.skills.install.shareUnavailable', 'This share is unavailable. The link may be invalid, expired, or revoked.'))
     } finally {
       setBusy(false)
     }
@@ -138,7 +138,7 @@ export function SkillInstallDialog({
     }
     const choice = workspaceChoices.find((candidate) => candidate.id === workspace)
     if (scope === 'workspace' && !choice) {
-      setError('Choose a workspace.')
+      setError(translate('auto.components.skills.install.chooseWorkspace', 'Choose a workspace.'))
       return
     }
     setBusy(true)
@@ -204,7 +204,7 @@ export function SkillInstallDialog({
       if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Manta account before installing.'
+            ? translate('auto.components.skills.install.reconnectBeforeInstalling', 'Reconnect your Manta account before installing.')
             : operation.message
         )
         return
@@ -215,7 +215,12 @@ export function SkillInstallDialog({
       }
     } catch (cause) {
       console.warn('[skills] install failed:', cause)
-      setError('Installation failed before Manta could verify the requested version.')
+      setError(
+        translate(
+          'auto.components.skills.install.requestedVersionVerificationFailed',
+          'Installation failed before Manta could verify the requested version.'
+        )
+      )
     } finally {
       installProgress.finish()
       setBusy(false)
@@ -231,7 +236,7 @@ export function SkillInstallDialog({
       ...(environmentId === 'local' || environmentId.startsWith('ssh:') ? {} : { environmentId })
     })
     if (!cancelled.cancelled) {
-      setError('The destination had already finished this installation.')
+      setError(translate('auto.components.skills.install.destinationAlreadyFinished', 'The destination had already finished this installation.'))
     }
   }
 

src/renderer/src/components/skills/SkillInstallManagementDialog.tsx
@@ -28,6 +28,7 @@ import {
 } from './skill-managed-install-groups'
 import { SkillInstallMachineSelect } from './SkillInstallMachineSelect'
 import { SkillInstallManagementStatus } from './SkillInstallManagementStatus'
+import { translate } from '@/i18n/i18n'
 
 export function SkillInstallManagementDialog({
   open,
@@ -90,7 +91,7 @@ export function SkillInstallManagementDialog({
         return
       }
       console.warn('[skills] managed install listing failed:', cause)
-      setError('Manta could not inspect managed installs on this machine.')
+      setError(translate('auto.components.skills.install.inspectManagedFailed', 'Manta could not inspect managed installs on this machine.'))
     } finally {
       if (generation === loadGeneration.current) {
         setBusy(false)
@@ -120,7 +121,7 @@ export function SkillInstallManagementDialog({
       if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Manta account to load version history.'
+            ? translate('auto.components.skills.install.reconnectForVersionHistory', 'Reconnect your Manta account to load version history.')
             : operation.message
         )
         return
@@ -132,7 +133,7 @@ export function SkillInstallManagementDialog({
         return
       }
       console.warn('[skills] package history failed:', cause)
-      setError('Version history is unavailable for this skill.')
+      setError(translate('auto.components.skills.install.versionHistoryUnavailable', 'Version history is unavailable for this skill.'))
     } finally {
       if (generation === detailGeneration.current) {
         setBusy(false)
@@ -168,7 +169,7 @@ export function SkillInstallManagementDialog({
           installedNames.has(skill.name)
         )
         if (selectedSkills.length === 0) {
-          setError('This version does not contain any of the installed bundle skills.')
+          setError(translate('auto.components.skills.install.bundleSkillsMissing', 'This version does not contain any of the installed bundle skills.'))
           return
         }
         const operation = await window.api.skills.installBundlePackageVersion({
@@ -193,7 +194,7 @@ export function SkillInstallManagementDialog({
         if (operation.status !== 'ok') {
           setError(
             operation.status === 'reconnect-required'
-              ? 'Reconnect your Manta account before changing versions.'
+              ? translate('auto.components.skills.install.reconnectBeforeVersionChange', 'Reconnect your Manta account before changing versions.')
               : operation.message
           )
           return
@@ -219,7 +220,7 @@ export function SkillInstallManagementDialog({
       if (operation.status !== 'ok') {
         setError(
           operation.status === 'reconnect-required'
-            ? 'Reconnect your Manta account before changing versions.'
+            ? translate('auto.components.skills.install.reconnectBeforeVersionChange', 'Reconnect your Manta account before changing versions.')
             : operation.message
         )
         return
@@ -233,7 +234,7 @@ export function SkillInstallManagementDialog({
       }
     } catch (cause) {
       console.warn('[skills] version installation failed:', cause)
-      setError('Manta could not verify the requested version.')
+      setError(translate('auto.components.skills.install.versionVerificationFailed', 'Manta could not verify the requested version.'))
     } finally {
       installProgress.finish()
       setBusy(false)
@@ -249,7 +250,7 @@ export function SkillInstallManagementDialog({
       ...(environmentId === 'local' || environmentId.startsWith('ssh:') ? {} : { environmentId })
     })
     if (!cancelled.cancelled) {
-      setError('The destination had already finished this installation.')
+      setError(translate('auto.components.skills.install.destinationAlreadyFinished', 'The destination had already finished this installation.'))
     }
   }
 
@@ -294,7 +295,7 @@ export function SkillInstallManagementDialog({
       }
     } catch (cause) {
       console.warn('[skills] managed removal failed:', cause)
-      setError('Manta could not safely remove this skill.')
+      setError(translate('auto.components.skills.install.removeFailed', 'Manta could not safely remove this skill.'))
     } finally {
       setBusy(false)
     }

src/renderer/src/components/skills/SkillShareDialog.tsx
@@ -231,7 +231,12 @@ export function SkillShareDialog({
     } catch {
       cancellationRequested.current = false
       setCancelling(false)
-      setError('Manta could not send the cancellation request. The upload may still finish.')
+      setError(
+        translate(
+          'auto.components.skills.SkillShareDialog.cancelRequestFailed',
+          'Manta could not send the cancellation request. The upload may still finish.'
+        )
+      )
     }
   }
 