2022zhang125/orca-yunxiao

fix(release): recover immutable patch validation gates (#16984)
* fix(release): recover immutable patch validation gates

* test(e2e): locate wrapped terminal file links

* test(e2e): keep sibling file links on one terminal row

tests/e2e/golden-terminal-file-link.spec.ts
@@ -18,6 +18,11 @@ type LinkClientPoint = { x: number; y: number }
 
 const LINK_SCAN_CHAR_LIMIT = 12_000
 
+function canonicalFileIdentity(value: string): string {
+  const normalized = path.resolve(value).replaceAll('\\', '/')
+  return process.platform === 'win32' ? normalized.toLowerCase() : normalized
+}
+
 async function locateLink(page: Page, needle: string): Promise<LinkProbe | null> {
   return page.evaluate((needle) => {
     const state = window.__store?.getState()
@@ -34,11 +39,19 @@ async function locateLink(page: Page, needle: string): Promise<LinkProbe | null>
       return null
     }
     const buffer = pane.terminal.buffer.active
-    for (let row = 0; row < pane.terminal.rows; row += 1) {
-      const line = buffer.getLine(buffer.viewportY + row)
-      const col = line?.translateToString(true).indexOf(needle) ?? -1
-      if (col >= 0) {
-        return { col: col + Math.floor(needle.length / 2), row, tabId }
+    // Preserve fixed-width cells so paths split across xterm rows stay searchable.
+    const visibleCells = Array.from({ length: pane.terminal.rows }, (_, row) =>
+      (buffer.getLine(buffer.viewportY + row)?.translateToString(false) ?? '').padEnd(
+        pane.terminal.cols
+      )
+    ).join('')
+    const start = visibleCells.indexOf(needle)
+    if (start !== -1) {
+      const center = start + Math.floor(needle.length / 2)
+      return {
+        col: center % pane.terminal.cols,
+        row: Math.floor(center / pane.terminal.cols),
+        tabId
       }
     }
     return null
@@ -162,14 +175,13 @@ test('opens a terminal file link and observes an external edit @golden', async (
     await expect(explorerRow).toHaveAttribute('data-selected', 'true', { timeout: 10_000 })
     await expect
       .poll(
-        () =>
-          orcaPage.evaluate(
-            (expectedPath) => window.__monacoEditorE2E?.filePath === expectedPath,
-            filePath
+        async () =>
+          canonicalFileIdentity(
+            (await orcaPage.evaluate(() => window.__monacoEditorE2E?.filePath)) ?? ''
           ),
         { timeout: 20_000, message: 'Monaco opened a different file identity' }
       )
-      .toBe(true)
+      .toBe(canonicalFileIdentity(filePath))
 
     writeFileSync(filePath, `${original.trimEnd()}\n\n${changedMarker}\n`)
     await expect
@@ -196,14 +208,18 @@ test('reuses a terminal file link already open in a sibling workspace @golden', 
   test.setTimeout(180_000)
   await waitForSessionReady(orcaPage)
   const sourceWorktreeId = await waitForActiveWorktree(orcaPage)
-  const sibling = await orcaPage.evaluate((sourceId) => {
+  const worktrees = await orcaPage.evaluate((sourceId) => {
     const state = window.__store?.getState()
-    return (
-      Object.values(state?.worktreesByRepo ?? {})
-        .flat()
-        .find((worktree) => worktree.id !== sourceId) ?? null
-    )
+    const entries = Object.values(state?.worktreesByRepo ?? {}).flat()
+    return {
+      source: entries.find((worktree) => worktree.id === sourceId) ?? null,
+      sibling: entries.find((worktree) => worktree.id !== sourceId) ?? null
+    }
   }, sourceWorktreeId)
+  const { source, sibling } = worktrees
+  if (!source) {
+    throw new Error('source worktree fixture unavailable')
+  }
   if (!sibling) {
     throw new Error('sibling worktree fixture unavailable')
   }
@@ -230,7 +246,24 @@ test('reuses a terminal file link already open in a sibling workspace @golden', 
 
   await ensureTerminalVisible(orcaPage)
   await waitForActiveTerminalManager(orcaPage, 30_000)
-  const ptyId = await waitForActivePanePtyId(orcaPage)
+  await orcaPage.evaluate(() => {
+    const state = window.__store?.getState()
+    state?.setSidebarOpen(false)
+    state?.setRightSidebarOpen(false)
+  })
+  await expect
+    .poll(
+      () =>
+        orcaPage.evaluate(() => {
+          const state = window.__store?.getState()
+          const tabId = state?.activeTabId
+          const manager = tabId ? window.__paneManagers?.get(tabId) : null
+          return manager?.getActivePane?.()?.terminal.cols ?? 0
+        }),
+      { message: 'terminal did not expand after closing the sidebars' }
+    )
+    .toBeGreaterThan(120)
+  const ptyId = await waitForActivePanePtyId(orcaPage, 30_000)
   await waitForPtyShellEcho(orcaPage, ptyId, 15_000)
   const printedPath = process.platform === 'win32' ? filePath.replaceAll('\\', '/') : filePath
   const command = nodeTerminalCommand(['-e', `console.log(${JSON.stringify(printedPath)})`])
@@ -261,13 +294,18 @@ test('reuses a terminal file link already open in a sibling workspace @golden', 
   await expect(editorHeader).toContainText('package.json', { timeout: 20_000 })
   await expect
     .poll(
-      () =>
-        orcaPage.evaluate(() => ({
-          filePath: window.__monacoEditorE2E?.filePath ?? null,
+      async () => {
+        const rendered = await orcaPage.evaluate(() => ({
+          filePath: window.__monacoEditorE2E?.filePath ?? '',
           activeWorktreeId: window.__store?.getState()?.activeWorktreeId ?? null
-        })),
+        }))
+        return {
+          filePath: canonicalFileIdentity(rendered.filePath),
+          activeWorktreeId: rendered.activeWorktreeId
+        }
+      },
       { timeout: 20_000, message: 'sibling workspace never rendered the linked file' }
     )
-    .toEqual({ filePath, activeWorktreeId: sibling.id })
+    .toEqual({ filePath: canonicalFileIdentity(filePath), activeWorktreeId: sibling.id })
   await expect(orcaPage.getByText('Loading...', { exact: true })).toHaveCount(0)
 })

paidaxingyo666/Manta

(cherry picked from commit 59515beb7089c4f451ca42b8a0c628ea44ac6ab0)

tests/e2e/golden-terminal-file-link.spec.ts
@@ -18,6 +18,11 @@ type LinkClientPoint = { x: number; y: number }
 
 const LINK_SCAN_CHAR_LIMIT = 12_000
 
+function canonicalFileIdentity(value: string): string {
+  const normalized = path.resolve(value).replaceAll('\\', '/')
+  return process.platform === 'win32' ? normalized.toLowerCase() : normalized
+}
+
 async function locateLink(page: Page, needle: string): Promise<LinkProbe | null> {
   return page.evaluate((needle) => {
     const state = window.__store?.getState()
@@ -34,11 +39,19 @@ async function locateLink(page: Page, needle: string): Promise<LinkProbe | null>
       return null
     }
     const buffer = pane.terminal.buffer.active
-    for (let row = 0; row < pane.terminal.rows; row += 1) {
-      const line = buffer.getLine(buffer.viewportY + row)
-      const col = line?.translateToString(true).indexOf(needle) ?? -1
-      if (col >= 0) {
-        return { col: col + Math.floor(needle.length / 2), row, tabId }
+    // Preserve fixed-width cells so paths split across xterm rows stay searchable.
+    const visibleCells = Array.from({ length: pane.terminal.rows }, (_, row) =>
+      (buffer.getLine(buffer.viewportY + row)?.translateToString(false) ?? '').padEnd(
+        pane.terminal.cols
+      )
+    ).join('')
+    const start = visibleCells.indexOf(needle)
+    if (start !== -1) {
+      const center = start + Math.floor(needle.length / 2)
+      return {
+        col: center % pane.terminal.cols,
+        row: Math.floor(center / pane.terminal.cols),
+        tabId
       }
     }
     return null
@@ -162,14 +175,13 @@ test('opens a terminal file link and observes an external edit @golden', async (
     await expect(explorerRow).toHaveAttribute('data-selected', 'true', { timeout: 10_000 })
     await expect
       .poll(
-        () =>
-          mantaPage.evaluate(
-            (expectedPath) => window.__monacoEditorE2E?.filePath === expectedPath,
-            filePath
+        async () =>
+          canonicalFileIdentity(
+            (await mantaPage.evaluate(() => window.__monacoEditorE2E?.filePath)) ?? ''
           ),
         { timeout: 20_000, message: 'Monaco opened a different file identity' }
       )
-      .toBe(true)
+      .toBe(canonicalFileIdentity(filePath))
 
     writeFileSync(filePath, `${original.trimEnd()}\n\n${changedMarker}\n`)
     await expect
@@ -196,14 +208,18 @@ test('reuses a terminal file link already open in a sibling workspace @golden', 
   test.setTimeout(180_000)
   await waitForSessionReady(mantaPage)
   const sourceWorktreeId = await waitForActiveWorktree(mantaPage)
-  const sibling = await mantaPage.evaluate((sourceId) => {
+  const worktrees = await mantaPage.evaluate((sourceId) => {
     const state = window.__store?.getState()
-    return (
-      Object.values(state?.worktreesByRepo ?? {})
-        .flat()
-        .find((worktree) => worktree.id !== sourceId) ?? null
-    )
+    const entries = Object.values(state?.worktreesByRepo ?? {}).flat()
+    return {
+      source: entries.find((worktree) => worktree.id === sourceId) ?? null,
+      sibling: entries.find((worktree) => worktree.id !== sourceId) ?? null
+    }
   }, sourceWorktreeId)
+  const { source, sibling } = worktrees
+  if (!source) {
+    throw new Error('source worktree fixture unavailable')
+  }
   if (!sibling) {
     throw new Error('sibling worktree fixture unavailable')
   }
@@ -230,7 +246,24 @@ test('reuses a terminal file link already open in a sibling workspace @golden', 
 
   await ensureTerminalVisible(mantaPage)
   await waitForActiveTerminalManager(mantaPage, 30_000)
-  const ptyId = await waitForActivePanePtyId(mantaPage)
+  await mantaPage.evaluate(() => {
+    const state = window.__store?.getState()
+    state?.setSidebarOpen(false)
+    state?.setRightSidebarOpen(false)
+  })
+  await expect
+    .poll(
+      () =>
+        mantaPage.evaluate(() => {
+          const state = window.__store?.getState()
+          const tabId = state?.activeTabId
+          const manager = tabId ? window.__paneManagers?.get(tabId) : null
+          return manager?.getActivePane?.()?.terminal.cols ?? 0
+        }),
+      { message: 'terminal did not expand after closing the sidebars' }
+    )
+    .toBeGreaterThan(120)
+  const ptyId = await waitForActivePanePtyId(mantaPage, 30_000)
   await waitForPtyShellEcho(mantaPage, ptyId, 15_000)
   const printedPath = process.platform === 'win32' ? filePath.replaceAll('\\', '/') : filePath
   const command = nodeTerminalCommand(['-e', `console.log(${JSON.stringify(printedPath)})`])
@@ -261,13 +294,18 @@ test('reuses a terminal file link already open in a sibling workspace @golden', 
   await expect(editorHeader).toContainText('package.json', { timeout: 20_000 })
   await expect
     .poll(
-      () =>
-        mantaPage.evaluate(() => ({
-          filePath: window.__monacoEditorE2E?.filePath ?? null,
+      async () => {
+        const rendered = await mantaPage.evaluate(() => ({
+          filePath: window.__monacoEditorE2E?.filePath ?? '',
           activeWorktreeId: window.__store?.getState()?.activeWorktreeId ?? null
-        })),
+        }))
+        return {
+          filePath: canonicalFileIdentity(rendered.filePath),
+          activeWorktreeId: rendered.activeWorktreeId
+        }
+      },
       { timeout: 20_000, message: 'sibling workspace never rendered the linked file' }
     )
-    .toEqual({ filePath, activeWorktreeId: sibling.id })
+    .toEqual({ filePath: canonicalFileIdentity(filePath), activeWorktreeId: sibling.id })
   await expect(mantaPage.getByText('Loading...', { exact: true })).toHaveCount(0)
 })