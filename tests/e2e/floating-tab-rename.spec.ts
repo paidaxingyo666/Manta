2022zhang125/orca-yunxiao

Prevent duplicate file renames when input unmounts after Enter (#16719)
When Enter is pressed to confirm a rename, the input unmounts and its onBlur
handler fires as it detaches from the DOM. Without consuming this event, a
second commitRename call would attempt to rename against the old path. Setting
the cancel flag after capturing the new name causes the trailing onBlur to
return early, preventing the duplicate operation.

tests/e2e/floating-tab-rename.spec.ts
@@ -141,6 +141,37 @@ test('concurrent floating Markdown renames do not clobber the destination', asyn
   ).toEqual(['first\n', 'second\n'])
 })
 
+test('Enter commits a floating Markdown rename only once', async ({ orcaPage }) => {
+  const seeded = await seedFloatingMarkdownFile(orcaPage)
+  await openFloatingPanel(orcaPage)
+
+  const panel = orcaPage.locator(OPEN_PANEL_SELECTOR)
+  const tab = panel.locator(`[data-tab-id="${seeded.tabId}"]`)
+  await tab.click({ button: 'right' })
+  await orcaPage.getByRole('menuitem').filter({ hasText: 'Rename' }).first().click()
+
+  const input = panel.getByRole('textbox', {
+    name: `Rename file ${seeded.originalName}`,
+    exact: true
+  })
+  await input.fill(seeded.renamedName)
+  await input.press('Enter')
+
+  await expect(tab).toContainText(seeded.renamedName)
+  await expect
+    .poll(() =>
+      orcaPage.evaluate(
+        async ({ originalPath, renamedPath }) => ({
+          originalExists: await window.api.fs.pathExists({ filePath: originalPath }),
+          renamedExists: await window.api.fs.pathExists({ filePath: renamedPath })
+        }),
+        { originalPath: seeded.originalPath, renamedPath: seeded.renamedPath }
+      )
+    )
+    .toEqual({ originalExists: false, renamedExists: true })
+  await expect(orcaPage.getByText(/Failed to rename/)).toHaveCount(0)
+})
+
 test('Electron serializes native Unicode rename aliases', async ({ orcaPage }) => {
   test.skip(process.platform !== 'darwin', 'Requires native Unicode aliasing')
   const directory = await orcaPage.evaluate(() => window.api.app.getFloatingMarkdownDirectory())

paidaxingyo666/Manta

(cherry picked from commit 9fb522023979c0d56761782be024c7630f6046f1)

tests/e2e/floating-tab-rename.spec.ts
@@ -141,6 +141,37 @@ test('concurrent floating Markdown renames do not clobber the destination', asyn
   ).toEqual(['first\n', 'second\n'])
 })
 
+test('Enter commits a floating Markdown rename only once', async ({ mantaPage }) => {
+  const seeded = await seedFloatingMarkdownFile(mantaPage)
+  await openFloatingPanel(mantaPage)
+
+  const panel = mantaPage.locator(OPEN_PANEL_SELECTOR)
+  const tab = panel.locator(`[data-tab-id="${seeded.tabId}"]`)
+  await tab.click({ button: 'right' })
+  await mantaPage.getByRole('menuitem').filter({ hasText: 'Rename' }).first().click()
+
+  const input = panel.getByRole('textbox', {
+    name: `Rename file ${seeded.originalName}`,
+    exact: true
+  })
+  await input.fill(seeded.renamedName)
+  await input.press('Enter')
+
+  await expect(tab).toContainText(seeded.renamedName)
+  await expect
+    .poll(() =>
+      mantaPage.evaluate(
+        async ({ originalPath, renamedPath }) => ({
+          originalExists: await window.api.fs.pathExists({ filePath: originalPath }),
+          renamedExists: await window.api.fs.pathExists({ filePath: renamedPath })
+        }),
+        { originalPath: seeded.originalPath, renamedPath: seeded.renamedPath }
+      )
+    )
+    .toEqual({ originalExists: false, renamedExists: true })
+  await expect(mantaPage.getByText(/Failed to rename/)).toHaveCount(0)
+})
+
 test('Electron serializes native Unicode rename aliases', async ({ mantaPage }) => {
   test.skip(process.platform !== 'darwin', 'Requires native Unicode aliasing')
   const directory = await mantaPage.evaluate(() => window.api.app.getFloatingMarkdownDirectory())