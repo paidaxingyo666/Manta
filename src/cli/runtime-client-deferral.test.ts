2022zhang125/orca-yunxiao

fix(agent-hooks): stop test runs and secondary profiles deleting the user's agent hooks (STA-5679) (#16980)
* fix(agent-hooks): stop startup from deleting another instance's managed hooks (STA-5679)

Startup reconciliation removed the managed agent hooks whenever THIS profile had
the agent-status-hooks off switch set. The hook files it removes are user-global
(~/.claude/settings.json, ~/.cursor/hooks.json), so a second Orca profile with the
switch off deleted the hooks every other running instance depends on.

Cursor is the only agent with no title-derived status fallback: its native title is
deliberately parsed as status-less, so a hookless Cursor pane is floored at 'idle'
rather than showing a spinner. A global hook wipe therefore surfaces as "Cursor
loading status missing from the sidebar" while Claude and Codex still paint status
from their own titles, which is why this reads as a Cursor-only bug. Codex is
unaffected either way because its hooks live in an Orca-owned runtime home.

Honoring the off switch only requires skipping the install; removal stays on the
explicit Settings toggle, which is the user-initiated path that should own it.

Regression from #2778, which restored the destructive startup branch.

* fix(cli-tests): stop the deferral suite deleting the developer's real agent hooks

runtime-client-deferral.test.ts runs the REAL `main()` and feeds it
`agent hooks off`. It mocks only ./runtime/environments and ./runtime-client, so
the production handler ran end to end: updateEnabledOnDisk() wrote its state file
and applyAgentStatusHooksEnabled(false) called removeManagedAgentHooks() against
the developer's OWN ~/.claude/settings.json and ~/.cursor/hooks.json.

A green test run therefore deleted every Orca-managed hook on the machine. Agent
status then stopped reporting until the next Orca restart reinstalled them —
silently, because the hook POSTs still return 204 and Cursor has no title-derived
status fallback at all.

The byte-for-byte equivalence twin

src/cli/runtime-client-deferral.test.ts
@@ -1,11 +1,20 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
-import { readFileSync } from 'node:fs'
+import { mkdtempSync, readFileSync, rmSync } from 'node:fs'
 import { join } from 'node:path'
+import { tmpdir } from 'node:os'
 
-const { constructorArgsMock, callMock, getCliStatusMock } = vi.hoisted(() => ({
+const {
+  applyAgentStatusHooksEnabledMock,
+  constructorArgsMock,
+  callMock,
+  getCliStatusMock,
+  testUserDataPathRef
+} = vi.hoisted(() => ({
+  applyAgentStatusHooksEnabledMock: vi.fn(async () => []),
   constructorArgsMock: vi.fn(),
   callMock: vi.fn(),
-  getCliStatusMock: vi.fn()
+  getCliStatusMock: vi.fn(),
+  testUserDataPathRef: { current: '' }
 }))
 
 // Why: `main` reaches RuntimeClient through `await import('./runtime-client.js')`
@@ -21,6 +30,20 @@ vi.mock('./runtime/environments', async (importOriginal) => {
   }
 })
 
+// Why: this suite runs the REAL `main()`, and `agent hooks off` below reaches the production
+// handler, which calls removeManagedAgentHooks() against the developer's OWN ~/.claude and
+// ~/.cursor — a green test run silently deleted every Orca-managed hook on the machine, so agent
+// status stopped reporting until the next Orca restart (STA-5679). The byte-for-byte equivalence
+// twin already refuses these tokens for exactly this reason
+// (config/scripts/cli-runtime-client-deferral-equivalence.mjs); this is the same guard for vitest.
+// Stubbed, not dropped: the row is the only case that reads ctx.client, so it carries the
+// null-vs-undefined coverage the rest of the table cannot.
+vi.mock('../main/agent-hooks/managed-agent-hook-controls', () => ({
+  applyAgentStatusHooksEnabled: applyAgentStatusHooksEnabledMock,
+  getManagedAgentHookStatuses: vi.fn(() => []),
+  prepareManagedCodexHomeBeforeShellLaunch: vi.fn(async () => {})
+}))
+
 vi.mock('./runtime-client', () => {
   class RuntimeClient {
     call = callMock
@@ -31,7 +54,7 @@ vi.mock('./runtime-client', () => {
       constructorArgsMock(...args)
     }
   }
-  return { RuntimeClient, getDefaultUserDataPath: () => '/tmp/orca-user-data' }
+  return { RuntimeClient, getDefaultUserDataPath: () => testUserDataPathRef.current }
 })
 
 import { main } from './index'
@@ -44,6 +67,8 @@ describe('RuntimeClient module-graph deferral', () => {
   let errorSpy: ReturnType<typeof vi.spyOn>
 
   beforeEach(() => {
+    testUserDataPathRef.current = mkdtempSync(join(tmpdir(), 'orca-runtime-deferral-userdata-'))
+    applyAgentStatusHooksEnabledMock.mockClear()
     constructorArgsMock.mockClear()
     callMock.mockReset()
     getCliStatusMock.mockReset()
@@ -55,6 +80,7 @@ describe('RuntimeClient module-graph deferral', () => {
     logSpy.mockRestore()
     errorSpy.mockRestore()
     vi.unstubAllEnvs()
+    rmSync(testUserDataPathRef.current, { recursive: true, force: true })
     process.exitCode = 0
   })
 
@@ -131,6 +157,20 @@ describe('RuntimeClient module-graph deferral', () => {
       for (const call of calls) {
         expect(call[2], `${argv.join(' ')} pairing code`).toBeNull()
         expect(call[3], `${argv.join(' ')} environment`).toBeNull()
+      }
+      if (argv.join(' ') === 'agent hooks off') {
+        expect(
+          applyAgentStatusHooksEnabledMock,
+          `${argv.join(' ')} hook application`
+        ).toHaveBeenCalledExactlyOnceWith(false, {
+          agentCmdOverrides: {},
+          disabledTuiAgents: []
+        })
+      } else {
+        expect(
+          applyAgentStatusHooksEnabledMock,
+          `${argv.join(' ')} hook application`
+        ).not.toHaveBeenCalled()
       }
     }
   )

paidaxingyo666/Manta

(cherry picked from commit 2c86d2a3bd677ab432f3eca02a5ae738866e2d91)

src/cli/runtime-client-deferral.test.ts
@@ -1,11 +1,20 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
-import { readFileSync } from 'node:fs'
+import { mkdtempSync, readFileSync, rmSync } from 'node:fs'
 import { join } from 'node:path'
+import { tmpdir } from 'node:os'
 
-const { constructorArgsMock, callMock, getCliStatusMock } = vi.hoisted(() => ({
+const {
+  applyAgentStatusHooksEnabledMock,
+  constructorArgsMock,
+  callMock,
+  getCliStatusMock,
+  testUserDataPathRef
+} = vi.hoisted(() => ({
+  applyAgentStatusHooksEnabledMock: vi.fn(async () => []),
   constructorArgsMock: vi.fn(),
   callMock: vi.fn(),
-  getCliStatusMock: vi.fn()
+  getCliStatusMock: vi.fn(),
+  testUserDataPathRef: { current: '' }
 }))
 
 // Why: `main` reaches RuntimeClient through `await import('./runtime-client.js')`
@@ -21,6 +30,20 @@ vi.mock('./runtime/environments', async (importOriginal) => {
   }
 })
 
+// Why: this suite runs the REAL `main()`, and `agent hooks off` below reaches the production
+// handler, which calls removeManagedAgentHooks() against the developer's OWN ~/.claude and
+// ~/.cursor — a green test run silently deleted every Manta-managed hook on the machine, so agent
+// status stopped reporting until the next Manta restart (STA-5679). The byte-for-byte equivalence
+// twin already refuses these tokens for exactly this reason
+// (config/scripts/cli-runtime-client-deferral-equivalence.mjs); this is the same guard for vitest.
+// Stubbed, not dropped: the row is the only case that reads ctx.client, so it carries the
+// null-vs-undefined coverage the rest of the table cannot.
+vi.mock('../main/agent-hooks/managed-agent-hook-controls', () => ({
+  applyAgentStatusHooksEnabled: applyAgentStatusHooksEnabledMock,
+  getManagedAgentHookStatuses: vi.fn(() => []),
+  prepareManagedCodexHomeBeforeShellLaunch: vi.fn(async () => {})
+}))
+
 vi.mock('./runtime-client', () => {
   class RuntimeClient {
     call = callMock
@@ -31,7 +54,7 @@ vi.mock('./runtime-client', () => {
       constructorArgsMock(...args)
     }
   }
-  return { RuntimeClient, getDefaultUserDataPath: () => '/tmp/manta-user-data' }
+  return { RuntimeClient, getDefaultUserDataPath: () => testUserDataPathRef.current }
 })
 
 import { main } from './index'
@@ -44,6 +67,8 @@ describe('RuntimeClient module-graph deferral', () => {
   let errorSpy: ReturnType<typeof vi.spyOn>
 
   beforeEach(() => {
+    testUserDataPathRef.current = mkdtempSync(join(tmpdir(), 'manta-runtime-deferral-userdata-'))
+    applyAgentStatusHooksEnabledMock.mockClear()
     constructorArgsMock.mockClear()
     callMock.mockReset()
     getCliStatusMock.mockReset()
@@ -55,6 +80,7 @@ describe('RuntimeClient module-graph deferral', () => {
     logSpy.mockRestore()
     errorSpy.mockRestore()
     vi.unstubAllEnvs()
+    rmSync(testUserDataPathRef.current, { recursive: true, force: true })
     process.exitCode = 0
   })
 
@@ -131,6 +157,20 @@ describe('RuntimeClient module-graph deferral', () => {
       for (const call of calls) {
         expect(call[2], `${argv.join(' ')} pairing code`).toBeNull()
         expect(call[3], `${argv.join(' ')} environment`).toBeNull()
+      }
+      if (argv.join(' ') === 'agent hooks off') {
+        expect(
+          applyAgentStatusHooksEnabledMock,
+          `${argv.join(' ')} hook application`
+        ).toHaveBeenCalledExactlyOnceWith(false, {
+          agentCmdOverrides: {},
+          disabledTuiAgents: []
+        })
+      } else {
+        expect(
+          applyAgentStatusHooksEnabledMock,
+          `${argv.join(' ')} hook application`
+        ).not.toHaveBeenCalled()
       }
     }
   )