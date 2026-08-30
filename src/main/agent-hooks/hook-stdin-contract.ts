2022zhang125/orca-yunxiao

fix(agent-hooks): deliver hook events that fire while Orca is restarting (STA-5329) (#16685)
* fix(agent-hooks): correct durable spool delivery

* fix(agent-hooks): spool curl failures after retries

* fix(agent-hooks): keep replay out of runtime observations

* test(agent-hooks): pin managed hooks inert outside an Orca terminal

* fix(agent-hooks): address review findings on the durable spool

- claude: pass the literal source; options.agent does not exist (typecheck)
- kimi: the windows-local ordering runs its guard pre-stdin and before the
  function exists, so it no longer spools there (printed command-not-found)
- writer: require a readable endpoint file before creating a spool tree
- antigravity: carry its out-of-band event name into the record and filter on it
- drain: truncate only the bytes consumed, preserving concurrent appends and a
  torn trailing line

* fix(agent-hooks): ignore spool events without pane attribution

* fix(agent-hooks): make spool replay and appends robust

* test(agent-hooks): type spool replay records

* fix(agent-hooks): defer unterminated spool records

* fix(agent-hooks): replay spool events through relays

* fix(agent-hooks): preserve Codex prompt across child replay

* fix(relay): keep startup alive when spool replay fails

* fix(relay): simplify spool replay startup guard

src/main/agent-hooks/hook-stdin-contract.ts
@@ -22,6 +22,52 @@ export function buildPosixHookPayloadCapture(
   ]
 }
 
+/** Shell-side durable fallback shared by every POSIX managed hook.
+ *  `eventNameVar` is for providers that send the event name out-of-band rather than in the
+ *  payload JSON; without it both the progress filter and replay would miss the event name. */
+export function buildPosixHookSpoolLines(source: string, eventNameVar?: string): string[] {
+  // Why: the event name must be a printf ARG, not inlined in the single-quoted format,
+  // where a command substitution would be emitted literally.
+  const eventFormat = eventNameVar ? '"hookEventName":"%s",' : ''
+  const eventArg = eventNameVar ? ` "$(spool_json_escape "\${${eventNameVar}:-}")"` : ''
+  const spoolRecordLine = "  { printf '\\n{".concat(
+    eventFormat,
+    '"paneKey":"%s","tabId":"%s","worktreeId":"%s","env":"%s","version":"%s","launchToken":"%s","source":"%s","receivedAt":%s,"payload":%s}\\n\'',
+    eventArg,
+    ' "$(spool_json_escape "${ORCA_PANE_KEY:-}")" "$(spool_json_escape "${ORCA_TAB_ID:-}")" "$(spool_json_escape "${ORCA_WORKTREE_ID:-}")" "$(spool_json_escape "${ORCA_AGENT_HOOK_ENV:-}")" "$(spool_json_escape "${ORCA_AGENT_HOOK_VERSION:-}")" "$(spool_json_escape "${ORCA_AGENT_LAUNCH_TOKEN:-}")" "$(spool_json_escape "',
+    source,
+    '")" "$spool_now" "$payload"; } >> "$spool_file" 2>/dev/null || :'
+  )
+  return [
+    'spool_hook_event() {',
+    eventNameVar
+      ? `  case "\${${eventNameVar}:-}" in PreToolUse|PostToolUse|PostToolUseFailure) return 0 ;; esac`
+      : '  case "$payload" in *\'"PreToolUse"\'*|*\'"PostToolUse"\'*|*\'"PostToolUseFailure"\'*) return 0 ;; esac',
+    '  [ -n "${ORCA_AGENT_HOOK_ENDPOINT:-}" ] || return 0',
+    // Why: an endpoint can linger in a parent shell after leaving Orca; without a pane key
+    // the record is un-attributable and would accumulate as pane-unknown.jsonl.
+    '  [ -n "${ORCA_PANE_KEY:-}" ] || return 0',
+    // Why: a stale env var must not create a spool tree for an Orca that is not installed here.
+    '  [ -r "$ORCA_AGENT_HOOK_ENDPOINT" ] || return 0',
+    '  spool_base=${ORCA_AGENT_HOOK_ENDPOINT%/*}',
+    '  spool_dir="$spool_base/spool"',
+    '  mkdir -p "$spool_dir" 2>/dev/null || return 0',
+    '  chmod 700 "$spool_dir" 2>/dev/null || :',
+    "  spool_id=$(printf %s \"${ORCA_PANE_KEY:-unknown}\" | tail -c 36 | tr '/:' '__')",
+    '  spool_file="$spool_dir/pane-$spool_id.jsonl"',
+    '  if [ -f "$spool_file" ] && find "$spool_file" -mtime +7 -print -quit 2>/dev/null | grep -q .; then : > "$spool_file"; fi',
+    '  [ -f "$spool_file" ] || : > "$spool_file"',
+    '  spool_size=$(wc -c < "$spool_file" 2>/dev/null || printf 0)',
+    '  [ "$spool_size" -lt 5242880 ] || return 0',
+    '  spool_now=$(date +%s 2>/dev/null || printf 0)',
+    '  spool_now=$((spool_now * 1000))',
+    '  spool_json_escape() { printf %s "$1" | sed \'s/\\\\/\\\\\\\\/g; s/"/\\\\"/g; s/[[:cntrl:]]/ /g\'; }',
+    spoolRecordLine,
+    '  chmod 600 "$spool_file" 2>/dev/null || :',
+    '}'
+  ]
+}
+
 export const WINDOWS_HOOK_STDIN_DRAIN_LABEL = 'orca_agent_hook_drain_stdin'
 // Why: qualify the stdin reader because Windows searches the worktree for
 // executables before PATH and hook payloads must not reach repo-local code.

paidaxingyo666/Manta

(cherry picked from commit aaef5e8c9fbd201cfb8003f268badfc6b7a1610b)

src/main/agent-hooks/hook-stdin-contract.ts
@@ -22,6 +22,52 @@ export function buildPosixHookPayloadCapture(
   ]
 }
 
+/** Shell-side durable fallback shared by every POSIX managed hook.
+ *  `eventNameVar` is for providers that send the event name out-of-band rather than in the
+ *  payload JSON; without it both the progress filter and replay would miss the event name. */
+export function buildPosixHookSpoolLines(source: string, eventNameVar?: string): string[] {
+  // Why: the event name must be a printf ARG, not inlined in the single-quoted format,
+  // where a command substitution would be emitted literally.
+  const eventFormat = eventNameVar ? '"hookEventName":"%s",' : ''
+  const eventArg = eventNameVar ? ` "$(spool_json_escape "\${${eventNameVar}:-}")"` : ''
+  const spoolRecordLine = "  { printf '\\n{".concat(
+    eventFormat,
+    '"paneKey":"%s","tabId":"%s","worktreeId":"%s","env":"%s","version":"%s","launchToken":"%s","source":"%s","receivedAt":%s,"payload":%s}\\n\'',
+    eventArg,
+    ' "$(spool_json_escape "${MANTA_PANE_KEY:-}")" "$(spool_json_escape "${MANTA_TAB_ID:-}")" "$(spool_json_escape "${MANTA_WORKTREE_ID:-}")" "$(spool_json_escape "${MANTA_AGENT_HOOK_ENV:-}")" "$(spool_json_escape "${MANTA_AGENT_HOOK_VERSION:-}")" "$(spool_json_escape "${MANTA_AGENT_LAUNCH_TOKEN:-}")" "$(spool_json_escape "',
+    source,
+    '")" "$spool_now" "$payload"; } >> "$spool_file" 2>/dev/null || :'
+  )
+  return [
+    'spool_hook_event() {',
+    eventNameVar
+      ? `  case "\${${eventNameVar}:-}" in PreToolUse|PostToolUse|PostToolUseFailure) return 0 ;; esac`
+      : '  case "$payload" in *\'"PreToolUse"\'*|*\'"PostToolUse"\'*|*\'"PostToolUseFailure"\'*) return 0 ;; esac',
+    '  [ -n "${MANTA_AGENT_HOOK_ENDPOINT:-}" ] || return 0',
+    // Why: an endpoint can linger in a parent shell after leaving Manta; without a pane key
+    // the record is un-attributable and would accumulate as pane-unknown.jsonl.
+    '  [ -n "${MANTA_PANE_KEY:-}" ] || return 0',
+    // Why: a stale env var must not create a spool tree for a Manta that is not installed here.
+    '  [ -r "$MANTA_AGENT_HOOK_ENDPOINT" ] || return 0',
+    '  spool_base=${MANTA_AGENT_HOOK_ENDPOINT%/*}',
+    '  spool_dir="$spool_base/spool"',
+    '  mkdir -p "$spool_dir" 2>/dev/null || return 0',
+    '  chmod 700 "$spool_dir" 2>/dev/null || :',
+    "  spool_id=$(printf %s \"${MANTA_PANE_KEY:-unknown}\" | tail -c 36 | tr '/:' '__')",
+    '  spool_file="$spool_dir/pane-$spool_id.jsonl"',
+    '  if [ -f "$spool_file" ] && find "$spool_file" -mtime +7 -print -quit 2>/dev/null | grep -q .; then : > "$spool_file"; fi',
+    '  [ -f "$spool_file" ] || : > "$spool_file"',
+    '  spool_size=$(wc -c < "$spool_file" 2>/dev/null || printf 0)',
+    '  [ "$spool_size" -lt 5242880 ] || return 0',
+    '  spool_now=$(date +%s 2>/dev/null || printf 0)',
+    '  spool_now=$((spool_now * 1000))',
+    '  spool_json_escape() { printf %s "$1" | sed \'s/\\\\/\\\\\\\\/g; s/"/\\\\"/g; s/[[:cntrl:]]/ /g\'; }',
+    spoolRecordLine,
+    '  chmod 600 "$spool_file" 2>/dev/null || :',
+    '}'
+  ]
+}
+
 export const WINDOWS_HOOK_STDIN_DRAIN_LABEL = 'manta_agent_hook_drain_stdin'
 // Why: qualify the stdin reader because Windows searches the worktree for
 // executables before PATH and hook payloads must not reach repo-local code.