2022zhang125/orca-yunxiao

fix(terminal): recover OMP from stale working directories (#17128)
* fix(terminal): recover OMP from stale cwd

* fix(terminal): harden OMP cwd recovery

src/main/pty/omp-shell-wrapper.node-pty.test.ts
@@ -1,5 +1,13 @@
 import { spawnSync } from 'node:child_process'
-import { chmodSync, mkdtempSync, mkdirSync, readFileSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  chmodSync,
+  mkdtempSync,
+  mkdirSync,
+  readFileSync,
+  realpathSync,
+  rmSync,
+  writeFileSync
+} from 'node:fs'
 import { tmpdir } from 'node:os'
 import { join } from 'node:path'
 import * as pty from 'node-pty'
@@ -8,7 +16,11 @@ import { getPosixOmpShellWrapper } from './omp-shell-wrapper'
 
 const describePosix = process.platform === 'win32' ? describe.skip : describe
 const hasBash = process.platform !== 'win32' && spawnSync('bash', ['--version']).status === 0
+const hasZsh = process.platform !== 'win32' && spawnSync('zsh', ['--version']).status === 0
 const itWithBash = hasBash ? it : it.skip
+const itWithZsh = hasZsh ? it : it.skip
+
+type PosixShell = 'bash' | 'zsh'
 
 const tempDirs: string[] = []
 
@@ -31,33 +43,38 @@ fi
 {
   printf 'PI=%s\\n' "$PI_CODING_AGENT_DIR"
   printf 'EFFECTIVE=%s\\n' "$agent_dir"
+  printf 'CWD=%s\\n' "$(pwd -P)"
   i=0
   for arg in "$@"; do
     i=$((i + 1))
     printf 'ARG%s=%s\\n' "$i" "$arg"
   done
 } > "$ORCA_CAPTURE_FILE"
+exit "\${ORCA_TEST_FAKE_OMP_EXIT_CODE:-0}"
 `,
     { mode: 0o755 }
   )
   chmodSync(ompPath, 0o755)
 }
 
-async function runInteractiveBashPty(args: {
+async function runInteractivePosixPty(args: {
   rcfileContent: string
   env: Record<string, string>
   input: string
   cwd: string
+  shell?: PosixShell
 }): Promise<string> {
   const rcfile = join(args.cwd, 'rcfile')
   writeFileSync(rcfile, args.rcfileContent)
+  const shell = args.shell ?? 'bash'
+  const shellArgs = shell === 'bash' ? ['--noprofile', '--rcfile', rcfile, '-i'] : ['-f', '-i']
 
-  const proc = pty.spawn('bash', ['--noprofile', '--rcfile', rcfile, '-i'], {
+  const proc = pty.spawn(shell, shellArgs, {
     name: 'xterm-256color',
     cols: 100,
     rows: 30,
     cwd: args.cwd,
-    env: args.env
+    env: { ...args.env, ORCA_TEST_RCFILE: rcfile }
   })
 
   let output = ''
@@ -72,13 +89,14 @@ async function runInteractiveBashPty(args: {
   let timeout: ReturnType<typeof setTimeout> | null = null
   const timeoutPromise = new Promise<never>((_resolve, reject) => {
     timeout = setTimeout(
-      () => reject(new Error(`timed out waiting for bash PTY output:\n${output}`)),
+      () => reject(new Error(`timed out waiting for ${shell} PTY output:\n${output}`)),
       5000
     )
   })
 
   try {
-    proc.write(args.input.replace(/\n/g, '\r'))
+    const input = shell === 'zsh' ? `source "$ORCA_TEST_RCFILE"\n${args.input}` : args.input
+    proc.write(input.replace(/\n/g, '\r'))
     const { exitCode } = await Promise.race([exitPromise, timeoutPromise])
     expect(exitCode).toBe(0)
     return output
@@ -130,7 +148,7 @@ describePosix('OMP shell wrapper node-pty reproduction', () => {
 
     const unwrappedCapture = join(tempDir, 'unwrapped-capture')
     const unwrappedAfterPi = join(tempDir, 'unwrapped-after-pi')
-    await runInteractiveBashPty({
+    await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: '',
       env: makeEnv(unwrappedCapture, unwrappedAfterPi),
@@ -148,7 +166,7 @@ exit 0
 
     const wrappedCapture = join(tempDir, 'wrapped-capture')
     const wrappedAfterPi = join(tempDir, 'wrapped-after-pi')
-    const wrappedOutput = await runInteractiveBashPty({
+    const wrappedOutput = await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: getPosixOmpShellWrapper(),
       env: makeEnv(wrappedCapture, wrappedAfterPi),
@@ -182,7 +200,7 @@ exit 0
     writeFakeOmp(binDir)
 
     const captureFile = join(tempDir, 'config-capture')
-    await runInteractiveBashPty({
+    await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: getPosixOmpShellWrapper(),
       env: {
@@ -237,7 +255,7 @@ exit 0
     writeFakeOmp(binDir)
 
     const captureFile = join(tempDir, `${subcommand}-capture`)
-    await runInteractiveBashPty({
+    await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: getPosixOmpShellWrapper(),
       env: {
@@ -280,7 +298,7 @@ exit 0
       writeFakeOmp(binDir)
 
       const captureFile = join(tempDir, 'default-config-capture')
-      await runInteractiveBashPty({
+      await runInteractivePosixPty({
         cwd: tempDir,
         rcfileContent: getPosixOmpShellWrapper(),
         env: {
@@ -308,4 +326,94 @@ exit 0
       )
     }
   )
+
+  async function expectStaleCwdRecovery(shell: PosixShell): Promise<void> {
+    const tempDir = makeTempDir()
+    const workspaceDir = join(tempDir, 'workspace')
+    const projectDir = join(workspaceDir, 'project')
+    const homeDir = join(tempDir, 'home')
+    const binDir = join(tempDir, 'bin')
+    const extensionDir = join(tempDir, 'extensions')
+    mkdirSync(projectDir, { recursive: true })
+    mkdirSync(homeDir)
+    mkdirSync(binDir)
+    mkdirSync(extensionDir)
+    const expectedProjectDir = realpathSync(projectDir)
+    const statusExtension = join(extensionDir, 'orca-agent-status.ts')
+    writeFileSync(statusExtension, 'export default {}')
+    writeFakeOmp(binDir)
+
+    const unsetPwdCaptureFile = join(tempDir, 'unset-pwd-capture')
+    const staleCaptureFile = join(tempDir, 'stale-cwd-capture')
+    const resultFile = join(tempDir, 'stale-cwd-result')
+    const output = await runInteractivePosixPty({
+      shell,
+      cwd: projectDir,
+      rcfileContent: `cd() { return 97; }
+${getPosixOmpShellWrapper()}`,
+      env: {
+        INPUTRC: '/dev/null',
+        PROMPT_COMMAND: '',
+        ORCA_STALE_PROJECT_DIR: projectDir,
+        ORCA_UNSET_PWD_CAPTURE_FILE: unsetPwdCaptureFile,
+        ORCA_STALE_CAPTURE_FILE: staleCaptureFile,
+        ORCA_WORKTREE_PATH: workspaceDir,
+        HOME: homeDir,
+        PATH: `${binDir}:/usr/bin:/bin:/usr/sbin:/sbin`,
+        ORCA_OMP_STATUS_EXTENSION: statusExtension,
+        ORCA_CAPTURE_FILE: staleCaptureFile,
+        ORCA_RESULT_FILE: resultFile,
+        ORCA_TEST_FAKE_OMP_EXIT_CODE: '23',
+        TERM: 'xterm-256color'
+      },
+      input: `ORCA_CAPTURE_FILE="$ORCA_UNSET_PWD_CAPTURE_FILE"
+unset PWD
+omp
+__orca_test_unset_status=$?
+if [[ -z "\${PWD+x}" ]]; then
+  __orca_test_pwd_state=unset
+else
+  __orca_test_pwd_state=set
+fi
+builtin cd -P -- "$ORCA_STALE_PROJECT_DIR"
+ORCA_CAPTURE_FILE="$ORCA_STALE_CAPTURE_FILE"
+/bin/rm -rf -- "$ORCA_STALE_PROJECT_DIR"
+/bin/mkdir -p -- "$ORCA_STALE_PROJECT_DIR"
+omp
+__orca_test_first_status=$?
+if [[ "$PWD" -ef . ]]; then
+  __orca_test_parent_state=live
+else
+  __orca_test_parent_state=stale
+fi
+/bin/rm -rf -- "$ORCA_STALE_PROJECT_DIR"
+omp
+__orca_test_missing_status=$?
+printf 'UNSET=%s\nPWD=%s\nFIRST=%s\nPARENT=%s\nMISSING=%s\n' "$__orca_test_unset_status" "$__orca_test_pwd_state" "$__orca_test_first_status" "$__orca_test_parent_state" "$__orca_test_missing_status" > "$ORCA_RESULT_FILE"
+exit 0
+`
+    })
+
+    const unsetPwdCapture = readFileSync(unsetPwdCaptureFile, 'utf8')
+    expect(unsetPwdCapture).toContain(`CWD=${expectedProjectDir}`)
+    const staleCapture = readFileSync(staleCaptureFile, 'utf8')
+    expect(staleCapture).toContain(`CWD=${expectedProjectDir}`)
+    expect(staleCapture.split('\n').filter((line) => line.startsWith('ARG'))).toEqual([
+      'ARG1=--extension',
+      `ARG2=${statusExtension}`
+    ])
+    expect(staleCapture).not.toContain('--cwd')
+    expect(readFileSync(resultFile, 'utf8')).toBe(
+      'UNSET=23\nPWD=unset\nFIRST=23\nPARENT=stale\nMISSING=1\n'
+    )
+    expect(output).toContain('Orca: OMP cannot access the terminal working directory')
+  }
+
+  itWithBash('rebinds a stale Bash cwd before launching OMP', async () => {
+    await expectStaleCwdRecovery('bash')
+  })
+
+  itWithZsh('rebinds a stale Zsh cwd before launching OMP', async () => {
+    await expectStaleCwdRecovery('zsh')
+  })
 })

src/main/pty/omp-shell-wrapper.ts
@@ -51,9 +51,17 @@ __orca_omp_should_skip_extension() {
   esac
   return 1
 }
-__orca_omp() {
-  local __orca_use_extension=1
-  __orca_omp_should_skip_extension "\${1:-}" && __orca_use_extension=0
+__orca_omp_cwd_is_usable() {
+  [[ -x . ]] || return 1
+  if [[ -n "\${PWD:-}" ]]; then
+    [[ -d "\${PWD}" && "\${PWD}" -ef . ]]
+  else
+    builtin pwd -P >/dev/null 2>&1
+  fi
+}
+__orca_omp_invoke() {
+  local __orca_use_extension="$1"
+  shift
   if [[ $__orca_use_extension -eq 1 && -n "\${ORCA_OMP_STATUS_EXTENSION:-}" && -f "\${ORCA_OMP_STATUS_EXTENSION}" ]]; then
     if [[ "\${1:-}" == "launch" ]]; then
       shift
@@ -63,6 +71,27 @@ __orca_omp() {
     fi
   else
     command omp "$@"
+  fi
+}
+__orca_omp() {
+  local __orca_use_extension=1
+  __orca_omp_should_skip_extension "\${1:-}" && __orca_use_extension=0
+  if [[ $__orca_use_extension -eq 1 ]] && ! __orca_omp_cwd_is_usable; then
+    local __orca_logical_cwd="\${PWD:-\${ORCA_WORKTREE_PATH:-\${ORCA_ROOT_PATH:-}}}"
+    # Why: a restored shell can retain the deleted directory inode after its path is recreated.
+    (
+      if [[ -z "$__orca_logical_cwd" ]]; then
+        printf 'Orca: OMP cannot start because no terminal working directory is available. Open a new terminal in an existing directory.\\n' >&2
+        return 1
+      fi
+      if ! builtin cd -P -- "$__orca_logical_cwd" 2>/dev/null; then
+        printf 'Orca: OMP cannot access the terminal working directory "%s". Open a new terminal in an existing directory.\\n' "$__orca_logical_cwd" >&2
+        return 1
+      fi
+      __orca_omp_invoke "$__orca_use_extension" "$@"
+    )
+  else
+    __orca_omp_invoke "$__orca_use_extension" "$@"
   fi
 }
 if [[ -n "\${ORCA_OMP_STATUS_EXTENSION:-}" ]]; then

paidaxingyo666/Manta

(cherry picked from commit 9db319dc06cab48bddde9045ddbfe95b3709726e)

src/main/pty/omp-shell-wrapper.node-pty.test.ts
@@ -1,5 +1,13 @@
 import { spawnSync } from 'node:child_process'
-import { chmodSync, mkdtempSync, mkdirSync, readFileSync, rmSync, writeFileSync } from 'node:fs'
+import {
+  chmodSync,
+  mkdtempSync,
+  mkdirSync,
+  readFileSync,
+  realpathSync,
+  rmSync,
+  writeFileSync
+} from 'node:fs'
 import { tmpdir } from 'node:os'
 import { join } from 'node:path'
 import * as pty from 'node-pty'
@@ -8,7 +16,11 @@ import { getPosixOmpShellWrapper } from './omp-shell-wrapper'
 
 const describePosix = process.platform === 'win32' ? describe.skip : describe
 const hasBash = process.platform !== 'win32' && spawnSync('bash', ['--version']).status === 0
+const hasZsh = process.platform !== 'win32' && spawnSync('zsh', ['--version']).status === 0
 const itWithBash = hasBash ? it : it.skip
+const itWithZsh = hasZsh ? it : it.skip
+
+type PosixShell = 'bash' | 'zsh'
 
 const tempDirs: string[] = []
 
@@ -31,33 +43,38 @@ fi
 {
   printf 'PI=%s\\n' "$PI_CODING_AGENT_DIR"
   printf 'EFFECTIVE=%s\\n' "$agent_dir"
+  printf 'CWD=%s\\n' "$(pwd -P)"
   i=0
   for arg in "$@"; do
     i=$((i + 1))
     printf 'ARG%s=%s\\n' "$i" "$arg"
   done
 } > "$MANTA_CAPTURE_FILE"
+exit "\${MANTA_TEST_FAKE_OMP_EXIT_CODE:-0}"
 `,
     { mode: 0o755 }
   )
   chmodSync(ompPath, 0o755)
 }
 
-async function runInteractiveBashPty(args: {
+async function runInteractivePosixPty(args: {
   rcfileContent: string
   env: Record<string, string>
   input: string
   cwd: string
+  shell?: PosixShell
 }): Promise<string> {
   const rcfile = join(args.cwd, 'rcfile')
   writeFileSync(rcfile, args.rcfileContent)
+  const shell = args.shell ?? 'bash'
+  const shellArgs = shell === 'bash' ? ['--noprofile', '--rcfile', rcfile, '-i'] : ['-f', '-i']
 
-  const proc = pty.spawn('bash', ['--noprofile', '--rcfile', rcfile, '-i'], {
+  const proc = pty.spawn(shell, shellArgs, {
     name: 'xterm-256color',
     cols: 100,
     rows: 30,
     cwd: args.cwd,
-    env: args.env
+    env: { ...args.env, MANTA_TEST_RCFILE: rcfile }
   })
 
   let output = ''
@@ -72,13 +89,14 @@ async function runInteractiveBashPty(args: {
   let timeout: ReturnType<typeof setTimeout> | null = null
   const timeoutPromise = new Promise<never>((_resolve, reject) => {
     timeout = setTimeout(
-      () => reject(new Error(`timed out waiting for bash PTY output:\n${output}`)),
+      () => reject(new Error(`timed out waiting for ${shell} PTY output:\n${output}`)),
       5000
     )
   })
 
   try {
-    proc.write(args.input.replace(/\n/g, '\r'))
+    const input = shell === 'zsh' ? `source "$MANTA_TEST_RCFILE"\n${args.input}` : args.input
+    proc.write(input.replace(/\n/g, '\r'))
     const { exitCode } = await Promise.race([exitPromise, timeoutPromise])
     expect(exitCode).toBe(0)
     return output
@@ -130,7 +148,7 @@ describePosix('OMP shell wrapper node-pty reproduction', () => {
 
     const unwrappedCapture = join(tempDir, 'unwrapped-capture')
     const unwrappedAfterPi = join(tempDir, 'unwrapped-after-pi')
-    await runInteractiveBashPty({
+    await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: '',
       env: makeEnv(unwrappedCapture, unwrappedAfterPi),
@@ -148,7 +166,7 @@ exit 0
 
     const wrappedCapture = join(tempDir, 'wrapped-capture')
     const wrappedAfterPi = join(tempDir, 'wrapped-after-pi')
-    const wrappedOutput = await runInteractiveBashPty({
+    const wrappedOutput = await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: getPosixOmpShellWrapper(),
       env: makeEnv(wrappedCapture, wrappedAfterPi),
@@ -182,7 +200,7 @@ exit 0
     writeFakeOmp(binDir)
 
     const captureFile = join(tempDir, 'config-capture')
-    await runInteractiveBashPty({
+    await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: getPosixOmpShellWrapper(),
       env: {
@@ -237,7 +255,7 @@ exit 0
     writeFakeOmp(binDir)
 
     const captureFile = join(tempDir, `${subcommand}-capture`)
-    await runInteractiveBashPty({
+    await runInteractivePosixPty({
       cwd: tempDir,
       rcfileContent: getPosixOmpShellWrapper(),
       env: {
@@ -280,7 +298,7 @@ exit 0
       writeFakeOmp(binDir)
 
       const captureFile = join(tempDir, 'default-config-capture')
-      await runInteractiveBashPty({
+      await runInteractivePosixPty({
         cwd: tempDir,
         rcfileContent: getPosixOmpShellWrapper(),
         env: {
@@ -308,4 +326,94 @@ exit 0
       )
     }
   )
+
+  async function expectStaleCwdRecovery(shell: PosixShell): Promise<void> {
+    const tempDir = makeTempDir()
+    const workspaceDir = join(tempDir, 'workspace')
+    const projectDir = join(workspaceDir, 'project')
+    const homeDir = join(tempDir, 'home')
+    const binDir = join(tempDir, 'bin')
+    const extensionDir = join(tempDir, 'extensions')
+    mkdirSync(projectDir, { recursive: true })
+    mkdirSync(homeDir)
+    mkdirSync(binDir)
+    mkdirSync(extensionDir)
+    const expectedProjectDir = realpathSync(projectDir)
+    const statusExtension = join(extensionDir, 'manta-agent-status.ts')
+    writeFileSync(statusExtension, 'export default {}')
+    writeFakeOmp(binDir)
+
+    const unsetPwdCaptureFile = join(tempDir, 'unset-pwd-capture')
+    const staleCaptureFile = join(tempDir, 'stale-cwd-capture')
+    const resultFile = join(tempDir, 'stale-cwd-result')
+    const output = await runInteractivePosixPty({
+      shell,
+      cwd: projectDir,
+      rcfileContent: `cd() { return 97; }
+${getPosixOmpShellWrapper()}`,
+      env: {
+        INPUTRC: '/dev/null',
+        PROMPT_COMMAND: '',
+        MANTA_STALE_PROJECT_DIR: projectDir,
+        MANTA_UNSET_PWD_CAPTURE_FILE: unsetPwdCaptureFile,
+        MANTA_STALE_CAPTURE_FILE: staleCaptureFile,
+        MANTA_WORKTREE_PATH: workspaceDir,
+        HOME: homeDir,
+        PATH: `${binDir}:/usr/bin:/bin:/usr/sbin:/sbin`,
+        MANTA_OMP_STATUS_EXTENSION: statusExtension,
+        MANTA_CAPTURE_FILE: staleCaptureFile,
+        MANTA_RESULT_FILE: resultFile,
+        MANTA_TEST_FAKE_OMP_EXIT_CODE: '23',
+        TERM: 'xterm-256color'
+      },
+      input: `MANTA_CAPTURE_FILE="$MANTA_UNSET_PWD_CAPTURE_FILE"
+unset PWD
+omp
+__manta_test_unset_status=$?
+if [[ -z "\${PWD+x}" ]]; then
+  __manta_test_pwd_state=unset
+else
+  __manta_test_pwd_state=set
+fi
+builtin cd -P -- "$MANTA_STALE_PROJECT_DIR"
+MANTA_CAPTURE_FILE="$MANTA_STALE_CAPTURE_FILE"
+/bin/rm -rf -- "$MANTA_STALE_PROJECT_DIR"
+/bin/mkdir -p -- "$MANTA_STALE_PROJECT_DIR"
+omp
+__manta_test_first_status=$?
+if [[ "$PWD" -ef . ]]; then
+  __manta_test_parent_state=live
+else
+  __manta_test_parent_state=stale
+fi
+/bin/rm -rf -- "$MANTA_STALE_PROJECT_DIR"
+omp
+__manta_test_missing_status=$?
+printf 'UNSET=%s\nPWD=%s\nFIRST=%s\nPARENT=%s\nMISSING=%s\n' "$__manta_test_unset_status" "$__manta_test_pwd_state" "$__manta_test_first_status" "$__manta_test_parent_state" "$__manta_test_missing_status" > "$MANTA_RESULT_FILE"
+exit 0
+`
+    })
+
+    const unsetPwdCapture = readFileSync(unsetPwdCaptureFile, 'utf8')
+    expect(unsetPwdCapture).toContain(`CWD=${expectedProjectDir}`)
+    const staleCapture = readFileSync(staleCaptureFile, 'utf8')
+    expect(staleCapture).toContain(`CWD=${expectedProjectDir}`)
+    expect(staleCapture.split('\n').filter((line) => line.startsWith('ARG'))).toEqual([
+      'ARG1=--extension',
+      `ARG2=${statusExtension}`
+    ])
+    expect(staleCapture).not.toContain('--cwd')
+    expect(readFileSync(resultFile, 'utf8')).toBe(
+      'UNSET=23\nPWD=unset\nFIRST=23\nPARENT=stale\nMISSING=1\n'
+    )
+    expect(output).toContain('Manta: OMP cannot access the terminal working directory')
+  }
+
+  itWithBash('rebinds a stale Bash cwd before launching OMP', async () => {
+    await expectStaleCwdRecovery('bash')
+  })
+
+  itWithZsh('rebinds a stale Zsh cwd before launching OMP', async () => {
+    await expectStaleCwdRecovery('zsh')
+  })
 })

src/main/pty/omp-shell-wrapper.ts
@@ -51,9 +51,17 @@ __manta_omp_should_skip_extension() {
   esac
   return 1
 }
-__manta_omp() {
-  local __manta_use_extension=1
-  __manta_omp_should_skip_extension "\${1:-}" && __manta_use_extension=0
+__manta_omp_cwd_is_usable() {
+  [[ -x . ]] || return 1
+  if [[ -n "\${PWD:-}" ]]; then
+    [[ -d "\${PWD}" && "\${PWD}" -ef . ]]
+  else
+    builtin pwd -P >/dev/null 2>&1
+  fi
+}
+__manta_omp_invoke() {
+  local __manta_use_extension="$1"
+  shift
   if [[ $__manta_use_extension -eq 1 && -n "\${MANTA_OMP_STATUS_EXTENSION:-}" && -f "\${MANTA_OMP_STATUS_EXTENSION}" ]]; then
     if [[ "\${1:-}" == "launch" ]]; then
       shift
@@ -63,6 +71,27 @@ __manta_omp() {
     fi
   else
     command omp "$@"
+  fi
+}
+__manta_omp() {
+  local __manta_use_extension=1
+  __manta_omp_should_skip_extension "\${1:-}" && __manta_use_extension=0
+  if [[ $__manta_use_extension -eq 1 ]] && ! __manta_omp_cwd_is_usable; then
+    local __manta_logical_cwd="\${PWD:-\${MANTA_WORKTREE_PATH:-\${MANTA_ROOT_PATH:-}}}"
+    # Why: a restored shell can retain the deleted directory inode after its path is recreated.
+    (
+      if [[ -z "$__manta_logical_cwd" ]]; then
+        printf 'Manta: OMP cannot start because no terminal working directory is available. Open a new terminal in an existing directory.\\n' >&2
+        return 1
+      fi
+      if ! builtin cd -P -- "$__manta_logical_cwd" 2>/dev/null; then
+        printf 'Manta: OMP cannot access the terminal working directory "%s". Open a new terminal in an existing directory.\\n' "$__manta_logical_cwd" >&2
+        return 1
+      fi
+      __manta_omp_invoke "$__manta_use_extension" "$@"
+    )
+  else
+    __manta_omp_invoke "$__manta_use_extension" "$@"
   fi
 }
 if [[ -n "\${MANTA_OMP_STATUS_EXTENSION:-}" ]]; then