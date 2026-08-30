2022zhang125/orca-yunxiao

Open target=_blank links and unnamed popups in new Orca tabs (#16720)
* feat(browser): open target=_blank links and unnamed popups in new Orca t

- Treat target=_blank as a new-tab request matching browser behavior
- Route unnamed, featureless window.open() calls to Orca tabs instead of native popups
- Add rate limiting to prevent page-initiated tab loops
- Inherit session profiles when opening links to maintain isolation boundaries

* fix(browser): deny new-tab window.open when renderer is destroyed

Move deny action outside conditional to ensure new-tab intents are
safely rejected even if renderer vanishes mid-open, preventing native
popup fallthrough. Add test coverage and simplify comments.

* Share page-initiated tab budget across opener popup tree

Prevent pages from bypassing the new-tab rate limit by chaining popup
windows. The page-initiated tab quota is now shared by all popups in
an opener tree (root + named children), so child windows inherit their
root's budget instead of each getting a fresh allocation.

src/main/browser/browser-clicked-link-routing.test.ts
@@ -64,7 +64,7 @@ describe('browser clicked-link routing', () => {
     vi.restoreAllMocks()
   })
 
-  it('routes plain target=_blank links back into the current Orca tab', () => {
+  it('routes plain target=_blank links into a new Orca tab', () => {
     const link = document.createElement('a')
     link.href = 'https://docs.example.com/guide'
     link.target = '_blank'
@@ -73,9 +73,8 @@ describe('browser clicked-link routing', () => {
 
     const { event, open } = clickLink(link)
 
-    expect(event.defaultPrevented).toBe(false)
-    expect(open).not.toHaveBeenCalled()
-    expect(link.getAttribute('target')).toBe('_self')
+    expect(event.defaultPrevented).toBe(true)
+    expect(open).toHaveBeenCalledWith('https://docs.example.com/guide', FOREGROUND_FRAME_NAME)
   })
 
   it('routes the host-platform modifier without trusting an emulated guest user agent', () => {
@@ -127,9 +126,11 @@ describe('browser clicked-link routing', () => {
 
     expect(clickLink(cancelled).open).not.toHaveBeenCalled()
     expect(cancelled.getAttribute('target')).toBe('_blank')
-    expect(clickLink(rewritten).open).not.toHaveBeenCalled()
-    expect(rewritten.href).toBe('https://example.com/rewritten')
-    expect(rewritten.getAttribute('target')).toBe('_self')
+    // The page's own handler ran first, so routing must follow the rewritten href.
+    expect(clickLink(rewritten).open).toHaveBeenCalledWith(
+      'https://example.com/rewritten',
+      FOREGROUND_FRAME_NAME
+    )
   })
 
   it('routes SVG links but leaves download links and links without href alone', () => {
@@ -145,8 +146,10 @@ describe('browser clicked-link routing', () => {
     document.body.append(svgLink, areaDownload, noHref)
     installRouting()
 
-    expect(clickLink(svgLink).open).not.toHaveBeenCalled()
-    expect(svgLink.getAttribute('target')).toBe('_self')
+    expect(clickLink(svgLink).open).toHaveBeenCalledWith(
+      'https://example.com/svg',
+      FOREGROUND_FRAME_NAME
+    )
     expect(clickLink(areaDownload).open).not.toHaveBeenCalled()
     expect(clickLink(noHref).open).not.toHaveBeenCalled()
   })
@@ -195,7 +198,7 @@ describe('browser clicked-link routing', () => {
     expect(script).not.toContain('BrowserClickedLinkRoutingState')
   })
 
-  it('routes plain iframe target=_blank links into the top-level guest', () => {
+  it('routes plain iframe target=_blank links into a new Orca tab', () => {
     const link = document.createElement('a')
     link.href = 'https://example.com/from-frame'
     link.target = '_blank'
@@ -204,9 +207,8 @@ describe('browser clicked-link routing', () => {
 
     const { event, open } = clickLink(link)
 
-    expect(event.defaultPrevented).toBe(false)
-    expect(open).not.toHaveBeenCalled()
-    expect(link.getAttribute('target')).toBe('_top')
+    expect(event.defaultPrevented).toBe(true)
+    expect(open).toHaveBeenCalledWith('https://example.com/from-frame', FOREGROUND_FRAME_NAME)
   })
 
   it('routes explicit iframe new-tab gestures through one-use frame names', () => {

tests/e2e/browser-tab.spec.ts
@@ -680,7 +680,7 @@ test.describe('Browser Tab', () => {
     }
   })
 
-  test('plain links stay current while explicit new-tab gestures activate Orca tabs', async ({
+  test('every new-tab link gesture activates an Orca tab and never a native window', async ({
     electronApp,
     orcaPage
   }) => {
@@ -698,23 +698,20 @@ test.describe('Browser Tab', () => {
       const baseWindowCount = await electronApp.evaluate(
         ({ BaseWindow }) => BaseWindow.getAllWindows().length
       )
-      const baseTabCount = await orcaPage.locator('[data-tab-id]').count()
-      await clickBrowserLink(orcaPage, sourceTab!.id, '#external-link')
-
+      // A plain target=_blank click is a new-tab request, in the main frame and in an iframe;
+      // the source tab must stay put rather than navigate away under it.
       const sourceTabLocator = orcaPage.locator(`[data-tab-id="${sourceTab!.id}"]`)
-      await expect(sourceTabLocator).toContainText('Linked destination', { timeout: 10_000 })
-      await expect(orcaPage.locator('[data-tab-id]')).toHaveCount(baseTabCount)
+      await clickBrowserLink(orcaPage, sourceTab!.id, '#external-link')
+      await expectBrowserTabActive(orcaPage, 'Linked destination')
+      await expect(sourceTabLocator).toContainText('Source page')
+      await switchToBrowserTab(orcaPage, worktreeId, sourceTab!.id)
 
-      await clickBrowserLink(orcaPage, sourceTab!.id, '#return-link')
-      await expect(sourceTabLocator).toContainText('Source page', { timeout: 10_000 })
       await clickBrowserLink(orcaPage, sourceTab!.id, '#frame-link', {
         frameSelector: '#link-frame'
       })
-      await expect(sourceTabLocator).toContainText('Frame destination', { timeout: 10_000 })
-      await expect(orcaPage.locator('[data-tab-id]')).toHaveCount(baseTabCount)
-
-      await clickBrowserLink(orcaPage, sourceTab!.id, '#return-link')
-      await expect(sourceTabLocator).toContainText('Source page', { timeout: 10_000 })
+      await expectBrowserTabActive(orcaPage, 'Frame destination')
+      await expect(sourceTabLocator).toContainText('Source page')
+      await switchToBrowserTab(orcaPage, worktreeId, sourceTab!.id)
 
       await clickBrowserLink(orcaPage, sourceTab!.id, '#frame-modifier-link', {
         frameSelector: '#link-frame',

paidaxingyo666/Manta

(cherry picked from commit 07b7e9e68d304d24af068ed8aa6b4bfec0ee718b)

src/main/browser/browser-clicked-link-routing.test.ts
@@ -64,7 +64,7 @@ describe('browser clicked-link routing', () => {
     vi.restoreAllMocks()
   })
 
-  it('routes plain target=_blank links back into the current Manta tab', () => {
+  it('routes plain target=_blank links into a new Manta tab', () => {
     const link = document.createElement('a')
     link.href = 'https://docs.example.com/guide'
     link.target = '_blank'
@@ -73,9 +73,8 @@ describe('browser clicked-link routing', () => {
 
     const { event, open } = clickLink(link)
 
-    expect(event.defaultPrevented).toBe(false)
-    expect(open).not.toHaveBeenCalled()
-    expect(link.getAttribute('target')).toBe('_self')
+    expect(event.defaultPrevented).toBe(true)
+    expect(open).toHaveBeenCalledWith('https://docs.example.com/guide', FOREGROUND_FRAME_NAME)
   })
 
   it('routes the host-platform modifier without trusting an emulated guest user agent', () => {
@@ -127,9 +126,11 @@ describe('browser clicked-link routing', () => {
 
     expect(clickLink(cancelled).open).not.toHaveBeenCalled()
     expect(cancelled.getAttribute('target')).toBe('_blank')
-    expect(clickLink(rewritten).open).not.toHaveBeenCalled()
-    expect(rewritten.href).toBe('https://example.com/rewritten')
-    expect(rewritten.getAttribute('target')).toBe('_self')
+    // The page's own handler ran first, so routing must follow the rewritten href.
+    expect(clickLink(rewritten).open).toHaveBeenCalledWith(
+      'https://example.com/rewritten',
+      FOREGROUND_FRAME_NAME
+    )
   })
 
   it('routes SVG links but leaves download links and links without href alone', () => {
@@ -145,8 +146,10 @@ describe('browser clicked-link routing', () => {
     document.body.append(svgLink, areaDownload, noHref)
     installRouting()
 
-    expect(clickLink(svgLink).open).not.toHaveBeenCalled()
-    expect(svgLink.getAttribute('target')).toBe('_self')
+    expect(clickLink(svgLink).open).toHaveBeenCalledWith(
+      'https://example.com/svg',
+      FOREGROUND_FRAME_NAME
+    )
     expect(clickLink(areaDownload).open).not.toHaveBeenCalled()
     expect(clickLink(noHref).open).not.toHaveBeenCalled()
   })
@@ -195,7 +198,7 @@ describe('browser clicked-link routing', () => {
     expect(script).not.toContain('BrowserClickedLinkRoutingState')
   })
 
-  it('routes plain iframe target=_blank links into the top-level guest', () => {
+  it('routes plain iframe target=_blank links into a new Manta tab', () => {
     const link = document.createElement('a')
     link.href = 'https://example.com/from-frame'
     link.target = '_blank'
@@ -204,9 +207,8 @@ describe('browser clicked-link routing', () => {
 
     const { event, open } = clickLink(link)
 
-    expect(event.defaultPrevented).toBe(false)
-    expect(open).not.toHaveBeenCalled()
-    expect(link.getAttribute('target')).toBe('_top')
+    expect(event.defaultPrevented).toBe(true)
+    expect(open).toHaveBeenCalledWith('https://example.com/from-frame', FOREGROUND_FRAME_NAME)
   })
 
   it('routes explicit iframe new-tab gestures through one-use frame names', () => {

tests/e2e/browser-tab.spec.ts
@@ -690,7 +690,7 @@ test.describe('Browser Tab', () => {
     }
   })
 
-  test('plain links stay current while explicit new-tab gestures activate Manta tabs', async ({
+  test('every new-tab link gesture activates a Manta tab and never a native window', async ({
     electronApp,
     mantaPage
   }) => {
@@ -708,23 +708,20 @@ test.describe('Browser Tab', () => {
       const baseWindowCount = await electronApp.evaluate(
         ({ BaseWindow }) => BaseWindow.getAllWindows().length
       )
-      const baseTabCount = await mantaPage.locator('[data-tab-id]').count()
-      await clickBrowserLink(mantaPage, sourceTab!.id, '#external-link')
-
+      // A plain target=_blank click is a new-tab request, in the main frame and in an iframe;
+      // the source tab must stay put rather than navigate away under it.
       const sourceTabLocator = mantaPage.locator(`[data-tab-id="${sourceTab!.id}"]`)
-      await expect(sourceTabLocator).toContainText('Linked destination', { timeout: 10_000 })
-      await expect(mantaPage.locator('[data-tab-id]')).toHaveCount(baseTabCount)
+      await clickBrowserLink(mantaPage, sourceTab!.id, '#external-link')
+      await expectBrowserTabActive(mantaPage, 'Linked destination')
+      await expect(sourceTabLocator).toContainText('Source page')
+      await switchToBrowserTab(mantaPage, worktreeId, sourceTab!.id)
 
-      await clickBrowserLink(mantaPage, sourceTab!.id, '#return-link')
-      await expect(sourceTabLocator).toContainText('Source page', { timeout: 10_000 })
       await clickBrowserLink(mantaPage, sourceTab!.id, '#frame-link', {
         frameSelector: '#link-frame'
       })
-      await expect(sourceTabLocator).toContainText('Frame destination', { timeout: 10_000 })
-      await expect(mantaPage.locator('[data-tab-id]')).toHaveCount(baseTabCount)
-
-      await clickBrowserLink(mantaPage, sourceTab!.id, '#return-link')
-      await expect(sourceTabLocator).toContainText('Source page', { timeout: 10_000 })
+      await expectBrowserTabActive(mantaPage, 'Frame destination')
+      await expect(sourceTabLocator).toContainText('Source page')
+      await switchToBrowserTab(mantaPage, worktreeId, sourceTab!.id)
 
       await clickBrowserLink(mantaPage, sourceTab!.id, '#frame-modifier-link', {
         frameSelector: '#link-frame',