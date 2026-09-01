aniruddhaadak80/orca

refactor(preload): split bridge API modules

src/preload/api/browser-bridge-part-1.ts
@@ -1,0 +1,188 @@
+import { ipcRenderer } from 'electron'
+import type { BrowserViewportOverride } from '../../shared/browser-workspace-types'
+import type {
+  BrowserWebAuthnAccountRequest,
+  BrowserWebAuthnAccountResponse
+} from '../../shared/browser-webauthn-account'
+import { readBrowserClientHostIdArgument } from '../../shared/browser-client-host-id-argument'
+import { browserClientPageRendererRequests } from '../preload-runtime-support'
+
+export const browserApiPart1 = {
+  onClientPageRendererRequest: browserClientPageRendererRequests.subscribe,
+  readClientHostId: (): string | null => readBrowserClientHostIdArgument(process.argv),
+  registerGuest: (args: {
+    browserPageId: string
+    workspaceId: string
+    worktreeId: string
+    sessionProfileId?: string | null
+    webContentsId: number
+  }): Promise<boolean> => ipcRenderer.invoke('browser:registerGuest', args),
+  isGuestRegistered: (args: { browserPageId: string; webContentsId: number }): Promise<boolean> =>
+    ipcRenderer.invoke('browser:isGuestRegistered', args),
+  repairGuestRegistration: (args: {
+    browserPageId: string
+    workspaceId: string
+    worktreeId: string
+    sessionProfileId?: string | null
+    webContentsId: number
+  }): Promise<boolean> => ipcRenderer.invoke('browser:repairGuestRegistration', args),
+  unregisterGuest: (args: { browserPageId: string }): Promise<void> =>
+    ipcRenderer.invoke('browser:unregisterGuest', args),
+  onWebAuthnAccountRequest: (
+    callback: (request: BrowserWebAuthnAccountRequest) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      request: BrowserWebAuthnAccountRequest
+    ): void => callback(request)
+    ipcRenderer.on('browser:webauthn-account-requested', listener)
+    return () => ipcRenderer.removeListener('browser:webauthn-account-requested', listener)
+  },
+  onWebAuthnAccountRequestClosed: (
+    callback: (event: { requestId: string }) => void
+  ): (() => void) => {
+    const listener = (_event: Electron.IpcRendererEvent, data: { requestId: string }): void =>
+      callback(data)
+    ipcRenderer.on('browser:webauthn-account-request-closed', listener)
+    return () => ipcRenderer.removeListener('browser:webauthn-account-request-closed', listener)
+  },
+  respondWebAuthnAccount: (response: BrowserWebAuthnAccountResponse): Promise<boolean> =>
+    ipcRenderer.invoke('browser:respondWebAuthnAccount', response),
+  openDevTools: (args: { browserPageId: string }): Promise<boolean> =>
+    ipcRenderer.invoke('browser:openDevTools', args),
+  setViewportOverride: (args: {
+    browserPageId: string
+    override: BrowserViewportOverride | null
+  }): Promise<boolean> => ipcRenderer.invoke('browser:setViewportOverride', args),
+  setAnnotationViewportBridge: (args): Promise<boolean> =>
+    ipcRenderer.invoke('browser:setAnnotationViewportBridge', args),
+  publishClientPageMetadata: (args) =>
+    ipcRenderer.invoke('browser:publishClientPageMetadata', args),
+  onGuestLoadFailed: (
+    callback: (args: {
+      browserPageId: string
+      loadError: { code: number; description: string; validatedUrl: string }
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId: string
+        loadError: { code: number; description: string; validatedUrl: string }
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:guest-load-failed', listener)
+    return () => ipcRenderer.removeListener('browser:guest-load-failed', listener)
+  },
+  onCertificateFailureChanged: (callback): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: Parameters<typeof callback>[0]
+    ): void => callback(data)
+    ipcRenderer.on('browser:certificate-failure-changed', listener)
+    return () => ipcRenderer.removeListener('browser:certificate-failure-changed', listener)
+  },
+  proceedCertificate: (args) => ipcRenderer.invoke('browser:proceedCertificate', args),
+  onPermissionDenied: (
+    callback: (event: { browserPageId: string; permission: string; origin: string }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: { browserPageId: string; permission: string; origin: string }
+    ) => callback(data)
+    ipcRenderer.on('browser:permission-denied', listener)
+    return () => ipcRenderer.removeListener('browser:permission-denied', listener)
+  },
+  onPopup: (
+    callback: (event: {
+      browserPageId: string
+      origin: string
+      action: 'opened-in-orca' | 'opened-external' | 'blocked'
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId: string
+        origin: string
+        action: 'opened-in-orca' | 'opened-external' | 'blocked'
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:popup', listener)
+    return () => ipcRenderer.removeListener('browser:popup', listener)
+  },
+  onDownloadRequested: (
+    callback: (event: {
+      browserPageId: string
+      downloadId: string
+      origin: string
+      filename: string
+      totalBytes: number | null
+      mimeType: string | null
+      savePath: string
+      status: 'downloading'
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId: string
+        downloadId: string
+        origin: string
+        filename: string
+        totalBytes: number | null
+        mimeType: string | null
+        savePath: string
+        status: 'downloading'
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:download-requested', listener)
+    return () => ipcRenderer.removeListener('browser:download-requested', listener)
+  },
+  onDownloadProgress: (
+    callback: (event: {
+      browserPageId?: string
+      downloadId: string
+      receivedBytes: number
+      totalBytes: number | null
+      state: 'progressing' | 'interrupted' | null
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId?: string
+        downloadId: string
+        receivedBytes: number
+        totalBytes: number | null
+        state: 'progressing' | 'interrupted' | null
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:download-progress', listener)
+    return () => ipcRenderer.removeListener('browser:download-progress', listener)
+  },
+  onDownloadFinished: (
+    callback: (event: {
+      browserPageId?: string
+      downloadId: string
+      status: 'completed' | 'canceled' | 'failed'
+      savePath: string | null
+      remoteDestination?: { workspaceRelativePath: string; hostLabel: string }
+      error: string | null
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId?: string
+        downloadId: string
+        status: 'completed' | 'canceled' | 'failed'
+        savePath: string | null
+        remoteDestination?: { workspaceRelativePath: string; hostLabel: string }
+        error: string | null
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:download-finished', listener)
+    return () => ipcRenderer.removeListener('browser:download-finished', listener)
+  }
+}

src/preload/preload-runtime-support.ts
@@ -1,0 +1,165 @@
+import { ipcRenderer, webUtils } from 'electron'
+import { createBrowserClientPageRendererRequests } from './browser-client-page-renderer-requests'
+import { createBrowserFindSubscriptions } from './browser-find-subscriptions'
+import { registerRendererRestartIpcRelays } from './renderer-restart-wiring'
+import { createUpdaterQuitAbortRelay } from '../shared/renderer-restart-preparation'
+import { ORCA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT } from '../shared/updater-renderer-events'
+import {
+  ORCA_INTERNAL_FILE_DRAG_TYPE,
+  createNativeFileDropPayload,
+  createRejectedNativeFileDropPayload,
+  hasNativeFileDragTypes,
+  NATIVE_FILE_DROP_MAX_PATHS,
+  resolveNativeFileDropPath,
+  type NativeDropResolution,
+  type NativeFileDropPayload,
+  type NativeFileDropPathEntry
+} from '../shared/native-file-drop'
+
+/** Joins the synchronous unload checkpoint with its durable renderer write. */
+export async function awaitBeforeUnloadCheckpoint(): Promise<void> {
+  const result = (await ipcRenderer.invoke('app:await-before-unload-checkpoint')) as {
+    ok?: unknown
+  }
+  if (result?.ok !== true) {
+    throw new Error('Failed to persist renderer state before unload.')
+  }
+}
+
+export const startupDiagnosticsEnabled = process.env.ORCA_STARTUP_DIAGNOSTICS === '1'
+
+export function getLinuxDisplayServer(): 'wayland' | 'x11' | null {
+  if (process.platform !== 'linux') {
+    return null
+  }
+  if (
+    process.env.WAYLAND_DISPLAY ||
+    process.env.XDG_SESSION_TYPE?.toLowerCase() === 'wayland' ||
+    process.env.ELECTRON_OZONE_PLATFORM_HINT?.toLowerCase() === 'wayland'
+  ) {
+    return 'wayland'
+  }
+  return process.env.DISPLAY ? 'x11' : null
+}
+
+type NativeFileDropCallback = (data: NativeFileDropPayload) => void
+const nativeFileDropCallbacks: NativeFileDropCallback[] = []
+let nativeFileDropListenerRegistered = false
+
+const onNativeFileDrop = (_event: Electron.IpcRendererEvent, data: NativeFileDropPayload): void => {
+  for (const callback of Array.from(nativeFileDropCallbacks)) {
+    callback(data)
+  }
+}
+
+export function subscribeNativeFileDrop(callback: NativeFileDropCallback): () => void {
+  nativeFileDropCallbacks.push(callback)
+  if (!nativeFileDropListenerRegistered) {
+    ipcRenderer.on('terminal:file-drop', onNativeFileDrop)
+    nativeFileDropListenerRegistered = true
+  }
+  return () => {
+    const callbackIndex = nativeFileDropCallbacks.indexOf(callback)
+    if (callbackIndex !== -1) {
+      nativeFileDropCallbacks.splice(callbackIndex, 1)
+    }
+    if (nativeFileDropCallbacks.length === 0 && nativeFileDropListenerRegistered) {
+      ipcRenderer.removeListener('terminal:file-drop', onNativeFileDrop)
+      nativeFileDropListenerRegistered = false
+    }
+  }
+}
+
+function resolveNativeFileDrop(event: DragEvent): NativeDropResolution | null {
+  const pathEntries: NativeFileDropPathEntry[] = []
+  for (const entry of event.composedPath()) {
+    if (entry instanceof HTMLElement) {
+      pathEntries.push({
+        nativeFileDropTarget: entry.dataset.nativeFileDropTarget,
+        nativeFileDropDir: entry.dataset.nativeFileDropDir,
+        terminalTabId: entry.dataset.terminalTabId,
+        terminalPaneLeafId: entry.dataset.terminalPaneLeafId ?? entry.dataset.leafId
+      })
+    }
+  }
+  return resolveNativeFileDropPath(pathEntries)
+}
+
+/** Installs the one preload-side listener that converts native File objects to paths. */
+export function installNativeFileDropHandlers(): void {
+  document.addEventListener(
+    'dragover',
+    (event) => {
+      if (event.dataTransfer && !hasNativeFileDragTypes(event.dataTransfer.types)) {
+        return
+      }
+      event.preventDefault()
+      if (event.dataTransfer) {
+        event.dataTransfer.dropEffect = 'copy'
+      }
+    },
+    true
+  )
+  document.addEventListener(
+    'drop',
+    (event) => {
+      if (event.dataTransfer?.types.includes(ORCA_INTERNAL_FILE_DRAG_TYPE)) {
+        return
+      }
+      event.preventDefault()
+      event.stopPropagation()
+      const files = event.dataTransfer?.files
+      if (!files || files.length === 0) {
+        return
+      }
+      const resolution = resolveNativeFileDrop(event)
+      if (files.length > NATIVE_FILE_DROP_MAX_PATHS) {
+        ipcRenderer.send(
+          'terminal:file-dropped-from-preload',
+          createRejectedNativeFileDropPayload({
+            byteLength: 0,
+            pathCount: files.length,
+            reason: 'too-many-paths',
+            status: 'rejected'
+          })
+        )
+        return
+      }
+      const paths: string[] = []
+      for (let index = 0; index < files.length; index += 1) {
+        const filePath = webUtils.getPathForFile(files[index])
+        if (filePath) {
+          paths.push(filePath)
+        }
+      }
+      if (paths.length === 0 || resolution?.target === 'rejected') {
+        return
+      }
+      const payload = createNativeFileDropPayload(resolution, paths)
+      if (payload) {
+        ipcRenderer.send('terminal:file-dropped-from-preload', payload)
+      }
+    },
+    true
+  )
+}
+
+export const browserFindSubscriptions = createBrowserFindSubscriptions()
+export const browserClientPageRendererRequests = createBrowserClientPageRendererRequests({
+  ipc: ipcRenderer,
+  isTopFrame: () => window.top === window
+})
+
+/** Registers browser find forwarding once for this preload context. */
+export function installBrowserFindListener(): void {
+  ipcRenderer.on('ui:findInBrowserPage', (_event, source: unknown) => {
+    browserFindSubscriptions.dispatch(source)
+  })
+}
+
+export const updaterQuitAbortRelay = createUpdaterQuitAbortRelay(
+  window,
+  ORCA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT
+)
+
+registerRendererRestartIpcRelays(ipcRenderer, window, updaterQuitAbortRelay)

paidaxingyo666/Manta

(cherry picked from commit b77e31873bc8bc5683703be1bfdc90b35f13f9f0)

src/preload/api/browser-bridge-part-1.ts
@@ -1,0 +1,197 @@
+import { ipcRenderer } from 'electron'
+import type { BrowserViewportOverride } from '../../shared/browser-workspace-types'
+import type {
+  BrowserWebAuthnAccountRequest,
+  BrowserWebAuthnAccountResponse
+} from '../../shared/browser-webauthn-account'
+import { readBrowserClientHostIdArgument } from '../../shared/browser-client-host-id-argument'
+import { browserClientPageRendererRequests } from '../preload-runtime-support'
+
+export const browserApiPart1 = {
+  onClientPageRendererRequest: browserClientPageRendererRequests.subscribe,
+  readClientHostId: (): string | null => readBrowserClientHostIdArgument(process.argv),
+  registerGuest: (args: {
+    browserPageId: string
+    workspaceId: string
+    worktreeId: string
+    sessionProfileId?: string | null
+    webContentsId: number
+  }): Promise<boolean> => ipcRenderer.invoke('browser:registerGuest', args),
+  isGuestRegistered: (args: { browserPageId: string; webContentsId: number }): Promise<boolean> =>
+    ipcRenderer.invoke('browser:isGuestRegistered', args),
+  repairGuestRegistration: (args: {
+    browserPageId: string
+    workspaceId: string
+    worktreeId: string
+    sessionProfileId?: string | null
+    webContentsId: number
+  }): Promise<boolean> => ipcRenderer.invoke('browser:repairGuestRegistration', args),
+  unregisterGuest: (args: { browserPageId: string }): Promise<void> =>
+    ipcRenderer.invoke('browser:unregisterGuest', args),
+  onWebAuthnAccountRequest: (
+    callback: (request: BrowserWebAuthnAccountRequest) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      request: BrowserWebAuthnAccountRequest
+    ): void => callback(request)
+    ipcRenderer.on('browser:webauthn-account-requested', listener)
+    return () => ipcRenderer.removeListener('browser:webauthn-account-requested', listener)
+  },
+  onWebAuthnAccountRequestClosed: (
+    callback: (event: { requestId: string }) => void
+  ): (() => void) => {
+    const listener = (_event: Electron.IpcRendererEvent, data: { requestId: string }): void =>
+      callback(data)
+    ipcRenderer.on('browser:webauthn-account-request-closed', listener)
+    return () => ipcRenderer.removeListener('browser:webauthn-account-request-closed', listener)
+  },
+  respondWebAuthnAccount: (response: BrowserWebAuthnAccountResponse): Promise<boolean> =>
+    ipcRenderer.invoke('browser:respondWebAuthnAccount', response),
+  openDevTools: (args: { browserPageId: string }): Promise<boolean> =>
+    ipcRenderer.invoke('browser:openDevTools', args),
+  setViewportOverride: (args: {
+    browserPageId: string
+    override: BrowserViewportOverride | null
+  }): Promise<boolean> => ipcRenderer.invoke('browser:setViewportOverride', args),
+  reportViewportScrollState: (args: {
+    browserPageId: string
+    state: {
+      scrollLeft: number
+      scrollTop: number
+      maxScrollLeft: number
+      maxScrollTop: number
+    }
+  }): void => ipcRenderer.send('browser:reportViewportScrollState', args),
+  setAnnotationViewportBridge: (args): Promise<boolean> =>
+    ipcRenderer.invoke('browser:setAnnotationViewportBridge', args),
+  publishClientPageMetadata: (args) =>
+    ipcRenderer.invoke('browser:publishClientPageMetadata', args),
+  onGuestLoadFailed: (
+    callback: (args: {
+      browserPageId: string
+      loadError: { code: number; description: string; validatedUrl: string }
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId: string
+        loadError: { code: number; description: string; validatedUrl: string }
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:guest-load-failed', listener)
+    return () => ipcRenderer.removeListener('browser:guest-load-failed', listener)
+  },
+  onCertificateFailureChanged: (callback): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: Parameters<typeof callback>[0]
+    ): void => callback(data)
+    ipcRenderer.on('browser:certificate-failure-changed', listener)
+    return () => ipcRenderer.removeListener('browser:certificate-failure-changed', listener)
+  },
+  proceedCertificate: (args) => ipcRenderer.invoke('browser:proceedCertificate', args),
+  onPermissionDenied: (
+    callback: (event: { browserPageId: string; permission: string; origin: string }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: { browserPageId: string; permission: string; origin: string }
+    ) => callback(data)
+    ipcRenderer.on('browser:permission-denied', listener)
+    return () => ipcRenderer.removeListener('browser:permission-denied', listener)
+  },
+  onPopup: (
+    callback: (event: {
+      browserPageId: string
+      origin: string
+      action: 'opened-in-manta' | 'opened-external' | 'blocked'
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId: string
+        origin: string
+        action: 'opened-in-manta' | 'opened-external' | 'blocked'
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:popup', listener)
+    return () => ipcRenderer.removeListener('browser:popup', listener)
+  },
+  onDownloadRequested: (
+    callback: (event: {
+      browserPageId: string
+      downloadId: string
+      origin: string
+      filename: string
+      totalBytes: number | null
+      mimeType: string | null
+      savePath: string
+      status: 'downloading'
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId: string
+        downloadId: string
+        origin: string
+        filename: string
+        totalBytes: number | null
+        mimeType: string | null
+        savePath: string
+        status: 'downloading'
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:download-requested', listener)
+    return () => ipcRenderer.removeListener('browser:download-requested', listener)
+  },
+  onDownloadProgress: (
+    callback: (event: {
+      browserPageId?: string
+      downloadId: string
+      receivedBytes: number
+      totalBytes: number | null
+      state: 'progressing' | 'interrupted' | null
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId?: string
+        downloadId: string
+        receivedBytes: number
+        totalBytes: number | null
+        state: 'progressing' | 'interrupted' | null
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:download-progress', listener)
+    return () => ipcRenderer.removeListener('browser:download-progress', listener)
+  },
+  onDownloadFinished: (
+    callback: (event: {
+      browserPageId?: string
+      downloadId: string
+      status: 'completed' | 'canceled' | 'failed'
+      savePath: string | null
+      remoteDestination?: { workspaceRelativePath: string; hostLabel: string }
+      error: string | null
+    }) => void
+  ): (() => void) => {
+    const listener = (
+      _event: Electron.IpcRendererEvent,
+      data: {
+        browserPageId?: string
+        downloadId: string
+        status: 'completed' | 'canceled' | 'failed'
+        savePath: string | null
+        remoteDestination?: { workspaceRelativePath: string; hostLabel: string }
+        error: string | null
+      }
+    ) => callback(data)
+    ipcRenderer.on('browser:download-finished', listener)
+    return () => ipcRenderer.removeListener('browser:download-finished', listener)
+  }
+}

src/preload/preload-runtime-support.ts
@@ -1,0 +1,165 @@
+import { ipcRenderer, webUtils } from 'electron'
+import { createBrowserClientPageRendererRequests } from './browser-client-page-renderer-requests'
+import { createBrowserFindSubscriptions } from './browser-find-subscriptions'
+import { registerRendererRestartIpcRelays } from './renderer-restart-wiring'
+import { createUpdaterQuitAbortRelay } from '../shared/renderer-restart-preparation'
+import { MANTA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT } from '../shared/updater-renderer-events'
+import {
+  MANTA_INTERNAL_FILE_DRAG_TYPE,
+  createNativeFileDropPayload,
+  createRejectedNativeFileDropPayload,
+  hasNativeFileDragTypes,
+  NATIVE_FILE_DROP_MAX_PATHS,
+  resolveNativeFileDropPath,
+  type NativeDropResolution,
+  type NativeFileDropPayload,
+  type NativeFileDropPathEntry
+} from '../shared/native-file-drop'
+
+/** Joins the synchronous unload checkpoint with its durable renderer write. */
+export async function awaitBeforeUnloadCheckpoint(): Promise<void> {
+  const result = (await ipcRenderer.invoke('app:await-before-unload-checkpoint')) as {
+    ok?: unknown
+  }
+  if (result?.ok !== true) {
+    throw new Error('Failed to persist renderer state before unload.')
+  }
+}
+
+export const startupDiagnosticsEnabled = process.env.MANTA_STARTUP_DIAGNOSTICS === '1'
+
+export function getLinuxDisplayServer(): 'wayland' | 'x11' | null {
+  if (process.platform !== 'linux') {
+    return null
+  }
+  if (
+    process.env.WAYLAND_DISPLAY ||
+    process.env.XDG_SESSION_TYPE?.toLowerCase() === 'wayland' ||
+    process.env.ELECTRON_OZONE_PLATFORM_HINT?.toLowerCase() === 'wayland'
+  ) {
+    return 'wayland'
+  }
+  return process.env.DISPLAY ? 'x11' : null
+}
+
+type NativeFileDropCallback = (data: NativeFileDropPayload) => void
+const nativeFileDropCallbacks: NativeFileDropCallback[] = []
+let nativeFileDropListenerRegistered = false
+
+const onNativeFileDrop = (_event: Electron.IpcRendererEvent, data: NativeFileDropPayload): void => {
+  for (const callback of Array.from(nativeFileDropCallbacks)) {
+    callback(data)
+  }
+}
+
+export function subscribeNativeFileDrop(callback: NativeFileDropCallback): () => void {
+  nativeFileDropCallbacks.push(callback)
+  if (!nativeFileDropListenerRegistered) {
+    ipcRenderer.on('terminal:file-drop', onNativeFileDrop)
+    nativeFileDropListenerRegistered = true
+  }
+  return () => {
+    const callbackIndex = nativeFileDropCallbacks.indexOf(callback)
+    if (callbackIndex !== -1) {
+      nativeFileDropCallbacks.splice(callbackIndex, 1)
+    }
+    if (nativeFileDropCallbacks.length === 0 && nativeFileDropListenerRegistered) {
+      ipcRenderer.removeListener('terminal:file-drop', onNativeFileDrop)
+      nativeFileDropListenerRegistered = false
+    }
+  }
+}
+
+function resolveNativeFileDrop(event: DragEvent): NativeDropResolution | null {
+  const pathEntries: NativeFileDropPathEntry[] = []
+  for (const entry of event.composedPath()) {
+    if (entry instanceof HTMLElement) {
+      pathEntries.push({
+        nativeFileDropTarget: entry.dataset.nativeFileDropTarget,
+        nativeFileDropDir: entry.dataset.nativeFileDropDir,
+        terminalTabId: entry.dataset.terminalTabId,
+        terminalPaneLeafId: entry.dataset.terminalPaneLeafId ?? entry.dataset.leafId
+      })
+    }
+  }
+  return resolveNativeFileDropPath(pathEntries)
+}
+
+/** Installs the one preload-side listener that converts native File objects to paths. */
+export function installNativeFileDropHandlers(): void {
+  document.addEventListener(
+    'dragover',
+    (event) => {
+      if (event.dataTransfer && !hasNativeFileDragTypes(event.dataTransfer.types)) {
+        return
+      }
+      event.preventDefault()
+      if (event.dataTransfer) {
+        event.dataTransfer.dropEffect = 'copy'
+      }
+    },
+    true
+  )
+  document.addEventListener(
+    'drop',
+    (event) => {
+      if (event.dataTransfer?.types.includes(MANTA_INTERNAL_FILE_DRAG_TYPE)) {
+        return
+      }
+      event.preventDefault()
+      event.stopPropagation()
+      const files = event.dataTransfer?.files
+      if (!files || files.length === 0) {
+        return
+      }
+      const resolution = resolveNativeFileDrop(event)
+      if (files.length > NATIVE_FILE_DROP_MAX_PATHS) {
+        ipcRenderer.send(
+          'terminal:file-dropped-from-preload',
+          createRejectedNativeFileDropPayload({
+            byteLength: 0,
+            pathCount: files.length,
+            reason: 'too-many-paths',
+            status: 'rejected'
+          })
+        )
+        return
+      }
+      const paths: string[] = []
+      for (let index = 0; index < files.length; index += 1) {
+        const filePath = webUtils.getPathForFile(files[index])
+        if (filePath) {
+          paths.push(filePath)
+        }
+      }
+      if (paths.length === 0 || resolution?.target === 'rejected') {
+        return
+      }
+      const payload = createNativeFileDropPayload(resolution, paths)
+      if (payload) {
+        ipcRenderer.send('terminal:file-dropped-from-preload', payload)
+      }
+    },
+    true
+  )
+}
+
+export const browserFindSubscriptions = createBrowserFindSubscriptions()
+export const browserClientPageRendererRequests = createBrowserClientPageRendererRequests({
+  ipc: ipcRenderer,
+  isTopFrame: () => window.top === window
+})
+
+/** Registers browser find forwarding once for this preload context. */
+export function installBrowserFindListener(): void {
+  ipcRenderer.on('ui:findInBrowserPage', (_event, source: unknown) => {
+    browserFindSubscriptions.dispatch(source)
+  })
+}
+
+export const updaterQuitAbortRelay = createUpdaterQuitAbortRelay(
+  window,
+  MANTA_UPDATER_QUIT_AND_INSTALL_ABORTED_EVENT
+)
+
+registerRendererRestartIpcRelays(ipcRenderer, window, updaterQuitAbortRelay)