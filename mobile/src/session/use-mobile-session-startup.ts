aniruddhaadak80/orca

refactor(mobile): split session and terminal surfaces

mobile/src/session/use-mobile-session-startup.ts
@@ -1,0 +1,178 @@
+import { useEffect } from 'react'
+import type { RpcSuccess } from '../transport/types'
+import { headlessActivationNeedsHostRenderer } from '../worktree/worktree-activation-result'
+import { createInitialSessionAutoCreateState } from './use-initial-session-terminal-autocreate'
+import type { MobileSessionKeyboardStateModel } from './use-mobile-session-keyboard-state'
+
+export function useMobileSessionStartup(scope: MobileSessionKeyboardStateModel) {
+  const {
+    hostId,
+    worktreeId,
+    created,
+    isFloatingWorkspaceRoute,
+    connState,
+    client,
+    setTerminals,
+    terminalsRef,
+    setSessionTabs,
+    appliedSnapshotMarkerRef,
+    closedTabTombstonesRef,
+    setTerminalsLoaded,
+    setActiveHandle,
+    setActiveSessionTabId,
+    setMarkdownDocs,
+    setFileDocs,
+    terminalGestureInputQueuesRef,
+    terminalGestureInputInFlightRef,
+    sessionTabActionSheetKeyboardHideSubRef,
+    sessionTabActionSheetRequestSeqRef,
+    initializedHandlesRef,
+    terminalDiagnosticsRef,
+    activeHandleRef,
+    activeSessionTabTypeRef,
+    pendingActiveSessionTabIdRef,
+    pendingActiveTerminalHandleRef,
+    pendingBrowserFocusPageIdRef,
+    pendingTerminalActivationAttemptRef,
+    initialSessionAutoCreateRef,
+    clearPendingLiveInputCommit,
+    clearDelayedActionTimers,
+    showToast,
+    clearTerminalCache,
+    fetchTerminals,
+    ensureSessionTabs
+  } = scope
+  useEffect(() => {
+    // Why: Expo reuses this screen across worktrees; reset route state so it can't open stale UI or reject the next snapshot.
+    sessionTabActionSheetRequestSeqRef.current += 1
+    sessionTabActionSheetKeyboardHideSubRef.current?.remove()
+    sessionTabActionSheetKeyboardHideSubRef.current = null
+    clearTerminalCache()
+    activeHandleRef.current = null
+    activeSessionTabTypeRef.current = null
+    pendingActiveSessionTabIdRef.current = null
+    pendingActiveTerminalHandleRef.current = null
+    pendingBrowserFocusPageIdRef.current = null
+    pendingTerminalActivationAttemptRef.current = null
+    initialSessionAutoCreateRef.current = createInitialSessionAutoCreateState()
+    terminalDiagnosticsRef.current.resetRoute()
+    appliedSnapshotMarkerRef.current = { epoch: null, version: -1 }
+    closedTabTombstonesRef.current.clear()
+    for (const queued of terminalGestureInputQueuesRef.current.values()) {
+      if (queued.timer) {
+        clearTimeout(queued.timer)
+      }
+    }
+    terminalGestureInputQueuesRef.current.clear()
+    terminalGestureInputInFlightRef.current.clear()
+    setActiveHandle(null)
+    setTerminals([])
+    terminalsRef.current = []
+    setSessionTabs([])
+    setActiveSessionTabId(null)
+    clearPendingLiveInputCommit()
+    setMarkdownDocs(new Map())
+    setFileDocs(new Map())
+    clearDelayedActionTimers()
+    return () => {
+      sessionTabActionSheetRequestSeqRef.current += 1
+      sessionTabActionSheetKeyboardHideSubRef.current?.remove()
+      clearPendingLiveInputCommit()
+      clearDelayedActionTimers()
+    }
+  }, [
+    clearDelayedActionTimers,
+    clearPendingLiveInputCommit,
+    clearTerminalCache,
+    hostId,
+    worktreeId
+  ])
+
+  useEffect(() => {
+    if (connState !== 'connected') {
+      return
+    }
+    // Why: keep the current xterm visible while the reconnect snapshot hydrates, not a blank "Loading terminals" surface.
+    if (initializedHandlesRef.current.size === 0) {
+      setTerminalsLoaded(false)
+    }
+    // Why: clear the initialized flag so the reconnect scrollback replaces stale content instead of being dropped.
+    initializedHandlesRef.current.clear()
+    let disposed = false
+    const timers: ReturnType<typeof setTimeout>[] = []
+    function addTimer(fn: () => void, ms: number) {
+      if (disposed) {
+        return
+      }
+      timers.push(setTimeout(fn, ms))
+    }
+    void (async () => {
+      const reportActivationOutcome = (response: RpcSuccess | null): void => {
+        if (!disposed && response && headlessActivationNeedsHostRenderer(response.result)) {
+          showToast('Open Orca on the host to wake sleeping agents.', 3000)
+        }
+      }
+      if (client && created !== '1' && !isFloatingWorkspaceRoute) {
+        // Why: hydrate host-owned tabs without pulling other paired clients (esp. desktop) into this worktree.
+        void client
+          .sendRequest('worktree.activate', {
+            worktree: `id:${worktreeId}`,
+            notifyClients: false,
+            navigation: 'caller'
+          })
+          .then((response) => reportActivationOutcome(response.ok ? response : null))
+          .catch(() => null)
+      }
+      if (disposed) {
+        return
+      }
+      await ensureSessionTabs().catch(() => null)
+      if (disposed) {
+        return
+      }
+      await fetchTerminals({ allowEmptyLoaded: false })
+      if (disposed) {
+        return
+      }
+      addTimer(() => void fetchTerminals({ allowEmptyLoaded: false }), 750)
+      addTimer(() => void fetchTerminals({ allowEmptyLoaded: true }), 1500)
+      if (client && created === '1' && !isFloatingWorkspaceRoute) {
+        addTimer(() => {
+          if (activeHandleRef.current) {
+            return
+          }
+          void (async () => {
+            const activationResponse = await client
+              .sendRequest('worktree.activate', {
+                worktree: `id:${worktreeId}`,
+                notifyClients: false,
+                navigation: 'caller'
+              })
+              .catch(() => null)
+            reportActivationOutcome(activationResponse?.ok ? activationResponse : null)
+            if (disposed) {
+              return
+            }
+            await fetchTerminals({ allowEmptyLoaded: true })
+            addTimer(() => void fetchTerminals({ allowEmptyLoaded: true }), 750)
+          })()
+        }, 1800)
+      }
+    })()
+    return () => {
+      disposed = true
+      for (const t of timers) {
+        clearTimeout(t)
+      }
+    }
+  }, [
+    client,
+    connState,
+    created,
+    fetchTerminals,
+    ensureSessionTabs,
+    isFloatingWorkspaceRoute,
+    showToast,
+    worktreeId
+  ])
+}

mobile/src/terminal/terminal-webview-html/fragment-09.ts
@@ -1,0 +1,138 @@
+import { TERMINAL_TAP_DISPATCH_JS } from '../terminal-webview-tap-dispatch-injected'
+import { TERMINAL_WHEEL_SCROLL_JS } from '../terminal-webview-wheel-scroll-injected'
+import { TERMINAL_MOUSE_CLICK_DRAG_JS } from '../terminal-webview-mouse-click-drag-injected'
+
+export const TERMINAL_HTML_FRAGMENT_09 = `  ${TERMINAL_TAP_DISPATCH_JS}
+
+  // External mouse / trackpad scroll: see
+  // terminal-webview-wheel-scroll-injected.ts (extracted for max-lines).
+  ${TERMINAL_WHEEL_SCROLL_JS}
+
+  // External mouse click/drag: see
+  // terminal-webview-mouse-click-drag-injected.ts (extracted for max-lines).
+  ${TERMINAL_MOUSE_CLICK_DRAG_JS}
+
+  btnCopy.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!term) return;
+    var text = term.getSelection ? term.getSelection() : '';
+    if (text && text.length > 0) {
+      notify({ type: 'selection', text: text });
+    } else {
+      cancelSelect();
+    }
+  });
+
+  btnSelAll.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!term) return;
+    try {
+      term.selectAll();
+      var b = term.buffer.active;
+      sel = {
+        anchor: { col: 0, row: 0 },
+        focus: { col: term.cols - 1, row: b.length - 1 },
+        activeHandle: null
+      };
+      repositionOverlay();
+    } catch (err) {}
+  });
+
+  var ts = {
+    lastX: 0, lastY: 0, lastTime: 0, velY: 0,
+    accumDelta: 0, momentumId: null, isPinching: false,
+    pinchDist: 0, pinchScale: 0, pinchSurfX: 0, pinchSurfY: 0
+  };
+
+  function updateTouchVelocity(deltaY, dt) {
+    if (dt <= 0) return;
+    var instantVelocity = deltaY / dt;
+    if (!isFinite(instantVelocity)) return;
+    // Why: touchmove cadence is uneven in WebView. Blend recent samples so
+    // momentum launch doesn't inherit a one-frame spike or stall.
+    ts.velY = ts.velY === 0 ? instantVelocity : ts.velY * 0.55 + instantVelocity * 0.45;
+  }
+
+  function getDistance(a, b) {
+    var dx = a.clientX - b.clientX, dy = a.clientY - b.clientY;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  function attachSurfaceEventHandlers(targetSurface) {
+    if (!targetSurface || targetSurface.__orcaSurfaceHandlersAttached) return;
+    targetSurface.__orcaSurfaceHandlersAttached = true;
+    // Why: init() swaps in a new hidden surface to avoid flicker; each
+    // replacement needs gesture handlers or tab-switch replays stop scrolling.
+    targetSurface.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); }, true);
+    targetSurface.addEventListener('click', function(e) { e.preventDefault(); e.stopPropagation(); }, true);
+
+    attachSurfaceWheelHandler(targetSurface);
+    attachSurfaceMouseClickDragHandler(targetSurface);
+
+    targetSurface.addEventListener('touchstart', function(e) {
+      if (dispatcherShouldBlockSurface()) return;
+      if (ts.momentumId) {
+        cancelAnimationFrame(ts.momentumId);
+        ts.momentumId = null;
+      }
+      if (e.touches.length === 2) {
+        ts.isPinching = true;
+        smoothScrollOffsetY = 0;
+        ts.pinchDist = getDistance(e.touches[0], e.touches[1]);
+        ts.pinchScale = userScale;
+        var mx = (e.touches[0].clientX + e.touches[1].clientX) / 2;
+        var my = (e.touches[0].clientY + e.touches[1].clientY) / 2;
+        var total = getTotalScale();
+        ts.pinchSurfX = (mx - panX) / total;
+        ts.pinchSurfY = (my - panY) / total;
+      } else if (e.touches.length === 1) {
+        ts.isPinching = false;
+        ts.lastX = e.touches[0].clientX;
+        ts.lastY = e.touches[0].clientY;
+        ts.lastTime = Date.now();
+        ts.velY = 0;
+        ts.accumDelta = 0;
+      }
+    }, { capture: true, passive: true });
+
+    targetSurface.addEventListener('touchmove', function(e) {
+      if (dispatcherShouldBlockSurface()) return;
+      if (!term) return;
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (e.touches.length === 2) {
+        ts.isPinching = true;
+        var dist = getDistance(e.touches[0], e.touches[1]);
+        var mx = (e.touches[0].clientX + e.touches[1].clientX) / 2;
+        var my = (e.touches[0].clientY + e.touches[1].clientY) / 2;
+
+        var ratio = dist / ts.pinchDist;
+        // Why: userScale is a CSS multiplier on the current font size; bound it so
+        // the resulting apparent size (currentTextScale × userScale) stays within
+        // the preset range, since release snaps to one of those presets.
+        var loScale = MIN_TEXT_SCALE / currentTextScale;
+        var hiScale = MAX_TEXT_SCALE / currentTextScale;
+        userScale = Math.max(loScale, Math.min(hiScale, ts.pinchScale * ratio));
+
+        var total = getTotalScale();
+        panX = mx - ts.pinchSurfX * total;
+        panY = my - ts.pinchSurfY * total;
+        clampPan();
+        updateTransform();
+
+      } else if (e.touches.length === 1 && !ts.isPinching) {
+        var x = e.touches[0].clientX, y = e.touches[0].clientY;
+        var now = Date.now(), dt = now - ts.lastTime;
+
+        // Why: pan horizontally only when content overflows the viewport (larger
+        // than fit) — same check clampPan() uses. Vertical always drives buffer
+        // scroll so scrollback stays reachable at any text size; calling the
+        // never-defined contentWiderThanViewport() here threw and killed all
+        // single-finger scrolling, scrollback included.
+        if (term.element && term.element.scrollWidth * getTotalScale() > window.innerWidth + 1) {
+          panX += x - ts.lastX;
+          clampPan();
+`

paidaxingyo666/Manta

(cherry picked from commit f129f2926a989028888c0708bd86ba50a27d6401)
Mirror-Of: 7caf7ab1e1c733d16c106b345831b441c39829bd

mobile/src/session/use-mobile-session-startup.ts
@@ -1,0 +1,178 @@
+import { useEffect } from 'react'
+import type { RpcSuccess } from '../transport/types'
+import { headlessActivationNeedsHostRenderer } from '../worktree/worktree-activation-result'
+import { createInitialSessionAutoCreateState } from './use-initial-session-terminal-autocreate'
+import type { MobileSessionKeyboardStateModel } from './use-mobile-session-keyboard-state'
+
+export function useMobileSessionStartup(scope: MobileSessionKeyboardStateModel) {
+  const {
+    hostId,
+    worktreeId,
+    created,
+    isFloatingWorkspaceRoute,
+    connState,
+    client,
+    setTerminals,
+    terminalsRef,
+    setSessionTabs,
+    appliedSnapshotMarkerRef,
+    closedTabTombstonesRef,
+    setTerminalsLoaded,
+    setActiveHandle,
+    setActiveSessionTabId,
+    setMarkdownDocs,
+    setFileDocs,
+    terminalGestureInputQueuesRef,
+    terminalGestureInputInFlightRef,
+    sessionTabActionSheetKeyboardHideSubRef,
+    sessionTabActionSheetRequestSeqRef,
+    initializedHandlesRef,
+    terminalDiagnosticsRef,
+    activeHandleRef,
+    activeSessionTabTypeRef,
+    pendingActiveSessionTabIdRef,
+    pendingActiveTerminalHandleRef,
+    pendingBrowserFocusPageIdRef,
+    pendingTerminalActivationAttemptRef,
+    initialSessionAutoCreateRef,
+    clearPendingLiveInputCommit,
+    clearDelayedActionTimers,
+    showToast,
+    clearTerminalCache,
+    fetchTerminals,
+    ensureSessionTabs
+  } = scope
+  useEffect(() => {
+    // Why: Expo reuses this screen across worktrees; reset route state so it can't open stale UI or reject the next snapshot.
+    sessionTabActionSheetRequestSeqRef.current += 1
+    sessionTabActionSheetKeyboardHideSubRef.current?.remove()
+    sessionTabActionSheetKeyboardHideSubRef.current = null
+    clearTerminalCache()
+    activeHandleRef.current = null
+    activeSessionTabTypeRef.current = null
+    pendingActiveSessionTabIdRef.current = null
+    pendingActiveTerminalHandleRef.current = null
+    pendingBrowserFocusPageIdRef.current = null
+    pendingTerminalActivationAttemptRef.current = null
+    initialSessionAutoCreateRef.current = createInitialSessionAutoCreateState()
+    terminalDiagnosticsRef.current.resetRoute()
+    appliedSnapshotMarkerRef.current = { epoch: null, version: -1 }
+    closedTabTombstonesRef.current.clear()
+    for (const queued of terminalGestureInputQueuesRef.current.values()) {
+      if (queued.timer) {
+        clearTimeout(queued.timer)
+      }
+    }
+    terminalGestureInputQueuesRef.current.clear()
+    terminalGestureInputInFlightRef.current.clear()
+    setActiveHandle(null)
+    setTerminals([])
+    terminalsRef.current = []
+    setSessionTabs([])
+    setActiveSessionTabId(null)
+    clearPendingLiveInputCommit()
+    setMarkdownDocs(new Map())
+    setFileDocs(new Map())
+    clearDelayedActionTimers()
+    return () => {
+      sessionTabActionSheetRequestSeqRef.current += 1
+      sessionTabActionSheetKeyboardHideSubRef.current?.remove()
+      clearPendingLiveInputCommit()
+      clearDelayedActionTimers()
+    }
+  }, [
+    clearDelayedActionTimers,
+    clearPendingLiveInputCommit,
+    clearTerminalCache,
+    hostId,
+    worktreeId
+  ])
+
+  useEffect(() => {
+    if (connState !== 'connected') {
+      return
+    }
+    // Why: keep the current xterm visible while the reconnect snapshot hydrates, not a blank "Loading terminals" surface.
+    if (initializedHandlesRef.current.size === 0) {
+      setTerminalsLoaded(false)
+    }
+    // Why: clear the initialized flag so the reconnect scrollback replaces stale content instead of being dropped.
+    initializedHandlesRef.current.clear()
+    let disposed = false
+    const timers: ReturnType<typeof setTimeout>[] = []
+    function addTimer(fn: () => void, ms: number) {
+      if (disposed) {
+        return
+      }
+      timers.push(setTimeout(fn, ms))
+    }
+    void (async () => {
+      const reportActivationOutcome = (response: RpcSuccess | null): void => {
+        if (!disposed && response && headlessActivationNeedsHostRenderer(response.result)) {
+          showToast('Open Manta on the host to wake sleeping agents.', 3000)
+        }
+      }
+      if (client && created !== '1' && !isFloatingWorkspaceRoute) {
+        // Why: hydrate host-owned tabs without pulling other paired clients (esp. desktop) into this worktree.
+        void client
+          .sendRequest('worktree.activate', {
+            worktree: `id:${worktreeId}`,
+            notifyClients: false,
+            navigation: 'caller'
+          })
+          .then((response) => reportActivationOutcome(response.ok ? response : null))
+          .catch(() => null)
+      }
+      if (disposed) {
+        return
+      }
+      await ensureSessionTabs().catch(() => null)
+      if (disposed) {
+        return
+      }
+      await fetchTerminals({ allowEmptyLoaded: false })
+      if (disposed) {
+        return
+      }
+      addTimer(() => void fetchTerminals({ allowEmptyLoaded: false }), 750)
+      addTimer(() => void fetchTerminals({ allowEmptyLoaded: true }), 1500)
+      if (client && created === '1' && !isFloatingWorkspaceRoute) {
+        addTimer(() => {
+          if (activeHandleRef.current) {
+            return
+          }
+          void (async () => {
+            const activationResponse = await client
+              .sendRequest('worktree.activate', {
+                worktree: `id:${worktreeId}`,
+                notifyClients: false,
+                navigation: 'caller'
+              })
+              .catch(() => null)
+            reportActivationOutcome(activationResponse?.ok ? activationResponse : null)
+            if (disposed) {
+              return
+            }
+            await fetchTerminals({ allowEmptyLoaded: true })
+            addTimer(() => void fetchTerminals({ allowEmptyLoaded: true }), 750)
+          })()
+        }, 1800)
+      }
+    })()
+    return () => {
+      disposed = true
+      for (const t of timers) {
+        clearTimeout(t)
+      }
+    }
+  }, [
+    client,
+    connState,
+    created,
+    fetchTerminals,
+    ensureSessionTabs,
+    isFloatingWorkspaceRoute,
+    showToast,
+    worktreeId
+  ])
+}

mobile/src/terminal/terminal-webview-html/fragment-09.ts
@@ -1,0 +1,138 @@
+import { TERMINAL_TAP_DISPATCH_JS } from '../terminal-webview-tap-dispatch-injected'
+import { TERMINAL_WHEEL_SCROLL_JS } from '../terminal-webview-wheel-scroll-injected'
+import { TERMINAL_MOUSE_CLICK_DRAG_JS } from '../terminal-webview-mouse-click-drag-injected'
+
+export const TERMINAL_HTML_FRAGMENT_09 = `  ${TERMINAL_TAP_DISPATCH_JS}
+
+  // External mouse / trackpad scroll: see
+  // terminal-webview-wheel-scroll-injected.ts (extracted for max-lines).
+  ${TERMINAL_WHEEL_SCROLL_JS}
+
+  // External mouse click/drag: see
+  // terminal-webview-mouse-click-drag-injected.ts (extracted for max-lines).
+  ${TERMINAL_MOUSE_CLICK_DRAG_JS}
+
+  btnCopy.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!term) return;
+    var text = term.getSelection ? term.getSelection() : '';
+    if (text && text.length > 0) {
+      notify({ type: 'selection', text: text });
+    } else {
+      cancelSelect();
+    }
+  });
+
+  btnSelAll.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!term) return;
+    try {
+      term.selectAll();
+      var b = term.buffer.active;
+      sel = {
+        anchor: { col: 0, row: 0 },
+        focus: { col: term.cols - 1, row: b.length - 1 },
+        activeHandle: null
+      };
+      repositionOverlay();
+    } catch (err) {}
+  });
+
+  var ts = {
+    lastX: 0, lastY: 0, lastTime: 0, velY: 0,
+    accumDelta: 0, momentumId: null, isPinching: false,
+    pinchDist: 0, pinchScale: 0, pinchSurfX: 0, pinchSurfY: 0
+  };
+
+  function updateTouchVelocity(deltaY, dt) {
+    if (dt <= 0) return;
+    var instantVelocity = deltaY / dt;
+    if (!isFinite(instantVelocity)) return;
+    // Why: touchmove cadence is uneven in WebView. Blend recent samples so
+    // momentum launch doesn't inherit a one-frame spike or stall.
+    ts.velY = ts.velY === 0 ? instantVelocity : ts.velY * 0.55 + instantVelocity * 0.45;
+  }
+
+  function getDistance(a, b) {
+    var dx = a.clientX - b.clientX, dy = a.clientY - b.clientY;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  function attachSurfaceEventHandlers(targetSurface) {
+    if (!targetSurface || targetSurface.__mantaSurfaceHandlersAttached) return;
+    targetSurface.__mantaSurfaceHandlersAttached = true;
+    // Why: init() swaps in a new hidden surface to avoid flicker; each
+    // replacement needs gesture handlers or tab-switch replays stop scrolling.
+    targetSurface.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); }, true);
+    targetSurface.addEventListener('click', function(e) { e.preventDefault(); e.stopPropagation(); }, true);
+
+    attachSurfaceWheelHandler(targetSurface);
+    attachSurfaceMouseClickDragHandler(targetSurface);
+
+    targetSurface.addEventListener('touchstart', function(e) {
+      if (dispatcherShouldBlockSurface()) return;
+      if (ts.momentumId) {
+        cancelAnimationFrame(ts.momentumId);
+        ts.momentumId = null;
+      }
+      if (e.touches.length === 2) {
+        ts.isPinching = true;
+        smoothScrollOffsetY = 0;
+        ts.pinchDist = getDistance(e.touches[0], e.touches[1]);
+        ts.pinchScale = userScale;
+        var mx = (e.touches[0].clientX + e.touches[1].clientX) / 2;
+        var my = (e.touches[0].clientY + e.touches[1].clientY) / 2;
+        var total = getTotalScale();
+        ts.pinchSurfX = (mx - panX) / total;
+        ts.pinchSurfY = (my - panY) / total;
+      } else if (e.touches.length === 1) {
+        ts.isPinching = false;
+        ts.lastX = e.touches[0].clientX;
+        ts.lastY = e.touches[0].clientY;
+        ts.lastTime = Date.now();
+        ts.velY = 0;
+        ts.accumDelta = 0;
+      }
+    }, { capture: true, passive: true });
+
+    targetSurface.addEventListener('touchmove', function(e) {
+      if (dispatcherShouldBlockSurface()) return;
+      if (!term) return;
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (e.touches.length === 2) {
+        ts.isPinching = true;
+        var dist = getDistance(e.touches[0], e.touches[1]);
+        var mx = (e.touches[0].clientX + e.touches[1].clientX) / 2;
+        var my = (e.touches[0].clientY + e.touches[1].clientY) / 2;
+
+        var ratio = dist / ts.pinchDist;
+        // Why: userScale is a CSS multiplier on the current font size; bound it so
+        // the resulting apparent size (currentTextScale × userScale) stays within
+        // the preset range, since release snaps to one of those presets.
+        var loScale = MIN_TEXT_SCALE / currentTextScale;
+        var hiScale = MAX_TEXT_SCALE / currentTextScale;
+        userScale = Math.max(loScale, Math.min(hiScale, ts.pinchScale * ratio));
+
+        var total = getTotalScale();
+        panX = mx - ts.pinchSurfX * total;
+        panY = my - ts.pinchSurfY * total;
+        clampPan();
+        updateTransform();
+
+      } else if (e.touches.length === 1 && !ts.isPinching) {
+        var x = e.touches[0].clientX, y = e.touches[0].clientY;
+        var now = Date.now(), dt = now - ts.lastTime;
+
+        // Why: pan horizontally only when content overflows the viewport (larger
+        // than fit) — same check clampPan() uses. Vertical always drives buffer
+        // scroll so scrollback stays reachable at any text size; calling the
+        // never-defined contentWiderThanViewport() here threw and killed all
+        // single-finger scrolling, scrollback included.
+        if (term.element && term.element.scrollWidth * getTotalScale() > window.innerWidth + 1) {
+          panX += x - ts.lastX;
+          clampPan();
+`