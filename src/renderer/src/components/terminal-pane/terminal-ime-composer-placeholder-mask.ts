2022zhang125/orca-yunxiao

fix(terminal): mask the agent composer's dim placeholder during a preedit (#17377)
Split out of #17170, which now carries only the xterm composition-overlay work.

Codex and Claude draw an all-dim, full-row ghost placeholder. The opaque preedit
overlay reproduces the committed row tail it covers, so without this the ghost is
repeated to the right of the composing syllable instead of staying masked. The
binding keys off the `.xterm-composition-remainder` class that #17170 adds and
hides it through CSS while a composition owns a structurally verified placeholder
row — bold prompt glyph plus a dimmed model footer below a blank gap for Codex, a
frame line above the prompt for Claude. Arbitrary dim output, shell lookalikes,
and any row carrying typed text keep their tail visible.

readTerminalCursorLineContext moves from src/main/daemon to src/shared because the
renderer now needs the same reader the daemon uses; the move is import-only.

Depends on #17170.

src/renderer/src/components/terminal-pane/terminal-ime-composer-placeholder-mask.ts
@@ -1,0 +1,76 @@
+import type { IDisposable, Terminal } from '@xterm/xterm'
+import { hasTerminalComposerPlaceholder } from '../../../../shared/terminal-composer-draft'
+import { readTerminalCursorLineContext } from '../../../../shared/terminal-cursor-line-context'
+import {
+  XTERM_COMPOSITION_SESSION_END_EVENT,
+  XTERM_COMPOSITION_SESSION_START_EVENT
+} from './terminal-ime-composition-route'
+
+export const TERMINAL_IME_COMPOSER_PLACEHOLDER_CLASS = 'orca-ime-composer-placeholder'
+
+function compositionSessionId(event: Event): number | null {
+  if (!(event instanceof CustomEvent)) {
+    return null
+  }
+  const id = (event.detail as { id?: unknown } | null)?.id
+  return Number.isSafeInteger(id) && Number(id) > 0 ? Number(id) : null
+}
+
+export function installTerminalImeComposerPlaceholderMask(terminal: Terminal): IDisposable {
+  const element = terminal.element
+  if (!element) {
+    return { dispose: () => undefined }
+  }
+
+  // xterm renders one composition view; a newer transaction supersedes any older pending one.
+  // Keeping only its id makes malformed/repeated starts bounded without evicting live ownership.
+  let activeSessionId: number | null = null
+  const syncPlaceholderOwnership = (): void => {
+    const ownsPlaceholder =
+      activeSessionId !== null &&
+      hasTerminalComposerPlaceholder(readTerminalCursorLineContext(terminal, terminal.rows))
+    element.classList.toggle(TERMINAL_IME_COMPOSER_PLACEHOLDER_CLASS, ownsPlaceholder)
+  }
+  const handleSessionStart = (event: Event): void => {
+    const id = compositionSessionId(event)
+    if (id === null) {
+      return
+    }
+    activeSessionId = id
+    syncPlaceholderOwnership()
+  }
+  const handleSessionEnd = (event: Event): void => {
+    const id = compositionSessionId(event)
+    if (id === null) {
+      return
+    }
+    if (activeSessionId === id) {
+      activeSessionId = null
+    }
+    syncPlaceholderOwnership()
+  }
+  const handleBlur = (): void => {
+    activeSessionId = null
+    syncPlaceholderOwnership()
+  }
+
+  element.addEventListener(XTERM_COMPOSITION_SESSION_START_EVENT, handleSessionStart)
+  element.addEventListener(XTERM_COMPOSITION_SESSION_END_EVENT, handleSessionEnd)
+  element.addEventListener('blur', handleBlur, true)
+  const renderDisposable = terminal.onRender(() => {
+    if (activeSessionId !== null) {
+      syncPlaceholderOwnership()
+    }
+  })
+
+  return {
+    dispose: () => {
+      activeSessionId = null
+      element.classList.remove(TERMINAL_IME_COMPOSER_PLACEHOLDER_CLASS)
+      element.removeEventListener(XTERM_COMPOSITION_SESSION_START_EVENT, handleSessionStart)
+      element.removeEventListener(XTERM_COMPOSITION_SESSION_END_EVENT, handleSessionEnd)
+      element.removeEventListener('blur', handleBlur, true)
+      renderDisposable.dispose()
+    }
+  }
+}

tests/e2e/terminal-korean-midline-preedit-occlusion.spec.ts
@@ -36,12 +36,15 @@ import { samplePreeditOverlay } from './terminal-ime-preedit-overlay-probe'
  * assertion is about. The readiness gate is deliberately blind to the tail, so it holds identically
  * with and without the fix and the occlusion assertion is what discriminates.
  */
-async function sampleOpenComposition(page: Page): Promise<MidlinePreeditOcclusionSample> {
+async function sampleOpenComposition(
+  page: Page,
+  expectedPreedit = '가'
+): Promise<MidlinePreeditOcclusionSample> {
   await expect
     .poll(
       async () => {
         const overlay = await samplePreeditOverlay(page)
-        return overlay.active && overlay.rect.width > 0 && overlay.text.startsWith('가')
+        return overlay.active && overlay.rect.width > 0 && overlay.text.startsWith(expectedPreedit)
       },
       { message: 'the preedit never reached the overlay at a non-zero size' }
     )
@@ -65,6 +68,46 @@ function rendersEverythingItCovers(sample: MidlinePreeditOcclusionSample): boole
 }
 
 test.describe('Terminal mid-line Korean preedit occlusion', () => {
+  test('masks a semantically owned Codex placeholder during its first Korean preedit', async ({
+    orcaPage
+  }, testInfo) => {
+    const arena = await openTerminalImePaneArena(orcaPage)
+    let completed = false
+    try {
+      const placeholder = 'Ask Codex to do anything'
+      await writeToActiveTerminal(
+        orcaPage,
+        [
+          '\x1b[2J\x1b[H\x1b[1m›\x1b[22m \x1b7',
+          `\x1b[2m${placeholder}\x1b[22m`,
+          '\r\n\r\n\x1b[2mgpt-5.6 · ~/repo\x1b[22m\x1b8'
+        ].join('')
+      )
+      await setImeComposition(arena.session, '아')
+
+      const sample = await sampleOpenComposition(orcaPage, '아')
+      expect(sample.cursorColumn, 'the cursor is not after the Codex prompt').toBe(2)
+      expect(sample.rowTailFromCursor, 'the Codex placeholder is not under the cursor').toBe(
+        placeholder
+      )
+      expect(
+        sample.hiddenByOverlay,
+        `the opaque overlay does not mask the full placeholder — ${describeOcclusion(sample)}`
+      ).toBe(placeholder)
+      expect(sample.overlayText, 'the Codex placeholder is repeated after the preedit').toBe('아')
+      expect(sample.remainderText, 'the hidden span lost the placeholder width').toBe(placeholder)
+      expect(sample.remainderVisibility, 'the Codex placeholder is still painted').toBe('hidden')
+      completed = true
+    } finally {
+      await closeTerminalImePaneArena(
+        arena,
+        testInfo,
+        'korean-codex-placeholder-preedit',
+        !completed
+      )
+    }
+  })
+
   test('renders the row tail it covers, so the character after the cursor stays readable', async ({
     orcaPage
   }, testInfo) => {

paidaxingyo666/Manta

src/renderer/src/components/terminal-pane/terminal-ime-composer-placeholder-mask.ts
@@ -1,0 +1,76 @@
+import type { IDisposable, Terminal } from '@xterm/xterm'
+import { hasTerminalComposerPlaceholder } from '../../../../shared/terminal-composer-draft'
+import { readTerminalCursorLineContext } from '../../../../shared/terminal-cursor-line-context'
+import {
+  XTERM_COMPOSITION_SESSION_END_EVENT,
+  XTERM_COMPOSITION_SESSION_START_EVENT
+} from './terminal-ime-composition-route'
+
+export const TERMINAL_IME_COMPOSER_PLACEHOLDER_CLASS = 'manta-ime-composer-placeholder'
+
+function compositionSessionId(event: Event): number | null {
+  if (!(event instanceof CustomEvent)) {
+    return null
+  }
+  const id = (event.detail as { id?: unknown } | null)?.id
+  return Number.isSafeInteger(id) && Number(id) > 0 ? Number(id) : null
+}
+
+export function installTerminalImeComposerPlaceholderMask(terminal: Terminal): IDisposable {
+  const element = terminal.element
+  if (!element) {
+    return { dispose: () => undefined }
+  }
+
+  // xterm renders one composition view; a newer transaction supersedes any older pending one.
+  // Keeping only its id makes malformed/repeated starts bounded without evicting live ownership.
+  let activeSessionId: number | null = null
+  const syncPlaceholderOwnership = (): void => {
+    const ownsPlaceholder =
+      activeSessionId !== null &&
+      hasTerminalComposerPlaceholder(readTerminalCursorLineContext(terminal, terminal.rows))
+    element.classList.toggle(TERMINAL_IME_COMPOSER_PLACEHOLDER_CLASS, ownsPlaceholder)
+  }
+  const handleSessionStart = (event: Event): void => {
+    const id = compositionSessionId(event)
+    if (id === null) {
+      return
+    }
+    activeSessionId = id
+    syncPlaceholderOwnership()
+  }
+  const handleSessionEnd = (event: Event): void => {
+    const id = compositionSessionId(event)
+    if (id === null) {
+      return
+    }
+    if (activeSessionId === id) {
+      activeSessionId = null
+    }
+    syncPlaceholderOwnership()
+  }
+  const handleBlur = (): void => {
+    activeSessionId = null
+    syncPlaceholderOwnership()
+  }
+
+  element.addEventListener(XTERM_COMPOSITION_SESSION_START_EVENT, handleSessionStart)
+  element.addEventListener(XTERM_COMPOSITION_SESSION_END_EVENT, handleSessionEnd)
+  element.addEventListener('blur', handleBlur, true)
+  const renderDisposable = terminal.onRender(() => {
+    if (activeSessionId !== null) {
+      syncPlaceholderOwnership()
+    }
+  })
+
+  return {
+    dispose: () => {
+      activeSessionId = null
+      element.classList.remove(TERMINAL_IME_COMPOSER_PLACEHOLDER_CLASS)
+      element.removeEventListener(XTERM_COMPOSITION_SESSION_START_EVENT, handleSessionStart)
+      element.removeEventListener(XTERM_COMPOSITION_SESSION_END_EVENT, handleSessionEnd)
+      element.removeEventListener('blur', handleBlur, true)
+      renderDisposable.dispose()
+    }
+  }
+}

tests/e2e/terminal-korean-midline-preedit-occlusion.spec.ts
@@ -36,12 +36,15 @@ import { samplePreeditOverlay } from './terminal-ime-preedit-overlay-probe'
  * assertion is about. The readiness gate is deliberately blind to the tail, so it holds identically
  * with and without the fix and the occlusion assertion is what discriminates.
  */
-async function sampleOpenComposition(page: Page): Promise<MidlinePreeditOcclusionSample> {
+async function sampleOpenComposition(
+  page: Page,
+  expectedPreedit = '가'
+): Promise<MidlinePreeditOcclusionSample> {
   await expect
     .poll(
       async () => {
         const overlay = await samplePreeditOverlay(page)
-        return overlay.active && overlay.rect.width > 0 && overlay.text.startsWith('가')
+        return overlay.active && overlay.rect.width > 0 && overlay.text.startsWith(expectedPreedit)
       },
       { message: 'the preedit never reached the overlay at a non-zero size' }
     )
@@ -65,6 +68,46 @@ function rendersEverythingItCovers(sample: MidlinePreeditOcclusionSample): boole
 }
 
 test.describe('Terminal mid-line Korean preedit occlusion', () => {
+  test('masks a semantically owned Codex placeholder during its first Korean preedit', async ({
+    mantaPage
+  }, testInfo) => {
+    const arena = await openTerminalImePaneArena(mantaPage)
+    let completed = false
+    try {
+      const placeholder = 'Ask Codex to do anything'
+      await writeToActiveTerminal(
+        mantaPage,
+        [
+          '\x1b[2J\x1b[H\x1b[1m›\x1b[22m \x1b7',
+          `\x1b[2m${placeholder}\x1b[22m`,
+          '\r\n\r\n\x1b[2mgpt-5.6 · ~/repo\x1b[22m\x1b8'
+        ].join('')
+      )
+      await setImeComposition(arena.session, '아')
+
+      const sample = await sampleOpenComposition(mantaPage, '아')
+      expect(sample.cursorColumn, 'the cursor is not after the Codex prompt').toBe(2)
+      expect(sample.rowTailFromCursor, 'the Codex placeholder is not under the cursor').toBe(
+        placeholder
+      )
+      expect(
+        sample.hiddenByOverlay,
+        `the opaque overlay does not mask the full placeholder — ${describeOcclusion(sample)}`
+      ).toBe(placeholder)
+      expect(sample.overlayText, 'the Codex placeholder is repeated after the preedit').toBe('아')
+      expect(sample.remainderText, 'the hidden span lost the placeholder width').toBe(placeholder)
+      expect(sample.remainderVisibility, 'the Codex placeholder is still painted').toBe('hidden')
+      completed = true
+    } finally {
+      await closeTerminalImePaneArena(
+        arena,
+        testInfo,
+        'korean-codex-placeholder-preedit',
+        !completed
+      )
+    }
+  })
+
   test('renders the row tail it covers, so the character after the cursor stays readable', async ({
     mantaPage
   }, testInfo) => {