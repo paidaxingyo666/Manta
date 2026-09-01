2022zhang125/orca-yunxiao

test(codex): pin Codex read-repair with a real-binary contract check (#17300)
* test(codex): pin Codex read-repair with a real-binary contract check

Orca's session index-heal depends on a Codex behavior: a `thread/read` of an
unindexed rollout performs a read-repair that inserts the `threads` row. All 55
existing heal tests drive a stub app-server and assert "healed" as "the call did
not error", so if Codex ever dropped the repair they would all stay green while
the subsystem went silently inert.

Adds a real-binary contract check built to the same shape as the Git binary
compatibility contract (src/shared/git-binary-compatibility.test.ts): env-gated
test file, version asserted against the binary, dedicated path-filtered PR job.

Pins only the four arms ablation established Orca relies on:
  - a read of an unindexed rollout inserts the state row
  - a session with no read inserts nothing (the negative control that makes the
    insert causal rather than incidental)
  - re-reading an indexed thread inserts nothing
  - an archived thread stays archived rather than being resurrected

Written against codex-cli 0.150.1. The job sets ORCA_CODEX_CONTRACT_REQUIRED=1
so a missing or failed CLI install fails red instead of silently skipping.

Existing heal tests are unchanged.

* test(codex): register the contract job in the verify aggregate contract

`pr-workflow-parallelism.test.mjs` pins `verify.needs` exactly, so adding the
job to pr.yml without updating that list failed the shard. Adds the entry, and
adds a workflow contract test mirroring `git-binary-compatibility-workflow.test.mjs`:

  - the pinned CODEX_CLI_VERSION is the single source for both the npm install
    and the runtime version assertion, so the two cannot drift apart
  - the install prefix and the binary path the test is pointed at are the same tree
  - ORCA_CODEX_CONTRACT_REQUIRED=1 is set, so a failed install fails red rather
    than turning the job into a green no-op

Removing the REQUIRED env from pr.

config/scripts/codex-index-heal-contract-workflow.test.mjs
@@ -1,0 +1,37 @@
+import { readFileSync } from 'node:fs'
+import { parse } from 'yaml'
+import { describe, expect, it } from 'vitest'
+
+describe('Codex index-heal contract PR gate', () => {
+  const workflow = parse(readFileSync('.github/workflows/pr.yml', 'utf8'))
+  const job = workflow.jobs.codex_index_heal_contract
+
+  it('installs and verifies against one pinned Codex version', () => {
+    const install = job.steps.find((step) => step.name === 'Install pinned Codex CLI')
+    const verify = job.steps.find((step) => step.name === 'Verify Codex index-heal contract')
+
+    // Why one source: the install and the runtime version assertion drifting apart is
+    // the failure that would leave this job verifying a Codex nobody declared.
+    expect(job.env.CODEX_CLI_VERSION).toMatch(/^\d+\.\d+\.\d+$/)
+    expect(install.run).toContain('"@openai/codex@$CODEX_CLI_VERSION"')
+    expect(verify.env.ORCA_CODEX_CONTRACT_VERSION).toBe('${{ env.CODEX_CLI_VERSION }}')
+
+    // The install prefix and the binary the test is pointed at must be the same tree.
+    expect(install.run).toContain('--prefix "$RUNNER_TEMP/codex-cli"')
+    expect(verify.run).toContain(
+      'ORCA_CODEX_CONTRACT_BINARY="$RUNNER_TEMP/codex-cli/node_modules/.bin/codex"'
+    )
+    expect(verify.run).toContain('src/main/codex/codex-index-heal-binary-contract.test.ts')
+  })
+
+  it('fails rather than skipping when the Codex binary is missing', () => {
+    const verify = job.steps.find((step) => step.name === 'Verify Codex index-heal contract')
+
+    // Why asserted: the contract skips itself without a binary, so a failed install
+    // would otherwise turn this job into a green no-op that verifies nothing.
+    expect(verify.env.ORCA_CODEX_CONTRACT_REQUIRED).toBe('1')
+    expect(job.steps.find((step) => step.name === 'Install pinned Codex CLI').run).toContain(
+      'set -euo pipefail'
+    )
+  })
+})

src/main/codex/codex-index-heal-binary-contract.test.ts
@@ -1,0 +1,210 @@
+import { execFile } from 'node:child_process'
+import { mkdirSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { promisify } from 'node:util'
+import { afterEach, beforeAll, describe, expect, it } from 'vitest'
+import SyncDatabase from '../sqlite/sync-database'
+import { runCodexAppServerSession, type CodexAppServerRpc } from './codex-app-server-session'
+import { findNewestCodexStateDbPath } from './codex-state-db'
+
+// Why this file exists: every other index-heal test drives a stub app-server and
+// asserts "healed" as "the `thread/read` call did not error". That pins Orca's half
+// of the contract and nothing about Codex's. The behavior Orca actually depends on
+// lives in the Codex binary — a read of an unindexed rollout performs a read-repair
+// that inserts the `threads` row. If Codex ever dropped that repair, the stub-driven
+// tests would all stay green while the subsystem went silently inert. This is the
+// real-binary backstop, built to the same shape as the Git binary compatibility
+// contract in src/shared/git-binary-compatibility.test.ts.
+//
+// Keep it narrow. It pins the four arms that ablation established Orca relies on,
+// and deliberately asserts nothing else about the app-server, so an unrelated Codex
+// release does not redden it into being disabled.
+
+const execFileAsync = promisify(execFile)
+const binary = process.env.ORCA_CODEX_CONTRACT_BINARY
+const expectedVersion = process.env.ORCA_CODEX_CONTRACT_VERSION
+const describeCodexContract = binary ? describe : describe.skip
+
+// Why this guard: skipping is the right local-dev default, but a CI job whose whole
+// purpose is the real binary must not pass by reporting zero assertions. The job sets
+// ORCA_CODEX_CONTRACT_REQUIRED=1, which turns a missing binary into a red test.
+describe.runIf(process.env.ORCA_CODEX_CONTRACT_REQUIRED === '1' && !binary)(
+  'codex binary index-heal contract prerequisites',
+  () => {
+    it('was given a Codex binary to run against', () => {
+      expect.fail(
+        'ORCA_CODEX_CONTRACT_REQUIRED=1 but ORCA_CODEX_CONTRACT_BINARY is unset, so the contract would have silently skipped'
+      )
+    })
+  }
+)
+
+// Why fixed: `thread/read` never reaches the network, and a whole session is
+// spawn + initialize + one RPC. A generous ceiling still fails fast on a wedged child.
+const SESSION_TIMEOUT_MS = 60_000
+// Why: each contract case can use three bounded app-server sessions; keep Vitest's
+// watchdog longer than both child deadlines so cleanup cannot race a test timeout.
+const CONTRACT_TEST_TIMEOUT_MS = SESSION_TIMEOUT_MS * 3 + 10_000
+
+type ThreadRow = { id: string; archived: number }
+
+describeCodexContract(
+  'codex binary index-heal contract',
+  { timeout: CONTRACT_TEST_TIMEOUT_MS },
+  () => {
+  const disposableHomes: string[] = []
+
+  beforeAll(async () => {
+    // Why assert the version: the whole point of a real-binary check is that the
+    // binary drifts. A job that quietly ran some other Codex would report a
+    // contract this repo never verified.
+    const { stdout } = await execFileAsync(binary!, ['--version'], { timeout: SESSION_TIMEOUT_MS })
+    expect(stdout.trim()).toBe(`codex-cli ${expectedVersion}`)
+  })
+
+  afterEach(() => {
+    while (disposableHomes.length > 0) {
+      rmSync(disposableHomes.pop() as string, { recursive: true, force: true })
+    }
+  })
+
+  /**
+   * Builds a disposable CODEX_HOME in the state Orca actually heals from: Codex's
+   * own one-shot sqlite backfill has already run and stamped itself `complete`, so
+   * rollouts that appear afterwards are exactly the ones it will never index on its
+   * own. Never points at the user's real ~/.codex.
+   */
+  async function createBackfilledCodexHome(): Promise<string> {
+    const home = mkdtempSync(join(tmpdir(), 'orca-codex-heal-contract-'))
+    disposableHomes.push(home)
+    mkdirSync(join(home, 'sessions'), { recursive: true })
+    // An app-server session over an empty sessions tree is what stamps the backfill complete.
+    await runAppServerSession(home, async () => undefined)
+    expect(readThreadRows(home)).toEqual([])
+    return home
+  }
+
+  function writeRollout(home: string, threadId: string, stamp: string): void {
+    const dayDir = join(home, 'sessions', '2026', '08', '29')
+    mkdirSync(dayDir, { recursive: true })
+    const meta = {
+      timestamp: '2026-08-29T21:17:33.760Z',
+      ordinal: 0,
+      type: 'session_meta',
+      payload: {
+        session_id: threadId,
+        id: threadId,
+        timestamp: '2026-08-29T21:17:26.840Z',
+        cwd: tmpdir(),
+        originator: 'codex-tui',
+        cli_version: expectedVersion,
+        source: 'cli',
+        thread_source: 'user',
+        model_provider: 'openai'
+      }
+    }
+    const userMessage = {
+      timestamp: '2026-08-29T21:17:40.000Z',
+      ordinal: 1,
+      type: 'event_msg',
+      payload: { type: 'user_message', message: 'index-heal contract fixture' }
+    }
+    writeFileSync(
+      join(dayDir, `rollout-${stamp}-${threadId}.jsonl`),
+      `${JSON.stringify(meta)}\n${JSON.stringify(userMessage)}\n`
+    )
+  }
+
+  // Why reuse runCodexAppServerSession rather than a local JSON-RPC client: it is the
+  // transport the heal itself uses, so a framing or handshake regression that would
+  // break the heal breaks this check too.
+  async function runAppServerSession<T>(
+    home: string,
+    body: (rpc: CodexAppServerRpc) => Promise<T>
+  ): Promise<T> {
+    return runCodexAppServerSession(
+      {
+        command: binary!,
+        args: ['app-server'],
+        cliPath: binary!,
+        env: { CODEX_HOME: home },
+        timeoutMs: SESSION_TIMEOUT_MS
+      },
+      body
+    )
+  }
+
+  function readThreadRows(home: string): ThreadRow[] {
+    const stateDbPath = findNewestCodexStateDbPath(home)
+    if (!stateDbPath) {
+      return []
+    }
+    const db = new SyncDatabase(stateDbPath, { readonly: true, fileMustExist: true })
+    try {
+      return db
+        .prepare('SELECT id, archived FROM threads ORDER BY id')
+        .all() as unknown as ThreadRow[]
+    } finally {
+      db.close()
+    }
+  }
+
+  // Arms 1 and 2 are one test on purpose: the "no read" pass is the negative control
+  // that makes the insert causal rather than incidental. Split across two tests they
+  // would run against two different homes and prove nothing about each other.
+  it('inserts the state row because of the read, not because the server ran', async () => {
+    const home = await createBackfilledCodexHome()
+    const threadId = '01a04f62-715e-7830-9371-50db585caa71'
+    writeRollout(home, threadId, '2026-08-29T14-17-26')
+
+    // Control: a complete app-server session that issues no `thread/read`.
+    await runAppServerSession(home, async () => undefined)
+    expect(readThreadRows(home)).toEqual([])
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 0 }])
+  })
+
+  it('leaves an already-indexed thread as a single row', async () => {
+    const home = await createBackfilledCodexHome()
+    const threadId = '01a04f62-715e-7830-9371-50db585caa72'
+    writeRollout(home, threadId, '2026-08-29T15-00-00')
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 0 }])
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 0 }])
+  })
+
+  // Why this arm: the heal reads tens of thousands of rollouts. If a read cleared
+  // `archived`, the pass would silently resurrect every thread the user had archived.
+  it('stamps an archived thread archived rather than resurrecting it', async () => {
+    const home = await createBackfilledCodexHome()
+    const threadId = '01a04f62-715e-7830-9371-50db585caa73'
+    writeRollout(home, threadId, '2026-08-29T16-00-00')
+
+    // The archived state is created here, never assumed: a real Codex home may
+    // never have had a thread archived, and this arm would then pass vacuously.
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+      await rpc.request('thread/archive', { threadId })
+    })
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 1 }])
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 1 }])
+  })
+})

paidaxingyo666/Manta

config/scripts/codex-index-heal-contract-workflow.test.mjs
@@ -1,0 +1,37 @@
+import { readFileSync } from 'node:fs'
+import { parse } from 'yaml'
+import { describe, expect, it } from 'vitest'
+
+describe('Codex index-heal contract PR gate', () => {
+  const workflow = parse(readFileSync('.github/workflows/pr.yml', 'utf8'))
+  const job = workflow.jobs.codex_index_heal_contract
+
+  it('installs and verifies against one pinned Codex version', () => {
+    const install = job.steps.find((step) => step.name === 'Install pinned Codex CLI')
+    const verify = job.steps.find((step) => step.name === 'Verify Codex index-heal contract')
+
+    // Why one source: the install and the runtime version assertion drifting apart is
+    // the failure that would leave this job verifying a Codex nobody declared.
+    expect(job.env.CODEX_CLI_VERSION).toMatch(/^\d+\.\d+\.\d+$/)
+    expect(install.run).toContain('"@openai/codex@$CODEX_CLI_VERSION"')
+    expect(verify.env.MANTA_CODEX_CONTRACT_VERSION).toBe('${{ env.CODEX_CLI_VERSION }}')
+
+    // The install prefix and the binary the test is pointed at must be the same tree.
+    expect(install.run).toContain('--prefix "$RUNNER_TEMP/codex-cli"')
+    expect(verify.run).toContain(
+      'MANTA_CODEX_CONTRACT_BINARY="$RUNNER_TEMP/codex-cli/node_modules/.bin/codex"'
+    )
+    expect(verify.run).toContain('src/main/codex/codex-index-heal-binary-contract.test.ts')
+  })
+
+  it('fails rather than skipping when the Codex binary is missing', () => {
+    const verify = job.steps.find((step) => step.name === 'Verify Codex index-heal contract')
+
+    // Why asserted: the contract skips itself without a binary, so a failed install
+    // would otherwise turn this job into a green no-op that verifies nothing.
+    expect(verify.env.MANTA_CODEX_CONTRACT_REQUIRED).toBe('1')
+    expect(job.steps.find((step) => step.name === 'Install pinned Codex CLI').run).toContain(
+      'set -euo pipefail'
+    )
+  })
+})

src/main/codex/codex-index-heal-binary-contract.test.ts
@@ -1,0 +1,210 @@
+import { execFile } from 'node:child_process'
+import { mkdirSync, mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { promisify } from 'node:util'
+import { afterEach, beforeAll, describe, expect, it } from 'vitest'
+import SyncDatabase from '../sqlite/sync-database'
+import { runCodexAppServerSession, type CodexAppServerRpc } from './codex-app-server-session'
+import { findNewestCodexStateDbPath } from './codex-state-db'
+
+// Why this file exists: every other index-heal test drives a stub app-server and
+// asserts "healed" as "the `thread/read` call did not error". That pins Manta's half
+// of the contract and nothing about Codex's. The behavior Manta actually depends on
+// lives in the Codex binary — a read of an unindexed rollout performs a read-repair
+// that inserts the `threads` row. If Codex ever dropped that repair, the stub-driven
+// tests would all stay green while the subsystem went silently inert. This is the
+// real-binary backstop, built to the same shape as the Git binary compatibility
+// contract in src/shared/git-binary-compatibility.test.ts.
+//
+// Keep it narrow. It pins the four arms that ablation established Manta relies on,
+// and deliberately asserts nothing else about the app-server, so an unrelated Codex
+// release does not redden it into being disabled.
+
+const execFileAsync = promisify(execFile)
+const binary = process.env.MANTA_CODEX_CONTRACT_BINARY
+const expectedVersion = process.env.MANTA_CODEX_CONTRACT_VERSION
+const describeCodexContract = binary ? describe : describe.skip
+
+// Why this guard: skipping is the right local-dev default, but a CI job whose whole
+// purpose is the real binary must not pass by reporting zero assertions. The job sets
+// MANTA_CODEX_CONTRACT_REQUIRED=1, which turns a missing binary into a red test.
+describe.runIf(process.env.MANTA_CODEX_CONTRACT_REQUIRED === '1' && !binary)(
+  'codex binary index-heal contract prerequisites',
+  () => {
+    it('was given a Codex binary to run against', () => {
+      expect.fail(
+        'MANTA_CODEX_CONTRACT_REQUIRED=1 but MANTA_CODEX_CONTRACT_BINARY is unset, so the contract would have silently skipped'
+      )
+    })
+  }
+)
+
+// Why fixed: `thread/read` never reaches the network, and a whole session is
+// spawn + initialize + one RPC. A generous ceiling still fails fast on a wedged child.
+const SESSION_TIMEOUT_MS = 60_000
+// Why: each contract case can use three bounded app-server sessions; keep Vitest's
+// watchdog longer than both child deadlines so cleanup cannot race a test timeout.
+const CONTRACT_TEST_TIMEOUT_MS = SESSION_TIMEOUT_MS * 3 + 10_000
+
+type ThreadRow = { id: string; archived: number }
+
+describeCodexContract(
+  'codex binary index-heal contract',
+  { timeout: CONTRACT_TEST_TIMEOUT_MS },
+  () => {
+  const disposableHomes: string[] = []
+
+  beforeAll(async () => {
+    // Why assert the version: the whole point of a real-binary check is that the
+    // binary drifts. A job that quietly ran some other Codex would report a
+    // contract this repo never verified.
+    const { stdout } = await execFileAsync(binary!, ['--version'], { timeout: SESSION_TIMEOUT_MS })
+    expect(stdout.trim()).toBe(`codex-cli ${expectedVersion}`)
+  })
+
+  afterEach(() => {
+    while (disposableHomes.length > 0) {
+      rmSync(disposableHomes.pop() as string, { recursive: true, force: true })
+    }
+  })
+
+  /**
+   * Builds a disposable CODEX_HOME in the state Manta actually heals from: Codex's
+   * own one-shot sqlite backfill has already run and stamped itself `complete`, so
+   * rollouts that appear afterwards are exactly the ones it will never index on its
+   * own. Never points at the user's real ~/.codex.
+   */
+  async function createBackfilledCodexHome(): Promise<string> {
+    const home = mkdtempSync(join(tmpdir(), 'manta-codex-heal-contract-'))
+    disposableHomes.push(home)
+    mkdirSync(join(home, 'sessions'), { recursive: true })
+    // An app-server session over an empty sessions tree is what stamps the backfill complete.
+    await runAppServerSession(home, async () => undefined)
+    expect(readThreadRows(home)).toEqual([])
+    return home
+  }
+
+  function writeRollout(home: string, threadId: string, stamp: string): void {
+    const dayDir = join(home, 'sessions', '2026', '08', '29')
+    mkdirSync(dayDir, { recursive: true })
+    const meta = {
+      timestamp: '2026-08-29T21:17:33.760Z',
+      ordinal: 0,
+      type: 'session_meta',
+      payload: {
+        session_id: threadId,
+        id: threadId,
+        timestamp: '2026-08-29T21:17:26.840Z',
+        cwd: tmpdir(),
+        originator: 'codex-tui',
+        cli_version: expectedVersion,
+        source: 'cli',
+        thread_source: 'user',
+        model_provider: 'openai'
+      }
+    }
+    const userMessage = {
+      timestamp: '2026-08-29T21:17:40.000Z',
+      ordinal: 1,
+      type: 'event_msg',
+      payload: { type: 'user_message', message: 'index-heal contract fixture' }
+    }
+    writeFileSync(
+      join(dayDir, `rollout-${stamp}-${threadId}.jsonl`),
+      `${JSON.stringify(meta)}\n${JSON.stringify(userMessage)}\n`
+    )
+  }
+
+  // Why reuse runCodexAppServerSession rather than a local JSON-RPC client: it is the
+  // transport the heal itself uses, so a framing or handshake regression that would
+  // break the heal breaks this check too.
+  async function runAppServerSession<T>(
+    home: string,
+    body: (rpc: CodexAppServerRpc) => Promise<T>
+  ): Promise<T> {
+    return runCodexAppServerSession(
+      {
+        command: binary!,
+        args: ['app-server'],
+        cliPath: binary!,
+        env: { CODEX_HOME: home },
+        timeoutMs: SESSION_TIMEOUT_MS
+      },
+      body
+    )
+  }
+
+  function readThreadRows(home: string): ThreadRow[] {
+    const stateDbPath = findNewestCodexStateDbPath(home)
+    if (!stateDbPath) {
+      return []
+    }
+    const db = new SyncDatabase(stateDbPath, { readonly: true, fileMustExist: true })
+    try {
+      return db
+        .prepare('SELECT id, archived FROM threads ORDER BY id')
+        .all() as unknown as ThreadRow[]
+    } finally {
+      db.close()
+    }
+  }
+
+  // Arms 1 and 2 are one test on purpose: the "no read" pass is the negative control
+  // that makes the insert causal rather than incidental. Split across two tests they
+  // would run against two different homes and prove nothing about each other.
+  it('inserts the state row because of the read, not because the server ran', async () => {
+    const home = await createBackfilledCodexHome()
+    const threadId = '01a04f62-715e-7830-9371-50db585caa71'
+    writeRollout(home, threadId, '2026-08-29T14-17-26')
+
+    // Control: a complete app-server session that issues no `thread/read`.
+    await runAppServerSession(home, async () => undefined)
+    expect(readThreadRows(home)).toEqual([])
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 0 }])
+  })
+
+  it('leaves an already-indexed thread as a single row', async () => {
+    const home = await createBackfilledCodexHome()
+    const threadId = '01a04f62-715e-7830-9371-50db585caa72'
+    writeRollout(home, threadId, '2026-08-29T15-00-00')
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 0 }])
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 0 }])
+  })
+
+  // Why this arm: the heal reads tens of thousands of rollouts. If a read cleared
+  // `archived`, the pass would silently resurrect every thread the user had archived.
+  it('stamps an archived thread archived rather than resurrecting it', async () => {
+    const home = await createBackfilledCodexHome()
+    const threadId = '01a04f62-715e-7830-9371-50db585caa73'
+    writeRollout(home, threadId, '2026-08-29T16-00-00')
+
+    // The archived state is created here, never assumed: a real Codex home may
+    // never have had a thread archived, and this arm would then pass vacuously.
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+      await rpc.request('thread/archive', { threadId })
+    })
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 1 }])
+
+    await runAppServerSession(home, async (rpc) => {
+      await rpc.request('thread/read', { threadId })
+    })
+
+    expect(readThreadRows(home)).toEqual([{ id: threadId, archived: 1 }])
+  })
+})