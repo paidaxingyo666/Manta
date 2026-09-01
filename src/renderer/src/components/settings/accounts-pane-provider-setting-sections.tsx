2022zhang125/orca-yunxiao

Split accounts pane responsibilities (#17286)
* Split speech session lifecycle

* Split terminal output scheduler pipeline

* Split mobile browser pane modules

* Prune resolved max-lines suppressions

* Split pane tree equalization logic

* Extract mobile troubleshoot screen styles

* Split external automation manager

* Split main window service attachments

* Split hosted review creation checks

* Split automation dispatch event handling

* Split settings navigation metadata

* Split daemon initialization lifecycle

* Split GitLab item dialog

* Split relay dispatcher layers

* Split mobile host screen

* Retarget mobile view settings source test

* Split runtime file client layers

* Split ports panel layers

* Split runtime environments pane layers

* Split local PTY provider responsibilities

* Split CDP bridge responsibilities

* Split relay Git handler responsibilities

* Track moved relay Git fetch audit

* Split Linear item drawer responsibilities

* Split telemetry event schema responsibilities

* Split resource usage status responsibilities

* Split remote terminal multiplexer responsibilities

* Split Git worktree responsibilities

* Split Codex hook service responsibilities

* Keep mirrored hook trust type private

* Split web runtime session responsibilities

* Split GitHub project view read path

* Split Claude runtime auth responsibilities

* Split runtime RPC server responsibilities

* Split Settings page responsibilities

* Split filesystem watcher responsibilities

* refactor codex account service modules

* fix duplicate managed home path import

* Split activity page responsibilities

* Split accounts pane responsibilities

* Fix F3-speech for #17123

* Fix F1-cycle for #17131

* Fix F4-navtest for #17157

* Fix F2-allowlist for #17161

* Fix F7F8-codex for #17277

src/renderer/src/components/settings/accounts-pane-provider-setting-sections.tsx
@@ -1,0 +1,227 @@
+import { translate } from '@/i18n/i18n'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
+import { Switch } from '../ui/switch'
+import { GeminiIcon, OpenCodeGoIcon } from '../status-bar/icons'
+import { SearchableSetting } from './SearchableSetting'
+import type { AccountsPaneSectionModel } from './accounts-pane-types'
+
+export function renderGeminiAccountsSection(model: AccountsPaneSectionModel): React.JSX.Element {
+  const { localAccountRuntimeSentenceLabel, recordFeatureInteraction, settings, updateSettings } =
+    model
+  return (
+    <section key="gemini" id="accounts-gemini" className="space-y-4 scroll-mt-6">
+      <div className="space-y-1">
+        <h3 className="flex items-center gap-2 text-sm font-semibold">
+          <GeminiIcon size={16} />
+          {translate('auto.components.settings.AccountsPane.0c64dc2a64', 'Gemini')}
+        </h3>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.973741a871',
+            'Configure Gemini provider settings.'
+          )}
+        </p>
+      </div>
+
+      <SearchableSetting
+        title={translate(
+          'auto.components.settings.AccountsPane.0c7f915b01',
+          'Use Gemini CLI credentials'
+        )}
+        description={translate(
+          'auto.components.settings.AccountsPane.d676c41fc6',
+          'Extracts OAuth credentials from your local Gemini CLI installation to authenticate with Google. This uses credentials issued to the Gemini CLI app, not Orca. May break if Google updates the CLI. Use at your own risk.'
+        )}
+        keywords={[
+          'gemini',
+          'cli',
+          'oauth',
+          'credentials',
+          'experimental',
+          'rate limit',
+          'status bar'
+        ]}
+        className="flex items-center justify-between gap-4 py-2"
+      >
+        <div className="space-y-0.5">
+          <Label>
+            {translate(
+              'auto.components.settings.AccountsPane.96f3649526',
+              'Use Gemini CLI credentials (experimental)'
+            )}
+          </Label>
+          <p className="text-xs text-muted-foreground">
+            {translate(
+              'auto.components.settings.AccountsPane.c2aee76420',
+              'Extracts OAuth credentials from your local Gemini CLI installation to authenticate with Google for {{value0}}. This uses credentials issued to the Gemini CLI app, not Orca. May break if Google updates the CLI. Use at your own risk.',
+              { value0: localAccountRuntimeSentenceLabel }
+            )}
+          </p>
+        </div>
+        <Switch
+          aria-label={translate(
+            'auto.components.settings.AccountsPane.96f3649526',
+            'Use Gemini CLI credentials (experimental)'
+          )}
+          checked={settings.geminiCliOAuthEnabled}
+          onCheckedChange={(checked) => {
+            recordFeatureInteraction('usage-tracking')
+            updateSettings({
+              geminiCliOAuthEnabled: checked
+            })
+          }}
+        />
+      </SearchableSetting>
+    </section>
+  )
+}
+
+export function renderOpenCodeAccountsSection(model: AccountsPaneSectionModel): React.JSX.Element {
+  const { recordFeatureInteraction, recordOpenCodeSettingEdit, settings, updateSettings } = model
+  return (
+    <section key="opencode-go" id="accounts-opencode-go" className="space-y-4 scroll-mt-6">
+      <div className="space-y-1">
+        <h3 className="flex items-center gap-2 text-sm font-semibold">
+          <OpenCodeGoIcon size={16} />
+          {translate('auto.components.settings.AccountsPane.4ac10b4d08', 'OpenCode Go')}
+        </h3>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.ea631977b5',
+            'Configure OpenCode Go provider settings.'
+          )}
+        </p>
+      </div>
+
+      <SearchableSetting
+        title={translate(
+          'auto.components.settings.AccountsPane.36223200ac',
+          'OpenCode Go Session Cookie'
+        )}
+        description={translate(
+          'auto.components.settings.AccountsPane.b2b1aa936d',
+          'Paste your opencode.ai session cookie for rate limit fetching.'
+        )}
+        keywords={['opencode', 'cookie', 'session', 'rate limit', 'status bar']}
+        className="space-y-2"
+      >
+        <Label>
+          {translate(
+            'auto.components.settings.AccountsPane.67e3c33670',
+            'OpenCode Go session cookie'
+          )}
+        </Label>
+        <div className="flex gap-2">
+          <Input
+            type="password"
+            value={settings.opencodeSessionCookie}
+            onChange={(e) => {
+              recordOpenCodeSettingEdit('cookie')
+              updateSettings({ opencodeSessionCookie: e.target.value })
+            }}
+            placeholder={translate(
+              'auto.components.settings.AccountsPane.a7e38affcd',
+              'Fe26.2**… token or auth=Fe26.2**… header'
+            )}
+            spellCheck={false}
+            className="flex-1 text-xs"
+          />
+          {settings.opencodeSessionCookie && (
+            <Button
+              variant="ghost"
+              size="xs"
+              onClick={() => {
+                recordFeatureInteraction('usage-tracking')
+                updateSettings({ opencodeSessionCookie: '' })
+              }}
+              className="h-7 shrink-0 text-xs text-muted-foreground hover:text-foreground"
+            >
+              {translate('auto.components.settings.AccountsPane.b398b834c9', 'Clear')}
+            </Button>
+          )}
+        </div>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.0023cc336e',
+            'Paste either the raw token value (e.g.'
+          )}{' '}
+          <code className="text-xs">
+            {translate('auto.components.settings.AccountsPane.922b51e02d', 'Fe26.2**…')}
+          </code>
+          {translate(
+            'auto.components.settings.AccountsPane.338820326a',
+            ') or the full cookie header (e.g.'
+          )}{' '}
+          <code className="text-xs">
+            {translate('auto.components.settings.AccountsPane.8951c5309f', 'auth=Fe26.2**…')}
+          </code>
+          {translate(
+            'auto.components.settings.AccountsPane.7ce0e1907c',
+            "). Find it in your browser's DevTools → Network → any opencode.ai request → Cookie header. OpenCode Go auth is web-based and shared across Windows and WSL terminals."
+          )}
+        </p>
+      </SearchableSetting>
+
+      <SearchableSetting
+        title={translate(
+          'auto.components.settings.AccountsPane.02cb127710',
+          'OpenCode Go Workspace ID'
+        )}
+        description={translate(
+          'auto.components.settings.AccountsPane.d70a5287a4',
+          'Optional workspace ID override if the automatic lookup fails.'
+        )}
+        keywords={['opencode', 'workspace', 'id', 'wrk', 'rate limit', 'status bar']}
+        className="space-y-2"
+      >
+        <Label>
+          {translate('auto.components.settings.AccountsPane.dbdb0b0bd8', 'Workspace ID override')}
+        </Label>
+        <div className="flex gap-2">
+          <Input
+            type="text"
+            value={settings.opencodeWorkspaceId}
+            onChange={(e) => {
+              recordOpenCodeSettingEdit('workspaceId')
+              updateSettings({ opencodeWorkspaceId: e.target.value })
+            }}
+            placeholder={translate(
+              'auto.components.settings.AccountsPane.a122332371',
+              'wrk_… (leave blank for automatic lookup)'
+            )}
+            spellCheck={false}
+            className="flex-1 text-xs"
+          />
+          {settings.opencodeWorkspaceId && (
+            <Button
+              variant="ghost"
+              size="xs"
+              onClick={() => {
+                recordFeatureInteraction('usage-tracking')
+                updateSettings({ opencodeWorkspaceId: '' })
+              }}
+              className="h-7 shrink-0 text-xs text-muted-foreground hover:text-foreground"
+            >
+              {translate('auto.components.settings.AccountsPane.b398b834c9', 'Clear')}
+            </Button>
+          )}
+        </div>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.51c9104e13',
+            'Find this in the URL after logging into opencode.ai (e.g.'
+          )}{' '}
+          <code className="text-xs">
+            {translate(
+              'auto.components.settings.AccountsPane.ae3b21eb6c',
+              'opencode.ai/workspace/wrk_…/go'
+            )}
+          </code>
+          ).
+        </p>
+      </SearchableSetting>
+    </section>
+  )
+}

src/renderer/src/components/settings/accounts-pane-removal-dialogs.tsx
@@ -1,0 +1,119 @@
+import { translate } from '@/i18n/i18n'
+import {
+  removeClaudeProviderAccount,
+  removeCodexProviderAccount
+} from '@/runtime/runtime-provider-accounts-client'
+import { Button } from '../ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '../ui/dialog'
+import type { AccountsPaneSectionModel, RemoveAccountTarget } from './accounts-pane-types'
+
+export function renderAccountsRemovalDialogs(
+  model: AccountsPaneSectionModel,
+  removeCodexTarget: RemoveAccountTarget | null,
+  removeClaudeTarget: RemoveAccountTarget | null
+): React.JSX.Element {
+  const {
+    runClaudeAccountAction,
+    runCodexAccountAction,
+    setRemoveClaudeTarget,
+    setRemoveCodexTarget,
+    settings
+  } = model
+  return (
+    <>
+      <Dialog
+        open={removeCodexTarget !== null}
+        onOpenChange={(open) => !open && setRemoveCodexTarget(null)}
+      >
+        <DialogContent showCloseButton={false}>
+          <DialogHeader>
+            <DialogTitle>
+              {translate(
+                'auto.components.settings.AccountsPane.0d47394635',
+                'Remove Codex Account?'
+              )}
+            </DialogTitle>
+            <DialogDescription>
+              {translate(
+                'auto.components.settings.AccountsPane.380a7736cc',
+                'Removing this account permanently deletes its managed Codex home, including all Codex session history and MCP logins stored inside. This cannot be undone. If the account is currently active, Orca falls back to the system default Codex login.'
+              )}
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setRemoveCodexTarget(null)}>
+              {translate('auto.components.settings.AccountsPane.dbb9626ed1', 'Cancel')}
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={() => {
+                const target = removeCodexTarget
+                if (!target) {
+                  return
+                }
+                setRemoveCodexTarget(null)
+                void runCodexAccountAction(
+                  `remove:${target.id}`,
+                  () => removeCodexProviderAccount(settings, target.id),
+                  target.runtime
+                )
+              }}
+            >
+              {translate('auto.components.settings.AccountsPane.c2d2751587', 'Remove Account')}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Dialog
+        open={removeClaudeTarget !== null}
+        onOpenChange={(open) => !open && setRemoveClaudeTarget(null)}
+      >
+        <DialogContent showCloseButton={false}>
+          <DialogHeader>
+            <DialogTitle>
+              {translate(
+                'auto.components.settings.AccountsPane.63843e37e2',
+                'Remove Claude Account?'
+              )}
+            </DialogTitle>
+            <DialogDescription>
+              {translate(
+                'auto.components.settings.AccountsPane.854ebbcc45',
+                'Orca will delete the managed Claude auth for this saved account. If it is currently active, Orca falls back to the system default Claude login.'
+              )}
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setRemoveClaudeTarget(null)}>
+              {translate('auto.components.settings.AccountsPane.dbb9626ed1', 'Cancel')}
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={() => {
+                const target = removeClaudeTarget
+                if (!target) {
+                  return
+                }
+                setRemoveClaudeTarget(null)
+                void runClaudeAccountAction(
+                  `remove:${target.id}`,
+                  () => removeClaudeProviderAccount(settings, target.id),
+                  target.runtime
+                )
+              }}
+            >
+              {translate('auto.components.settings.AccountsPane.c2d2751587', 'Remove Account')}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}

paidaxingyo666/Manta

(cherry picked from commit 14d39bd0f0bf30b31d12804b36513d9de06e6afb)

src/renderer/src/components/settings/accounts-pane-provider-setting-sections.tsx
@@ -1,0 +1,227 @@
+import { translate } from '@/i18n/i18n'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
+import { Switch } from '../ui/switch'
+import { GeminiIcon, OpenCodeGoIcon } from '../status-bar/icons'
+import { SearchableSetting } from './SearchableSetting'
+import type { AccountsPaneSectionModel } from './accounts-pane-types'
+
+export function renderGeminiAccountsSection(model: AccountsPaneSectionModel): React.JSX.Element {
+  const { localAccountRuntimeSentenceLabel, recordFeatureInteraction, settings, updateSettings } =
+    model
+  return (
+    <section key="gemini" id="accounts-gemini" className="space-y-4 scroll-mt-6">
+      <div className="space-y-1">
+        <h3 className="flex items-center gap-2 text-sm font-semibold">
+          <GeminiIcon size={16} />
+          {translate('auto.components.settings.AccountsPane.0c64dc2a64', 'Gemini')}
+        </h3>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.973741a871',
+            'Configure Gemini provider settings.'
+          )}
+        </p>
+      </div>
+
+      <SearchableSetting
+        title={translate(
+          'auto.components.settings.AccountsPane.0c7f915b01',
+          'Use Gemini CLI credentials'
+        )}
+        description={translate(
+          'auto.components.settings.AccountsPane.d676c41fc6',
+          'Extracts OAuth credentials from your local Gemini CLI installation to authenticate with Google. This uses credentials issued to the Gemini CLI app, not Manta. May break if Google updates the CLI. Use at your own risk.'
+        )}
+        keywords={[
+          'gemini',
+          'cli',
+          'oauth',
+          'credentials',
+          'experimental',
+          'rate limit',
+          'status bar'
+        ]}
+        className="flex items-center justify-between gap-4 py-2"
+      >
+        <div className="space-y-0.5">
+          <Label>
+            {translate(
+              'auto.components.settings.AccountsPane.96f3649526',
+              'Use Gemini CLI credentials (experimental)'
+            )}
+          </Label>
+          <p className="text-xs text-muted-foreground">
+            {translate(
+              'auto.components.settings.AccountsPane.c2aee76420',
+              'Extracts OAuth credentials from your local Gemini CLI installation to authenticate with Google for {{value0}}. This uses credentials issued to the Gemini CLI app, not Manta. May break if Google updates the CLI. Use at your own risk.',
+              { value0: localAccountRuntimeSentenceLabel }
+            )}
+          </p>
+        </div>
+        <Switch
+          aria-label={translate(
+            'auto.components.settings.AccountsPane.96f3649526',
+            'Use Gemini CLI credentials (experimental)'
+          )}
+          checked={settings.geminiCliOAuthEnabled}
+          onCheckedChange={(checked) => {
+            recordFeatureInteraction('usage-tracking')
+            updateSettings({
+              geminiCliOAuthEnabled: checked
+            })
+          }}
+        />
+      </SearchableSetting>
+    </section>
+  )
+}
+
+export function renderOpenCodeAccountsSection(model: AccountsPaneSectionModel): React.JSX.Element {
+  const { recordFeatureInteraction, recordOpenCodeSettingEdit, settings, updateSettings } = model
+  return (
+    <section key="opencode-go" id="accounts-opencode-go" className="space-y-4 scroll-mt-6">
+      <div className="space-y-1">
+        <h3 className="flex items-center gap-2 text-sm font-semibold">
+          <OpenCodeGoIcon size={16} />
+          {translate('auto.components.settings.AccountsPane.4ac10b4d08', 'OpenCode Go')}
+        </h3>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.ea631977b5',
+            'Configure OpenCode Go provider settings.'
+          )}
+        </p>
+      </div>
+
+      <SearchableSetting
+        title={translate(
+          'auto.components.settings.AccountsPane.36223200ac',
+          'OpenCode Go Session Cookie'
+        )}
+        description={translate(
+          'auto.components.settings.AccountsPane.b2b1aa936d',
+          'Paste your opencode.ai session cookie for rate limit fetching.'
+        )}
+        keywords={['opencode', 'cookie', 'session', 'rate limit', 'status bar']}
+        className="space-y-2"
+      >
+        <Label>
+          {translate(
+            'auto.components.settings.AccountsPane.67e3c33670',
+            'OpenCode Go session cookie'
+          )}
+        </Label>
+        <div className="flex gap-2">
+          <Input
+            type="password"
+            value={settings.opencodeSessionCookie}
+            onChange={(e) => {
+              recordOpenCodeSettingEdit('cookie')
+              updateSettings({ opencodeSessionCookie: e.target.value })
+            }}
+            placeholder={translate(
+              'auto.components.settings.AccountsPane.a7e38affcd',
+              'Fe26.2**… token or auth=Fe26.2**… header'
+            )}
+            spellCheck={false}
+            className="flex-1 text-xs"
+          />
+          {settings.opencodeSessionCookie && (
+            <Button
+              variant="ghost"
+              size="xs"
+              onClick={() => {
+                recordFeatureInteraction('usage-tracking')
+                updateSettings({ opencodeSessionCookie: '' })
+              }}
+              className="h-7 shrink-0 text-xs text-muted-foreground hover:text-foreground"
+            >
+              {translate('auto.components.settings.AccountsPane.b398b834c9', 'Clear')}
+            </Button>
+          )}
+        </div>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.0023cc336e',
+            'Paste either the raw token value (e.g.'
+          )}{' '}
+          <code className="text-xs">
+            {translate('auto.components.settings.AccountsPane.922b51e02d', 'Fe26.2**…')}
+          </code>
+          {translate(
+            'auto.components.settings.AccountsPane.338820326a',
+            ') or the full cookie header (e.g.'
+          )}{' '}
+          <code className="text-xs">
+            {translate('auto.components.settings.AccountsPane.8951c5309f', 'auth=Fe26.2**…')}
+          </code>
+          {translate(
+            'auto.components.settings.AccountsPane.7ce0e1907c',
+            "). Find it in your browser's DevTools → Network → any opencode.ai request → Cookie header. OpenCode Go auth is web-based and shared across Windows and WSL terminals."
+          )}
+        </p>
+      </SearchableSetting>
+
+      <SearchableSetting
+        title={translate(
+          'auto.components.settings.AccountsPane.02cb127710',
+          'OpenCode Go Workspace ID'
+        )}
+        description={translate(
+          'auto.components.settings.AccountsPane.d70a5287a4',
+          'Optional workspace ID override if the automatic lookup fails.'
+        )}
+        keywords={['opencode', 'workspace', 'id', 'wrk', 'rate limit', 'status bar']}
+        className="space-y-2"
+      >
+        <Label>
+          {translate('auto.components.settings.AccountsPane.dbdb0b0bd8', 'Workspace ID override')}
+        </Label>
+        <div className="flex gap-2">
+          <Input
+            type="text"
+            value={settings.opencodeWorkspaceId}
+            onChange={(e) => {
+              recordOpenCodeSettingEdit('workspaceId')
+              updateSettings({ opencodeWorkspaceId: e.target.value })
+            }}
+            placeholder={translate(
+              'auto.components.settings.AccountsPane.a122332371',
+              'wrk_… (leave blank for automatic lookup)'
+            )}
+            spellCheck={false}
+            className="flex-1 text-xs"
+          />
+          {settings.opencodeWorkspaceId && (
+            <Button
+              variant="ghost"
+              size="xs"
+              onClick={() => {
+                recordFeatureInteraction('usage-tracking')
+                updateSettings({ opencodeWorkspaceId: '' })
+              }}
+              className="h-7 shrink-0 text-xs text-muted-foreground hover:text-foreground"
+            >
+              {translate('auto.components.settings.AccountsPane.b398b834c9', 'Clear')}
+            </Button>
+          )}
+        </div>
+        <p className="text-xs text-muted-foreground">
+          {translate(
+            'auto.components.settings.AccountsPane.51c9104e13',
+            'Find this in the URL after logging into opencode.ai (e.g.'
+          )}{' '}
+          <code className="text-xs">
+            {translate(
+              'auto.components.settings.AccountsPane.ae3b21eb6c',
+              'opencode.ai/workspace/wrk_…/go'
+            )}
+          </code>
+          ).
+        </p>
+      </SearchableSetting>
+    </section>
+  )
+}

src/renderer/src/components/settings/accounts-pane-removal-dialogs.tsx
@@ -1,0 +1,119 @@
+import { translate } from '@/i18n/i18n'
+import {
+  removeClaudeProviderAccount,
+  removeCodexProviderAccount
+} from '@/runtime/runtime-provider-accounts-client'
+import { Button } from '../ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '../ui/dialog'
+import type { AccountsPaneSectionModel, RemoveAccountTarget } from './accounts-pane-types'
+
+export function renderAccountsRemovalDialogs(
+  model: AccountsPaneSectionModel,
+  removeCodexTarget: RemoveAccountTarget | null,
+  removeClaudeTarget: RemoveAccountTarget | null
+): React.JSX.Element {
+  const {
+    runClaudeAccountAction,
+    runCodexAccountAction,
+    setRemoveClaudeTarget,
+    setRemoveCodexTarget,
+    settings
+  } = model
+  return (
+    <>
+      <Dialog
+        open={removeCodexTarget !== null}
+        onOpenChange={(open) => !open && setRemoveCodexTarget(null)}
+      >
+        <DialogContent showCloseButton={false}>
+          <DialogHeader>
+            <DialogTitle>
+              {translate(
+                'auto.components.settings.AccountsPane.0d47394635',
+                'Remove Codex Account?'
+              )}
+            </DialogTitle>
+            <DialogDescription>
+              {translate(
+                'auto.components.settings.AccountsPane.380a7736cc',
+                'Removing this account permanently deletes its managed Codex home, including all Codex session history and MCP logins stored inside. This cannot be undone. If the account is currently active, Manta falls back to the system default Codex login.'
+              )}
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setRemoveCodexTarget(null)}>
+              {translate('auto.components.settings.AccountsPane.dbb9626ed1', 'Cancel')}
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={() => {
+                const target = removeCodexTarget
+                if (!target) {
+                  return
+                }
+                setRemoveCodexTarget(null)
+                void runCodexAccountAction(
+                  `remove:${target.id}`,
+                  () => removeCodexProviderAccount(settings, target.id),
+                  target.runtime
+                )
+              }}
+            >
+              {translate('auto.components.settings.AccountsPane.c2d2751587', 'Remove Account')}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Dialog
+        open={removeClaudeTarget !== null}
+        onOpenChange={(open) => !open && setRemoveClaudeTarget(null)}
+      >
+        <DialogContent showCloseButton={false}>
+          <DialogHeader>
+            <DialogTitle>
+              {translate(
+                'auto.components.settings.AccountsPane.63843e37e2',
+                'Remove Claude Account?'
+              )}
+            </DialogTitle>
+            <DialogDescription>
+              {translate(
+                'auto.components.settings.AccountsPane.854ebbcc45',
+                'Manta will delete the managed Claude auth for this saved account. If it is currently active, Manta falls back to the system default Claude login.'
+              )}
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setRemoveClaudeTarget(null)}>
+              {translate('auto.components.settings.AccountsPane.dbb9626ed1', 'Cancel')}
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={() => {
+                const target = removeClaudeTarget
+                if (!target) {
+                  return
+                }
+                setRemoveClaudeTarget(null)
+                void runClaudeAccountAction(
+                  `remove:${target.id}`,
+                  () => removeClaudeProviderAccount(settings, target.id),
+                  target.runtime
+                )
+              }}
+            >
+              {translate('auto.components.settings.AccountsPane.c2d2751587', 'Remove Account')}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}