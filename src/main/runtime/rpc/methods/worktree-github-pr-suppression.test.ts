2022zhang125/orca-yunxiao

Fix PR unlink for auto-detected reviews (#16898)
* fix: make PR unlink hide auto-detected reviews

* Type the empty-content test double against the real model

The literal narrowed suppressedGitHubPR to number and typed the callback
as Mock, so neither direction was comparable and tsconfig.tc.web.json
failed on TS2352. Keeping the 'as' cast preserves checking of the fields
the double does supply.

* Add localization keys for the unlinked checks-panel state

The unlinked title, relink action, and the remote-runtime upgrade notice
introduced untranslated keys that static analysis requires in en.json.

* Advertise PR suppression capability in the transport test

The client capability list is pinned by websocket-transport.test.ts, and
adding WORKTREE_GITHUB_PR_SUPPRESSION left the expected list stale.

* Fix stale PR suppression in Checks

* fix: harden PR unlink suppression state

* refactor: extract PR unlink state handling

* fix: show PR relink recovery in source control

* fix: add unlinked PR localization

* Clarify workspace-scoped PR unlinking

---------

Co-authored-by: Merge Sim <sim@local>

src/main/runtime/rpc/methods/worktree-github-pr-suppression.test.ts
@@ -1,0 +1,34 @@
+import { describe, expect, it, vi } from 'vitest'
+import type { OrcaRuntimeService } from '../../orca-runtime'
+import type { RpcRequest } from '../core'
+import { RpcDispatcher } from '../dispatcher'
+import { WORKTREE_METHODS } from './worktree'
+
+describe('worktree GitHub PR suppression RPC', () => {
+  it('forwards suppression writes to host-owned metadata persistence', async () => {
+    const runtime = {
+      getRuntimeId: () => 'test-runtime',
+      updateManagedWorktreeMeta: vi.fn().mockResolvedValue({ id: 'wt-1' })
+    } as unknown as OrcaRuntimeService
+    const request: RpcRequest = {
+      id: 'req-1',
+      authToken: 'tok',
+      method: 'worktree.set',
+      params: {
+        worktree: 'id:wt-1',
+        linkedPR: null,
+        suppressedGitHubPR: 42
+      }
+    }
+
+    const response = await new RpcDispatcher({ runtime, methods: WORKTREE_METHODS }).dispatch(
+      request
+    )
+
+    expect(response).toMatchObject({ ok: true })
+    expect(runtime.updateManagedWorktreeMeta).toHaveBeenCalledWith(
+      'id:wt-1',
+      expect.objectContaining({ linkedPR: null, suppressedGitHubPR: 42 })
+    )
+  })
+})

src/renderer/src/components/HostedReviewUnlinkMenuItem.tsx
@@ -1,0 +1,43 @@
+import type { JSX } from 'react'
+import { Unlink } from 'lucide-react'
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { translate } from '@/i18n/i18n'
+
+type HostedReviewUnlinkMenuItemProps = {
+  reviewLabel: string
+  reviewIdentifier: string
+  providerLabel: string
+  disabled?: boolean
+  onSelect: () => void
+}
+
+export function HostedReviewUnlinkMenuItem({
+  reviewLabel,
+  reviewIdentifier,
+  providerLabel,
+  disabled = false,
+  onSelect
+}: HostedReviewUnlinkMenuItemProps): JSX.Element {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <DropdownMenuItem disabled={disabled} onSelect={onSelect}>
+          <Unlink className="size-3.5" />
+          {translate(
+            'auto.components.HostedReviewUnlinkMenuItem.label',
+            'Unlink {{value0}} from workspace',
+            { value0: reviewLabel }
+          )}
+        </DropdownMenuItem>
+      </TooltipTrigger>
+      <TooltipContent side="left" sideOffset={8} className="max-w-72 text-pretty">
+        {translate(
+          'auto.components.HostedReviewUnlinkMenuItem.description',
+          'Orca will hide {{value0}} {{value1}} details for this workspace. The {{value0}} and branch on {{value2}} won’t be changed.',
+          { value0: reviewLabel, value1: reviewIdentifier, value2: providerLabel }
+        )}
+      </TooltipContent>
+    </Tooltip>
+  )
+}

src/renderer/src/components/right-sidebar/ChecksPanel.review-header.test.tsx
@@ -20,6 +20,12 @@ vi.mock('@/components/ui/dropdown-menu', () => ({
   }) => <div data-disabled={disabled ? 'true' : undefined}>{children}</div>
 }))
 
+vi.mock('@/components/ui/tooltip', () => ({
+  Tooltip: ({ children }: { children: ReactNode }) => <>{children}</>,
+  TooltipContent: ({ children }: { children: ReactNode }) => <>{children}</>,
+  TooltipTrigger: ({ children }: { children: ReactNode }) => <>{children}</>
+}))
+
 beforeEach(() => {
   vi.stubGlobal('navigator', { userAgent: 'Macintosh' })
 })
@@ -74,7 +80,10 @@ describe('ChecksPanelReviewHeader', () => {
     expect(markup).toContain('#2964')
     expect(markup).toContain('underline decoration-border underline-offset-2')
     expect(markup).toContain('More PR actions')
-    expect(markup).toContain('unlink PR')
+    expect(markup).toContain('Unlink PR from workspace')
+    expect(markup).toContain(
+      'Orca will hide PR #2964 details for this workspace. The PR and branch on GitHub won’t be changed.'
+    )
     expect(markup).toContain('Link another PR')
     expect(markup).toContain('lucide-ellipsis')
     expect(markup).not.toContain('lucide-external-link')
@@ -108,11 +117,11 @@ describe('ChecksPanelReviewHeader', () => {
     expect(markup).not.toContain('Ctrl+click to open')
   })
 
-  it('disables unlinking when the displayed PR is not manually linked', () => {
-    const markup = renderHeader({ canUnlinkReview: false })
+  it('enables unlinking for a displayed auto-detected PR', () => {
+    const markup = renderHeader({ canUnlinkReview: true })
 
-    expect(markup).toContain('data-disabled="true"')
-    expect(markup).toContain('unlink PR')
+    expect(markup).not.toContain('data-disabled="true"')
+    expect(markup).toContain('Unlink PR from workspace')
   })
 
   it('shows GitLab MR identity with provider-appropriate link management actions', () => {
@@ -121,7 +130,10 @@ describe('ChecksPanelReviewHeader', () => {
     expect(markup).toContain('Open on GitLab')
     expect(markup).toContain('!31')
     expect(markup).toContain('More MR actions')
-    expect(markup).toContain('Unlink MR')
+    expect(markup).toContain('Unlink MR from workspace')
+    expect(markup).toContain(
+      'Orca will hide MR !31 details for this workspace. The MR and branch on GitLab won’t be changed.'
+    )
     expect(markup).toContain('Link another MR')
   })
 })

src/renderer/src/components/sidebar/WorktreeCardMeta.test.tsx
@@ -144,13 +144,16 @@ describe('WorktreeCardDetailsHover', () => {
     expect(moreActionsIndex).toBeGreaterThan(-1)
     expect(markup).toContain('More PR actions')
     expect(markup).toContain('Copy link')
+    expect(markup).toContain('Unlink PR from workspace')
+    expect(markup).toContain(
+      'Orca will hide PR #456 details for this workspace. The PR and branch on GitHub won’t be changed.'
+    )
     expect(markup).toContain('Open in Orca browser')
-    expect(markup).toContain('Unlink PR')
     expect(moreActionsIndex).toBeLessThan(openInOrcaIndex)
     expect(openInOrcaIndex).toBeLessThan(viewOnGitHubIndex)
-    expect(markup).not.toContain('aria-label="Unlink PR"')
     expect(markup.indexOf('Open in Orca browser')).toBeLessThan(markup.indexOf('Copy link'))
-    expect(markup.indexOf('Copy link')).toBeLessThan(markup.indexOf('Unlink PR'))
+    expect(markup.indexOf('Copy link')).toBeLessThan(markup.indexOf('Unlink PR from workspace'))
+    expect(markup).not.toContain('aria-label="Unlink PR from workspace"')
   })
 
   it('puts issue copy menu before edit and open actions and keeps GitHub last', () => {
@@ -216,7 +219,10 @@ describe('WorktreeCardDetailsHover', () => {
     )
 
     expect(markup).toContain('aria-label="More MR actions"')
-    expect(markup).toContain('Unlink MR')
+    expect(markup).toContain('Unlink MR from workspace')
+    expect(markup).toContain(
+      'Orca will hide MR !77 details for this workspace. The MR and branch on GitLab won’t be changed.'
+    )
     expect(markup).toContain('View on GitLab')
     expect(markup).toContain('Open in Orca browser')
   })

paidaxingyo666/Manta

src/main/runtime/rpc/methods/worktree-github-pr-suppression.test.ts
@@ -1,0 +1,34 @@
+import { describe, expect, it, vi } from 'vitest'
+import type { MantaRuntimeService } from '../../manta-runtime'
+import type { RpcRequest } from '../core'
+import { RpcDispatcher } from '../dispatcher'
+import { WORKTREE_METHODS } from './worktree'
+
+describe('worktree GitHub PR suppression RPC', () => {
+  it('forwards suppression writes to host-owned metadata persistence', async () => {
+    const runtime = {
+      getRuntimeId: () => 'test-runtime',
+      updateManagedWorktreeMeta: vi.fn().mockResolvedValue({ id: 'wt-1' })
+    } as unknown as MantaRuntimeService
+    const request: RpcRequest = {
+      id: 'req-1',
+      authToken: 'tok',
+      method: 'worktree.set',
+      params: {
+        worktree: 'id:wt-1',
+        linkedPR: null,
+        suppressedGitHubPR: 42
+      }
+    }
+
+    const response = await new RpcDispatcher({ runtime, methods: WORKTREE_METHODS }).dispatch(
+      request
+    )
+
+    expect(response).toMatchObject({ ok: true })
+    expect(runtime.updateManagedWorktreeMeta).toHaveBeenCalledWith(
+      'id:wt-1',
+      expect.objectContaining({ linkedPR: null, suppressedGitHubPR: 42 })
+    )
+  })
+})

src/renderer/src/components/HostedReviewUnlinkMenuItem.tsx
@@ -1,0 +1,43 @@
+import type { JSX } from 'react'
+import { Unlink } from 'lucide-react'
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { translate } from '@/i18n/i18n'
+
+type HostedReviewUnlinkMenuItemProps = {
+  reviewLabel: string
+  reviewIdentifier: string
+  providerLabel: string
+  disabled?: boolean
+  onSelect: () => void
+}
+
+export function HostedReviewUnlinkMenuItem({
+  reviewLabel,
+  reviewIdentifier,
+  providerLabel,
+  disabled = false,
+  onSelect
+}: HostedReviewUnlinkMenuItemProps): JSX.Element {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <DropdownMenuItem disabled={disabled} onSelect={onSelect}>
+          <Unlink className="size-3.5" />
+          {translate(
+            'auto.components.HostedReviewUnlinkMenuItem.label',
+            'Unlink {{value0}} from workspace',
+            { value0: reviewLabel }
+          )}
+        </DropdownMenuItem>
+      </TooltipTrigger>
+      <TooltipContent side="left" sideOffset={8} className="max-w-72 text-pretty">
+        {translate(
+          'auto.components.HostedReviewUnlinkMenuItem.description',
+          'Manta will hide {{value0}} {{value1}} details for this workspace. The {{value0}} and branch on {{value2}} won’t be changed.',
+          { value0: reviewLabel, value1: reviewIdentifier, value2: providerLabel }
+        )}
+      </TooltipContent>
+    </Tooltip>
+  )
+}

src/renderer/src/components/right-sidebar/ChecksPanel.review-header.test.tsx
@@ -20,6 +20,12 @@ vi.mock('@/components/ui/dropdown-menu', () => ({
   }) => <div data-disabled={disabled ? 'true' : undefined}>{children}</div>
 }))
 
+vi.mock('@/components/ui/tooltip', () => ({
+  Tooltip: ({ children }: { children: ReactNode }) => <>{children}</>,
+  TooltipContent: ({ children }: { children: ReactNode }) => <>{children}</>,
+  TooltipTrigger: ({ children }: { children: ReactNode }) => <>{children}</>
+}))
+
 beforeEach(() => {
   vi.stubGlobal('navigator', { userAgent: 'Macintosh' })
 })
@@ -74,7 +80,10 @@ describe('ChecksPanelReviewHeader', () => {
     expect(markup).toContain('#2964')
     expect(markup).toContain('underline decoration-border underline-offset-2')
     expect(markup).toContain('More PR actions')
-    expect(markup).toContain('unlink PR')
+    expect(markup).toContain('Unlink PR from workspace')
+    expect(markup).toContain(
+      'Manta will hide PR #2964 details for this workspace. The PR and branch on GitHub won’t be changed.'
+    )
     expect(markup).toContain('Link another PR')
     expect(markup).toContain('lucide-ellipsis')
     expect(markup).not.toContain('lucide-external-link')
@@ -110,11 +119,11 @@ describe('ChecksPanelReviewHeader', () => {
     expect(markup).not.toContain('Ctrl+click to open')
   })
 
-  it('disables unlinking when the displayed PR is not manually linked', () => {
-    const markup = renderHeader({ canUnlinkReview: false })
+  it('enables unlinking for a displayed auto-detected PR', () => {
+    const markup = renderHeader({ canUnlinkReview: true })
 
-    expect(markup).toContain('data-disabled="true"')
-    expect(markup).toContain('unlink PR')
+    expect(markup).not.toContain('data-disabled="true"')
+    expect(markup).toContain('Unlink PR from workspace')
   })
 
   it('shows GitLab MR identity with provider-appropriate link management actions', () => {
@@ -123,7 +132,10 @@ describe('ChecksPanelReviewHeader', () => {
     expect(markup).toContain('Open on GitLab')
     expect(markup).toContain('!31')
     expect(markup).toContain('More MR actions')
-    expect(markup).toContain('Unlink MR')
+    expect(markup).toContain('Unlink MR from workspace')
+    expect(markup).toContain(
+      'Manta will hide MR !31 details for this workspace. The MR and branch on GitLab won’t be changed.'
+    )
     expect(markup).toContain('Link another MR')
   })
 })

src/renderer/src/components/sidebar/WorktreeCardMeta.test.tsx
@@ -144,13 +144,16 @@ describe('WorktreeCardDetailsHover', () => {
     expect(moreActionsIndex).toBeGreaterThan(-1)
     expect(markup).toContain('More PR actions')
     expect(markup).toContain('Copy link')
+    expect(markup).toContain('Unlink PR from workspace')
+    expect(markup).toContain(
+      'Manta will hide PR #456 details for this workspace. The PR and branch on GitHub won’t be changed.'
+    )
     expect(markup).toContain('Open in Manta browser')
-    expect(markup).toContain('Unlink PR')
     expect(moreActionsIndex).toBeLessThan(openInMantaIndex)
     expect(openInMantaIndex).toBeLessThan(viewOnGitHubIndex)
-    expect(markup).not.toContain('aria-label="Unlink PR"')
     expect(markup.indexOf('Open in Manta browser')).toBeLessThan(markup.indexOf('Copy link'))
-    expect(markup.indexOf('Copy link')).toBeLessThan(markup.indexOf('Unlink PR'))
+    expect(markup.indexOf('Copy link')).toBeLessThan(markup.indexOf('Unlink PR from workspace'))
+    expect(markup).not.toContain('aria-label="Unlink PR from workspace"')
   })
 
   it('puts issue copy menu before edit and open actions and keeps GitHub last', () => {
@@ -216,7 +219,10 @@ describe('WorktreeCardDetailsHover', () => {
     )
 
     expect(markup).toContain('aria-label="More MR actions"')
-    expect(markup).toContain('Unlink MR')
+    expect(markup).toContain('Unlink MR from workspace')
+    expect(markup).toContain(
+      'Manta will hide MR !77 details for this workspace. The MR and branch on GitLab won’t be changed.'
+    )
     expect(markup).toContain('View on GitLab')
     expect(markup).toContain('Open in Manta browser')
   })