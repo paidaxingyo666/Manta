2022zhang125/orca-yunxiao

STA-5496 offer Orca Browser for SSH terminal links (#16503)
Reuse direct SSH browser-route eligibility and route owner-pinned terminal links through the workspace createBrowserTab path. Keep the popover and modifier hints aligned with the eligible destination.

Preserve printed 0.0.0.0 and localhost URLs; the existing SSH SOCKS boundary normalizes wildcard listeners to remote loopback immediately before connect.

src/renderer/src/components/terminal-pane/terminal-link-action-routing.test.ts
@@ -1,7 +1,7 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import {
   registerHttpLinkStoreAccessor,
-  registerRuntimeHttpLinkBrowserOpener
+  registerWorkspaceHttpLinkBrowserOpener
 } from '@/lib/http-link-routing'
 import {
   closeTerminalLinkActionRequest,
@@ -48,11 +48,11 @@ beforeEach(() => {
     setActiveWorktree,
     createBrowserTab
   }))
-  registerRuntimeHttpLinkBrowserOpener(openRuntimeBrowserTab)
+  registerWorkspaceHttpLinkBrowserOpener(openRuntimeBrowserTab)
 })
 
 afterEach(() => {
-  registerRuntimeHttpLinkBrowserOpener(null)
+  registerWorkspaceHttpLinkBrowserOpener(null)
   vi.clearAllMocks()
   vi.unstubAllGlobals()
 })
@@ -207,6 +207,28 @@ describe('terminal link action routing', () => {
       expectedRuntimeEnvironmentId: 'env-1'
     })
     expect(createBrowserTab).not.toHaveBeenCalled()
+  })
+
+  it('routes an explicit Orca Browser action through the owning SSH workspace', () => {
+    const request = vi.fn()
+    const url = 'http://0.0.0.0:8000/'
+
+    handleTerminalHttpLink(url, plainEvent(), {
+      worktreeId: 'wt-1',
+      sourceOwner: { kind: 'ssh', connectionId: 'ssh-1' },
+      linkActionContext: actionContext(request),
+      actionDestinations: { primary: 'system', alternate: 'orca' }
+    })
+
+    request.mock.calls[0][0].alternate.run()
+    expect(openRuntimeBrowserTab).toHaveBeenCalledWith({
+      workspaceId: 'wt-1',
+      url,
+      intent: { kind: 'url' },
+      expectedSshConnectionId: 'ssh-1'
+    })
+    expect(createBrowserTab).not.toHaveBeenCalled()
+    expect(openUrl).not.toHaveBeenCalled()
   })
 
   it('uses Shift+modifier for the alternate local destination', () => {

src/renderer/src/components/terminal-pane/terminal-link-open-hints.test.ts
@@ -155,25 +155,47 @@ describe('terminalUrlOpenHintOptionsFor', () => {
 
     expect(options.modifierInverts).toBe(true)
   })
+
+  it('keeps inversion for an eligible SSH pane', () => {
+    const options = terminalUrlOpenHintOptionsFor(
+      {
+        openLinksInApp: false,
+        openLinksInAppModifierInverts: true
+      },
+      { kind: 'ssh', connectionId: 'ssh-1' },
+      true
+    )
+
+    expect(options.modifierInverts).toBe(true)
+  })
 })
 
 describe('terminalHttpLinkActionDestinationsFor', () => {
-  it('offers both destinations for a capable runtime and follows the preference', () => {
-    const owner = { kind: 'runtime', runtimeEnvironmentId: 'env-1' } as const
-
-    expect(terminalHttpLinkActionDestinationsFor({ openLinksInApp: true }, owner, true)).toEqual({
-      primary: 'orca',
-      alternate: 'system'
-    })
-    expect(terminalHttpLinkActionDestinationsFor({ openLinksInApp: false }, owner, true)).toEqual({
-      primary: 'system',
-      alternate: 'orca'
-    })
-  })
+  it.each([
+    ['local', { kind: 'local' } as const, false],
+    ['capable runtime', { kind: 'runtime', runtimeEnvironmentId: 'env-1' } as const, true],
+    ['eligible SSH', { kind: 'ssh', connectionId: 'ssh-1' } as const, true]
+  ])(
+    'offers both destinations for a %s owner and follows the preference',
+    (_label, owner, canOpen) => {
+      expect(
+        terminalHttpLinkActionDestinationsFor({ openLinksInApp: true }, owner, canOpen)
+      ).toEqual({
+        primary: 'orca',
+        alternate: 'system'
+      })
+      expect(
+        terminalHttpLinkActionDestinationsFor({ openLinksInApp: false }, owner, canOpen)
+      ).toEqual({
+        primary: 'system',
+        alternate: 'orca'
+      })
+    }
+  )
 
   it.each([
     ['incapable runtime', { kind: 'runtime', runtimeEnvironmentId: 'env-1' } as const],
-    ['SSH', { kind: 'ssh', connectionId: 'ssh-1' } as const],
+    ['ineligible SSH', { kind: 'ssh', connectionId: 'ssh-1' } as const],
     ['unknown owner', { kind: 'unknown' } as const]
   ])('offers only the system browser for an %s', (_label, owner) => {
     expect(terminalHttpLinkActionDestinationsFor({ openLinksInApp: true }, owner, false)).toEqual({

src/renderer/src/components/terminal-pane/terminal-link-open-hints.ts
@@ -37,13 +37,22 @@ export type TerminalUrlOpenHintOptions = {
   showActions?: boolean
 }
 
+function canSourceOwnerOpenInOrca(
+  sourceOwner: HttpLinkSourceOwner,
+  canOpenOwnedBrowser: boolean
+): boolean {
+  return (
+    sourceOwner.kind === 'local' ||
+    ((sourceOwner.kind === 'runtime' || sourceOwner.kind === 'ssh') && canOpenOwnedBrowser)
+  )
+}
+
 export function terminalHttpLinkActionDestinationsFor(
   settings: { openLinksInApp?: boolean } | null | undefined,
   sourceOwner: HttpLinkSourceOwner,
-  canOpenRuntimeBrowser: boolean
+  canOpenOwnedBrowser: boolean
 ): TerminalHttpLinkActionDestinations {
-  const canOpenInOrca =
-    sourceOwner.kind === 'local' || (sourceOwner.kind === 'runtime' && canOpenRuntimeBrowser)
+  const canOpenInOrca = canSourceOwnerOpenInOrca(sourceOwner, canOpenOwnedBrowser)
   if (!canOpenInOrca) {
     return { primary: 'system' }
   }
@@ -52,7 +61,7 @@ export function terminalHttpLinkActionDestinationsFor(
     : { primary: 'system', alternate: 'orca' }
 }
 
-// Why: only a capability-verified runtime can advertise the in-app destination.
+// Why: remote owners advertise Orca only when their existing browser route is eligible.
 export function terminalUrlOpenHintOptionsFor(
   settings:
     | {
@@ -63,10 +72,10 @@ export function terminalUrlOpenHintOptionsFor(
     | null
     | undefined,
   sourceOwner?: HttpLinkSourceOwner,
-  canOpenRuntimeBrowser = false
+  canOpenOwnedBrowser = false
 ): TerminalUrlOpenHintOptions {
   const sourceCanOpenInOrca = sourceOwner
-    ? sourceOwner.kind === 'local' || (sourceOwner.kind === 'runtime' && canOpenRuntimeBrowser)
+    ? canSourceOwnerOpenInOrca(sourceOwner, canOpenOwnedBrowser)
     : !settings?.activeRuntimeEnvironmentId?.trim()
   return {
     openLinksInApp: settings?.openLinksInApp === true,

paidaxingyo666/Manta

src/renderer/src/components/terminal-pane/terminal-link-action-routing.test.ts
@@ -1,7 +1,7 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import {
   registerHttpLinkStoreAccessor,
-  registerRuntimeHttpLinkBrowserOpener
+  registerWorkspaceHttpLinkBrowserOpener
 } from '@/lib/http-link-routing'
 import {
   closeTerminalLinkActionRequest,
@@ -48,11 +48,11 @@ beforeEach(() => {
     setActiveWorktree,
     createBrowserTab
   }))
-  registerRuntimeHttpLinkBrowserOpener(openRuntimeBrowserTab)
+  registerWorkspaceHttpLinkBrowserOpener(openRuntimeBrowserTab)
 })
 
 afterEach(() => {
-  registerRuntimeHttpLinkBrowserOpener(null)
+  registerWorkspaceHttpLinkBrowserOpener(null)
   vi.clearAllMocks()
   vi.unstubAllGlobals()
 })
@@ -207,6 +207,28 @@ describe('terminal link action routing', () => {
       expectedRuntimeEnvironmentId: 'env-1'
     })
     expect(createBrowserTab).not.toHaveBeenCalled()
+  })
+
+  it('routes an explicit Manta Browser action through the owning SSH workspace', () => {
+    const request = vi.fn()
+    const url = 'http://0.0.0.0:8000/'
+
+    handleTerminalHttpLink(url, plainEvent(), {
+      worktreeId: 'wt-1',
+      sourceOwner: { kind: 'ssh', connectionId: 'ssh-1' },
+      linkActionContext: actionContext(request),
+      actionDestinations: { primary: 'system', alternate: 'manta' }
+    })
+
+    request.mock.calls[0][0].alternate.run()
+    expect(openRuntimeBrowserTab).toHaveBeenCalledWith({
+      workspaceId: 'wt-1',
+      url,
+      intent: { kind: 'url' },
+      expectedSshConnectionId: 'ssh-1'
+    })
+    expect(createBrowserTab).not.toHaveBeenCalled()
+    expect(openUrl).not.toHaveBeenCalled()
   })
 
   it('uses Shift+modifier for the alternate local destination', () => {

src/renderer/src/components/terminal-pane/terminal-link-open-hints.test.ts
@@ -143,7 +143,7 @@ describe('terminalUrlOpenHintOptionsFor', () => {
     expect(getTerminalUrlOpenHint(options)).toContain('to open in Manta')
   })
 
-  it('keeps inversion for a runtime pane when its host can open a Manta browser', () => {
+  it('keeps inversion for a runtime pane when its host can open an Manta browser', () => {
     const options = terminalUrlOpenHintOptionsFor(
       {
         openLinksInApp: false,
@@ -155,25 +155,47 @@ describe('terminalUrlOpenHintOptionsFor', () => {
 
     expect(options.modifierInverts).toBe(true)
   })
+
+  it('keeps inversion for an eligible SSH pane', () => {
+    const options = terminalUrlOpenHintOptionsFor(
+      {
+        openLinksInApp: false,
+        openLinksInAppModifierInverts: true
+      },
+      { kind: 'ssh', connectionId: 'ssh-1' },
+      true
+    )
+
+    expect(options.modifierInverts).toBe(true)
+  })
 })
 
 describe('terminalHttpLinkActionDestinationsFor', () => {
-  it('offers both destinations for a capable runtime and follows the preference', () => {
-    const owner = { kind: 'runtime', runtimeEnvironmentId: 'env-1' } as const
-
-    expect(terminalHttpLinkActionDestinationsFor({ openLinksInApp: true }, owner, true)).toEqual({
-      primary: 'manta',
-      alternate: 'system'
-    })
-    expect(terminalHttpLinkActionDestinationsFor({ openLinksInApp: false }, owner, true)).toEqual({
-      primary: 'system',
-      alternate: 'manta'
-    })
-  })
+  it.each([
+    ['local', { kind: 'local' } as const, false],
+    ['capable runtime', { kind: 'runtime', runtimeEnvironmentId: 'env-1' } as const, true],
+    ['eligible SSH', { kind: 'ssh', connectionId: 'ssh-1' } as const, true]
+  ])(
+    'offers both destinations for a %s owner and follows the preference',
+    (_label, owner, canOpen) => {
+      expect(
+        terminalHttpLinkActionDestinationsFor({ openLinksInApp: true }, owner, canOpen)
+      ).toEqual({
+        primary: 'manta',
+        alternate: 'system'
+      })
+      expect(
+        terminalHttpLinkActionDestinationsFor({ openLinksInApp: false }, owner, canOpen)
+      ).toEqual({
+        primary: 'system',
+        alternate: 'manta'
+      })
+    }
+  )
 
   it.each([
     ['incapable runtime', { kind: 'runtime', runtimeEnvironmentId: 'env-1' } as const],
-    ['SSH', { kind: 'ssh', connectionId: 'ssh-1' } as const],
+    ['ineligible SSH', { kind: 'ssh', connectionId: 'ssh-1' } as const],
     ['unknown owner', { kind: 'unknown' } as const]
   ])('offers only the system browser for an %s', (_label, owner) => {
     expect(terminalHttpLinkActionDestinationsFor({ openLinksInApp: true }, owner, false)).toEqual({

src/renderer/src/components/terminal-pane/terminal-link-open-hints.ts
@@ -37,13 +37,22 @@ export type TerminalUrlOpenHintOptions = {
   showActions?: boolean
 }
 
+function canSourceOwnerOpenInManta(
+  sourceOwner: HttpLinkSourceOwner,
+  canOpenOwnedBrowser: boolean
+): boolean {
+  return (
+    sourceOwner.kind === 'local' ||
+    ((sourceOwner.kind === 'runtime' || sourceOwner.kind === 'ssh') && canOpenOwnedBrowser)
+  )
+}
+
 export function terminalHttpLinkActionDestinationsFor(
   settings: { openLinksInApp?: boolean } | null | undefined,
   sourceOwner: HttpLinkSourceOwner,
-  canOpenRuntimeBrowser: boolean
+  canOpenOwnedBrowser: boolean
 ): TerminalHttpLinkActionDestinations {
-  const canOpenInManta =
-    sourceOwner.kind === 'local' || (sourceOwner.kind === 'runtime' && canOpenRuntimeBrowser)
+  const canOpenInManta = canSourceOwnerOpenInManta(sourceOwner, canOpenOwnedBrowser)
   if (!canOpenInManta) {
     return { primary: 'system' }
   }
@@ -52,7 +61,7 @@ export function terminalHttpLinkActionDestinationsFor(
     : { primary: 'system', alternate: 'manta' }
 }
 
-// Why: only a capability-verified runtime can advertise the in-app destination.
+// Why: remote owners advertise Manta only when their existing browser route is eligible.
 export function terminalUrlOpenHintOptionsFor(
   settings:
     | {
@@ -63,10 +72,10 @@ export function terminalUrlOpenHintOptionsFor(
     | null
     | undefined,
   sourceOwner?: HttpLinkSourceOwner,
-  canOpenRuntimeBrowser = false
+  canOpenOwnedBrowser = false
 ): TerminalUrlOpenHintOptions {
   const sourceCanOpenInManta = sourceOwner
-    ? sourceOwner.kind === 'local' || (sourceOwner.kind === 'runtime' && canOpenRuntimeBrowser)
+    ? canSourceOwnerOpenInManta(sourceOwner, canOpenOwnedBrowser)
     : !settings?.activeRuntimeEnvironmentId?.trim()
   return {
     openLinksInApp: settings?.openLinksInApp === true,