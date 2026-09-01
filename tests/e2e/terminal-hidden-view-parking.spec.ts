2022zhang125/orca-yunxiao

test: correct 8 stale specs surfaced by the test-detected-bugs sweep (#17434)

tests/e2e/terminal-hidden-view-parking.spec.ts
@@ -2,6 +2,7 @@ import type { Page, TestInfo } from '@stablyai/playwright-test'
 import { randomUUID } from 'node:crypto'
 import { mkdirSync, rmSync, writeFileSync } from 'node:fs'
 import path from 'node:path'
+import { alternateScreenFixtureScript } from './alternate-screen-fixture-script'
 import { test, expect } from './helpers/orca-app'
 import { runNodeScriptInTerminal } from './helpers/run-node-script-in-terminal'
 import {
@@ -18,6 +19,7 @@ import {
   waitForPaneIdentitySnapshot
 } from './helpers/terminal'
 import { parkHiddenTabBehindDecoy, waitForTabParked } from './helpers/terminal-hidden-parking'
+import { waitForPtyShellEcho } from './terminal-pty-readiness'
 import { TERMINAL_TAB_PARK_FLIP_BURST_WINDOW_MS } from '../../src/renderer/src/components/terminal-pane/terminal-park-verdict-flip-telemetry'
 
 // Why: the parking wiring registers this handle (dev/exposeStore builds only)
@@ -73,7 +75,7 @@ function writeParkedFrameScript(scriptPath: string, runId: string): void {
   mkdirSync(path.dirname(scriptPath), { recursive: true })
   writeFileSync(
     scriptPath,
-    `setTimeout(() => process.stdout.write(${JSON.stringify(frames.join(''))}), ${PARKED_FRAME_SCRIPT_DELAY_MS})\n`
+    alternateScreenFixtureScript(frames.join(''), PARKED_FRAME_SCRIPT_DELAY_MS)
   )
 }
 
@@ -103,12 +105,7 @@ function cycleReferenceFrame(runId: string): string {
 
 function writeCycleReferenceScript(scriptPath: string, runId: string): void {
   mkdirSync(path.dirname(scriptPath), { recursive: true })
-  // Paint the frame once, then hold the process open so the alt-screen TUI
-  // stays on screen (and the parkable PTY session stays alive) across cycles.
-  writeFileSync(
-    scriptPath,
-    `process.stdout.write(${JSON.stringify(cycleReferenceFrame(runId))}); setInterval(() => {}, 1000)\n`
-  )
+  writeFileSync(scriptPath, alternateScreenFixtureScript(cycleReferenceFrame(runId)))
 }
 
 // Why: serialize() re-emits the buffer with cursor-restore trailer sequences
@@ -334,6 +331,17 @@ test.describe('Terminal hidden view parking', () => {
       expect(content).toContain('├')
       expect(content).toContain('█')
       expect(content).not.toContain('Orca skipped hidden terminal output')
+
+      // Why: the fixture TUI still owns the PTY foreground after the reveal, so
+      // interrupt it and wait for the shell to take input back before probing.
+      await sendToTerminal(orcaPage, tabAPtyId, '\x03')
+      // Why rethrow: the readiness failure reads as a dead shell, but the only new
+      // dependency here is Ctrl-C reaching the foreground TUI (ConPTY translates it).
+      await waitForPtyShellEcho(orcaPage, tabAPtyId, 15_000).catch((error: unknown) => {
+        throw new Error(
+          `Ctrl-C did not hand the PTY back from the fixture TUI: ${error instanceof Error ? error.message : String(error)}`
+        )
+      })
 
       // Why: the typed marker only appears joined in command *output*, so this
       // proves the revealed terminal accepts input end-to-end, not just echo.

tests/e2e/update-install-renderer-checkpoint-recovery.spec.ts
@@ -1,7 +1,14 @@
 import path from 'node:path'
 import { test, expect } from './helpers/orca-app'
 
-const CHECKPOINT_ERROR = 'Renderer shutdown checkpoint was not completed.'
+// Only the prefix is contract: the checkpoint error appends the swallowed persist
+// cause (STA-5505), whose wording belongs to whatever threw.
+const CHECKPOINT_ERROR_PREFIX = 'Renderer shutdown checkpoint was not completed: '
+
+// The null url is injected, never hydrated: the desktop session schema drops such a row, and
+// no other arrival path carries browserUrlHistory at all (paired web reads it unvalidated but
+// has no producer — STA-5668 follow-up). It is just a deterministic snapshot-build failure.
+const CORRUPT_HISTORY_ENTRY = { url: null, title: 'corrupt persisted history', lastVisitedAt: 0 }
 
 test('recovers update install from a corrupt clean session but preserves dirty drafts', async ({
   orcaPage,
@@ -15,16 +22,14 @@ test('recovers update install from a corrupt clean session but preserves dirty d
   })
 
   const dirtyResult = await orcaPage.evaluate(
-    async ({ filePath, worktreeId }) => {
+    async ({ filePath, worktreeId, corruptEntry }) => {
       const store = window.__store
       if (!store) {
         throw new Error('window.__store is not available')
       }
       const state = store.getState()
       const originalHistory = state.browserUrlHistory
-      state.browserUrlHistory = [
-        { url: null, title: 'corrupt persisted history', lastVisitedAt: 0 }
-      ] as unknown as typeof state.browserUrlHistory
+      state.browserUrlHistory = [corruptEntry] as unknown as typeof state.browserUrlHistory
       const fileId = state.openFile({
         filePath,
         relativePath: 'checkpoint-draft.txt',
@@ -48,22 +53,24 @@ test('recovers update install from a corrupt clean session but preserves dirty d
     },
     {
       filePath: path.join(testRepoPath, 'checkpoint-draft.txt'),
-      worktreeId: await orcaPage.evaluate(() => window.__store?.getState().activeWorktreeId ?? '')
+      worktreeId: await orcaPage.evaluate(() => window.__store?.getState().activeWorktreeId ?? ''),
+      corruptEntry: CORRUPT_HISTORY_ENTRY
     }
   )
 
-  expect(dirtyResult).toBe(CHECKPOINT_ERROR)
+  expect(dirtyResult).toContain(CHECKPOINT_ERROR_PREFIX)
+  // Pin the cause to the corrupt row, not just any named failure; only the member name
+  // survives V8 rewording of "Cannot read properties of null".
+  expect(dirtyResult).toContain('toLowerCase')
 
-  const cleanResult = await orcaPage.evaluate(async () => {
+  const cleanResult = await orcaPage.evaluate(async (corruptEntry) => {
     const store = window.__store
     if (!store) {
       throw new Error('window.__store is not available')
     }
     const state = store.getState()
     const originalHistory = state.browserUrlHistory
-    state.browserUrlHistory = [
-      { url: null, title: 'corrupt persisted history', lastVisitedAt: 0 }
-    ] as unknown as typeof state.browserUrlHistory
+    state.browserUrlHistory = [corruptEntry] as unknown as typeof state.browserUrlHistory
     try {
       await window.api.updater.quitAndInstall()
       return 'continued'
@@ -72,7 +79,7 @@ test('recovers update install from a corrupt clean session but preserves dirty d
     } finally {
       state.browserUrlHistory = originalHistory
     }
-  })
+  }, CORRUPT_HISTORY_ENTRY)
 
   expect(cleanResult).toBe('continued')
   expect(fallbackLogs).toHaveLength(1)

tests/e2e/worktree-jump-palette-filter.spec.ts
@@ -1,4 +1,4 @@
-import type { Page } from '@stablyai/playwright-test'
+import type { Locator, Page } from '@stablyai/playwright-test'
 import { expect, test } from './helpers/orca-app'
 import { waitForActiveWorktree, waitForSessionReady } from './helpers/store'
 
@@ -132,6 +132,23 @@ async function selectRemoteHost(page: Page, useKeyboard = false): Promise<void> 
   await filterTrigger(page).click()
 }
 
+async function openComposerFromTypedName(page: Page): Promise<Locator> {
+  await openPalette(page)
+  const input = palette(page).getByPlaceholder(SEARCH_PLACEHOLDER)
+  await input.fill(`cmd-j-enter-${Date.now()}`)
+  await expect(palette(page).locator('[cmdk-item][data-value="__create_worktree__"]')).toBeVisible()
+
+  await input.press('Enter')
+
+  const createDialog = page.getByRole('dialog', { name: /Create (Workspace|Worktree)/i })
+  await expect(createDialog).toBeVisible()
+  // Why assert focus: the composer auto-focuses the name field, so Escape always
+  // lands on an input the user never chose. A page-style "blur the field first"
+  // handler reachable from here would silently cost a second press.
+  await expect(createDialog.locator('[data-workspace-name-input="true"]')).toBeFocused()
+  return createDialog
+}
+
 test.describe('Worktree jump-palette filters', () => {
   test.beforeEach(async ({ orcaPage }) => {
     await waitForSessionReady(orcaPage)
@@ -182,22 +199,33 @@ test.describe('Worktree jump-palette filters', () => {
   })
 
   test('pressing Enter creates a worktree from a typed name', async ({ orcaPage }) => {
-    await openPalette(orcaPage)
-    const input = palette(orcaPage).getByPlaceholder(SEARCH_PLACEHOLDER)
-    await input.fill(`cmd-j-enter-${Date.now()}`)
-    await expect(
-      palette(orcaPage).locator('[cmdk-item][data-value="__create_worktree__"]')
-    ).toBeVisible()
+    const createDialog = await openComposerFromTypedName(orcaPage)
 
-    await input.press('Enter')
-
-    const createDialog = orcaPage.getByRole('dialog', { name: /Create (Workspace|Worktree)/i })
-    await expect(createDialog).toBeVisible()
-    // Why assert focus first: the composer auto-focuses the name field, so Escape
-    // always lands on an input the user never chose. A page-style "blur the field
-    // first" handler here would silently cost a second press.
-    await expect(createDialog.locator('[data-workspace-name-input="true"]')).toBeFocused()
     await orcaPage.keyboard.press('Escape')
+
     await expect(createDialog).toBeHidden()
+  })
+
+  test('Escape closes the composer opened over the Automations page', async ({ orcaPage }) => {
+    // Why this view: Cmd+J has no view guard, and a page mounted under the palette
+    // keeps its own capture-phase Escape listener registered. Window capture runs
+    // before Radix's document capture, so a preventDefault there vetoes dismissal.
+    await orcaPage.evaluate(() => window.__store?.getState().openAutomationsPage())
+    const automationsHeading = orcaPage.getByRole('heading', { name: 'Automations', level: 1 })
+    await expect(automationsHeading).toBeVisible()
+
+    const createDialog = await openComposerFromTypedName(orcaPage)
+
+    await orcaPage.keyboard.press('Escape')
+
+    await expect(createDialog).toBeHidden()
+    // The page declined the press rather than consuming it, so it is still open.
+    await expect(automationsHeading).toBeVisible()
+
+    // Why a second press: with nothing layered above, the real page chrome must not
+    // trip the overlay check, or Escape would never close Automations again.
+    await orcaPage.keyboard.press('Escape')
+
+    await expect(automationsHeading).toBeHidden()
   })
 })

paidaxingyo666/Manta

tests/e2e/terminal-hidden-view-parking.spec.ts
@@ -2,6 +2,7 @@ import type { Page, TestInfo } from '@stablyai/playwright-test'
 import { randomUUID } from 'node:crypto'
 import { mkdirSync, rmSync, writeFileSync } from 'node:fs'
 import path from 'node:path'
+import { alternateScreenFixtureScript } from './alternate-screen-fixture-script'
 import { test, expect } from './helpers/manta-app'
 import { runNodeScriptInTerminal } from './helpers/run-node-script-in-terminal'
 import {
@@ -18,6 +19,7 @@ import {
   waitForPaneIdentitySnapshot
 } from './helpers/terminal'
 import { parkHiddenTabBehindDecoy, waitForTabParked } from './helpers/terminal-hidden-parking'
+import { waitForPtyShellEcho } from './terminal-pty-readiness'
 import { TERMINAL_TAB_PARK_FLIP_BURST_WINDOW_MS } from '../../src/renderer/src/components/terminal-pane/terminal-park-verdict-flip-telemetry'
 
 // Why: the parking wiring registers this handle (dev/exposeStore builds only)
@@ -73,7 +75,7 @@ function writeParkedFrameScript(scriptPath: string, runId: string): void {
   mkdirSync(path.dirname(scriptPath), { recursive: true })
   writeFileSync(
     scriptPath,
-    `setTimeout(() => process.stdout.write(${JSON.stringify(frames.join(''))}), ${PARKED_FRAME_SCRIPT_DELAY_MS})\n`
+    alternateScreenFixtureScript(frames.join(''), PARKED_FRAME_SCRIPT_DELAY_MS)
   )
 }
 
@@ -103,12 +105,7 @@ function cycleReferenceFrame(runId: string): string {
 
 function writeCycleReferenceScript(scriptPath: string, runId: string): void {
   mkdirSync(path.dirname(scriptPath), { recursive: true })
-  // Paint the frame once, then hold the process open so the alt-screen TUI
-  // stays on screen (and the parkable PTY session stays alive) across cycles.
-  writeFileSync(
-    scriptPath,
-    `process.stdout.write(${JSON.stringify(cycleReferenceFrame(runId))}); setInterval(() => {}, 1000)\n`
-  )
+  writeFileSync(scriptPath, alternateScreenFixtureScript(cycleReferenceFrame(runId)))
 }
 
 // Why: serialize() re-emits the buffer with cursor-restore trailer sequences
@@ -334,6 +331,17 @@ test.describe('Terminal hidden view parking', () => {
       expect(content).toContain('├')
       expect(content).toContain('█')
       expect(content).not.toContain('Manta skipped hidden terminal output')
+
+      // Why: the fixture TUI still owns the PTY foreground after the reveal, so
+      // interrupt it and wait for the shell to take input back before probing.
+      await sendToTerminal(mantaPage, tabAPtyId, '\x03')
+      // Why rethrow: the readiness failure reads as a dead shell, but the only new
+      // dependency here is Ctrl-C reaching the foreground TUI (ConPTY translates it).
+      await waitForPtyShellEcho(mantaPage, tabAPtyId, 15_000).catch((error: unknown) => {
+        throw new Error(
+          `Ctrl-C did not hand the PTY back from the fixture TUI: ${error instanceof Error ? error.message : String(error)}`
+        )
+      })
 
       // Why: the typed marker only appears joined in command *output*, so this
       // proves the revealed terminal accepts input end-to-end, not just echo.

tests/e2e/update-install-renderer-checkpoint-recovery.spec.ts
@@ -1,7 +1,14 @@
 import path from 'node:path'
 import { test, expect } from './helpers/manta-app'
 
-const CHECKPOINT_ERROR = 'Renderer shutdown checkpoint was not completed.'
+// Only the prefix is contract: the checkpoint error appends the swallowed persist
+// cause (STA-5505), whose wording belongs to whatever threw.
+const CHECKPOINT_ERROR_PREFIX = 'Renderer shutdown checkpoint was not completed: '
+
+// The null url is injected, never hydrated: the desktop session schema drops such a row, and
+// no other arrival path carries browserUrlHistory at all (paired web reads it unvalidated but
+// has no producer — STA-5668 follow-up). It is just a deterministic snapshot-build failure.
+const CORRUPT_HISTORY_ENTRY = { url: null, title: 'corrupt persisted history', lastVisitedAt: 0 }
 
 test('recovers update install from a corrupt clean session but preserves dirty drafts', async ({
   mantaPage,
@@ -15,16 +22,14 @@ test('recovers update install from a corrupt clean session but preserves dirty d
   })
 
   const dirtyResult = await mantaPage.evaluate(
-    async ({ filePath, worktreeId }) => {
+    async ({ filePath, worktreeId, corruptEntry }) => {
       const store = window.__store
       if (!store) {
         throw new Error('window.__store is not available')
       }
       const state = store.getState()
       const originalHistory = state.browserUrlHistory
-      state.browserUrlHistory = [
-        { url: null, title: 'corrupt persisted history', lastVisitedAt: 0 }
-      ] as unknown as typeof state.browserUrlHistory
+      state.browserUrlHistory = [corruptEntry] as unknown as typeof state.browserUrlHistory
       const fileId = state.openFile({
         filePath,
         relativePath: 'checkpoint-draft.txt',
@@ -48,22 +53,24 @@ test('recovers update install from a corrupt clean session but preserves dirty d
     },
     {
       filePath: path.join(testRepoPath, 'checkpoint-draft.txt'),
-      worktreeId: await mantaPage.evaluate(() => window.__store?.getState().activeWorktreeId ?? '')
+      worktreeId: await mantaPage.evaluate(() => window.__store?.getState().activeWorktreeId ?? ''),
+      corruptEntry: CORRUPT_HISTORY_ENTRY
     }
   )
 
-  expect(dirtyResult).toBe(CHECKPOINT_ERROR)
+  expect(dirtyResult).toContain(CHECKPOINT_ERROR_PREFIX)
+  // Pin the cause to the corrupt row, not just any named failure; only the member name
+  // survives V8 rewording of "Cannot read properties of null".
+  expect(dirtyResult).toContain('toLowerCase')
 
-  const cleanResult = await mantaPage.evaluate(async () => {
+  const cleanResult = await mantaPage.evaluate(async (corruptEntry) => {
     const store = window.__store
     if (!store) {
       throw new Error('window.__store is not available')
     }
     const state = store.getState()
     const originalHistory = state.browserUrlHistory
-    state.browserUrlHistory = [
-      { url: null, title: 'corrupt persisted history', lastVisitedAt: 0 }
-    ] as unknown as typeof state.browserUrlHistory
+    state.browserUrlHistory = [corruptEntry] as unknown as typeof state.browserUrlHistory
     try {
       await window.api.updater.quitAndInstall()
       return 'continued'
@@ -72,7 +79,7 @@ test('recovers update install from a corrupt clean session but preserves dirty d
     } finally {
       state.browserUrlHistory = originalHistory
     }
-  })
+  }, CORRUPT_HISTORY_ENTRY)
 
   expect(cleanResult).toBe('continued')
   expect(fallbackLogs).toHaveLength(1)

tests/e2e/worktree-jump-palette-filter.spec.ts
@@ -1,4 +1,4 @@
-import type { Page } from '@stablyai/playwright-test'
+import type { Locator, Page } from '@stablyai/playwright-test'
 import { expect, test } from './helpers/manta-app'
 import { waitForActiveWorktree, waitForSessionReady } from './helpers/store'
 
@@ -132,6 +132,23 @@ async function selectRemoteHost(page: Page, useKeyboard = false): Promise<void> 
   await filterTrigger(page).click()
 }
 
+async function openComposerFromTypedName(page: Page): Promise<Locator> {
+  await openPalette(page)
+  const input = palette(page).getByPlaceholder(SEARCH_PLACEHOLDER)
+  await input.fill(`cmd-j-enter-${Date.now()}`)
+  await expect(palette(page).locator('[cmdk-item][data-value="__create_worktree__"]')).toBeVisible()
+
+  await input.press('Enter')
+
+  const createDialog = page.getByRole('dialog', { name: /Create (Workspace|Worktree)/i })
+  await expect(createDialog).toBeVisible()
+  // Why assert focus: the composer auto-focuses the name field, so Escape always
+  // lands on an input the user never chose. A page-style "blur the field first"
+  // handler reachable from here would silently cost a second press.
+  await expect(createDialog.locator('[data-workspace-name-input="true"]')).toBeFocused()
+  return createDialog
+}
+
 test.describe('Worktree jump-palette filters', () => {
   test.beforeEach(async ({ mantaPage }) => {
     await waitForSessionReady(mantaPage)
@@ -163,9 +180,7 @@ test.describe('Worktree jump-palette filters', () => {
     await filterTrigger(mantaPage).click()
     await expect(palette(mantaPage).getByText('No results match the active filter')).toBeVisible()
     await expect(
-      palette(mantaPage).getByText(
-        'Clear the filter above, or widen it to more hosts and projects.'
-      )
+      palette(mantaPage).getByText('Clear the filter above, or widen it to more hosts and projects.')
     ).toBeVisible()
 
     // P3: clear restores both rows; closing drops the ephemeral filter.
@@ -184,22 +199,33 @@ test.describe('Worktree jump-palette filters', () => {
   })
 
   test('pressing Enter creates a worktree from a typed name', async ({ mantaPage }) => {
-    await openPalette(mantaPage)
-    const input = palette(mantaPage).getByPlaceholder(SEARCH_PLACEHOLDER)
-    await input.fill(`cmd-j-enter-${Date.now()}`)
-    await expect(
-      palette(mantaPage).locator('[cmdk-item][data-value="__create_worktree__"]')
-    ).toBeVisible()
+    const createDialog = await openComposerFromTypedName(mantaPage)
 
-    await input.press('Enter')
-
-    const createDialog = mantaPage.getByRole('dialog', { name: /Create (Workspace|Worktree)/i })
-    await expect(createDialog).toBeVisible()
-    // Why assert focus first: the composer auto-focuses the name field, so Escape
-    // always lands on an input the user never chose. A page-style "blur the field
-    // first" handler here would silently cost a second press.
-    await expect(createDialog.locator('[data-workspace-name-input="true"]')).toBeFocused()
     await mantaPage.keyboard.press('Escape')
+
     await expect(createDialog).toBeHidden()
+  })
+
+  test('Escape closes the composer opened over the Automations page', async ({ mantaPage }) => {
+    // Why this view: Cmd+J has no view guard, and a page mounted under the palette
+    // keeps its own capture-phase Escape listener registered. Window capture runs
+    // before Radix's document capture, so a preventDefault there vetoes dismissal.
+    await mantaPage.evaluate(() => window.__store?.getState().openAutomationsPage())
+    const automationsHeading = mantaPage.getByRole('heading', { name: 'Automations', level: 1 })
+    await expect(automationsHeading).toBeVisible()
+
+    const createDialog = await openComposerFromTypedName(mantaPage)
+
+    await mantaPage.keyboard.press('Escape')
+
+    await expect(createDialog).toBeHidden()
+    // The page declined the press rather than consuming it, so it is still open.
+    await expect(automationsHeading).toBeVisible()
+
+    // Why a second press: with nothing layered above, the real page chrome must not
+    // trip the overlay check, or Escape would never close Automations again.
+    await mantaPage.keyboard.press('Escape')
+
+    await expect(automationsHeading).toBeHidden()
   })
 })