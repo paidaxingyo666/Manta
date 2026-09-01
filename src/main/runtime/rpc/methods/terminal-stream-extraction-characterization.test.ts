2022zhang125/orca-yunxiao

fix(terminal): restore lossy initial remote snapshots (#17113)
* fix(terminal): restore lossy initial remote snapshots

* test(terminal): strengthen lossy snapshot causal oracle

src/main/runtime/rpc/methods/terminal-stream-extraction-characterization.test.ts
@@ -2,13 +2,16 @@ import { describe, expect, it, vi } from 'vitest'
 import {
   TerminalStreamOpcode,
   decodeTerminalStreamJson,
+  decodeTerminalStreamText,
   encodeTerminalStreamFrame
 } from '../../../../shared/terminal-stream-protocol'
 import { sendSnapshotFrames } from './terminal/terminal-snapshot-publication'
+import { publishMultiplexInitialSnapshot } from './terminal/terminal-multiplex-initial-snapshot'
 import { TerminalSourceRangeRegistry } from '../terminal-source-range-registry'
 import { initializeMultiplexStream } from './terminal/terminal-multiplex-stream-initialization'
 import type { TerminalMultiplexConnection } from './terminal/terminal-multiplex-connection'
 import type { OrcaRuntimeService } from '../../orca-runtime'
+import type { TerminalMultiplexStream } from './terminal/terminal-stream-types'
 
 // These assertions protect the permanent binary publication seam while its state machine
 // lives in concrete terminal domain modules.
@@ -101,6 +104,76 @@ describe('terminal stream extraction characterization', () => {
       TerminalStreamOpcode.SetOutputPaused,
       TerminalStreamOpcode.WriteUnavailable
     ]).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+  })
+
+  it('publishes the latest nonempty initial image when startup output overflows twice', async () => {
+    const frames: { opcode: TerminalStreamOpcode; payload: Uint8Array<ArrayBufferLike> }[] = []
+    const emit = vi.fn()
+    let readCount = 0
+    const stream = {
+      streamId: 7,
+      terminal: 'terminal',
+      ptyId: 'pty',
+      isMobile: false,
+      ackOutputSourceRanges: false,
+      supportsOutputPause: false,
+      pendingOutput: [{ data: 'lost-live', bytes: 9 }],
+      pendingOutputBytes: 9,
+      pendingOutputOverflowed: true,
+      outputBatcher: { push: vi.fn(), flush: vi.fn() }
+    } as unknown as TerminalMultiplexStream
+    const runtime = {
+      readTerminal: vi.fn(async () => {
+        readCount += 1
+        if (readCount === 2) {
+          stream.pendingOutputOverflowed = true
+        }
+        return { tail: ['fallback'] }
+      }),
+      serializeTerminalBuffer: vi.fn(async () => ({
+        data: 'AUTHORITATIVE_INITIAL_MARKER',
+        cols: 80,
+        rows: 24,
+        seq: 41,
+        scrollbackRows: 0,
+        truncatedByByteBudget: false
+      })),
+      getTerminalSize: vi.fn(() => ({ cols: 80, rows: 24 })),
+      getMobileDisplayMode: vi.fn(() => 'fit'),
+      getLayout: vi.fn(() => null)
+    } as unknown as OrcaRuntimeService
+    const state = {
+      runtime,
+      streams: new Map([[stream.streamId, stream]]),
+      emit,
+      closed: false,
+      sendFrame: vi.fn((streamId, opcode, payload = new Uint8Array()) => {
+        expect(streamId).toBe(stream.streamId)
+        frames.push({ opcode, payload })
+        return true
+      })
+    } as unknown as TerminalMultiplexConnection
+
+    await publishMultiplexInitialSnapshot(
+      state,
+      { streamId: stream.streamId, terminal: stream.terminal },
+      stream
+    )
+
+    expect(emit).toHaveBeenCalledWith(
+      expect.objectContaining({ type: 'subscribed', truncated: true })
+    )
+    expect(frames.map(({ opcode }) => opcode)).toEqual([
+      TerminalStreamOpcode.SnapshotStart,
+      TerminalStreamOpcode.SnapshotChunk,
+      TerminalStreamOpcode.SnapshotEnd
+    ])
+    expect(decodeTerminalStreamJson(frames[0]!.payload)).toMatchObject({
+      seq: 41,
+      truncated: true
+    })
+    expect(decodeTerminalStreamText(frames[1]!.payload)).toBe('AUTHORITATIVE_INITIAL_MARKER')
+    expect(stream.outputBatcher.push).not.toHaveBeenCalled()
   })
 
   it('lets a slot-handler registration throw escape before per-stream catch ownership begins', async () => {

src/main/runtime/rpc/methods/terminal/terminal-multiplex-initial-snapshot.ts
@@ -23,12 +23,14 @@ export async function publishMultiplexInitialSnapshot(
   const { runtime, streams, emit } = state
   const { ptyId } = stream
   const isMobile = stream.isMobile
+  const forcedInitialSnapshotTruncated =
+    process.env.ORCA_E2E_FORCE_REMOTE_TERMINAL_INITIAL_SNAPSHOT_TRUNCATED === '1'
   let read = await runtime.readTerminal(request.terminal)
   let serialized = await serializeBudgetedMobileSnapshot(runtime, ptyId, isMobile)
   if (state.closed || streams.get(request.streamId) !== stream) {
     return null
   }
-  let initialOutputOverflowed = false
+  let initialOutputOverflowed = forcedInitialSnapshotTruncated
   if (stream.pendingOutputOverflowed) {
     stream.pendingOutput.splice(0)
     stream.pendingOutputBytes = 0

tests/e2e/cross-version-wire/reported-lossy-initial-snapshot.unit.test.ts
@@ -1,0 +1,214 @@
+import { afterEach, beforeAll, describe, expect, it, vi } from 'vitest'
+import { Terminal } from '@xterm/headless'
+import { createHostTerminalRuntimeStub } from './host-terminal-runtime-stub'
+import { createTerminalWireLink } from './terminal-wire-link'
+import {
+  loadTerminalWireBuild,
+  WORKING_TREE,
+  type TerminalWireBuild
+} from './versioned-terminal-wire'
+
+const REPORTED_CLIENT_REF = '4cb013c0a9'
+const REPORTED_HOST_REF = '4bb337741c'
+const PRE_FIX_MAIN_REF = 'fd9125ea8c'
+const MARKER = 'REPORTED_LOSSY_INITIAL_MARKER'
+const RECOVERED_LIVE_MARKER = 'RECOVERED_LIVE_AFTER_INITIAL'
+const CONTINUED_LIVE_MARKER = 'CONTINUED_LIVE_AFTER_RECOVERY'
+const TIMEOUT_MS = 180_000
+
+let candidate: TerminalWireBuild
+let preFixMain: TerminalWireBuild
+let reportedClient: TerminalWireBuild
+let reportedHost: TerminalWireBuild
+
+beforeAll(async () => {
+  ;[candidate, preFixMain, reportedClient, reportedHost] = await Promise.all([
+    loadTerminalWireBuild(WORKING_TREE),
+    loadTerminalWireBuild(PRE_FIX_MAIN_REF),
+    loadTerminalWireBuild(REPORTED_CLIENT_REF),
+    loadTerminalWireBuild(REPORTED_HOST_REF)
+  ])
+}, TIMEOUT_MS)
+
+afterEach(() => {
+  expect(typeof globalThis.window).toBe('undefined')
+})
+
+async function runLossyInitialSnapshotPair(args: {
+  clientBuild: TerminalWireBuild
+  hostBuild: TerminalWireBuild
+  exerciseLiveRecovery?: boolean
+}): Promise<{
+  frames: string[]
+  missingRuntimeMethods: string[]
+  rejected: unknown[]
+  rendered: string
+  snapshotStarts: Record<string, unknown>[]
+  snapshots: string[]
+}> {
+  const hostStub = createHostTerminalRuntimeStub({
+    initialBuffer: MARKER,
+    overflowInitialSnapshots: true
+  })
+  const link = createTerminalWireLink({ ...args, hostStub })
+  const snapshots: string[] = []
+  const terminalModel = new Terminal({ cols: 120, rows: 40 })
+  let subscribed = 0
+  try {
+    const terminal = await args.clientBuild.client
+      .getRemoteRuntimeTerminalMultiplexer('reported-lossy-initial')
+      .subscribeTerminal({
+        terminal: hostStub.terminalHandle,
+        client: { id: 'reported-client', type: 'desktop' },
+        callbacks: {
+          onData: (data) => terminalModel.write(data),
+          onSnapshot: (data) => {
+            snapshots.push(data)
+            terminalModel.write(data)
+          },
+          onSubscribed: () => {
+            subscribed += 1
+          }
+        }
+      })
+    await vi.waitFor(() => expect(subscribed).toBe(1), { timeout: 10_000 })
+    if (args.exerciseLiveRecovery) {
+      hostStub.emitOutput(RECOVERED_LIVE_MARKER)
+      await vi.waitFor(
+        () => expect(readTerminalText(terminalModel)).toContain(RECOVERED_LIVE_MARKER),
+        { timeout: 10_000 }
+      )
+      hostStub.emitOutput(CONTINUED_LIVE_MARKER)
+      await vi.waitFor(
+        () => expect(readTerminalText(terminalModel)).toContain(CONTINUED_LIVE_MARKER),
+        { timeout: 10_000 }
+      )
+    }
+    terminal.close()
+    const snapshotStartOpcode = Number(args.clientBuild.codec.TerminalStreamOpcode.SnapshotStart)
+    return {
+      frames: link.observed.map((frame) => {
+        const codec =
+          frame.direction === 'host-to-client' ? args.clientBuild.codec : args.hostBuild.codec
+        const name = codec.TerminalStreamOpcode[frame.opcode]
+        return `${frame.direction}:${typeof name === 'string' ? name : frame.opcode}`
+      }),
+      missingRuntimeMethods: hostStub.missingRuntimeMethods,
+      rejected: link.rejected,
+      rendered: readTerminalText(terminalModel),
+      snapshotStarts: link.observed
+        .filter(
+          (frame) => frame.direction === 'host-to-client' && frame.opcode === snapshotStartOpcode
+        )
+        .map((frame) => frame.json ?? {}),
+      snapshots
+    }
+  } finally {
+    terminalModel.dispose()
+    await link.dispose()
+  }
+}
+
+function readTerminalText(terminal: Terminal): string {
+  const lines: string[] = []
+  for (let index = 0; index < terminal.buffer.active.length; index += 1) {
+    lines.push(terminal.buffer.active.getLine(index)?.translateToString(true) ?? '')
+  }
+  return lines.join('\n')
+}
+
+describe('reported mixed-version lossy initial snapshot', () => {
+  it(
+    'reconstructs the blank 1.4.192-client / 1.4.191-host boundary',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: reportedClient,
+        hostBuild: reportedHost
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) })
+      ])
+      expect(record.frames).toEqual([
+        'client-to-host:Subscribe',
+        'host-to-client:SnapshotStart',
+        'host-to-client:SnapshotChunk',
+        'host-to-client:SnapshotEnd',
+        'client-to-host:Unsubscribe'
+      ])
+      expect(record.snapshots).toEqual([])
+      expect(record.rendered.trim()).toBe('')
+    },
+    TIMEOUT_MS
+  )
+
+  it(
+    'reproduces the blank terminal with the exact pre-fix main client',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: preFixMain,
+        hostBuild: reportedHost
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) })
+      ])
+      expect(record.frames).toEqual([
+        'client-to-host:Subscribe',
+        'host-to-client:SnapshotStart',
+        'host-to-client:SnapshotChunk',
+        'host-to-client:SnapshotEnd',
+        'client-to-host:Unsubscribe'
+      ])
+      expect(record.snapshots).toEqual([])
+      expect(record.rendered.trim()).toBe('')
+    },
+    TIMEOUT_MS
+  )
+
+  it(
+    'paints the old host image exactly once with the candidate client',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: candidate,
+        hostBuild: reportedHost,
+        exerciseLiveRecovery: true
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) }),
+        expect.objectContaining({ truncated: false, seq: expect.any(Number) })
+      ])
+      expect(record.snapshots[0]).toBe(MARKER)
+      expect(record.rejected).toEqual([])
+      expect(record.missingRuntimeMethods).toEqual([])
+      expect(record.rendered.split(MARKER)).toHaveLength(2)
+      expect(record.rendered.split(RECOVERED_LIVE_MARKER)).toHaveLength(2)
+      expect(record.rendered.split(CONTINUED_LIVE_MARKER)).toHaveLength(2)
+    },
+    TIMEOUT_MS
+  )
+
+  it(
+    'paints the latest host image exactly once with the candidate client',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: candidate,
+        hostBuild: candidate,
+        exerciseLiveRecovery: true
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) }),
+        expect.objectContaining({ truncated: false, seq: expect.any(Number) })
+      ])
+      expect(record.snapshots[0]).toBe(MARKER)
+      expect(record.rejected).toEqual([])
+      expect(record.missingRuntimeMethods).toEqual([])
+      expect(record.rendered.split(MARKER)).toHaveLength(2)
+      expect(record.rendered.split(RECOVERED_LIVE_MARKER)).toHaveLength(2)
+      expect(record.rendered.split(CONTINUED_LIVE_MARKER)).toHaveLength(2)
+    },
+    TIMEOUT_MS
+  )
+})

paidaxingyo666/Manta

src/main/runtime/rpc/methods/terminal-stream-extraction-characterization.test.ts
@@ -2,13 +2,16 @@ import { describe, expect, it, vi } from 'vitest'
 import {
   TerminalStreamOpcode,
   decodeTerminalStreamJson,
+  decodeTerminalStreamText,
   encodeTerminalStreamFrame
 } from '../../../../shared/terminal-stream-protocol'
 import { sendSnapshotFrames } from './terminal/terminal-snapshot-publication'
+import { publishMultiplexInitialSnapshot } from './terminal/terminal-multiplex-initial-snapshot'
 import { TerminalSourceRangeRegistry } from '../terminal-source-range-registry'
 import { initializeMultiplexStream } from './terminal/terminal-multiplex-stream-initialization'
 import type { TerminalMultiplexConnection } from './terminal/terminal-multiplex-connection'
 import type { MantaRuntimeService } from '../../manta-runtime'
+import type { TerminalMultiplexStream } from './terminal/terminal-stream-types'
 
 // These assertions protect the permanent binary publication seam while its state machine
 // lives in concrete terminal domain modules.
@@ -101,6 +104,76 @@ describe('terminal stream extraction characterization', () => {
       TerminalStreamOpcode.SetOutputPaused,
       TerminalStreamOpcode.WriteUnavailable
     ]).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+  })
+
+  it('publishes the latest nonempty initial image when startup output overflows twice', async () => {
+    const frames: { opcode: TerminalStreamOpcode; payload: Uint8Array<ArrayBufferLike> }[] = []
+    const emit = vi.fn()
+    let readCount = 0
+    const stream = {
+      streamId: 7,
+      terminal: 'terminal',
+      ptyId: 'pty',
+      isMobile: false,
+      ackOutputSourceRanges: false,
+      supportsOutputPause: false,
+      pendingOutput: [{ data: 'lost-live', bytes: 9 }],
+      pendingOutputBytes: 9,
+      pendingOutputOverflowed: true,
+      outputBatcher: { push: vi.fn(), flush: vi.fn() }
+    } as unknown as TerminalMultiplexStream
+    const runtime = {
+      readTerminal: vi.fn(async () => {
+        readCount += 1
+        if (readCount === 2) {
+          stream.pendingOutputOverflowed = true
+        }
+        return { tail: ['fallback'] }
+      }),
+      serializeTerminalBuffer: vi.fn(async () => ({
+        data: 'AUTHORITATIVE_INITIAL_MARKER',
+        cols: 80,
+        rows: 24,
+        seq: 41,
+        scrollbackRows: 0,
+        truncatedByByteBudget: false
+      })),
+      getTerminalSize: vi.fn(() => ({ cols: 80, rows: 24 })),
+      getMobileDisplayMode: vi.fn(() => 'fit'),
+      getLayout: vi.fn(() => null)
+    } as unknown as MantaRuntimeService
+    const state = {
+      runtime,
+      streams: new Map([[stream.streamId, stream]]),
+      emit,
+      closed: false,
+      sendFrame: vi.fn((streamId, opcode, payload = new Uint8Array()) => {
+        expect(streamId).toBe(stream.streamId)
+        frames.push({ opcode, payload })
+        return true
+      })
+    } as unknown as TerminalMultiplexConnection
+
+    await publishMultiplexInitialSnapshot(
+      state,
+      { streamId: stream.streamId, terminal: stream.terminal },
+      stream
+    )
+
+    expect(emit).toHaveBeenCalledWith(
+      expect.objectContaining({ type: 'subscribed', truncated: true })
+    )
+    expect(frames.map(({ opcode }) => opcode)).toEqual([
+      TerminalStreamOpcode.SnapshotStart,
+      TerminalStreamOpcode.SnapshotChunk,
+      TerminalStreamOpcode.SnapshotEnd
+    ])
+    expect(decodeTerminalStreamJson(frames[0]!.payload)).toMatchObject({
+      seq: 41,
+      truncated: true
+    })
+    expect(decodeTerminalStreamText(frames[1]!.payload)).toBe('AUTHORITATIVE_INITIAL_MARKER')
+    expect(stream.outputBatcher.push).not.toHaveBeenCalled()
   })
 
   it('lets a slot-handler registration throw escape before per-stream catch ownership begins', async () => {

src/main/runtime/rpc/methods/terminal/terminal-multiplex-initial-snapshot.ts
@@ -23,12 +23,14 @@ export async function publishMultiplexInitialSnapshot(
   const { runtime, streams, emit } = state
   const { ptyId } = stream
   const isMobile = stream.isMobile
+  const forcedInitialSnapshotTruncated =
+    process.env.MANTA_E2E_FORCE_REMOTE_TERMINAL_INITIAL_SNAPSHOT_TRUNCATED === '1'
   let read = await runtime.readTerminal(request.terminal)
   let serialized = await serializeBudgetedMobileSnapshot(runtime, ptyId, isMobile)
   if (state.closed || streams.get(request.streamId) !== stream) {
     return null
   }
-  let initialOutputOverflowed = false
+  let initialOutputOverflowed = forcedInitialSnapshotTruncated
   if (stream.pendingOutputOverflowed) {
     stream.pendingOutput.splice(0)
     stream.pendingOutputBytes = 0

tests/e2e/cross-version-wire/reported-lossy-initial-snapshot.unit.test.ts
@@ -1,0 +1,214 @@
+import { afterEach, beforeAll, describe, expect, it, vi } from 'vitest'
+import { Terminal } from '@xterm/headless'
+import { createHostTerminalRuntimeStub } from './host-terminal-runtime-stub'
+import { createTerminalWireLink } from './terminal-wire-link'
+import {
+  loadTerminalWireBuild,
+  WORKING_TREE,
+  type TerminalWireBuild
+} from './versioned-terminal-wire'
+
+const REPORTED_CLIENT_REF = '4cb013c0a9251275fa3d20ea33b45429e07aa6be'
+const REPORTED_HOST_REF = '4bb337741c335cfcc428d3b4271023566e2dadb8'
+const PRE_FIX_MAIN_REF = 'fd9125ea8c7b347cd8b675a4095e31cd3c865d25'
+const MARKER = 'REPORTED_LOSSY_INITIAL_MARKER'
+const RECOVERED_LIVE_MARKER = 'RECOVERED_LIVE_AFTER_INITIAL'
+const CONTINUED_LIVE_MARKER = 'CONTINUED_LIVE_AFTER_RECOVERY'
+const TIMEOUT_MS = 180_000
+
+let candidate: TerminalWireBuild
+let preFixMain: TerminalWireBuild
+let reportedClient: TerminalWireBuild
+let reportedHost: TerminalWireBuild
+
+beforeAll(async () => {
+  ;[candidate, preFixMain, reportedClient, reportedHost] = await Promise.all([
+    loadTerminalWireBuild(WORKING_TREE),
+    loadTerminalWireBuild(PRE_FIX_MAIN_REF),
+    loadTerminalWireBuild(REPORTED_CLIENT_REF),
+    loadTerminalWireBuild(REPORTED_HOST_REF)
+  ])
+}, TIMEOUT_MS)
+
+afterEach(() => {
+  expect(typeof globalThis.window).toBe('undefined')
+})
+
+async function runLossyInitialSnapshotPair(args: {
+  clientBuild: TerminalWireBuild
+  hostBuild: TerminalWireBuild
+  exerciseLiveRecovery?: boolean
+}): Promise<{
+  frames: string[]
+  missingRuntimeMethods: string[]
+  rejected: unknown[]
+  rendered: string
+  snapshotStarts: Record<string, unknown>[]
+  snapshots: string[]
+}> {
+  const hostStub = createHostTerminalRuntimeStub({
+    initialBuffer: MARKER,
+    overflowInitialSnapshots: true
+  })
+  const link = createTerminalWireLink({ ...args, hostStub })
+  const snapshots: string[] = []
+  const terminalModel = new Terminal({ cols: 120, rows: 40 })
+  let subscribed = 0
+  try {
+    const terminal = await args.clientBuild.client
+      .getRemoteRuntimeTerminalMultiplexer('reported-lossy-initial')
+      .subscribeTerminal({
+        terminal: hostStub.terminalHandle,
+        client: { id: 'reported-client', type: 'desktop' },
+        callbacks: {
+          onData: (data) => terminalModel.write(data),
+          onSnapshot: (data) => {
+            snapshots.push(data)
+            terminalModel.write(data)
+          },
+          onSubscribed: () => {
+            subscribed += 1
+          }
+        }
+      })
+    await vi.waitFor(() => expect(subscribed).toBe(1), { timeout: 10_000 })
+    if (args.exerciseLiveRecovery) {
+      hostStub.emitOutput(RECOVERED_LIVE_MARKER)
+      await vi.waitFor(
+        () => expect(readTerminalText(terminalModel)).toContain(RECOVERED_LIVE_MARKER),
+        { timeout: 10_000 }
+      )
+      hostStub.emitOutput(CONTINUED_LIVE_MARKER)
+      await vi.waitFor(
+        () => expect(readTerminalText(terminalModel)).toContain(CONTINUED_LIVE_MARKER),
+        { timeout: 10_000 }
+      )
+    }
+    terminal.close()
+    const snapshotStartOpcode = Number(args.clientBuild.codec.TerminalStreamOpcode.SnapshotStart)
+    return {
+      frames: link.observed.map((frame) => {
+        const codec =
+          frame.direction === 'host-to-client' ? args.clientBuild.codec : args.hostBuild.codec
+        const name = codec.TerminalStreamOpcode[frame.opcode]
+        return `${frame.direction}:${typeof name === 'string' ? name : frame.opcode}`
+      }),
+      missingRuntimeMethods: hostStub.missingRuntimeMethods,
+      rejected: link.rejected,
+      rendered: readTerminalText(terminalModel),
+      snapshotStarts: link.observed
+        .filter(
+          (frame) => frame.direction === 'host-to-client' && frame.opcode === snapshotStartOpcode
+        )
+        .map((frame) => frame.json ?? {}),
+      snapshots
+    }
+  } finally {
+    terminalModel.dispose()
+    await link.dispose()
+  }
+}
+
+function readTerminalText(terminal: Terminal): string {
+  const lines: string[] = []
+  for (let index = 0; index < terminal.buffer.active.length; index += 1) {
+    lines.push(terminal.buffer.active.getLine(index)?.translateToString(true) ?? '')
+  }
+  return lines.join('\n')
+}
+
+describe('reported mixed-version lossy initial snapshot', () => {
+  it(
+    'reconstructs the blank 1.4.192-client / 1.4.191-host boundary',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: reportedClient,
+        hostBuild: reportedHost
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) })
+      ])
+      expect(record.frames).toEqual([
+        'client-to-host:Subscribe',
+        'host-to-client:SnapshotStart',
+        'host-to-client:SnapshotChunk',
+        'host-to-client:SnapshotEnd',
+        'client-to-host:Unsubscribe'
+      ])
+      expect(record.snapshots).toEqual([])
+      expect(record.rendered.trim()).toBe('')
+    },
+    TIMEOUT_MS
+  )
+
+  it(
+    'reproduces the blank terminal with the exact pre-fix main client',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: preFixMain,
+        hostBuild: reportedHost
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) })
+      ])
+      expect(record.frames).toEqual([
+        'client-to-host:Subscribe',
+        'host-to-client:SnapshotStart',
+        'host-to-client:SnapshotChunk',
+        'host-to-client:SnapshotEnd',
+        'client-to-host:Unsubscribe'
+      ])
+      expect(record.snapshots).toEqual([])
+      expect(record.rendered.trim()).toBe('')
+    },
+    TIMEOUT_MS
+  )
+
+  it(
+    'paints the old host image exactly once with the candidate client',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: candidate,
+        hostBuild: reportedHost,
+        exerciseLiveRecovery: true
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) }),
+        expect.objectContaining({ truncated: false, seq: expect.any(Number) })
+      ])
+      expect(record.snapshots[0]).toBe(MARKER)
+      expect(record.rejected).toEqual([])
+      expect(record.missingRuntimeMethods).toEqual([])
+      expect(record.rendered.split(MARKER)).toHaveLength(2)
+      expect(record.rendered.split(RECOVERED_LIVE_MARKER)).toHaveLength(2)
+      expect(record.rendered.split(CONTINUED_LIVE_MARKER)).toHaveLength(2)
+    },
+    TIMEOUT_MS
+  )
+
+  it(
+    'paints the latest host image exactly once with the candidate client',
+    async () => {
+      const record = await runLossyInitialSnapshotPair({
+        clientBuild: candidate,
+        hostBuild: candidate,
+        exerciseLiveRecovery: true
+      })
+
+      expect(record.snapshotStarts).toEqual([
+        expect.objectContaining({ truncated: true, seq: expect.any(Number) }),
+        expect.objectContaining({ truncated: false, seq: expect.any(Number) })
+      ])
+      expect(record.snapshots[0]).toBe(MARKER)
+      expect(record.rejected).toEqual([])
+      expect(record.missingRuntimeMethods).toEqual([])
+      expect(record.rendered.split(MARKER)).toHaveLength(2)
+      expect(record.rendered.split(RECOVERED_LIVE_MARKER)).toHaveLength(2)
+      expect(record.rendered.split(CONTINUED_LIVE_MARKER)).toHaveLength(2)
+    },
+    TIMEOUT_MS
+  )
+})