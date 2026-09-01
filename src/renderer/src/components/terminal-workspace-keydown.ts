aniruddhaadak80/orca

refactor(renderer): split oversized UI surfaces

src/renderer/src/components/terminal-workspace-keydown.ts
@@ -1,0 +1,268 @@
+import { toast } from 'sonner'
+import type { KeybindingActionId } from '../../../shared/keybindings'
+import { keybindingMatchesAction } from '../../../shared/keybindings'
+import { matchesRecentTabSwitcherChord } from '../../../shared/window-shortcut-policy'
+import { useAppStore } from '../store'
+import { ORCA_EDITOR_REQUEST_CMD_SAVE_EVENT } from './editor/editor-autosave'
+import {
+  handleSwitchRecentTab,
+  handleSwitchTab,
+  handleSwitchTabAcrossAllTypes,
+  handleSwitchTerminalTab
+} from '../hooks/ipc-tab-switch'
+import {
+  createFloatingWorkspaceBrowserTab,
+  createFloatingWorkspaceMarkdownTab,
+  createFloatingWorkspaceTerminalTab,
+  handleEmptyFloatingWorkspacePanelCloseShortcut,
+  isEventTargetInsideFloatingWorkspacePanel,
+  isFloatingWorkspacePanelFocused,
+  switchFloatingWorkspaceTab
+} from '@/lib/floating-workspace-terminal-actions'
+import { showTerminalShortcutCaptureNotification } from '@/lib/terminal-shortcut-capture-notification'
+import { translate } from '@/i18n/i18n'
+import { getKeybindingContext } from './terminal-workspace-model'
+import { resolveTerminalAgentTabShortcut } from './terminal-agent-tab-shortcut'
+import type { TerminalActivationController } from './use-terminal-activation-actions'
+
+export function handleTerminalWorkspaceKeyDown(
+  event: KeyboardEvent,
+  controller: TerminalActivationController,
+  shortcutPlatform: NodeJS.Platform
+): void {
+  const {
+    activeWorktreeId,
+    handleCloseAllFiles,
+    handleCloseBrowserTab,
+    handleCloseFile,
+    handleNewAgentTab,
+    handleNewBrowserTab,
+    handleNewFile,
+    handleNewSimulatorTab,
+    handleNewTab,
+    keybindings,
+    mobileEmulatorEnabled,
+    terminalShortcutPolicy
+  } = controller
+  if (!activeWorktreeId) {
+    return
+  }
+  const context = getKeybindingContext(event.target)
+  const floatingWorkspaceFocused = isFloatingWorkspacePanelFocused()
+  const matchShortcut = (actionId: KeybindingActionId): boolean =>
+    keybindingMatchesAction(actionId, event, shortcutPlatform, keybindings, {
+      context,
+      terminalShortcutPolicy
+    })
+  const notifyTerminalCapture = (actionId: KeybindingActionId): void => {
+    if (context !== 'terminal' || terminalShortcutPolicy !== 'orca-first') {
+      return
+    }
+    showTerminalShortcutCaptureNotification({
+      actionId,
+      platform: shortcutPlatform,
+      keybindings
+    })
+  }
+  if (!event.repeat && matchShortcut('tab.newTerminal')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newTerminal')
+    if (floatingWorkspaceFocused) {
+      void createFloatingWorkspaceTerminalTab(useAppStore.getState())
+      return
+    }
+    handleNewTab()
+    return
+  }
+
+  if (!event.repeat) {
+    const agentShortcut = resolveTerminalAgentTabShortcut({
+      activeWorktreeId,
+      keybindings,
+      matchShortcut
+    })
+    if (agentShortcut.actionId) {
+      event.preventDefault()
+      notifyTerminalCapture(agentShortcut.actionId)
+      if (agentShortcut.agent) {
+        handleNewAgentTab(agentShortcut.agent)
+      } else {
+        toast.message(
+          translate(
+            'auto.components.Terminal.5b2c1a9e44',
+            'No agent CLI detected — install one or pick a default agent in Settings.'
+          )
+        )
+      }
+      return
+    }
+  }
+
+  if (!event.repeat && matchShortcut('tab.reopenClosed')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.reopenClosed')
+    useAppStore.getState().reopenClosedTab(activeWorktreeId)
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.newBrowser')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newBrowser')
+    if (floatingWorkspaceFocused) {
+      void createFloatingWorkspaceBrowserTab(useAppStore.getState())
+      return
+    }
+    handleNewBrowserTab()
+    return
+  }
+  if (!event.repeat && mobileEmulatorEnabled && matchShortcut('tab.newSimulator')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newSimulator')
+    if (!floatingWorkspaceFocused) {
+      handleNewSimulatorTab()
+    }
+    return
+  }
+  if (!event.repeat && matchShortcut('editor.save')) {
+    const target = event.target as HTMLElement | null
+    const inEditor =
+      target?.closest('.monaco-editor, [contenteditable]') !== null ||
+      target?.closest('textarea:not(.xterm-helper-textarea), input') !== null
+    if (!inEditor) {
+      const state = useAppStore.getState()
+      if (state.activeTabType === 'editor' && state.activeFileId) {
+        event.preventDefault()
+        notifyTerminalCapture('editor.save')
+        window.dispatchEvent(new Event(ORCA_EDITOR_REQUEST_CMD_SAVE_EVENT))
+        return
+      }
+    }
+  }
+  if (!event.repeat && matchShortcut('editor.toggleWordWrap')) {
+    const state = useAppStore.getState()
+    if (state.activeTabType === 'editor' && state.activeFileId) {
+      event.preventDefault()
+      notifyTerminalCapture('editor.toggleWordWrap')
+      const activeFile = state.openFiles.find((file) => file.id === state.activeFileId)
+      if (activeFile?.mode === 'diff') {
+        const wrapOn = state.settings?.diffWordWrap === true
+        void state.updateSettings({ diffWordWrap: !wrapOn })
+      } else {
+        const wrapOn = state.settings?.editorWordWrap !== false
+        void state.updateSettings({ editorWordWrap: !wrapOn })
+      }
+      return
+    }
+  }
+  if (!event.repeat && matchShortcut('tab.newMarkdown')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newMarkdown')
+    if (floatingWorkspaceFocused) {
+      void createFloatingWorkspaceMarkdownTab(useAppStore.getState()).catch((error) => {
+        toast.error(
+          error instanceof Error
+            ? error.message
+            : translate(
+                'auto.components.Terminal.f0600556b3',
+                'Failed to create untitled markdown file.'
+              )
+        )
+      })
+      return
+    }
+    void handleNewFile()
+    return
+  }
+  if (handleEmptyFloatingWorkspacePanelCloseShortcut(event, shortcutPlatform, keybindings)) {
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.close')) {
+    const floatingPanelOwnsEvent =
+      isEventTargetInsideFloatingWorkspacePanel(event.target) || floatingWorkspaceFocused
+    if (floatingPanelOwnsEvent) {
+      return
+    }
+    const state = useAppStore.getState()
+    if (state.activeTabType === 'terminal' && context === 'terminal') {
+      return
+    }
+    event.preventDefault()
+    notifyTerminalCapture('tab.close')
+    if (state.activeTabType === 'editor' && state.activeFileId) {
+      handleCloseFile(state.activeFileId)
+    } else if (state.activeTabType === 'browser' && state.activeBrowserTabId) {
+      handleCloseBrowserTab(state.activeBrowserTabId)
+    }
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.closeAll')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.closeAll')
+    handleCloseAllFiles()
+    return
+  }
+  if (
+    matchesRecentTabSwitcherChord(event, shortcutPlatform, keybindings, {
+      context,
+      terminalShortcutPolicy
+    })
+  ) {
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.previousRecent')) {
+    event.preventDefault()
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    handleSwitchRecentTab()
+    return
+  }
+  const switchSameTypeDirection = matchShortcut('tab.nextSameType')
+    ? 1
+    : matchShortcut('tab.previousSameType')
+      ? -1
+      : null
+  const switchAllTypesDirection = matchShortcut('tab.nextAllTypes')
+    ? 1
+    : matchShortcut('tab.previousAllTypes')
+      ? -1
+      : null
+  if (!event.repeat && (switchSameTypeDirection !== null || switchAllTypesDirection !== null)) {
+    event.preventDefault()
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    notifyTerminalCapture(
+      switchAllTypesDirection !== null
+        ? switchAllTypesDirection === 1
+          ? 'tab.nextAllTypes'
+          : 'tab.previousAllTypes'
+        : switchSameTypeDirection === 1
+          ? 'tab.nextSameType'
+          : 'tab.previousSameType'
+    )
+    if (floatingWorkspaceFocused) {
+      switchFloatingWorkspaceTab(
+        useAppStore.getState(),
+        switchAllTypesDirection ?? switchSameTypeDirection ?? 1,
+        switchAllTypesDirection !== null ? 'all-types' : 'same-type'
+      )
+    } else if (switchAllTypesDirection !== null) {
+      handleSwitchTabAcrossAllTypes(switchAllTypesDirection)
+    } else {
+      handleSwitchTab(switchSameTypeDirection ?? 1)
+    }
+  }
+  const terminalTabDirection = matchShortcut('tab.nextTerminal')
+    ? 1
+    : matchShortcut('tab.previousTerminal')
+      ? -1
+      : null
+  if (!event.repeat && terminalTabDirection !== null) {
+    event.preventDefault()
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    if (floatingWorkspaceFocused) {
+      switchFloatingWorkspaceTab(useAppStore.getState(), terminalTabDirection, 'terminal')
+    } else {
+      handleSwitchTerminalTab(terminalTabDirection)
+    }
+  }
+}

src/renderer/src/components/worktree-jump-palette-empty-state.ts
@@ -1,0 +1,71 @@
+import { translate } from '@/i18n/i18n'
+import type { WorktreeJumpPaletteController } from './use-worktree-jump-palette-controller'
+
+export function getWorktreeJumpPaletteResultCount(
+  controller: WorktreeJumpPaletteController
+): number {
+  return controller.hasQuery
+    ? controller.worktreeItems.length +
+        controller.projectTargetItems.length +
+        controller.middleItems.length +
+        controller.openTabItems.length
+    : controller.selectableItems.length
+}
+
+export function getWorktreeJumpPaletteEmptyState(controller: WorktreeJumpPaletteController): {
+  title: string
+  subtitle: string
+} {
+  if (controller.filterActive) {
+    return {
+      title: translate(
+        'worktreeJumpPalette.filter.emptyTitle',
+        'No results match the active filter'
+      ),
+      subtitle: translate(
+        'worktreeJumpPalette.filter.emptySubtitle',
+        'Clear the filter above, or widen it to more hosts and projects.'
+      )
+    }
+  }
+  if (
+    (controller.hasAnySearchableWorktrees ||
+      controller.hasAnyProjectSearchCandidates ||
+      controller.hasAnyMiddleResults ||
+      controller.hasAnyOpenTabs) &&
+    controller.hasQuery
+  ) {
+    return {
+      title: translate(
+        'auto.components.WorktreeJumpPalette.dbd9d87eec',
+        'No results match your search'
+      ),
+      subtitle: translate(
+        'auto.components.WorktreeJumpPalette.c4afa68159',
+        'Try a worktree, project, setting, action, tab title, agent prompt, URL, PR, or port.'
+      )
+    }
+  }
+  if (!controller.hasQuery && controller.hasAnyWorktrees && !controller.hasAnyOpenTabs) {
+    return {
+      title: translate(
+        'auto.components.WorktreeJumpPalette.f60f8730be',
+        'No other worktrees to switch to'
+      ),
+      subtitle: translate(
+        'auto.components.WorktreeJumpPalette.b781ae05e3',
+        'Type to search worktrees, settings, tabs, and actions.'
+      )
+    }
+  }
+  return {
+    title: translate(
+      'auto.components.WorktreeJumpPalette.1628fd7dfa',
+      'No active worktrees, settings, actions, or open tabs'
+    ),
+    subtitle: translate(
+      'auto.components.WorktreeJumpPalette.f7fda8d562',
+      'Create a worktree or open a tab in Orca to get started.'
+    )
+  }
+}

paidaxingyo666/Manta

(cherry picked from commit da89be434509f8ba5c4156c4a8193ef0bcdbc70e)
Mirror-Of: 286c21005b0be89f204fb75034980803525dc1ce

src/renderer/src/components/terminal-workspace-keydown.ts
@@ -1,0 +1,268 @@
+import { toast } from 'sonner'
+import type { KeybindingActionId } from '../../../shared/keybindings'
+import { keybindingMatchesAction } from '../../../shared/keybindings'
+import { matchesRecentTabSwitcherChord } from '../../../shared/window-shortcut-policy'
+import { useAppStore } from '../store'
+import { MANTA_EDITOR_REQUEST_CMD_SAVE_EVENT } from './editor/editor-autosave'
+import {
+  handleSwitchRecentTab,
+  handleSwitchTab,
+  handleSwitchTabAcrossAllTypes,
+  handleSwitchTerminalTab
+} from '../hooks/ipc-tab-switch'
+import {
+  createFloatingWorkspaceBrowserTab,
+  createFloatingWorkspaceMarkdownTab,
+  createFloatingWorkspaceTerminalTab,
+  handleEmptyFloatingWorkspacePanelCloseShortcut,
+  isEventTargetInsideFloatingWorkspacePanel,
+  isFloatingWorkspacePanelFocused,
+  switchFloatingWorkspaceTab
+} from '@/lib/floating-workspace-terminal-actions'
+import { showTerminalShortcutCaptureNotification } from '@/lib/terminal-shortcut-capture-notification'
+import { translate } from '@/i18n/i18n'
+import { getKeybindingContext } from './terminal-workspace-model'
+import { resolveTerminalAgentTabShortcut } from './terminal-agent-tab-shortcut'
+import type { TerminalActivationController } from './use-terminal-activation-actions'
+
+export function handleTerminalWorkspaceKeyDown(
+  event: KeyboardEvent,
+  controller: TerminalActivationController,
+  shortcutPlatform: NodeJS.Platform
+): void {
+  const {
+    activeWorktreeId,
+    handleCloseAllFiles,
+    handleCloseBrowserTab,
+    handleCloseFile,
+    handleNewAgentTab,
+    handleNewBrowserTab,
+    handleNewFile,
+    handleNewSimulatorTab,
+    handleNewTab,
+    keybindings,
+    mobileEmulatorEnabled,
+    terminalShortcutPolicy
+  } = controller
+  if (!activeWorktreeId) {
+    return
+  }
+  const context = getKeybindingContext(event.target)
+  const floatingWorkspaceFocused = isFloatingWorkspacePanelFocused()
+  const matchShortcut = (actionId: KeybindingActionId): boolean =>
+    keybindingMatchesAction(actionId, event, shortcutPlatform, keybindings, {
+      context,
+      terminalShortcutPolicy
+    })
+  const notifyTerminalCapture = (actionId: KeybindingActionId): void => {
+    if (context !== 'terminal' || terminalShortcutPolicy !== 'manta-first') {
+      return
+    }
+    showTerminalShortcutCaptureNotification({
+      actionId,
+      platform: shortcutPlatform,
+      keybindings
+    })
+  }
+  if (!event.repeat && matchShortcut('tab.newTerminal')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newTerminal')
+    if (floatingWorkspaceFocused) {
+      void createFloatingWorkspaceTerminalTab(useAppStore.getState())
+      return
+    }
+    handleNewTab()
+    return
+  }
+
+  if (!event.repeat) {
+    const agentShortcut = resolveTerminalAgentTabShortcut({
+      activeWorktreeId,
+      keybindings,
+      matchShortcut
+    })
+    if (agentShortcut.actionId) {
+      event.preventDefault()
+      notifyTerminalCapture(agentShortcut.actionId)
+      if (agentShortcut.agent) {
+        handleNewAgentTab(agentShortcut.agent)
+      } else {
+        toast.message(
+          translate(
+            'auto.components.Terminal.5b2c1a9e44',
+            'No agent CLI detected — install one or pick a default agent in Settings.'
+          )
+        )
+      }
+      return
+    }
+  }
+
+  if (!event.repeat && matchShortcut('tab.reopenClosed')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.reopenClosed')
+    useAppStore.getState().reopenClosedTab(activeWorktreeId)
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.newBrowser')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newBrowser')
+    if (floatingWorkspaceFocused) {
+      void createFloatingWorkspaceBrowserTab(useAppStore.getState())
+      return
+    }
+    handleNewBrowserTab()
+    return
+  }
+  if (!event.repeat && mobileEmulatorEnabled && matchShortcut('tab.newSimulator')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newSimulator')
+    if (!floatingWorkspaceFocused) {
+      handleNewSimulatorTab()
+    }
+    return
+  }
+  if (!event.repeat && matchShortcut('editor.save')) {
+    const target = event.target as HTMLElement | null
+    const inEditor =
+      target?.closest('.monaco-editor, [contenteditable]') !== null ||
+      target?.closest('textarea:not(.xterm-helper-textarea), input') !== null
+    if (!inEditor) {
+      const state = useAppStore.getState()
+      if (state.activeTabType === 'editor' && state.activeFileId) {
+        event.preventDefault()
+        notifyTerminalCapture('editor.save')
+        window.dispatchEvent(new Event(MANTA_EDITOR_REQUEST_CMD_SAVE_EVENT))
+        return
+      }
+    }
+  }
+  if (!event.repeat && matchShortcut('editor.toggleWordWrap')) {
+    const state = useAppStore.getState()
+    if (state.activeTabType === 'editor' && state.activeFileId) {
+      event.preventDefault()
+      notifyTerminalCapture('editor.toggleWordWrap')
+      const activeFile = state.openFiles.find((file) => file.id === state.activeFileId)
+      if (activeFile?.mode === 'diff') {
+        const wrapOn = state.settings?.diffWordWrap === true
+        void state.updateSettings({ diffWordWrap: !wrapOn })
+      } else {
+        const wrapOn = state.settings?.editorWordWrap !== false
+        void state.updateSettings({ editorWordWrap: !wrapOn })
+      }
+      return
+    }
+  }
+  if (!event.repeat && matchShortcut('tab.newMarkdown')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.newMarkdown')
+    if (floatingWorkspaceFocused) {
+      void createFloatingWorkspaceMarkdownTab(useAppStore.getState()).catch((error) => {
+        toast.error(
+          error instanceof Error
+            ? error.message
+            : translate(
+                'auto.components.Terminal.f0600556b3',
+                'Failed to create untitled markdown file.'
+              )
+        )
+      })
+      return
+    }
+    void handleNewFile()
+    return
+  }
+  if (handleEmptyFloatingWorkspacePanelCloseShortcut(event, shortcutPlatform, keybindings)) {
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.close')) {
+    const floatingPanelOwnsEvent =
+      isEventTargetInsideFloatingWorkspacePanel(event.target) || floatingWorkspaceFocused
+    if (floatingPanelOwnsEvent) {
+      return
+    }
+    const state = useAppStore.getState()
+    if (state.activeTabType === 'terminal' && context === 'terminal') {
+      return
+    }
+    event.preventDefault()
+    notifyTerminalCapture('tab.close')
+    if (state.activeTabType === 'editor' && state.activeFileId) {
+      handleCloseFile(state.activeFileId)
+    } else if (state.activeTabType === 'browser' && state.activeBrowserTabId) {
+      handleCloseBrowserTab(state.activeBrowserTabId)
+    }
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.closeAll')) {
+    event.preventDefault()
+    notifyTerminalCapture('tab.closeAll')
+    handleCloseAllFiles()
+    return
+  }
+  if (
+    matchesRecentTabSwitcherChord(event, shortcutPlatform, keybindings, {
+      context,
+      terminalShortcutPolicy
+    })
+  ) {
+    return
+  }
+  if (!event.repeat && matchShortcut('tab.previousRecent')) {
+    event.preventDefault()
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    handleSwitchRecentTab()
+    return
+  }
+  const switchSameTypeDirection = matchShortcut('tab.nextSameType')
+    ? 1
+    : matchShortcut('tab.previousSameType')
+      ? -1
+      : null
+  const switchAllTypesDirection = matchShortcut('tab.nextAllTypes')
+    ? 1
+    : matchShortcut('tab.previousAllTypes')
+      ? -1
+      : null
+  if (!event.repeat && (switchSameTypeDirection !== null || switchAllTypesDirection !== null)) {
+    event.preventDefault()
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    notifyTerminalCapture(
+      switchAllTypesDirection !== null
+        ? switchAllTypesDirection === 1
+          ? 'tab.nextAllTypes'
+          : 'tab.previousAllTypes'
+        : switchSameTypeDirection === 1
+          ? 'tab.nextSameType'
+          : 'tab.previousSameType'
+    )
+    if (floatingWorkspaceFocused) {
+      switchFloatingWorkspaceTab(
+        useAppStore.getState(),
+        switchAllTypesDirection ?? switchSameTypeDirection ?? 1,
+        switchAllTypesDirection !== null ? 'all-types' : 'same-type'
+      )
+    } else if (switchAllTypesDirection !== null) {
+      handleSwitchTabAcrossAllTypes(switchAllTypesDirection)
+    } else {
+      handleSwitchTab(switchSameTypeDirection ?? 1)
+    }
+  }
+  const terminalTabDirection = matchShortcut('tab.nextTerminal')
+    ? 1
+    : matchShortcut('tab.previousTerminal')
+      ? -1
+      : null
+  if (!event.repeat && terminalTabDirection !== null) {
+    event.preventDefault()
+    event.stopPropagation()
+    event.stopImmediatePropagation()
+    if (floatingWorkspaceFocused) {
+      switchFloatingWorkspaceTab(useAppStore.getState(), terminalTabDirection, 'terminal')
+    } else {
+      handleSwitchTerminalTab(terminalTabDirection)
+    }
+  }
+}

src/renderer/src/components/worktree-jump-palette-empty-state.ts
@@ -1,0 +1,71 @@
+import { translate } from '@/i18n/i18n'
+import type { WorktreeJumpPaletteController } from './use-worktree-jump-palette-controller'
+
+export function getWorktreeJumpPaletteResultCount(
+  controller: WorktreeJumpPaletteController
+): number {
+  return controller.hasQuery
+    ? controller.worktreeItems.length +
+        controller.projectTargetItems.length +
+        controller.middleItems.length +
+        controller.openTabItems.length
+    : controller.selectableItems.length
+}
+
+export function getWorktreeJumpPaletteEmptyState(controller: WorktreeJumpPaletteController): {
+  title: string
+  subtitle: string
+} {
+  if (controller.filterActive) {
+    return {
+      title: translate(
+        'worktreeJumpPalette.filter.emptyTitle',
+        'No results match the active filter'
+      ),
+      subtitle: translate(
+        'worktreeJumpPalette.filter.emptySubtitle',
+        'Clear the filter above, or widen it to more hosts and projects.'
+      )
+    }
+  }
+  if (
+    (controller.hasAnySearchableWorktrees ||
+      controller.hasAnyProjectSearchCandidates ||
+      controller.hasAnyMiddleResults ||
+      controller.hasAnyOpenTabs) &&
+    controller.hasQuery
+  ) {
+    return {
+      title: translate(
+        'auto.components.WorktreeJumpPalette.dbd9d87eec',
+        'No results match your search'
+      ),
+      subtitle: translate(
+        'auto.components.WorktreeJumpPalette.c4afa68159',
+        'Try a worktree, project, setting, action, tab title, agent prompt, URL, PR, or port.'
+      )
+    }
+  }
+  if (!controller.hasQuery && controller.hasAnyWorktrees && !controller.hasAnyOpenTabs) {
+    return {
+      title: translate(
+        'auto.components.WorktreeJumpPalette.f60f8730be',
+        'No other worktrees to switch to'
+      ),
+      subtitle: translate(
+        'auto.components.WorktreeJumpPalette.b781ae05e3',
+        'Type to search worktrees, settings, tabs, and actions.'
+      )
+    }
+  }
+  return {
+    title: translate(
+      'auto.components.WorktreeJumpPalette.1628fd7dfa',
+      'No active worktrees, settings, actions, or open tabs'
+    ),
+    subtitle: translate(
+      'auto.components.WorktreeJumpPalette.f7fda8d562',
+      'Create a worktree or open a tab in Manta to get started.'
+    )
+  }
+}