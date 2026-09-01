2022zhang125/orca-yunxiao

ci: gate PRs on a real input method, and prove the lane engaged one (#17365)
* ci: gate PRs on a real input method, and prove the lane engaged one

No job on the PR gate has ever run a real input method. pr.yml and e2e.yml are
ubuntu-latest with CDP `Input.imeSetComposition`, which is a synthetic
composition; the only job that drives ibus-hangul through xdotool is
terminal-ime-e2e.yml, and it is schedule + dispatch only. A PR could turn the
real-IME path red and merge green.

Route IME source to that lane from pr.yml through the existing
pr-e2e-source-routing mechanism, so it runs on IME-touching PRs and nothing
else. The lane stays out of verify.needs — advisory, like `e2e` — because its
reliability is known only from nightly main runs. Deliberately no
continue-on-error: that reports green and hides the signal.

The harness fails open in ways that all look like success: Playwright reports a
skipped test as a pass, so an unset ORCA_E2E_NATIVE_IBUS_HANGUL, a renamed test,
or a session with no engine all exit 0 having exercised nothing. The specs now
append an engagement receipt only after observing real composition events, and
the runner requires one per expected test before the lane may report success.

Also drop the native spec from changed-e2e: it was already routed there by its
own filename, where it self-skips for want of an ibus session and reported that
skip as coverage.

* ci: let the real-IME step report even when the synthetic step failed

config/scripts/pr-e2e-gate-contract.test.mjs
@@ -4,11 +4,17 @@ import { parse as parseJsonc } from 'jsonc-parser'
 import { describe, expect, it } from 'vitest'
 import { parse as parseYaml } from 'yaml'
 import {
+  hasNativeImeSourceChange,
   hasSshSourceChange,
+  NATIVE_IME_SOURCE_ROUTE_IDS,
   PR_E2E_SOURCE_ROUTES,
   selectPrE2eSpecs,
   SSH_SOURCE_ROUTE_IDS
 } from './pr-e2e-source-routing.mjs'
+import {
+  EXPECTED_NATIVE_IME_TESTS,
+  IME_ENGAGEMENT_RECEIPT_ENV
+} from './terminal-ime-engagement-receipt.mjs'
 
 const projectDir = resolve(import.meta.dirname, '../..')
 const prWorkflow = parseYaml(readFileSync(join(projectDir, '.github/workflows/pr.yml'), 'utf8'))
@@ -19,6 +25,17 @@ const reliabilityManifest = parseJsonc(
 const playwrightConfig = readFileSync(join(projectDir, 'tests/playwright.config.ts'), 'utf8')
 const sshDockerRunner = readFileSync(
   join(projectDir, 'config/scripts/run-ssh-docker-terminal-parking-e2e.mjs'),
+  'utf8'
+)
+const nativeImeWorkflow = parseYaml(
+  readFileSync(join(projectDir, '.github/workflows/terminal-ime-e2e.yml'), 'utf8')
+)
+const nativeImeRunner = readFileSync(
+  join(projectDir, 'config/scripts/run-terminal-ibus-hangul-e2e.mjs'),
+  'utf8'
+)
+const nativeImeSpec = readFileSync(
+  join(projectDir, 'tests/e2e/terminal-ibus-hangul-native.spec.ts'),
   'utf8'
 )
 
@@ -191,7 +208,10 @@ describe('PR E2E gate contract', () => {
     }
   })
 
-  it('keeps dedicated E2E workflows out of pull request CI', () => {
+  it('keeps dedicated E2E workflows from self-triggering on pull requests', () => {
+    // Why this still holds for terminal-ime-e2e.yml now that pr.yml runs it: pr.yml reaches it
+    // through workflow_call, behind the path filter. A pull_request trigger here would run a
+    // real ibus session on every PR, which is the cost the filter exists to avoid.
     const dedicatedWorkflows = [
       'golden-e2e-experiment.yml',
       'linux-wayland-gpu-sandbox.yml',
@@ -484,6 +504,147 @@ describe('PR E2E gate contract', () => {
     ).not.toContain('tests/e2e/paired-remote-terminal-materialization-reconnect.spec.ts')
     expect(selectPrE2eSpecs(['src/main/ipc/pty.ts'])).not.toContain(
       'tests/e2e/paired-remote-terminal-materialization-reconnect.spec.ts'
+    )
+  })
+
+  it('puts the real-IME lane on the PR gate behind the IME source filter', () => {
+    // Why a whole lane and not a spec in changed-e2e: the harness is an ibus-daemon, an xfwm4
+    // session, and an X11 display; the generic lane has none of them and the spec would skip.
+    expect(nativeImeWorkflow.on.workflow_call).toBeDefined()
+    expect(prWorkflow.jobs.terminal_ime_native.uses).toBe(
+      './.github/workflows/terminal-ime-e2e.yml'
+    )
+    expect(prWorkflow.jobs.terminal_ime_native.needs).toBe('e2e-paths')
+    expect(prWorkflow.jobs.terminal_ime_native.if).toBe(
+      "needs.e2e-paths.outputs.native_ime_source_changed == 'true'"
+    )
+    expect(prWorkflow.jobs['e2e-paths'].outputs.native_ime_source_changed).toBe(
+      '${{ steps.filter.outputs.native_ime_source_changed }}'
+    )
+    expect(filterStep.run).toContain('pr-e2e-source-routing.mjs --native-ime-source')
+    expect(filterStep.run).toContain('native_ime_source_changed=$NATIVE_IME_SOURCE_CHANGED')
+
+    // Why: continue-on-error would report the lane green and hide every failure it exists to
+    // surface. Advisory here means "absent from verify.needs", not "always passes".
+    expect(prWorkflow.jobs.terminal_ime_native['continue-on-error']).toBeUndefined()
+    expect(prWorkflow.jobs.verify.needs).not.toContain('terminal_ime_native')
+    expect(verifyStep.env.TERMINAL_IME_NATIVE).toBeUndefined()
+
+    for (const id of NATIVE_IME_SOURCE_ROUTE_IDS) {
+      expect(
+        PR_E2E_SOURCE_ROUTES.map((route) => route.id),
+        id
+      ).toContain(id)
+    }
+  })
+
+  it('triggers the real-IME lane from every surface an input method can judge', () => {
+    for (const file of [
+      'src/renderer/src/components/terminal-pane/terminal-ime-composition-route.ts',
+      'src/renderer/src/components/terminal-pane/terminal-ime-native-text-forwarder.ts',
+      'src/renderer/src/components/terminal-pane/terminal-ios-hangul-preedit.ts',
+      'src/renderer/src/components/terminal-pane/xterm-bypass-policy.ts',
+      'src/renderer/src/lib/pane-manager/terminal-ime-anchor.ts',
+      'src/shared/terminal-unicode-provider.ts',
+      // The xterm fork owns the helper textarea the IME attaches to; no file here says "ime".
+      'config/patches/@xterm__xterm@6.1.0-beta.287.patch',
+      'config/patches/xterm-src/browser/Terminal.ts',
+      // The harness is source too: breaking the runner or a probe is how the lane goes blind.
+      'config/scripts/run-terminal-ibus-hangul-e2e.mjs',
+      'config/scripts/terminal-ime-engagement-receipt.mjs',
+      'tests/e2e/terminal-ime-boundary-probe.ts',
+      'tests/e2e/terminal-ime-byte-reader.ts',
+      'tests/e2e/terminal-ime-engagement-receipt.ts',
+      'tests/e2e/terminal-ibus-hangul-native.spec.ts'
+    ]) {
+      expect(hasNativeImeSourceChange([file]), file).toBe(true)
+    }
+
+    // Why: a real ibus session on a Git or tab-bar edit is the cost the filter exists to avoid,
+    // and a unit test beside the source must not summon a three-and-a-half-minute lane.
+    for (const file of [
+      'src/main/git/git-status.ts',
+      'src/renderer/src/components/tab-bar/BrowserTab.tsx',
+      'src/main/terminal/pty-manager.ts',
+      'docs/STYLEGUIDE.md',
+      'src/renderer/src/components/terminal-pane/terminal-ime-composition-route.test.ts',
+      'src/renderer/src/lib/pane-manager/terminal-ime-anchor.test.ts'
+    ]) {
+      expect(hasNativeImeSourceChange([file]), file).toBe(false)
+    }
+  })
+
+  it('gives every input-method-gated spec a lane that runs it, or an honest exemption', () => {
+    // Why this shape: a spec gated on a native-IME env var that no runner sets is a skip that
+    // reports as a pass. This repo already carries such specs; the point is that they are named
+    // as gaps rather than counted as coverage.
+    const nativeGateExpression = /ORCA_E2E_NATIVE_(?:IBUS_HANGUL|MACOS_KOREAN)\s*[!=]==\s*['"]1['"]/
+    const nativeGatedSpecs = readdirSync(join(projectDir, 'tests/e2e'))
+      .filter((file) => file.endsWith('.spec.ts'))
+      .map((file) => `tests/e2e/${file}`)
+      .filter((spec) => nativeGateExpression.test(readFileSync(join(projectDir, spec), 'utf8')))
+    expect(nativeGatedSpecs.length).toBeGreaterThan(0)
+
+    // Why exempt: the digit repro needs a nested gnome-shell, which no hosted runner provides
+    // (headless mutter never answers RemoteDesktop.CreateSession); the macOS spec needs a real
+    // macOS input source, and no macOS runner exists on any PR or scheduled lane.
+    const unreachableSpecs = new Set([
+      'tests/e2e/terminal-hangul-terminating-digit-native.spec.ts',
+      'tests/e2e/terminal-macos-2set-korean-native.spec.ts'
+    ])
+    const unclaimed = nativeGatedSpecs.filter(
+      (spec) => !unreachableSpecs.has(spec) && !nativeImeRunner.includes(spec)
+    )
+    expect(
+      unclaimed,
+      `Native-IME-gated specs claimed by no lane runner: ${unclaimed.join(', ')}`
+    ).toEqual([])
+
+    for (const spec of unreachableSpecs) {
+      expect(nativeGatedSpecs, spec).toContain(spec)
+      expect(nativeImeRunner.includes(spec), `${spec} is exempt but still invoked`).toBe(false)
+    }
+  })
+
+  it('requires proof an input method engaged before the lane may report success', () => {
+    // Why this is the assertion that matters: every other check in this file protects a job from
+    // not running. This one protects a job that ran from having exercised nothing.
+    expect(nativeImeRunner).toContain('verifyImeEngagementReceipts')
+    expect(nativeImeRunner).toContain(`[IME_ENGAGEMENT_RECEIPT_ENV]: receiptPath`)
+    expect(nativeImeSpec).toContain('appendImeEngagementReceipt(testInfo.title, trace)')
+
+    // Why: the synthetic CDP step runs first in the same job. Under the default success()
+    // condition its failure skipped the real-IME step, so the half that needs an input method
+    // reported nothing on exactly the changes that broke IME code.
+    const nativeStep = nativeImeWorkflow.jobs['linux-x11'].steps.find(
+      (step) => step.name === 'Run native IBus Hangul exact-byte tests'
+    )
+    expect(nativeStep.if).toBe('!cancelled()')
+
+    // Why a literal comparison: the spec cannot import the .mjs module, so the env var name is
+    // written twice and would otherwise drift into a receipt nobody reads.
+    const specSideReceipt = readFileSync(
+      join(projectDir, 'tests/e2e/terminal-ime-engagement-receipt.ts'),
+      'utf8'
+    )
+    expect(specSideReceipt).toContain(`'${IME_ENGAGEMENT_RECEIPT_ENV}'`)
+
+    // Why pin the titles: the runner requires one receipt per name, so a rename that nobody
+    // mirrored here would fail the lane loudly instead of quietly halving it.
+    for (const title of EXPECTED_NATIVE_IME_TESTS) {
+      expect(nativeImeSpec, title).toContain(title)
+    }
+  })
+
+  it('keeps the native IME spec out of the lane that would silently skip it', () => {
+    const changedRun = e2eWorkflow.jobs['changed-e2e'].steps.find(
+      (step) => step.name === 'Run changed E2E specs'
+    )
+    expect(changedRun.run).toContain('. != "tests/e2e/terminal-ibus-hangul-native.spec.ts"')
+    // Why it still has to be routed: the dedicated lane is selected by the same route, so the
+    // spec appearing in test_files is how a spec-only edit reaches the real-IME lane at all.
+    expect(selectPrE2eSpecs(['src/shared/terminal-unicode-provider.ts'])).toContain(
+      'tests/e2e/terminal-ibus-hangul-native.spec.ts'
     )
   })
 

config/scripts/terminal-ime-engagement-receipt.mjs
@@ -1,0 +1,81 @@
+/**
+ * The proof that a native IME run was real.
+ *
+ * Playwright reports a skipped test as a pass, so every documented way this harness fails open —
+ * an unset ORCA_E2E_NATIVE_IBUS_HANGUL, a renamed test the grep no longer selects, a stale
+ * ibus-daemon that wins the XIM selection and leaves the session with no engine — produces a
+ * green run that exercised nothing. The specs append a receipt only after they have observed
+ * real composition events, and the runner requires one per expected test.
+ */
+
+export const IME_ENGAGEMENT_RECEIPT_ENV = 'ORCA_E2E_IME_ENGAGEMENT_RECEIPT'
+
+/** The tests that must each leave a receipt. Pinned so deleting one cannot quietly shrink the lane. */
+export const EXPECTED_NATIVE_IME_TESTS = [
+  'forwards the issue exact-byte sequence without loss or duplication',
+  'forwards the issue sentence stress sequence without leaked ASCII'
+]
+
+function parseReceipts(text) {
+  const entries = []
+  for (const line of text.split('\n')) {
+    if (line.trim() === '') {
+      continue
+    }
+    try {
+      entries.push(JSON.parse(line))
+    } catch {
+      entries.push({ malformed: line })
+    }
+  }
+  return entries
+}
+
+/**
+ * Returns the reasons a run must not be trusted. An empty array means an input method
+ * demonstrably composed text for every expected test.
+ */
+export function verifyImeEngagementReceipts(text, expectedTests = EXPECTED_NATIVE_IME_TESTS) {
+  const entries = parseReceipts(text ?? '')
+  const problems = []
+
+  for (const entry of entries) {
+    if (entry.malformed !== undefined) {
+      problems.push(`malformed receipt line: ${entry.malformed}`)
+    }
+  }
+
+  const seen = new Map()
+  for (const entry of entries) {
+    if (typeof entry.test === 'string') {
+      seen.set(entry.test, entry)
+    }
+  }
+
+  for (const test of expectedTests) {
+    const entry = seen.get(test)
+    if (!entry) {
+      problems.push(
+        `no engagement receipt for "${test}" — it was skipped, filtered out, or renamed`
+      )
+      continue
+    }
+    // Why both: compositionstart alone fires for a bare keypress under some engines, and a
+    // Hangul update alone would not prove the composition lifecycle ran.
+    if (!(entry.compositionStart > 0)) {
+      problems.push(`"${test}" recorded no compositionstart — the IME never engaged`)
+    }
+    if (!(entry.hangulComposition > 0)) {
+      problems.push(
+        `"${test}" recorded no Hangul composition data — the engine produced no syllables`
+      )
+    }
+  }
+
+  const unexpected = [...seen.keys()].filter((test) => !expectedTests.includes(test))
+  for (const test of unexpected) {
+    problems.push(`unexpected engagement receipt for "${test}" — update EXPECTED_NATIVE_IME_TESTS`)
+  }
+
+  return problems
+}

tests/e2e/terminal-ime-engagement-receipt.ts
@@ -1,0 +1,35 @@
+import { appendFileSync, mkdirSync } from 'node:fs'
+import path from 'node:path'
+import type { TerminalImeBoundaryTrace } from './terminal-ime-boundary-probe'
+
+/** Kept identical to config/scripts/terminal-ime-engagement-receipt.mjs; pinned by a contract test. */
+const IME_ENGAGEMENT_RECEIPT_ENV = 'ORCA_E2E_IME_ENGAGEMENT_RECEIPT'
+
+const HANGUL_SYLLABLE = /[\uac00-\ud7af]/
+
+/**
+ * Records that a real input method composed text during this test. Only the session runner sets
+ * the receipt path, so a spec run by hand or by the generic changed-spec lane writes nothing.
+ */
+export function appendImeEngagementReceipt(
+  testTitle: string,
+  trace: TerminalImeBoundaryTrace
+): void {
+  const receiptPath = process.env[IME_ENGAGEMENT_RECEIPT_ENV]
+  if (!receiptPath) {
+    return
+  }
+  const entry = {
+    test: testTitle,
+    compositionStart: trace.dom.filter((event) => event.type === 'compositionstart').length,
+    hangulComposition: trace.dom.filter(
+      (event) =>
+        (event.type === 'compositionupdate' ||
+          (event.type === 'input' && event.inputType === 'insertText')) &&
+        HANGUL_SYLLABLE.test(event.data ?? '')
+    ).length,
+    onDataChunks: trace.onData.length
+  }
+  mkdirSync(path.dirname(receiptPath), { recursive: true })
+  appendFileSync(receiptPath, `${JSON.stringify(entry)}\n`)
+}

paidaxingyo666/Manta

config/scripts/pr-e2e-gate-contract.test.mjs
@@ -4,11 +4,17 @@ import { parse as parseJsonc } from 'jsonc-parser'
 import { describe, expect, it } from 'vitest'
 import { parse as parseYaml } from 'yaml'
 import {
+  hasNativeImeSourceChange,
   hasSshSourceChange,
+  NATIVE_IME_SOURCE_ROUTE_IDS,
   PR_E2E_SOURCE_ROUTES,
   selectPrE2eSpecs,
   SSH_SOURCE_ROUTE_IDS
 } from './pr-e2e-source-routing.mjs'
+import {
+  EXPECTED_NATIVE_IME_TESTS,
+  IME_ENGAGEMENT_RECEIPT_ENV
+} from './terminal-ime-engagement-receipt.mjs'
 
 const projectDir = resolve(import.meta.dirname, '../..')
 const prWorkflow = parseYaml(readFileSync(join(projectDir, '.github/workflows/pr.yml'), 'utf8'))
@@ -19,6 +25,17 @@ const reliabilityManifest = parseJsonc(
 const playwrightConfig = readFileSync(join(projectDir, 'tests/playwright.config.ts'), 'utf8')
 const sshDockerRunner = readFileSync(
   join(projectDir, 'config/scripts/run-ssh-docker-terminal-parking-e2e.mjs'),
+  'utf8'
+)
+const nativeImeWorkflow = parseYaml(
+  readFileSync(join(projectDir, '.github/workflows/terminal-ime-e2e.yml'), 'utf8')
+)
+const nativeImeRunner = readFileSync(
+  join(projectDir, 'config/scripts/run-terminal-ibus-hangul-e2e.mjs'),
+  'utf8'
+)
+const nativeImeSpec = readFileSync(
+  join(projectDir, 'tests/e2e/terminal-ibus-hangul-native.spec.ts'),
   'utf8'
 )
 
@@ -191,7 +208,10 @@ describe('PR E2E gate contract', () => {
     }
   })
 
-  it('keeps dedicated E2E workflows out of pull request CI', () => {
+  it('keeps dedicated E2E workflows from self-triggering on pull requests', () => {
+    // Why this still holds for terminal-ime-e2e.yml now that pr.yml runs it: pr.yml reaches it
+    // through workflow_call, behind the path filter. A pull_request trigger here would run a
+    // real ibus session on every PR, which is the cost the filter exists to avoid.
     const dedicatedWorkflows = [
       'golden-e2e-experiment.yml',
       'linux-wayland-gpu-sandbox.yml',
@@ -484,6 +504,147 @@ describe('PR E2E gate contract', () => {
     ).not.toContain('tests/e2e/paired-remote-terminal-materialization-reconnect.spec.ts')
     expect(selectPrE2eSpecs(['src/main/ipc/pty.ts'])).not.toContain(
       'tests/e2e/paired-remote-terminal-materialization-reconnect.spec.ts'
+    )
+  })
+
+  it('puts the real-IME lane on the PR gate behind the IME source filter', () => {
+    // Why a whole lane and not a spec in changed-e2e: the harness is an ibus-daemon, an xfwm4
+    // session, and an X11 display; the generic lane has none of them and the spec would skip.
+    expect(nativeImeWorkflow.on.workflow_call).toBeDefined()
+    expect(prWorkflow.jobs.terminal_ime_native.uses).toBe(
+      './.github/workflows/terminal-ime-e2e.yml'
+    )
+    expect(prWorkflow.jobs.terminal_ime_native.needs).toBe('e2e-paths')
+    expect(prWorkflow.jobs.terminal_ime_native.if).toBe(
+      "needs.e2e-paths.outputs.native_ime_source_changed == 'true'"
+    )
+    expect(prWorkflow.jobs['e2e-paths'].outputs.native_ime_source_changed).toBe(
+      '${{ steps.filter.outputs.native_ime_source_changed }}'
+    )
+    expect(filterStep.run).toContain('pr-e2e-source-routing.mjs --native-ime-source')
+    expect(filterStep.run).toContain('native_ime_source_changed=$NATIVE_IME_SOURCE_CHANGED')
+
+    // Why: continue-on-error would report the lane green and hide every failure it exists to
+    // surface. Advisory here means "absent from verify.needs", not "always passes".
+    expect(prWorkflow.jobs.terminal_ime_native['continue-on-error']).toBeUndefined()
+    expect(prWorkflow.jobs.verify.needs).not.toContain('terminal_ime_native')
+    expect(verifyStep.env.TERMINAL_IME_NATIVE).toBeUndefined()
+
+    for (const id of NATIVE_IME_SOURCE_ROUTE_IDS) {
+      expect(
+        PR_E2E_SOURCE_ROUTES.map((route) => route.id),
+        id
+      ).toContain(id)
+    }
+  })
+
+  it('triggers the real-IME lane from every surface an input method can judge', () => {
+    for (const file of [
+      'src/renderer/src/components/terminal-pane/terminal-ime-composition-route.ts',
+      'src/renderer/src/components/terminal-pane/terminal-ime-native-text-forwarder.ts',
+      'src/renderer/src/components/terminal-pane/terminal-ios-hangul-preedit.ts',
+      'src/renderer/src/components/terminal-pane/xterm-bypass-policy.ts',
+      'src/renderer/src/lib/pane-manager/terminal-ime-anchor.ts',
+      'src/shared/terminal-unicode-provider.ts',
+      // The xterm fork owns the helper textarea the IME attaches to; no file here says "ime".
+      'config/patches/@xterm__xterm@6.1.0-beta.287.patch',
+      'config/patches/xterm-src/browser/Terminal.ts',
+      // The harness is source too: breaking the runner or a probe is how the lane goes blind.
+      'config/scripts/run-terminal-ibus-hangul-e2e.mjs',
+      'config/scripts/terminal-ime-engagement-receipt.mjs',
+      'tests/e2e/terminal-ime-boundary-probe.ts',
+      'tests/e2e/terminal-ime-byte-reader.ts',
+      'tests/e2e/terminal-ime-engagement-receipt.ts',
+      'tests/e2e/terminal-ibus-hangul-native.spec.ts'
+    ]) {
+      expect(hasNativeImeSourceChange([file]), file).toBe(true)
+    }
+
+    // Why: a real ibus session on a Git or tab-bar edit is the cost the filter exists to avoid,
+    // and a unit test beside the source must not summon a three-and-a-half-minute lane.
+    for (const file of [
+      'src/main/git/git-status.ts',
+      'src/renderer/src/components/tab-bar/BrowserTab.tsx',
+      'src/main/terminal/pty-manager.ts',
+      'docs/STYLEGUIDE.md',
+      'src/renderer/src/components/terminal-pane/terminal-ime-composition-route.test.ts',
+      'src/renderer/src/lib/pane-manager/terminal-ime-anchor.test.ts'
+    ]) {
+      expect(hasNativeImeSourceChange([file]), file).toBe(false)
+    }
+  })
+
+  it('gives every input-method-gated spec a lane that runs it, or an honest exemption', () => {
+    // Why this shape: a spec gated on a native-IME env var that no runner sets is a skip that
+    // reports as a pass. This repo already carries such specs; the point is that they are named
+    // as gaps rather than counted as coverage.
+    const nativeGateExpression = /MANTA_E2E_NATIVE_(?:IBUS_HANGUL|MACOS_KOREAN)\s*[!=]==\s*['"]1['"]/
+    const nativeGatedSpecs = readdirSync(join(projectDir, 'tests/e2e'))
+      .filter((file) => file.endsWith('.spec.ts'))
+      .map((file) => `tests/e2e/${file}`)
+      .filter((spec) => nativeGateExpression.test(readFileSync(join(projectDir, spec), 'utf8')))
+    expect(nativeGatedSpecs.length).toBeGreaterThan(0)
+
+    // Why exempt: the digit repro needs a nested gnome-shell, which no hosted runner provides
+    // (headless mutter never answers RemoteDesktop.CreateSession); the macOS spec needs a real
+    // macOS input source, and no macOS runner exists on any PR or scheduled lane.
+    const unreachableSpecs = new Set([
+      'tests/e2e/terminal-hangul-terminating-digit-native.spec.ts',
+      'tests/e2e/terminal-macos-2set-korean-native.spec.ts'
+    ])
+    const unclaimed = nativeGatedSpecs.filter(
+      (spec) => !unreachableSpecs.has(spec) && !nativeImeRunner.includes(spec)
+    )
+    expect(
+      unclaimed,
+      `Native-IME-gated specs claimed by no lane runner: ${unclaimed.join(', ')}`
+    ).toEqual([])
+
+    for (const spec of unreachableSpecs) {
+      expect(nativeGatedSpecs, spec).toContain(spec)
+      expect(nativeImeRunner.includes(spec), `${spec} is exempt but still invoked`).toBe(false)
+    }
+  })
+
+  it('requires proof an input method engaged before the lane may report success', () => {
+    // Why this is the assertion that matters: every other check in this file protects a job from
+    // not running. This one protects a job that ran from having exercised nothing.
+    expect(nativeImeRunner).toContain('verifyImeEngagementReceipts')
+    expect(nativeImeRunner).toContain(`[IME_ENGAGEMENT_RECEIPT_ENV]: receiptPath`)
+    expect(nativeImeSpec).toContain('appendImeEngagementReceipt(testInfo.title, trace)')
+
+    // Why: the synthetic CDP step runs first in the same job. Under the default success()
+    // condition its failure skipped the real-IME step, so the half that needs an input method
+    // reported nothing on exactly the changes that broke IME code.
+    const nativeStep = nativeImeWorkflow.jobs['linux-x11'].steps.find(
+      (step) => step.name === 'Run native IBus Hangul exact-byte tests'
+    )
+    expect(nativeStep.if).toBe('!cancelled()')
+
+    // Why a literal comparison: the spec cannot import the .mjs module, so the env var name is
+    // written twice and would otherwise drift into a receipt nobody reads.
+    const specSideReceipt = readFileSync(
+      join(projectDir, 'tests/e2e/terminal-ime-engagement-receipt.ts'),
+      'utf8'
+    )
+    expect(specSideReceipt).toContain(`'${IME_ENGAGEMENT_RECEIPT_ENV}'`)
+
+    // Why pin the titles: the runner requires one receipt per name, so a rename that nobody
+    // mirrored here would fail the lane loudly instead of quietly halving it.
+    for (const title of EXPECTED_NATIVE_IME_TESTS) {
+      expect(nativeImeSpec, title).toContain(title)
+    }
+  })
+
+  it('keeps the native IME spec out of the lane that would silently skip it', () => {
+    const changedRun = e2eWorkflow.jobs['changed-e2e'].steps.find(
+      (step) => step.name === 'Run changed E2E specs'
+    )
+    expect(changedRun.run).toContain('. != "tests/e2e/terminal-ibus-hangul-native.spec.ts"')
+    // Why it still has to be routed: the dedicated lane is selected by the same route, so the
+    // spec appearing in test_files is how a spec-only edit reaches the real-IME lane at all.
+    expect(selectPrE2eSpecs(['src/shared/terminal-unicode-provider.ts'])).toContain(
+      'tests/e2e/terminal-ibus-hangul-native.spec.ts'
     )
   })
 

config/scripts/terminal-ime-engagement-receipt.mjs
@@ -1,0 +1,81 @@
+/**
+ * The proof that a native IME run was real.
+ *
+ * Playwright reports a skipped test as a pass, so every documented way this harness fails open —
+ * an unset MANTA_E2E_NATIVE_IBUS_HANGUL, a renamed test the grep no longer selects, a stale
+ * ibus-daemon that wins the XIM selection and leaves the session with no engine — produces a
+ * green run that exercised nothing. The specs append a receipt only after they have observed
+ * real composition events, and the runner requires one per expected test.
+ */
+
+export const IME_ENGAGEMENT_RECEIPT_ENV = 'MANTA_E2E_IME_ENGAGEMENT_RECEIPT'
+
+/** The tests that must each leave a receipt. Pinned so deleting one cannot quietly shrink the lane. */
+export const EXPECTED_NATIVE_IME_TESTS = [
+  'forwards the issue exact-byte sequence without loss or duplication',
+  'forwards the issue sentence stress sequence without leaked ASCII'
+]
+
+function parseReceipts(text) {
+  const entries = []
+  for (const line of text.split('\n')) {
+    if (line.trim() === '') {
+      continue
+    }
+    try {
+      entries.push(JSON.parse(line))
+    } catch {
+      entries.push({ malformed: line })
+    }
+  }
+  return entries
+}
+
+/**
+ * Returns the reasons a run must not be trusted. An empty array means an input method
+ * demonstrably composed text for every expected test.
+ */
+export function verifyImeEngagementReceipts(text, expectedTests = EXPECTED_NATIVE_IME_TESTS) {
+  const entries = parseReceipts(text ?? '')
+  const problems = []
+
+  for (const entry of entries) {
+    if (entry.malformed !== undefined) {
+      problems.push(`malformed receipt line: ${entry.malformed}`)
+    }
+  }
+
+  const seen = new Map()
+  for (const entry of entries) {
+    if (typeof entry.test === 'string') {
+      seen.set(entry.test, entry)
+    }
+  }
+
+  for (const test of expectedTests) {
+    const entry = seen.get(test)
+    if (!entry) {
+      problems.push(
+        `no engagement receipt for "${test}" — it was skipped, filtered out, or renamed`
+      )
+      continue
+    }
+    // Why both: compositionstart alone fires for a bare keypress under some engines, and a
+    // Hangul update alone would not prove the composition lifecycle ran.
+    if (!(entry.compositionStart > 0)) {
+      problems.push(`"${test}" recorded no compositionstart — the IME never engaged`)
+    }
+    if (!(entry.hangulComposition > 0)) {
+      problems.push(
+        `"${test}" recorded no Hangul composition data — the engine produced no syllables`
+      )
+    }
+  }
+
+  const unexpected = [...seen.keys()].filter((test) => !expectedTests.includes(test))
+  for (const test of unexpected) {
+    problems.push(`unexpected engagement receipt for "${test}" — update EXPECTED_NATIVE_IME_TESTS`)
+  }
+
+  return problems
+}

tests/e2e/terminal-ime-engagement-receipt.ts
@@ -1,0 +1,35 @@
+import { appendFileSync, mkdirSync } from 'node:fs'
+import path from 'node:path'
+import type { TerminalImeBoundaryTrace } from './terminal-ime-boundary-probe'
+
+/** Kept identical to config/scripts/terminal-ime-engagement-receipt.mjs; pinned by a contract test. */
+const IME_ENGAGEMENT_RECEIPT_ENV = 'MANTA_E2E_IME_ENGAGEMENT_RECEIPT'
+
+const HANGUL_SYLLABLE = /[\uac00-\ud7af]/
+
+/**
+ * Records that a real input method composed text during this test. Only the session runner sets
+ * the receipt path, so a spec run by hand or by the generic changed-spec lane writes nothing.
+ */
+export function appendImeEngagementReceipt(
+  testTitle: string,
+  trace: TerminalImeBoundaryTrace
+): void {
+  const receiptPath = process.env[IME_ENGAGEMENT_RECEIPT_ENV]
+  if (!receiptPath) {
+    return
+  }
+  const entry = {
+    test: testTitle,
+    compositionStart: trace.dom.filter((event) => event.type === 'compositionstart').length,
+    hangulComposition: trace.dom.filter(
+      (event) =>
+        (event.type === 'compositionupdate' ||
+          (event.type === 'input' && event.inputType === 'insertText')) &&
+        HANGUL_SYLLABLE.test(event.data ?? '')
+    ).length,
+    onDataChunks: trace.onData.length
+  }
+  mkdirSync(path.dirname(receiptPath), { recursive: true })
+  appendFileSync(receiptPath, `${JSON.stringify(entry)}\n`)
+}