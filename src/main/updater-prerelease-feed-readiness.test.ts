2022zhang125/orca-yunxiao

fix(updater): accept GitHub release asset redirects on Windows
Accept manual GitHub release-asset redirects on Windows, preserve non-Windows probing, and cover redirect/error/timeout paths.

src/main/updater-prerelease-feed-readiness.test.ts
@@ -1,12 +1,16 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { installNetRequestFetchAdapter } from './updater-net-request.fixture'
 import { publishingIncident } from './updater-prerelease-feed-reproduction.fixture'
 
-const { netFetchMock } = vi.hoisted(() => ({
-  netFetchMock: vi.fn()
+const ORIGINAL_PLATFORM = process.platform
+
+const { netFetchMock, netRequestMock } = vi.hoisted(() => ({
+  netFetchMock: vi.fn(),
+  netRequestMock: vi.fn()
 }))
 
 vi.mock('electron', () => ({
-  net: { fetch: netFetchMock }
+  net: { fetch: netFetchMock, request: netRequestMock }
 }))
 
 function buildAtomFeed(tags: string[]): string {
@@ -31,6 +35,20 @@ function buildManifest(tag: string): string {
 
 function isPlatformManifestRequest(url: string): boolean {
   return /\/latest(?:-[a-z]+)?\.yml$/.test(url)
+}
+
+function setPlatformForTest(platform: NodeJS.Platform): void {
+  Object.defineProperty(process, 'platform', { value: platform })
+}
+
+function buildWindowsManifest(version: string): string {
+  return [
+    `version: ${version}`,
+    'files:',
+    '  - url: orca-windows-setup.exe',
+    '    sha512: test',
+    'path: orca-windows-setup.exe'
+  ].join('\n')
 }
 
 function respondWithAtom(
@@ -86,10 +104,158 @@ describe('fetchNewerReleaseTagsWithReadiness', () => {
   beforeEach(() => {
     vi.resetModules()
     netFetchMock.mockReset()
+    netRequestMock.mockReset()
+    installNetRequestFetchAdapter(netRequestMock, netFetchMock)
   })
 
   afterEach(() => {
     vi.unstubAllGlobals()
+    vi.useRealTimers()
+    setPlatformForTest(ORIGINAL_PLATFORM)
+  })
+
+  it("offers a Windows release from GitHub's asset redirect without probing Azure", async () => {
+    setPlatformForTest('win32')
+    const assetRequestInits: { method?: string; redirect?: string }[] = []
+
+    netFetchMock.mockImplementation(
+      (url: string, init?: { method?: string; redirect?: string }) => {
+        if (url === 'https://github.com/stablyai/orca/releases.atom') {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+          })
+        }
+        if (isPlatformManifestRequest(url)) {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+          })
+        }
+        if (init?.method === 'HEAD') {
+          assetRequestInits.push(init)
+          return Promise.resolve({ ok: false, status: 302, text: () => Promise.resolve('') })
+        }
+        return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+      }
+    )
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+
+    await expect(fetchNewerReleaseTagsWithReadiness('1.4.189', 1)).resolves.toEqual({
+      tags: ['v1.4.190'],
+      state: 'ready'
+    })
+    expect(assetRequestInits).toEqual([expect.objectContaining({ redirect: 'manual' })])
+  })
+
+  it.each([301, 307, 308])('accepts a GitHub %s asset redirect as ready', async (status) => {
+    setPlatformForTest('win32')
+    netFetchMock.mockImplementation(
+      (url: string, init?: { method?: string }) => {
+        if (url === 'https://github.com/stablyai/orca/releases.atom') {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+          })
+        }
+        if (isPlatformManifestRequest(url)) {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+          })
+        }
+        if (init?.method === 'HEAD') {
+          return Promise.resolve({ ok: false, status, text: () => Promise.resolve('') })
+        }
+        return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+      }
+    )
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+
+    await expect(fetchNewerReleaseTagsWithReadiness('1.4.189', 1)).resolves.toEqual({
+      tags: ['v1.4.190'],
+      state: 'ready'
+    })
+  })
+
+  it('reports a GitHub asset request error as unavailable', async () => {
+    setPlatformForTest('win32')
+    netFetchMock.mockImplementation((url: string, init?: { method?: string }) => {
+      if (url === 'https://github.com/stablyai/orca/releases.atom') {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+        })
+      }
+      if (isPlatformManifestRequest(url)) {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+        })
+      }
+      if (init?.method === 'HEAD') {
+        return Promise.reject(new Error('network down'))
+      }
+      return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+    })
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+
+    await expect(fetchNewerReleaseTagsWithReadiness('1.4.189', 1)).resolves.toEqual({
+      tags: [],
+      state: 'unavailable',
+      unavailableReason: 'manifest'
+    })
+  })
+
+  it('aborts a GitHub asset request that exceeds the timeout', async () => {
+    vi.useFakeTimers()
+    setPlatformForTest('win32')
+    let resolveAsset: (() => void) | undefined
+    const pendingAsset = new Promise<{ ok: boolean; status: number }>((resolve) => {
+      resolveAsset = () => resolve({ ok: false, status: 503 })
+    })
+    netFetchMock.mockImplementation((url: string, init?: { method?: string }) => {
+      if (url === 'https://github.com/stablyai/orca/releases.atom') {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+        })
+      }
+      if (isPlatformManifestRequest(url)) {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+        })
+      }
+      if (init?.method === 'HEAD') {
+        return pendingAsset
+      }
+      return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+    })
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+    const readiness = fetchNewerReleaseTagsWithReadiness('1.4.189', 1)
+    await vi.advanceTimersByTimeAsync(5000)
+
+    await expect(readiness).resolves.toEqual({
+      tags: [],
+      state: 'unavailable',
+      unavailableReason: 'manifest'
+    })
+    const request = netRequestMock.mock.results[0]?.value as { abort: ReturnType<typeof vi.fn> }
+    expect(request.abort).toHaveBeenCalledOnce()
+    resolveAsset?.()
   })
 
   it('reports not-ready with a verified last-good tag when the newest assets are unavailable', async () => {

paidaxingyo666/Manta

src/main/updater-prerelease-feed-readiness.test.ts
@@ -1,12 +1,16 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { installNetRequestFetchAdapter } from './updater-net-request.fixture'
 import { publishingIncident } from './updater-prerelease-feed-reproduction.fixture'
 
-const { netFetchMock } = vi.hoisted(() => ({
-  netFetchMock: vi.fn()
+const ORIGINAL_PLATFORM = process.platform
+
+const { netFetchMock, netRequestMock } = vi.hoisted(() => ({
+  netFetchMock: vi.fn(),
+  netRequestMock: vi.fn()
 }))
 
 vi.mock('electron', () => ({
-  net: { fetch: netFetchMock }
+  net: { fetch: netFetchMock, request: netRequestMock }
 }))
 
 function buildAtomFeed(tags: string[]): string {
@@ -31,6 +35,20 @@ function buildManifest(tag: string): string {
 
 function isPlatformManifestRequest(url: string): boolean {
   return /\/latest(?:-[a-z]+)?\.yml$/.test(url)
+}
+
+function setPlatformForTest(platform: NodeJS.Platform): void {
+  Object.defineProperty(process, 'platform', { value: platform })
+}
+
+function buildWindowsManifest(version: string): string {
+  return [
+    `version: ${version}`,
+    'files:',
+    '  - url: manta-windows-setup.exe',
+    '    sha512: test',
+    'path: manta-windows-setup.exe'
+  ].join('\n')
 }
 
 function respondWithAtom(
@@ -86,10 +104,158 @@ describe('fetchNewerReleaseTagsWithReadiness', () => {
   beforeEach(() => {
     vi.resetModules()
     netFetchMock.mockReset()
+    netRequestMock.mockReset()
+    installNetRequestFetchAdapter(netRequestMock, netFetchMock)
   })
 
   afterEach(() => {
     vi.unstubAllGlobals()
+    vi.useRealTimers()
+    setPlatformForTest(ORIGINAL_PLATFORM)
+  })
+
+  it("offers a Windows release from GitHub's asset redirect without probing Azure", async () => {
+    setPlatformForTest('win32')
+    const assetRequestInits: { method?: string; redirect?: string }[] = []
+
+    netFetchMock.mockImplementation(
+      (url: string, init?: { method?: string; redirect?: string }) => {
+        if (url === 'https://github.com/paidaxingyo666/Manta/releases.atom') {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+          })
+        }
+        if (isPlatformManifestRequest(url)) {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+          })
+        }
+        if (init?.method === 'HEAD') {
+          assetRequestInits.push(init)
+          return Promise.resolve({ ok: false, status: 302, text: () => Promise.resolve('') })
+        }
+        return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+      }
+    )
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+
+    await expect(fetchNewerReleaseTagsWithReadiness('1.4.189', 1)).resolves.toEqual({
+      tags: ['v1.4.190'],
+      state: 'ready'
+    })
+    expect(assetRequestInits).toEqual([expect.objectContaining({ redirect: 'manual' })])
+  })
+
+  it.each([301, 307, 308])('accepts a GitHub %s asset redirect as ready', async (status) => {
+    setPlatformForTest('win32')
+    netFetchMock.mockImplementation(
+      (url: string, init?: { method?: string }) => {
+        if (url === 'https://github.com/paidaxingyo666/Manta/releases.atom') {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+          })
+        }
+        if (isPlatformManifestRequest(url)) {
+          return Promise.resolve({
+            ok: true,
+            status: 200,
+            text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+          })
+        }
+        if (init?.method === 'HEAD') {
+          return Promise.resolve({ ok: false, status, text: () => Promise.resolve('') })
+        }
+        return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+      }
+    )
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+
+    await expect(fetchNewerReleaseTagsWithReadiness('1.4.189', 1)).resolves.toEqual({
+      tags: ['v1.4.190'],
+      state: 'ready'
+    })
+  })
+
+  it('reports a GitHub asset request error as unavailable', async () => {
+    setPlatformForTest('win32')
+    netFetchMock.mockImplementation((url: string, init?: { method?: string }) => {
+      if (url === 'https://github.com/paidaxingyo666/Manta/releases.atom') {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+        })
+      }
+      if (isPlatformManifestRequest(url)) {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+        })
+      }
+      if (init?.method === 'HEAD') {
+        return Promise.reject(new Error('network down'))
+      }
+      return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+    })
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+
+    await expect(fetchNewerReleaseTagsWithReadiness('1.4.189', 1)).resolves.toEqual({
+      tags: [],
+      state: 'unavailable',
+      unavailableReason: 'manifest'
+    })
+  })
+
+  it('aborts a GitHub asset request that exceeds the timeout', async () => {
+    vi.useFakeTimers()
+    setPlatformForTest('win32')
+    let resolveAsset: (() => void) | undefined
+    const pendingAsset = new Promise<{ ok: boolean; status: number }>((resolve) => {
+      resolveAsset = () => resolve({ ok: false, status: 503 })
+    })
+    netFetchMock.mockImplementation((url: string, init?: { method?: string }) => {
+      if (url === 'https://github.com/paidaxingyo666/Manta/releases.atom') {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildAtomFeed(['v1.4.190']))
+        })
+      }
+      if (isPlatformManifestRequest(url)) {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          text: () => Promise.resolve(buildWindowsManifest('1.4.190'))
+        })
+      }
+      if (init?.method === 'HEAD') {
+        return pendingAsset
+      }
+      return Promise.resolve({ ok: false, status: 503, text: () => Promise.resolve('') })
+    })
+
+    const { fetchNewerReleaseTagsWithReadiness } = await import('./updater-prerelease-feed')
+    const readiness = fetchNewerReleaseTagsWithReadiness('1.4.189', 1)
+    await vi.advanceTimersByTimeAsync(5000)
+
+    await expect(readiness).resolves.toEqual({
+      tags: [],
+      state: 'unavailable',
+      unavailableReason: 'manifest'
+    })
+    const request = netRequestMock.mock.results[0]?.value as { abort: ReturnType<typeof vi.fn> }
+    expect(request.abort).toHaveBeenCalledOnce()
+    resolveAsset?.()
   })
 
   it('reports not-ready with a verified last-good tag when the newest assets are unavailable', async () => {