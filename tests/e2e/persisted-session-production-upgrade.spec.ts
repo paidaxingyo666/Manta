2022zhang125/orca-yunxiao

test(e2e): cover session upgrade and Windows terminal recovery (#17289)
* coverage report

* rm test coverage

* test(e2e): cover session upgrade and Windows terminal recovery

* fix stub

tests/e2e/persisted-session-production-upgrade.spec.ts
@@ -1,0 +1,171 @@
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
+import type { ElectronApplication, Page } from '@stablyai/playwright-test'
+import { DEFAULT_LOCAL_ORCA_PROFILE_ID } from '../../src/shared/orca-profiles'
+import { PTY_SESSION_ID_SEPARATOR } from '../../src/shared/pty-session-id-format'
+import { test, expect } from './helpers/orca-app'
+import { TEST_REPO_PATH_FILE } from './global-setup'
+import { attachRepoAndOpenTerminal, createRestartSession } from './helpers/orca-restart'
+import { ensureTerminalVisible, waitForSessionReady } from './helpers/store'
+import {
+  discoverActivePtyId,
+  execInTerminal,
+  getTerminalContent,
+  waitForActiveTerminalManager,
+  waitForPaneCount,
+  waitForTerminalOutput
+} from './helpers/terminal'
+
+const FIXTURE_PATH = path.join(
+  process.cwd(),
+  'tests',
+  'e2e',
+  'fixtures',
+  'persisted-sessions',
+  'legacy-workspace-session-daemon-terminal.json'
+)
+// This fixture captures a legacy production schema boundary; the test runs the current build.
+const RESTORED_TITLE = 'Production agent session'
+
+type FixtureSession = {
+  _fixtureProvenance?: unknown
+  activeRepoId: string
+  activeWorktreeId: string
+  tabsByWorktree: Record<string, { ptyId: string; worktreeId: string }[]>
+  terminalLayoutsByTabId: Record<string, { ptyIdsByLeafId?: Record<string, string> }>
+  activeWorktreeIdsOnShutdown?: string[]
+  activeTabIdByWorktree?: Record<string, string>
+}
+
+function installProductionSessionFixture(
+  userDataDir: string,
+  repoId: string,
+  worktreeId: string,
+  ptyId: string
+): void {
+  const profilePath = path.join(
+    userDataDir,
+    'profiles',
+    DEFAULT_LOCAL_ORCA_PROFILE_ID,
+    'orca-data.json'
+  )
+  const profile = JSON.parse(readFileSync(profilePath, 'utf8')) as Record<string, unknown>
+  const fixture = JSON.parse(readFileSync(FIXTURE_PATH, 'utf8')) as FixtureSession
+  const fixtureTabs = fixture.tabsByWorktree.__WORKTREE_ID__
+  const fixtureLayout = fixture.terminalLayoutsByTabId['production-agent-tab']
+  if (!fixtureTabs || !fixtureLayout) {
+    throw new Error('Production session fixture is missing its terminal records')
+  }
+  delete fixture._fixtureProvenance
+  fixture.activeRepoId = repoId
+  fixture.activeWorktreeId = worktreeId
+  fixture.tabsByWorktree = {
+    [worktreeId]: fixtureTabs.map((tab) => ({
+      ...tab,
+      ptyId,
+      worktreeId
+    }))
+  }
+  fixtureLayout.ptyIdsByLeafId = {
+    'pane:production-agent': ptyId
+  }
+  fixture.activeWorktreeIdsOnShutdown = [worktreeId]
+  fixture.activeTabIdByWorktree = { [worktreeId]: 'production-agent-tab' }
+  profile.workspaceSession = fixture
+  writeFileSync(profilePath, `${JSON.stringify(profile, null, 2)}\n`)
+}
+
+async function expectProductionSessionRestored(
+  page: Page,
+  expected: { marker: string; ptyId: string; repoId: string; worktreeId: string }
+): Promise<void> {
+  await waitForSessionReady(page)
+  await ensureTerminalVisible(page)
+  await waitForActiveTerminalManager(page, 30_000)
+  await waitForPaneCount(page, 1, 30_000)
+  await waitForTerminalOutput(page, expected.marker, 30_000)
+
+  await expect(
+    page.locator('[data-testid="sortable-tab"]').filter({ hasText: RESTORED_TITLE })
+  ).toBeVisible({ timeout: 15_000 })
+  await expect(page.locator('[data-testid="sortable-tab"]')).toHaveCount(1)
+  await expect(page.locator('.xterm').first()).toBeVisible()
+  expect(await discoverActivePtyId(page)).toBe(expected.ptyId)
+  expect(await getTerminalContent(page)).toContain(expected.marker)
+  expect(
+    await page.evaluate(() => {
+      const state = window.__store?.getState()
+      return {
+        activeRepoId: state?.activeRepoId,
+        activeWorktreeId: state?.activeWorktreeId,
+        tabIds: state?.activeWorktreeId
+          ? state.tabsByWorktree[state.activeWorktreeId]?.map((tab) => tab.id)
+          : []
+      }
+    })
+  ).toEqual({
+    activeRepoId: expected.repoId,
+    activeWorktreeId: expected.worktreeId,
+    tabIds: ['production-agent-tab']
+  })
+}
+
+test('upgrades a legacy daemon session and keeps it stable after relaunch', async (// oxlint-disable-next-line no-empty-pattern -- this upgrade test owns its Electron launches.
+{}, testInfo) => {
+  test.setTimeout(300_000)
+  const repoPath = existsSync(TEST_REPO_PATH_FILE)
+    ? readFileSync(TEST_REPO_PATH_FILE, 'utf8').trim()
+    : ''
+  test.skip(!repoPath || !existsSync(repoPath), 'Seeded E2E repository is unavailable')
+
+  const session = createRestartSession(testInfo)
+  let oldApp: ElectronApplication | null = null
+  let currentApp: ElectronApplication | null = null
+  let stableApp: ElectronApplication | null = null
+
+  try {
+    const oldLaunch = await session.launch()
+    oldApp = oldLaunch.app
+    const worktreeId = await attachRepoAndOpenTerminal(oldLaunch.page, repoPath)
+    await waitForSessionReady(oldLaunch.page)
+    await ensureTerminalVisible(oldLaunch.page)
+    await waitForActiveTerminalManager(oldLaunch.page, 30_000)
+    await waitForPaneCount(oldLaunch.page, 1, 30_000)
+
+    const ptyId = await discoverActivePtyId(oldLaunch.page)
+    expect(ptyId).toContain(PTY_SESSION_ID_SEPARATOR)
+    const marker = `PRODUCTION_UPGRADE_${Date.now()}`
+    await execInTerminal(oldLaunch.page, ptyId, `echo ${marker}`)
+    await waitForTerminalOutput(oldLaunch.page, marker)
+    const repoId = await oldLaunch.page.evaluate(
+      (repoPath) => window.__store?.getState().repos.find((repo) => repo.path === repoPath)?.id,
+      repoPath
+    )
+    if (!repoId) {
+      throw new Error('Active repository was unavailable before fixture installation')
+    }
+
+    await session.close(oldApp)
+    oldApp = null
+    installProductionSessionFixture(session.userDataDir, repoId, worktreeId, ptyId)
+
+    const currentLaunch = await session.launch()
+    currentApp = currentLaunch.app
+    const expected = { marker, ptyId, repoId, worktreeId }
+    await expectProductionSessionRestored(currentLaunch.page, expected)
+
+    await session.close(currentApp)
+    currentApp = null
+
+    const stableLaunch = await session.launch()
+    stableApp = stableLaunch.app
+    await expectProductionSessionRestored(stableLaunch.page, expected)
+  } finally {
+    for (const app of [stableApp, currentApp, oldApp]) {
+      if (app) {
+        await session.close(app).catch(() => {})
+      }
+    }
+    await session.dispose()
+  }
+})

tests/e2e/terminal-windows-conpty-keyboard-reset.spec.ts
@@ -1,0 +1,102 @@
+import type { Page } from '@stablyai/playwright-test'
+import { expect, test } from './helpers/orca-app'
+import {
+  configureGoldenStubAgent,
+  getGoldenStubAgentLaunchEnv,
+  GOLDEN_STUB_EXIT_MARKER,
+  launchGoldenStubAgentFromNewTab
+} from './helpers/golden-stub-agent'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  focusActiveTerminalInput,
+  waitForActivePanePtyId,
+  waitForTerminalOutput
+} from './helpers/terminal'
+import {
+  clearTerminalPtyWriteLog,
+  installTerminalPtyWriteSpy,
+  readTerminalPtyWriteEntries
+} from './helpers/terminal-pty-write-spy'
+
+test.use({ launchEnv: getGoldenStubAgentLaunchEnv() })
+test.skip(process.platform !== 'win32', 'A real Windows ConPTY is required')
+
+async function getKittyKeyboardFlags(page: Page): Promise<number | null> {
+  return page.evaluate(() => {
+    const state = window.__store?.getState()
+    const worktreeId = state?.activeWorktreeId
+    const tabId =
+      state?.activeTabType === 'terminal'
+        ? state.activeTabId
+        : worktreeId
+          ? (state?.activeTabIdByWorktree?.[worktreeId] ?? null)
+          : null
+    const pane = tabId ? window.__paneManagers?.get(tabId)?.getActivePane?.() : null
+    const terminal = pane?.terminal as
+      | {
+          core?: { coreService?: { kittyKeyboard?: { flags?: number } } }
+          _core?: { coreService?: { kittyKeyboard?: { flags?: number } } }
+        }
+      | undefined
+    return (
+      terminal?.core?.coreService?.kittyKeyboard?.flags ??
+      terminal?._core?.coreService?.kittyKeyboard?.flags ??
+      null
+    )
+  })
+}
+
+test('resets standard keyboard bytes after a protocol-mode agent exits on ConPTY', async ({
+  electronApp,
+  orcaPage
+}) => {
+  await installTerminalPtyWriteSpy(electronApp)
+  await waitForSessionReady(orcaPage)
+  await waitForActiveWorktree(orcaPage)
+  await ensureTerminalVisible(orcaPage)
+  await configureGoldenStubAgent(orcaPage, { agentArgs: '--keyboard-protocol' })
+  await launchGoldenStubAgentFromNewTab(orcaPage)
+
+  const ptyId = await waitForActivePanePtyId(orcaPage)
+  await expect.poll(() => getKittyKeyboardFlags(orcaPage), { timeout: 10_000 }).toBe(1)
+
+  await clearTerminalPtyWriteLog(electronApp)
+  await orcaPage.keyboard.type('exit')
+  await orcaPage.keyboard.press('Enter')
+  await waitForTerminalOutput(orcaPage, GOLDEN_STUB_EXIT_MARKER, 15_000)
+  const protocolWrites = (await readTerminalPtyWriteEntries(electronApp))
+    .filter((entry) => entry.id === ptyId)
+    .map((entry) => entry.data)
+    .join('')
+  expect(protocolWrites.includes('\x1b[13u') || protocolWrites.includes('\x1b[13;1u')).toBe(true)
+  await expect.poll(() => getKittyKeyboardFlags(orcaPage), { timeout: 10_000 }).toBe(0)
+
+  await clearTerminalPtyWriteLog(electronApp)
+  await focusActiveTerminalInput(orcaPage)
+  await orcaPage.keyboard.type("Write-Output ('CONPTY_KEYBOARD_' + '")
+  await orcaPage.evaluate((text) => window.api.ui.writeClipboardText(text), 'REET_')
+  await orcaPage.keyboard.press('Control+V')
+  await orcaPage.keyboard.press('ArrowLeft')
+  await orcaPage.keyboard.press('ArrowLeft')
+  await orcaPage.keyboard.press('ArrowLeft')
+  await orcaPage.keyboard.type('S')
+  await orcaPage.keyboard.press('ArrowRight')
+  await orcaPage.keyboard.press('ArrowRight')
+  await orcaPage.keyboard.press('ArrowRight')
+  await orcaPage.keyboard.type('EXECUTEX')
+  await orcaPage.keyboard.press('Backspace')
+  await orcaPage.keyboard.type("D')")
+  await orcaPage.keyboard.press('Enter')
+  await waitForTerminalOutput(orcaPage, 'CONPTY_KEYBOARD_RESET_EXECUTED', 15_000)
+
+  const shellWrites = (await readTerminalPtyWriteEntries(electronApp))
+    .filter((entry) => entry.id === ptyId)
+    .map((entry) => entry.data)
+  const joinedShellWrites = shellWrites.join('')
+  expect(joinedShellWrites).toContain('REET_')
+  expect(shellWrites.filter((data) => data === '\x1b[D')).toHaveLength(3)
+  expect(shellWrites.filter((data) => data === '\x1b[C')).toHaveLength(3)
+  expect(shellWrites).toContain('\x7f')
+  expect(shellWrites).toContain('\r')
+  expect(joinedShellWrites).not.toMatch(new RegExp(`${String.fromCharCode(27)}\\[\\d+(?:;\\d+)*u`))
+})

paidaxingyo666/Manta

(cherry picked from commit 73ff0031478380fdd59b3b7ff508fa1e5204d697)

tests/e2e/persisted-session-production-upgrade.spec.ts
@@ -1,0 +1,171 @@
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
+import type { ElectronApplication, Page } from '@stablyai/playwright-test'
+import { DEFAULT_LOCAL_MANTA_PROFILE_ID } from '../../src/shared/manta-profiles'
+import { PTY_SESSION_ID_SEPARATOR } from '../../src/shared/pty-session-id-format'
+import { test, expect } from './helpers/manta-app'
+import { TEST_REPO_PATH_FILE } from './global-setup'
+import { attachRepoAndOpenTerminal, createRestartSession } from './helpers/manta-restart'
+import { ensureTerminalVisible, waitForSessionReady } from './helpers/store'
+import {
+  discoverActivePtyId,
+  execInTerminal,
+  getTerminalContent,
+  waitForActiveTerminalManager,
+  waitForPaneCount,
+  waitForTerminalOutput
+} from './helpers/terminal'
+
+const FIXTURE_PATH = path.join(
+  process.cwd(),
+  'tests',
+  'e2e',
+  'fixtures',
+  'persisted-sessions',
+  'legacy-workspace-session-daemon-terminal.json'
+)
+// This fixture captures a legacy production schema boundary; the test runs the current build.
+const RESTORED_TITLE = 'Production agent session'
+
+type FixtureSession = {
+  _fixtureProvenance?: unknown
+  activeRepoId: string
+  activeWorktreeId: string
+  tabsByWorktree: Record<string, { ptyId: string; worktreeId: string }[]>
+  terminalLayoutsByTabId: Record<string, { ptyIdsByLeafId?: Record<string, string> }>
+  activeWorktreeIdsOnShutdown?: string[]
+  activeTabIdByWorktree?: Record<string, string>
+}
+
+function installProductionSessionFixture(
+  userDataDir: string,
+  repoId: string,
+  worktreeId: string,
+  ptyId: string
+): void {
+  const profilePath = path.join(
+    userDataDir,
+    'profiles',
+    DEFAULT_LOCAL_MANTA_PROFILE_ID,
+    'manta-data.json'
+  )
+  const profile = JSON.parse(readFileSync(profilePath, 'utf8')) as Record<string, unknown>
+  const fixture = JSON.parse(readFileSync(FIXTURE_PATH, 'utf8')) as FixtureSession
+  const fixtureTabs = fixture.tabsByWorktree.__WORKTREE_ID__
+  const fixtureLayout = fixture.terminalLayoutsByTabId['production-agent-tab']
+  if (!fixtureTabs || !fixtureLayout) {
+    throw new Error('Production session fixture is missing its terminal records')
+  }
+  delete fixture._fixtureProvenance
+  fixture.activeRepoId = repoId
+  fixture.activeWorktreeId = worktreeId
+  fixture.tabsByWorktree = {
+    [worktreeId]: fixtureTabs.map((tab) => ({
+      ...tab,
+      ptyId,
+      worktreeId
+    }))
+  }
+  fixtureLayout.ptyIdsByLeafId = {
+    'pane:production-agent': ptyId
+  }
+  fixture.activeWorktreeIdsOnShutdown = [worktreeId]
+  fixture.activeTabIdByWorktree = { [worktreeId]: 'production-agent-tab' }
+  profile.workspaceSession = fixture
+  writeFileSync(profilePath, `${JSON.stringify(profile, null, 2)}\n`)
+}
+
+async function expectProductionSessionRestored(
+  page: Page,
+  expected: { marker: string; ptyId: string; repoId: string; worktreeId: string }
+): Promise<void> {
+  await waitForSessionReady(page)
+  await ensureTerminalVisible(page)
+  await waitForActiveTerminalManager(page, 30_000)
+  await waitForPaneCount(page, 1, 30_000)
+  await waitForTerminalOutput(page, expected.marker, 30_000)
+
+  await expect(
+    page.locator('[data-testid="sortable-tab"]').filter({ hasText: RESTORED_TITLE })
+  ).toBeVisible({ timeout: 15_000 })
+  await expect(page.locator('[data-testid="sortable-tab"]')).toHaveCount(1)
+  await expect(page.locator('.xterm').first()).toBeVisible()
+  expect(await discoverActivePtyId(page)).toBe(expected.ptyId)
+  expect(await getTerminalContent(page)).toContain(expected.marker)
+  expect(
+    await page.evaluate(() => {
+      const state = window.__store?.getState()
+      return {
+        activeRepoId: state?.activeRepoId,
+        activeWorktreeId: state?.activeWorktreeId,
+        tabIds: state?.activeWorktreeId
+          ? state.tabsByWorktree[state.activeWorktreeId]?.map((tab) => tab.id)
+          : []
+      }
+    })
+  ).toEqual({
+    activeRepoId: expected.repoId,
+    activeWorktreeId: expected.worktreeId,
+    tabIds: ['production-agent-tab']
+  })
+}
+
+test('upgrades a legacy daemon session and keeps it stable after relaunch', async (// oxlint-disable-next-line no-empty-pattern -- this upgrade test owns its Electron launches.
+{}, testInfo) => {
+  test.setTimeout(300_000)
+  const repoPath = existsSync(TEST_REPO_PATH_FILE)
+    ? readFileSync(TEST_REPO_PATH_FILE, 'utf8').trim()
+    : ''
+  test.skip(!repoPath || !existsSync(repoPath), 'Seeded E2E repository is unavailable')
+
+  const session = createRestartSession(testInfo)
+  let oldApp: ElectronApplication | null = null
+  let currentApp: ElectronApplication | null = null
+  let stableApp: ElectronApplication | null = null
+
+  try {
+    const oldLaunch = await session.launch()
+    oldApp = oldLaunch.app
+    const worktreeId = await attachRepoAndOpenTerminal(oldLaunch.page, repoPath)
+    await waitForSessionReady(oldLaunch.page)
+    await ensureTerminalVisible(oldLaunch.page)
+    await waitForActiveTerminalManager(oldLaunch.page, 30_000)
+    await waitForPaneCount(oldLaunch.page, 1, 30_000)
+
+    const ptyId = await discoverActivePtyId(oldLaunch.page)
+    expect(ptyId).toContain(PTY_SESSION_ID_SEPARATOR)
+    const marker = `PRODUCTION_UPGRADE_${Date.now()}`
+    await execInTerminal(oldLaunch.page, ptyId, `echo ${marker}`)
+    await waitForTerminalOutput(oldLaunch.page, marker)
+    const repoId = await oldLaunch.page.evaluate(
+      (repoPath) => window.__store?.getState().repos.find((repo) => repo.path === repoPath)?.id,
+      repoPath
+    )
+    if (!repoId) {
+      throw new Error('Active repository was unavailable before fixture installation')
+    }
+
+    await session.close(oldApp)
+    oldApp = null
+    installProductionSessionFixture(session.userDataDir, repoId, worktreeId, ptyId)
+
+    const currentLaunch = await session.launch()
+    currentApp = currentLaunch.app
+    const expected = { marker, ptyId, repoId, worktreeId }
+    await expectProductionSessionRestored(currentLaunch.page, expected)
+
+    await session.close(currentApp)
+    currentApp = null
+
+    const stableLaunch = await session.launch()
+    stableApp = stableLaunch.app
+    await expectProductionSessionRestored(stableLaunch.page, expected)
+  } finally {
+    for (const app of [stableApp, currentApp, oldApp]) {
+      if (app) {
+        await session.close(app).catch(() => {})
+      }
+    }
+    await session.dispose()
+  }
+})

tests/e2e/terminal-windows-conpty-keyboard-reset.spec.ts
@@ -1,0 +1,102 @@
+import type { Page } from '@stablyai/playwright-test'
+import { expect, test } from './helpers/manta-app'
+import {
+  configureGoldenStubAgent,
+  getGoldenStubAgentLaunchEnv,
+  GOLDEN_STUB_EXIT_MARKER,
+  launchGoldenStubAgentFromNewTab
+} from './helpers/golden-stub-agent'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  focusActiveTerminalInput,
+  waitForActivePanePtyId,
+  waitForTerminalOutput
+} from './helpers/terminal'
+import {
+  clearTerminalPtyWriteLog,
+  installTerminalPtyWriteSpy,
+  readTerminalPtyWriteEntries
+} from './helpers/terminal-pty-write-spy'
+
+test.use({ launchEnv: getGoldenStubAgentLaunchEnv() })
+test.skip(process.platform !== 'win32', 'A real Windows ConPTY is required')
+
+async function getKittyKeyboardFlags(page: Page): Promise<number | null> {
+  return page.evaluate(() => {
+    const state = window.__store?.getState()
+    const worktreeId = state?.activeWorktreeId
+    const tabId =
+      state?.activeTabType === 'terminal'
+        ? state.activeTabId
+        : worktreeId
+          ? (state?.activeTabIdByWorktree?.[worktreeId] ?? null)
+          : null
+    const pane = tabId ? window.__paneManagers?.get(tabId)?.getActivePane?.() : null
+    const terminal = pane?.terminal as
+      | {
+          core?: { coreService?: { kittyKeyboard?: { flags?: number } } }
+          _core?: { coreService?: { kittyKeyboard?: { flags?: number } } }
+        }
+      | undefined
+    return (
+      terminal?.core?.coreService?.kittyKeyboard?.flags ??
+      terminal?._core?.coreService?.kittyKeyboard?.flags ??
+      null
+    )
+  })
+}
+
+test('resets standard keyboard bytes after a protocol-mode agent exits on ConPTY', async ({
+  electronApp,
+  mantaPage
+}) => {
+  await installTerminalPtyWriteSpy(electronApp)
+  await waitForSessionReady(mantaPage)
+  await waitForActiveWorktree(mantaPage)
+  await ensureTerminalVisible(mantaPage)
+  await configureGoldenStubAgent(mantaPage, { agentArgs: '--keyboard-protocol' })
+  await launchGoldenStubAgentFromNewTab(mantaPage)
+
+  const ptyId = await waitForActivePanePtyId(mantaPage)
+  await expect.poll(() => getKittyKeyboardFlags(mantaPage), { timeout: 10_000 }).toBe(1)
+
+  await clearTerminalPtyWriteLog(electronApp)
+  await mantaPage.keyboard.type('exit')
+  await mantaPage.keyboard.press('Enter')
+  await waitForTerminalOutput(mantaPage, GOLDEN_STUB_EXIT_MARKER, 15_000)
+  const protocolWrites = (await readTerminalPtyWriteEntries(electronApp))
+    .filter((entry) => entry.id === ptyId)
+    .map((entry) => entry.data)
+    .join('')
+  expect(protocolWrites.includes('\x1b[13u') || protocolWrites.includes('\x1b[13;1u')).toBe(true)
+  await expect.poll(() => getKittyKeyboardFlags(mantaPage), { timeout: 10_000 }).toBe(0)
+
+  await clearTerminalPtyWriteLog(electronApp)
+  await focusActiveTerminalInput(mantaPage)
+  await mantaPage.keyboard.type("Write-Output ('CONPTY_KEYBOARD_' + '")
+  await mantaPage.evaluate((text) => window.api.ui.writeClipboardText(text), 'REET_')
+  await mantaPage.keyboard.press('Control+V')
+  await mantaPage.keyboard.press('ArrowLeft')
+  await mantaPage.keyboard.press('ArrowLeft')
+  await mantaPage.keyboard.press('ArrowLeft')
+  await mantaPage.keyboard.type('S')
+  await mantaPage.keyboard.press('ArrowRight')
+  await mantaPage.keyboard.press('ArrowRight')
+  await mantaPage.keyboard.press('ArrowRight')
+  await mantaPage.keyboard.type('EXECUTEX')
+  await mantaPage.keyboard.press('Backspace')
+  await mantaPage.keyboard.type("D')")
+  await mantaPage.keyboard.press('Enter')
+  await waitForTerminalOutput(mantaPage, 'CONPTY_KEYBOARD_RESET_EXECUTED', 15_000)
+
+  const shellWrites = (await readTerminalPtyWriteEntries(electronApp))
+    .filter((entry) => entry.id === ptyId)
+    .map((entry) => entry.data)
+  const joinedShellWrites = shellWrites.join('')
+  expect(joinedShellWrites).toContain('REET_')
+  expect(shellWrites.filter((data) => data === '\x1b[D')).toHaveLength(3)
+  expect(shellWrites.filter((data) => data === '\x1b[C')).toHaveLength(3)
+  expect(shellWrites).toContain('\x7f')
+  expect(shellWrites).toContain('\r')
+  expect(joinedShellWrites).not.toMatch(new RegExp(`${String.fromCharCode(27)}\\[\\d+(?:;\\d+)*u`))
+})