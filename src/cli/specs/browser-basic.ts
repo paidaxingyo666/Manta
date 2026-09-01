2022zhang125/orca-yunxiao

Fix opening HTTPS URLs from headless runtimes (#17467)
* fix(runtime): open headless browser URLs on paired client

* fix(pty): tolerate runtimes without browser relay probe

* fix(browser): require automation-capable client host

* fix(browser): map client URL opener in sidecar

---------

Co-authored-by: Merge Sim <sim@local>

src/cli/specs/browser-basic.ts
@@ -3,6 +3,12 @@ import { GLOBAL_FLAGS } from '../args'
 
 export const BROWSER_BASIC_COMMAND_SPECS: CommandSpec[] = [
   {
+    path: ['open-url'],
+    summary: 'Open a URL on the paired client that hosts this terminal',
+    usage: 'orca open-url --url <url> [--worktree <selector>] [--json]',
+    allowedFlags: [...GLOBAL_FLAGS, 'url', 'worktree']
+  },
+  {
     path: ['snapshot'],
     summary: 'Capture an accessibility snapshot of the active browser tab',
     usage: 'orca snapshot [--worktree <selector>] [--json]',

src/main/ipc/pty-spawn-env-terminal-basics.test.ts
@@ -58,6 +58,75 @@ describe('registerPtyHandlers', () => {
   const { handlers, mainWindow, spawnAndGetEnv, withBundledCli } = setupPtyIpcSuite()
 
   describe('spawn environment', () => {
+    it('routes headless browser launches through the owning Orca workspace', () => {
+      const inheritedBrowser = process.env.BROWSER
+      delete process.env.BROWSER
+      try {
+        const env = buildPtyHostEnv(
+          'pty-headless',
+          {},
+          {
+            isPackaged: true,
+            userDataPath: '/tmp/orca-user-data',
+            selectedCodexHomePath: null,
+            agentStatusHooksEnabled: false,
+            routeBrowserOpensToClient: true
+          }
+        )
+
+        expect(env.BROWSER).toBe('orca open-url --url %s')
+      } finally {
+        if (inheritedBrowser === undefined) {
+          delete process.env.BROWSER
+        } else {
+          process.env.BROWSER = inheritedBrowser
+        }
+      }
+    })
+
+    it('preserves an explicit browser command on headless runtimes', () => {
+      const env = buildPtyHostEnv(
+        'pty-custom-browser',
+        { BROWSER: 'custom-browser %s' },
+        {
+          isPackaged: true,
+          userDataPath: '/tmp/orca-user-data',
+          selectedCodexHomePath: null,
+          agentStatusHooksEnabled: false,
+          routeBrowserOpensToClient: true
+        }
+      )
+
+      expect(env.BROWSER).toBe('custom-browser %s')
+    })
+
+    it('uses the registered WSL CLI name for headless browser launches', () => {
+      const inheritedBrowser = process.env.BROWSER
+      delete process.env.BROWSER
+      try {
+        const env = buildPtyHostEnv(
+          'pty-headless-wsl',
+          {},
+          {
+            isPackaged: true,
+            userDataPath: '/tmp/orca-user-data',
+            selectedCodexHomePath: null,
+            isWsl: true,
+            agentStatusHooksEnabled: false,
+            routeBrowserOpensToClient: true
+          }
+        )
+
+        expect(env.BROWSER).toBe('orca-ide open-url --url %s')
+      } finally {
+        if (inheritedBrowser === undefined) {
+          delete process.env.BROWSER
+        } else {
+          process.env.BROWSER = inheritedBrowser
+        }
+      }
+    })
+
     it('passes the PTY-resolved Codex home to the WSL relay lane', () => {
       const runtimeHome =
         '\\\\wsl.localhost\\Ubuntu\\home\\jin\\.local\\share\\orca\\codex-runtime-home\\home'

src/main/ipc/pty/host-env/assembly.ts
@@ -264,6 +264,15 @@ export function buildPtyHostEnv(
       : bundledCliBin
   }
 
+  if (
+    opts.routeBrowserOpensToClient === true &&
+    baseEnv.BROWSER === undefined &&
+    process.env.BROWSER === undefined
+  ) {
+    const cliCommand = opts.isWsl ? (opts.isPackaged ? 'orca-ide' : 'orca-dev') : 'orca'
+    baseEnv.BROWSER = `${cliCommand} open-url --url %s`
+  }
+
   // Why: must run after the prepends above — they re-read PATH from the unscrubbed
   // process.env when baseEnv carries none, which is the daemon path's normal shape.
   stripLegacyTerminalShimEnv(baseEnv, process.platform)

src/main/runtime/rpc/methods/browser.test.ts
@@ -48,6 +48,26 @@ describe('browser RPC methods', () => {
     })
   })
 
+  it('routes host browser-open requests through the dedicated client opener', async () => {
+    const runtime = {
+      getRuntimeId: () => 'test-runtime',
+      browserOpenUrlOnClient: vi.fn().mockResolvedValue({ browserPageId: 'page-local' })
+    } as unknown as OrcaRuntimeService
+    const dispatcher = new RpcDispatcher({ runtime, methods: BROWSER_CORE_METHODS })
+
+    await dispatcher.dispatch(
+      makeRequest('browser.openUrl', {
+        url: 'https://example.com/login',
+        worktree: 'id:wt-1'
+      })
+    )
+
+    expect(runtime.browserOpenUrlOnClient).toHaveBeenCalledWith({
+      url: 'https://example.com/login',
+      worktree: 'id:wt-1'
+    })
+  })
+
   it('validates profile user-agent modes', () => {
     expect(
       ProfileCreate.safeParse({ label: 'Google', scope: 'isolated', userAgentMode: 'native' })

paidaxingyo666/Manta

src/cli/specs/browser-basic.ts
@@ -3,6 +3,12 @@ import { GLOBAL_FLAGS } from '../args'
 
 export const BROWSER_BASIC_COMMAND_SPECS: CommandSpec[] = [
   {
+    path: ['open-url'],
+    summary: 'Open a URL on the paired client that hosts this terminal',
+    usage: 'manta open-url --url <url> [--worktree <selector>] [--json]',
+    allowedFlags: [...GLOBAL_FLAGS, 'url', 'worktree']
+  },
+  {
     path: ['snapshot'],
     summary: 'Capture an accessibility snapshot of the active browser tab',
     usage: 'manta snapshot [--worktree <selector>] [--json]',

src/main/ipc/pty-spawn-env-terminal-basics.test.ts
@@ -58,6 +58,75 @@ describe('registerPtyHandlers', () => {
   const { handlers, mainWindow, spawnAndGetEnv, withBundledCli } = setupPtyIpcSuite()
 
   describe('spawn environment', () => {
+    it('routes headless browser launches through the owning Manta workspace', () => {
+      const inheritedBrowser = process.env.BROWSER
+      delete process.env.BROWSER
+      try {
+        const env = buildPtyHostEnv(
+          'pty-headless',
+          {},
+          {
+            isPackaged: true,
+            userDataPath: '/tmp/manta-user-data',
+            selectedCodexHomePath: null,
+            agentStatusHooksEnabled: false,
+            routeBrowserOpensToClient: true
+          }
+        )
+
+        expect(env.BROWSER).toBe('manta open-url --url %s')
+      } finally {
+        if (inheritedBrowser === undefined) {
+          delete process.env.BROWSER
+        } else {
+          process.env.BROWSER = inheritedBrowser
+        }
+      }
+    })
+
+    it('preserves an explicit browser command on headless runtimes', () => {
+      const env = buildPtyHostEnv(
+        'pty-custom-browser',
+        { BROWSER: 'custom-browser %s' },
+        {
+          isPackaged: true,
+          userDataPath: '/tmp/manta-user-data',
+          selectedCodexHomePath: null,
+          agentStatusHooksEnabled: false,
+          routeBrowserOpensToClient: true
+        }
+      )
+
+      expect(env.BROWSER).toBe('custom-browser %s')
+    })
+
+    it('uses the registered WSL CLI name for headless browser launches', () => {
+      const inheritedBrowser = process.env.BROWSER
+      delete process.env.BROWSER
+      try {
+        const env = buildPtyHostEnv(
+          'pty-headless-wsl',
+          {},
+          {
+            isPackaged: true,
+            userDataPath: '/tmp/manta-user-data',
+            selectedCodexHomePath: null,
+            isWsl: true,
+            agentStatusHooksEnabled: false,
+            routeBrowserOpensToClient: true
+          }
+        )
+
+        expect(env.BROWSER).toBe('manta-ide open-url --url %s')
+      } finally {
+        if (inheritedBrowser === undefined) {
+          delete process.env.BROWSER
+        } else {
+          process.env.BROWSER = inheritedBrowser
+        }
+      }
+    })
+
     it('passes the PTY-resolved Codex home to the WSL relay lane', () => {
       const runtimeHome =
         '\\\\wsl.localhost\\Ubuntu\\home\\jin\\.local\\share\\manta\\codex-runtime-home\\home'

src/main/ipc/pty/host-env/assembly.ts
@@ -264,6 +264,15 @@ export function buildPtyHostEnv(
       : bundledCliBin
   }
 
+  if (
+    opts.routeBrowserOpensToClient === true &&
+    baseEnv.BROWSER === undefined &&
+    process.env.BROWSER === undefined
+  ) {
+    const cliCommand = opts.isWsl ? (opts.isPackaged ? 'manta-ide' : 'manta-dev') : 'manta'
+    baseEnv.BROWSER = `${cliCommand} open-url --url %s`
+  }
+
   // Why: must run after the prepends above — they re-read PATH from the unscrubbed
   // process.env when baseEnv carries none, which is the daemon path's normal shape.
   stripLegacyTerminalShimEnv(baseEnv, process.platform)

src/main/runtime/rpc/methods/browser.test.ts
@@ -48,6 +48,26 @@ describe('browser RPC methods', () => {
     })
   })
 
+  it('routes host browser-open requests through the dedicated client opener', async () => {
+    const runtime = {
+      getRuntimeId: () => 'test-runtime',
+      browserOpenUrlOnClient: vi.fn().mockResolvedValue({ browserPageId: 'page-local' })
+    } as unknown as MantaRuntimeService
+    const dispatcher = new RpcDispatcher({ runtime, methods: BROWSER_CORE_METHODS })
+
+    await dispatcher.dispatch(
+      makeRequest('browser.openUrl', {
+        url: 'https://example.com/login',
+        worktree: 'id:wt-1'
+      })
+    )
+
+    expect(runtime.browserOpenUrlOnClient).toHaveBeenCalledWith({
+      url: 'https://example.com/login',
+      worktree: 'id:wt-1'
+    })
+  })
+
   it('validates profile user-agent modes', () => {
     expect(
       ProfileCreate.safeParse({ label: 'Google', scope: 'isolated', userAgentMode: 'native' })