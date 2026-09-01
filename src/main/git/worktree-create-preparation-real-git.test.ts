2022zhang125/orca-yunxiao

perf(worktree): prepare checkouts while the composer is open
Squashed merge of PR #17290.

src/main/git/worktree-create-preparation-real-git.test.ts
@@ -1,0 +1,127 @@
+import { execFileSync } from 'node:child_process'
+import { mkdir, mkdtemp, readFile, realpath, rm, writeFile } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { afterEach, describe, expect, it } from 'vitest'
+import {
+  createWorktreePreparationLockReason,
+  isWorktreeCreatePreparation,
+  WORKTREE_CREATE_PREPARATION_DIRECTORY
+} from '../../shared/worktree/create-preparation'
+import { listWorktrees } from './worktree'
+import {
+  discardPreparedWorktree,
+  finalizePreparedWorktree,
+  prepareWorktreeCreateCheckout
+} from './worktree-create-preparation'
+import { areWorktreePathsEqual } from './worktree-path-comparison'
+
+const tempRoots: string[] = []
+
+function git(cwd: string, args: string[]): string {
+  return execFileSync('git', args, {
+    cwd,
+    encoding: 'utf8',
+    stdio: ['pipe', 'pipe', 'pipe']
+  }).trim()
+}
+
+async function createRepo(): Promise<{ repoPath: string; root: string }> {
+  const root = await mkdtemp(join(tmpdir(), 'orca-prepared-worktree-'))
+  tempRoots.push(root)
+  const repoPath = join(root, 'repo')
+  execFileSync('git', ['init', '--quiet', repoPath])
+  git(repoPath, ['symbolic-ref', 'HEAD', 'refs/heads/main'])
+  git(repoPath, ['config', 'user.email', 'test@example.com'])
+  git(repoPath, ['config', 'user.name', 'Test User'])
+  git(repoPath, ['config', 'core.autocrlf', 'false'])
+  await writeFile(join(repoPath, 'version.txt'), 'one\n')
+  git(repoPath, ['add', 'version.txt'])
+  git(repoPath, ['commit', '--quiet', '-m', 'initial'])
+  return { repoPath, root }
+}
+
+afterEach(async () => {
+  await Promise.all(tempRoots.splice(0).map((root) => rm(root, { recursive: true, force: true })))
+})
+
+describe('prepared worktree creation with real Git', () => {
+  it('cleans up when the create signal is canceled', async () => {
+    const { repoPath, root } = await createRepo()
+    const preparationRoot = join(root, WORKTREE_CREATE_PREPARATION_DIRECTORY)
+    const preparedPath = join(preparationRoot, `${process.pid}-canceled`)
+    await mkdir(preparationRoot, { recursive: true })
+
+    await prepareWorktreeCreateCheckout(
+      repoPath,
+      preparedPath,
+      'main',
+      createWorktreePreparationLockReason('canceled-test')
+    )
+
+    const controller = new AbortController()
+    controller.abort()
+    await expect(
+      discardPreparedWorktree(repoPath, preparedPath, { signal: controller.signal })
+    ).resolves.toBeUndefined()
+
+    expect(await listWorktrees(repoPath, { includeCreatePreparations: true })).toHaveLength(1)
+  })
+
+  it('hides the preparation, retargets an advanced base, and attaches the final branch', async () => {
+    const { repoPath, root } = await createRepo()
+    const preparationRoot = join(root, WORKTREE_CREATE_PREPARATION_DIRECTORY)
+    const preparedPath = join(preparationRoot, `${process.pid}-test`)
+    const finalPath = join(root, 'final-worktree')
+    await mkdir(preparationRoot, { recursive: true })
+
+    await prepareWorktreeCreateCheckout(
+      repoPath,
+      preparedPath,
+      'main',
+      createWorktreePreparationLockReason('real-git-test')
+    )
+
+    const visibleBeforeSubmit = await listWorktrees(repoPath)
+    const allBeforeSubmit = await listWorktrees(repoPath, { includeCreatePreparations: true })
+    expect(visibleBeforeSubmit).toHaveLength(1)
+    expect(allBeforeSubmit).toHaveLength(2)
+    expect(allBeforeSubmit.find(isWorktreeCreatePreparation)).toMatchObject({
+      locked: true,
+      lockReason: expect.stringContaining('orca-create-preparation:v1:')
+    })
+
+    await writeFile(join(repoPath, 'version.txt'), 'two\n')
+    git(repoPath, ['add', 'version.txt'])
+    git(repoPath, ['commit', '--quiet', '-m', 'advance base'])
+    const latestHead = git(repoPath, ['rev-parse', 'HEAD'])
+
+    await finalizePreparedWorktree(
+      repoPath,
+      preparedPath,
+      finalPath,
+      'feature/prepared',
+      'main',
+      false
+    )
+
+    expect(git(finalPath, ['rev-parse', 'HEAD'])).toBe(latestHead)
+    expect(git(finalPath, ['branch', '--show-current'])).toBe('feature/prepared')
+    expect((await readFile(join(finalPath, 'version.txt'), 'utf8')).replaceAll('\r\n', '\n')).toBe(
+      'two\n'
+    )
+    expect(git(finalPath, ['config', '--get', 'branch.feature/prepared.base'])).toBe(
+      'refs/heads/main'
+    )
+    expect(git(finalPath, ['config', '--get', 'push.autoSetupRemote'])).toBe('true')
+    const listedWorktrees = await listWorktrees(repoPath)
+    const resolvedFinalPath = await realpath(finalPath)
+    expect(
+      listedWorktrees.some((worktree) => areWorktreePathsEqual(worktree.path, resolvedFinalPath))
+    ).toBe(true)
+    expect(
+      listedWorktrees.find((worktree) => areWorktreePathsEqual(worktree.path, resolvedFinalPath))
+        ?.locked
+    ).not.toBe(true)
+  })
+})

src/shared/git-binary-compatibility.test.ts
@@ -151,6 +151,36 @@ describeBinaryCompatibility('real Git binary compatibility', () => {
     await rm(join(repoPath, 'deferred-trash'), { recursive: true, force: true })
   })
 
+  it('supports prepared worktree creation and finalization', async () => {
+    await runGit(['worktree', 'add', '--detach', '--no-checkout', 'compat-prepared', 'HEAD'])
+    await runGit(['-C', 'compat-prepared', 'reset', '--hard', 'HEAD'])
+    await runGit([
+      'worktree',
+      'lock',
+      '--reason',
+      'orca-create-preparation:v1:compat',
+      'compat-prepared'
+    ])
+    // Why: `-f -f` moves a locked preparation while preserving its lock reason (Git >=2.25).
+    await runGit(['worktree', 'move', '-f', '-f', 'compat-prepared', 'compat-final'])
+    await runGit([
+      '-C',
+      'compat-final',
+      'checkout',
+      '--no-track',
+      '-b',
+      'compat-prepared-final',
+      'HEAD'
+    ])
+
+    await expect(runGit(['-C', 'compat-final', 'branch', '--show-current'])).resolves.toMatchObject(
+      { stdout: 'compat-prepared-final\n' }
+    )
+    await runGit(['worktree', 'unlock', 'compat-final'])
+    await runGit(['worktree', 'remove', '--force', 'compat-final'])
+    await runGit(['branch', '-D', 'compat-prepared-final'])
+  })
+
   it('recognizes ref and merge-tree compatibility boundaries', async () => {
     const fetchHeadPath = join(repoPath, '.git', 'FETCH_HEAD')
     await writeFile(fetchHeadPath, 'sentinel\n')

src/shared/worktree/create-preparation.ts
@@ -1,0 +1,42 @@
+export const WORKTREE_CREATE_PREPARATION_DIRECTORY = '.orca-preparing'
+export const WORKTREE_CREATE_PREPARATION_LOCK_PREFIX = 'orca-create-preparation:v1:'
+const WORKTREE_CREATE_PREPARATION_ID_PATTERN =
+  /^(\d+)-[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+export function createWorktreePreparationLockReason(sessionId: string): string {
+  return `${WORKTREE_CREATE_PREPARATION_LOCK_PREFIX}${process.pid}:${sessionId}`
+}
+
+export function parseWorktreePreparationOwnerPid(lockReason?: string): number | null {
+  if (!lockReason?.startsWith(WORKTREE_CREATE_PREPARATION_LOCK_PREFIX)) {
+    return null
+  }
+  const pid = Number(lockReason.slice(WORKTREE_CREATE_PREPARATION_LOCK_PREFIX.length).split(':')[0])
+  return Number.isSafeInteger(pid) && pid > 0 ? pid : null
+}
+
+export function parseWorktreePreparationPathOwnerPid(path: string): number | null {
+  const pathParts = path.split(/[\\/]+/)
+  const preparationIndex = pathParts.lastIndexOf(WORKTREE_CREATE_PREPARATION_DIRECTORY)
+  if (preparationIndex === -1) {
+    return null
+  }
+  const preparationId = pathParts[preparationIndex + 1]
+  if (!preparationId || !WORKTREE_CREATE_PREPARATION_ID_PATTERN.test(preparationId)) {
+    return null
+  }
+  const pid = Number(preparationId.split('-')[0])
+  return Number.isSafeInteger(pid) && pid > 0 ? pid : null
+}
+
+export function isWorktreeCreatePreparation(worktree: {
+  path: string
+  lockReason?: string
+  branch?: string
+}): boolean {
+  // The Git lock reason is the durable ownership proof. A path can be chosen
+  // by a user (including for an uncommitted detached worktree), so path shape
+  // alone must never hide or force-remove it. A crash before locking may leave
+  // an unlocked detached entry for manual cleanup, but cannot delete user data.
+  return parseWorktreePreparationOwnerPid(worktree.lockReason) !== null
+}

tests/tools/benchmarks/worktree-create-speculation-bench.mjs
@@ -1,0 +1,155 @@
+#!/usr/bin/env node
+import { spawnSync } from 'node:child_process'
+import { mkdtempSync, rmSync } from 'node:fs'
+import os from 'node:os'
+import path from 'node:path'
+import { performance } from 'node:perf_hooks'
+
+function parseArgs(argv) {
+  const options = { repo: process.cwd(), base: 'HEAD', iterations: 5 }
+  const firstFlag = argv.findIndex((value, index) => index > 0 && value.startsWith('--'))
+  for (let index = firstFlag === -1 ? argv.length : firstFlag; index < argv.length; index += 1) {
+    const flag = argv[index]
+    const value = argv[index + 1]
+    if (flag === '--repo' && value) {
+      options.repo = path.resolve(value)
+    } else if (flag === '--base' && value) {
+      options.base = value
+    } else if (flag === '--iterations' && value && Number.isInteger(Number(value))) {
+      options.iterations = Number(value)
+    } else {
+      throw new Error(`Unknown or incomplete argument: ${flag}`)
+    }
+    index += 1
+  }
+  if (options.iterations < 1) {
+    throw new Error('--iterations must be positive')
+  }
+  return options
+}
+
+function git(repo, args) {
+  const result = spawnSync('git', ['-C', repo, ...args], {
+    encoding: 'utf8',
+    maxBuffer: 64 * 1024 * 1024,
+    windowsHide: true
+  })
+  if (result.status !== 0) {
+    throw new Error(`git ${args.join(' ')} failed\n${result.stderr || result.stdout}`)
+  }
+  return result.stdout.trim()
+}
+
+function time(operation) {
+  const startedAt = performance.now()
+  operation()
+  return performance.now() - startedAt
+}
+
+function median(values) {
+  const sorted = [...values].sort((left, right) => left - right)
+  const middle = Math.floor(sorted.length / 2)
+  return sorted.length % 2 === 0 ? (sorted[middle - 1] + sorted[middle]) / 2 : sorted[middle]
+}
+
+function summarize(samples) {
+  return {
+    medianMs: Number(median(samples).toFixed(1)),
+    minMs: Number(Math.min(...samples).toFixed(1)),
+    maxMs: Number(Math.max(...samples).toFixed(1)),
+    samplesMs: samples.map((sample) => Number(sample.toFixed(1)))
+  }
+}
+
+function removeWorktree(repo, worktreePath, branch, locked = false) {
+  if (locked) {
+    git(repo, ['worktree', 'unlock', worktreePath])
+  }
+  git(repo, ['worktree', 'remove', '--force', worktreePath])
+  if (branch) {
+    git(repo, ['branch', '-D', branch])
+  }
+}
+
+function benchmark(options) {
+  const repo = git(options.repo, ['rev-parse', '--show-toplevel'])
+  const base = git(repo, ['rev-parse', '--verify', `${options.base}^{commit}`])
+  const retargetBase = git(repo, ['rev-parse', '--verify', `${base}~20^{commit}`])
+  const scratchRoot = mkdtempSync(path.join(path.dirname(repo), '.orca-create-bench-'))
+  const samples = { baseline: [], prepare: [], submit: [], retarget: [], cancel: [] }
+  const prefix = `orca-create-bench-${process.pid}-${Date.now()}`
+
+  try {
+    for (let index = 0; index < options.iterations; index += 1) {
+      const baselinePath = path.join(scratchRoot, `baseline-${index}`)
+      const baselineBranch = `${prefix}-baseline-${index}`
+      samples.baseline.push(
+        time(() =>
+          git(repo, ['worktree', 'add', '--no-track', '-b', baselineBranch, baselinePath, base])
+        )
+      )
+      removeWorktree(repo, baselinePath, baselineBranch)
+
+      const preparedPath = path.join(scratchRoot, `prepared-${index}`)
+      const finalPath = path.join(scratchRoot, `final-${index}`)
+      const finalBranch = `${prefix}-final-${index}`
+      samples.prepare.push(
+        time(() => {
+          git(repo, ['worktree', 'add', '--detach', '--no-checkout', preparedPath, base])
+          git(preparedPath, ['reset', '--hard', base])
+          git(repo, [
+            'worktree',
+            'lock',
+            '--reason',
+            `orca-create-preparation:v1:${process.pid}:${index}`,
+            preparedPath
+          ])
+        })
+      )
+      samples.submit.push(
+        time(() => {
+          const targetHead = git(repo, ['rev-parse', '--verify', `${base}^{commit}`])
+          git(preparedPath, ['rev-parse', '--verify', 'HEAD'])
+          git(repo, ['worktree', 'move', '-f', '-f', preparedPath, finalPath])
+          git(finalPath, ['checkout', '--no-track', '-b', finalBranch, targetHead])
+          git(repo, ['worktree', 'unlock', finalPath])
+        })
+      )
+      removeWorktree(repo, finalPath, finalBranch)
+
+      const retargetPath = path.join(scratchRoot, `retarget-${index}`)
+      git(repo, ['worktree', 'add', '--detach', '--no-checkout', retargetPath, base])
+      git(retargetPath, ['reset', '--hard', base])
+      git(repo, ['worktree', 'lock', '--reason', 'orca-create-preparation:v1:bench', retargetPath])
+      samples.retarget.push(time(() => git(retargetPath, ['reset', '--hard', retargetBase])))
+      removeWorktree(repo, retargetPath, undefined, true)
+
+      const cancelledPath = path.join(scratchRoot, `cancel-${index}`)
+      git(repo, ['worktree', 'add', '--detach', '--no-checkout', cancelledPath, base])
+      git(cancelledPath, ['reset', '--hard', base])
+      git(repo, ['worktree', 'lock', '--reason', 'orca-create-preparation:v1:bench', cancelledPath])
+      samples.cancel.push(time(() => removeWorktree(repo, cancelledPath, undefined, true)))
+    }
+  } finally {
+    rmSync(scratchRoot, { force: true, recursive: true })
+    git(repo, ['worktree', 'prune'])
+  }
+
+  return {
+    platform: `${process.platform}-${process.arch}`,
+    os: os.release(),
+    gitVersion: git(repo, ['--version']),
+    repo,
+    trackedFiles: Number(git(repo, ['ls-files']).split('\n').filter(Boolean).length),
+    iterations: options.iterations,
+    base,
+    retargetBase,
+    baselineSubmit: summarize(samples.baseline),
+    speculativePrepare: summarize(samples.prepare),
+    speculativeSubmit: summarize(samples.submit),
+    changeBaseAfterPrepare: summarize(samples.retarget),
+    cancelPrepared: summarize(samples.cancel)
+  }
+}
+
+console.log(JSON.stringify(benchmark(parseArgs(process.argv)), null, 2))

paidaxingyo666/Manta

src/main/git/worktree-create-preparation-real-git.test.ts
@@ -1,0 +1,127 @@
+import { mkdir, mkdtemp, readFile, realpath, rm, writeFile } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { afterEach, describe, expect, it } from 'vitest'
+import { runProcessSync } from '../../shared/child-process/run-process'
+import {
+  createWorktreePreparationLockReason,
+  isWorktreeCreatePreparation,
+  WORKTREE_CREATE_PREPARATION_DIRECTORY
+} from '../../shared/worktree/create-preparation'
+import { listWorktrees } from './worktree'
+import {
+  discardPreparedWorktree,
+  finalizePreparedWorktree,
+  prepareWorktreeCreateCheckout
+} from './worktree-create-preparation'
+import { areWorktreePathsEqual } from './worktree-path-comparison'
+
+const tempRoots: string[] = []
+
+function git(cwd: string, args: string[]): string {
+  const result = runProcessSync({ program: 'git', args, cwd })
+  if (result.code !== 0) {
+    throw new Error(`git ${args[0] ?? ''} failed: ${result.stderr}`)
+  }
+  return result.stdout.trim()
+}
+
+async function createRepo(): Promise<{ repoPath: string; root: string }> {
+  const root = await mkdtemp(join(tmpdir(), 'manta-prepared-worktree-'))
+  tempRoots.push(root)
+  const repoPath = join(root, 'repo')
+  git(root, ['init', '--quiet', repoPath])
+  git(repoPath, ['symbolic-ref', 'HEAD', 'refs/heads/main'])
+  git(repoPath, ['config', 'user.email', 'test@example.com'])
+  git(repoPath, ['config', 'user.name', 'Test User'])
+  git(repoPath, ['config', 'core.autocrlf', 'false'])
+  await writeFile(join(repoPath, 'version.txt'), 'one\n')
+  git(repoPath, ['add', 'version.txt'])
+  git(repoPath, ['commit', '--quiet', '-m', 'initial'])
+  return { repoPath, root }
+}
+
+afterEach(async () => {
+  await Promise.all(tempRoots.splice(0).map((root) => rm(root, { recursive: true, force: true })))
+})
+
+describe('prepared worktree creation with real Git', () => {
+  it('cleans up when the create signal is canceled', async () => {
+    const { repoPath, root } = await createRepo()
+    const preparationRoot = join(root, WORKTREE_CREATE_PREPARATION_DIRECTORY)
+    const preparedPath = join(preparationRoot, `${process.pid}-canceled`)
+    await mkdir(preparationRoot, { recursive: true })
+
+    await prepareWorktreeCreateCheckout(
+      repoPath,
+      preparedPath,
+      'main',
+      createWorktreePreparationLockReason('canceled-test')
+    )
+
+    const controller = new AbortController()
+    controller.abort()
+    await expect(
+      discardPreparedWorktree(repoPath, preparedPath, { signal: controller.signal })
+    ).resolves.toBeUndefined()
+
+    expect(await listWorktrees(repoPath, { includeCreatePreparations: true })).toHaveLength(1)
+  })
+
+  it('hides the preparation, retargets an advanced base, and attaches the final branch', async () => {
+    const { repoPath, root } = await createRepo()
+    const preparationRoot = join(root, WORKTREE_CREATE_PREPARATION_DIRECTORY)
+    const preparedPath = join(preparationRoot, `${process.pid}-test`)
+    const finalPath = join(root, 'final-worktree')
+    await mkdir(preparationRoot, { recursive: true })
+
+    await prepareWorktreeCreateCheckout(
+      repoPath,
+      preparedPath,
+      'main',
+      createWorktreePreparationLockReason('real-git-test')
+    )
+
+    const visibleBeforeSubmit = await listWorktrees(repoPath)
+    const allBeforeSubmit = await listWorktrees(repoPath, { includeCreatePreparations: true })
+    expect(visibleBeforeSubmit).toHaveLength(1)
+    expect(allBeforeSubmit).toHaveLength(2)
+    expect(allBeforeSubmit.find(isWorktreeCreatePreparation)).toMatchObject({
+      locked: true,
+      lockReason: expect.stringContaining('manta-create-preparation:v1:')
+    })
+
+    await writeFile(join(repoPath, 'version.txt'), 'two\n')
+    git(repoPath, ['add', 'version.txt'])
+    git(repoPath, ['commit', '--quiet', '-m', 'advance base'])
+    const latestHead = git(repoPath, ['rev-parse', 'HEAD'])
+
+    await finalizePreparedWorktree(
+      repoPath,
+      preparedPath,
+      finalPath,
+      'feature/prepared',
+      'main',
+      false
+    )
+
+    expect(git(finalPath, ['rev-parse', 'HEAD'])).toBe(latestHead)
+    expect(git(finalPath, ['branch', '--show-current'])).toBe('feature/prepared')
+    expect((await readFile(join(finalPath, 'version.txt'), 'utf8')).replaceAll('\r\n', '\n')).toBe(
+      'two\n'
+    )
+    expect(git(finalPath, ['config', '--get', 'branch.feature/prepared.base'])).toBe(
+      'refs/heads/main'
+    )
+    expect(git(finalPath, ['config', '--get', 'push.autoSetupRemote'])).toBe('true')
+    const listedWorktrees = await listWorktrees(repoPath)
+    const resolvedFinalPath = await realpath(finalPath)
+    expect(
+      listedWorktrees.some((worktree) => areWorktreePathsEqual(worktree.path, resolvedFinalPath))
+    ).toBe(true)
+    expect(
+      listedWorktrees.find((worktree) => areWorktreePathsEqual(worktree.path, resolvedFinalPath))
+        ?.locked
+    ).not.toBe(true)
+  })
+})

src/shared/git-binary-compatibility.test.ts
@@ -151,6 +151,36 @@ describeBinaryCompatibility('real Git binary compatibility', () => {
     await rm(join(repoPath, 'deferred-trash'), { recursive: true, force: true })
   })
 
+  it('supports prepared worktree creation and finalization', async () => {
+    await runGit(['worktree', 'add', '--detach', '--no-checkout', 'compat-prepared', 'HEAD'])
+    await runGit(['-C', 'compat-prepared', 'reset', '--hard', 'HEAD'])
+    await runGit([
+      'worktree',
+      'lock',
+      '--reason',
+      'manta-create-preparation:v1:compat',
+      'compat-prepared'
+    ])
+    // Why: `-f -f` moves a locked preparation while preserving its lock reason (Git >=2.25).
+    await runGit(['worktree', 'move', '-f', '-f', 'compat-prepared', 'compat-final'])
+    await runGit([
+      '-C',
+      'compat-final',
+      'checkout',
+      '--no-track',
+      '-b',
+      'compat-prepared-final',
+      'HEAD'
+    ])
+
+    await expect(runGit(['-C', 'compat-final', 'branch', '--show-current'])).resolves.toMatchObject(
+      { stdout: 'compat-prepared-final\n' }
+    )
+    await runGit(['worktree', 'unlock', 'compat-final'])
+    await runGit(['worktree', 'remove', '--force', 'compat-final'])
+    await runGit(['branch', '-D', 'compat-prepared-final'])
+  })
+
   it('recognizes ref and merge-tree compatibility boundaries', async () => {
     const fetchHeadPath = join(repoPath, '.git', 'FETCH_HEAD')
     await writeFile(fetchHeadPath, 'sentinel\n')

src/shared/worktree/create-preparation.ts
@@ -1,0 +1,42 @@
+export const WORKTREE_CREATE_PREPARATION_DIRECTORY = '.manta-preparing'
+export const WORKTREE_CREATE_PREPARATION_LOCK_PREFIX = 'manta-create-preparation:v1:'
+const WORKTREE_CREATE_PREPARATION_ID_PATTERN =
+  /^(\d+)-[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+export function createWorktreePreparationLockReason(sessionId: string): string {
+  return `${WORKTREE_CREATE_PREPARATION_LOCK_PREFIX}${process.pid}:${sessionId}`
+}
+
+export function parseWorktreePreparationOwnerPid(lockReason?: string): number | null {
+  if (!lockReason?.startsWith(WORKTREE_CREATE_PREPARATION_LOCK_PREFIX)) {
+    return null
+  }
+  const pid = Number(lockReason.slice(WORKTREE_CREATE_PREPARATION_LOCK_PREFIX.length).split(':')[0])
+  return Number.isSafeInteger(pid) && pid > 0 ? pid : null
+}
+
+export function parseWorktreePreparationPathOwnerPid(path: string): number | null {
+  const pathParts = path.split(/[\\/]+/)
+  const preparationIndex = pathParts.lastIndexOf(WORKTREE_CREATE_PREPARATION_DIRECTORY)
+  if (preparationIndex === -1) {
+    return null
+  }
+  const preparationId = pathParts[preparationIndex + 1]
+  if (!preparationId || !WORKTREE_CREATE_PREPARATION_ID_PATTERN.test(preparationId)) {
+    return null
+  }
+  const pid = Number(preparationId.split('-')[0])
+  return Number.isSafeInteger(pid) && pid > 0 ? pid : null
+}
+
+export function isWorktreeCreatePreparation(worktree: {
+  path: string
+  lockReason?: string
+  branch?: string
+}): boolean {
+  // The Git lock reason is the durable ownership proof. A path can be chosen
+  // by a user (including for an uncommitted detached worktree), so path shape
+  // alone must never hide or force-remove it. A crash before locking may leave
+  // an unlocked detached entry for manual cleanup, but cannot delete user data.
+  return parseWorktreePreparationOwnerPid(worktree.lockReason) !== null
+}

tests/tools/benchmarks/worktree-create-speculation-bench.mjs
@@ -1,0 +1,155 @@
+#!/usr/bin/env node
+import { spawnSync } from 'node:child_process'
+import { mkdtempSync, rmSync } from 'node:fs'
+import os from 'node:os'
+import path from 'node:path'
+import { performance } from 'node:perf_hooks'
+
+function parseArgs(argv) {
+  const options = { repo: process.cwd(), base: 'HEAD', iterations: 5 }
+  const firstFlag = argv.findIndex((value, index) => index > 0 && value.startsWith('--'))
+  for (let index = firstFlag === -1 ? argv.length : firstFlag; index < argv.length; index += 1) {
+    const flag = argv[index]
+    const value = argv[index + 1]
+    if (flag === '--repo' && value) {
+      options.repo = path.resolve(value)
+    } else if (flag === '--base' && value) {
+      options.base = value
+    } else if (flag === '--iterations' && value && Number.isInteger(Number(value))) {
+      options.iterations = Number(value)
+    } else {
+      throw new Error(`Unknown or incomplete argument: ${flag}`)
+    }
+    index += 1
+  }
+  if (options.iterations < 1) {
+    throw new Error('--iterations must be positive')
+  }
+  return options
+}
+
+function git(repo, args) {
+  const result = spawnSync('git', ['-C', repo, ...args], {
+    encoding: 'utf8',
+    maxBuffer: 64 * 1024 * 1024,
+    windowsHide: true
+  })
+  if (result.status !== 0) {
+    throw new Error(`git ${args.join(' ')} failed\n${result.stderr || result.stdout}`)
+  }
+  return result.stdout.trim()
+}
+
+function time(operation) {
+  const startedAt = performance.now()
+  operation()
+  return performance.now() - startedAt
+}
+
+function median(values) {
+  const sorted = [...values].sort((left, right) => left - right)
+  const middle = Math.floor(sorted.length / 2)
+  return sorted.length % 2 === 0 ? (sorted[middle - 1] + sorted[middle]) / 2 : sorted[middle]
+}
+
+function summarize(samples) {
+  return {
+    medianMs: Number(median(samples).toFixed(1)),
+    minMs: Number(Math.min(...samples).toFixed(1)),
+    maxMs: Number(Math.max(...samples).toFixed(1)),
+    samplesMs: samples.map((sample) => Number(sample.toFixed(1)))
+  }
+}
+
+function removeWorktree(repo, worktreePath, branch, locked = false) {
+  if (locked) {
+    git(repo, ['worktree', 'unlock', worktreePath])
+  }
+  git(repo, ['worktree', 'remove', '--force', worktreePath])
+  if (branch) {
+    git(repo, ['branch', '-D', branch])
+  }
+}
+
+function benchmark(options) {
+  const repo = git(options.repo, ['rev-parse', '--show-toplevel'])
+  const base = git(repo, ['rev-parse', '--verify', `${options.base}^{commit}`])
+  const retargetBase = git(repo, ['rev-parse', '--verify', `${base}~20^{commit}`])
+  const scratchRoot = mkdtempSync(path.join(path.dirname(repo), '.manta-create-bench-'))
+  const samples = { baseline: [], prepare: [], submit: [], retarget: [], cancel: [] }
+  const prefix = `manta-create-bench-${process.pid}-${Date.now()}`
+
+  try {
+    for (let index = 0; index < options.iterations; index += 1) {
+      const baselinePath = path.join(scratchRoot, `baseline-${index}`)
+      const baselineBranch = `${prefix}-baseline-${index}`
+      samples.baseline.push(
+        time(() =>
+          git(repo, ['worktree', 'add', '--no-track', '-b', baselineBranch, baselinePath, base])
+        )
+      )
+      removeWorktree(repo, baselinePath, baselineBranch)
+
+      const preparedPath = path.join(scratchRoot, `prepared-${index}`)
+      const finalPath = path.join(scratchRoot, `final-${index}`)
+      const finalBranch = `${prefix}-final-${index}`
+      samples.prepare.push(
+        time(() => {
+          git(repo, ['worktree', 'add', '--detach', '--no-checkout', preparedPath, base])
+          git(preparedPath, ['reset', '--hard', base])
+          git(repo, [
+            'worktree',
+            'lock',
+            '--reason',
+            `manta-create-preparation:v1:${process.pid}:${index}`,
+            preparedPath
+          ])
+        })
+      )
+      samples.submit.push(
+        time(() => {
+          const targetHead = git(repo, ['rev-parse', '--verify', `${base}^{commit}`])
+          git(preparedPath, ['rev-parse', '--verify', 'HEAD'])
+          git(repo, ['worktree', 'move', '-f', '-f', preparedPath, finalPath])
+          git(finalPath, ['checkout', '--no-track', '-b', finalBranch, targetHead])
+          git(repo, ['worktree', 'unlock', finalPath])
+        })
+      )
+      removeWorktree(repo, finalPath, finalBranch)
+
+      const retargetPath = path.join(scratchRoot, `retarget-${index}`)
+      git(repo, ['worktree', 'add', '--detach', '--no-checkout', retargetPath, base])
+      git(retargetPath, ['reset', '--hard', base])
+      git(repo, ['worktree', 'lock', '--reason', 'manta-create-preparation:v1:bench', retargetPath])
+      samples.retarget.push(time(() => git(retargetPath, ['reset', '--hard', retargetBase])))
+      removeWorktree(repo, retargetPath, undefined, true)
+
+      const cancelledPath = path.join(scratchRoot, `cancel-${index}`)
+      git(repo, ['worktree', 'add', '--detach', '--no-checkout', cancelledPath, base])
+      git(cancelledPath, ['reset', '--hard', base])
+      git(repo, ['worktree', 'lock', '--reason', 'manta-create-preparation:v1:bench', cancelledPath])
+      samples.cancel.push(time(() => removeWorktree(repo, cancelledPath, undefined, true)))
+    }
+  } finally {
+    rmSync(scratchRoot, { force: true, recursive: true })
+    git(repo, ['worktree', 'prune'])
+  }
+
+  return {
+    platform: `${process.platform}-${process.arch}`,
+    os: os.release(),
+    gitVersion: git(repo, ['--version']),
+    repo,
+    trackedFiles: Number(git(repo, ['ls-files']).split('\n').filter(Boolean).length),
+    iterations: options.iterations,
+    base,
+    retargetBase,
+    baselineSubmit: summarize(samples.baseline),
+    speculativePrepare: summarize(samples.prepare),
+    speculativeSubmit: summarize(samples.submit),
+    changeBaseAfterPrepare: summarize(samples.retarget),
+    cancelPrepared: summarize(samples.cancel)
+  }
+}
+
+console.log(JSON.stringify(benchmark(parseArgs(process.argv)), null, 2))