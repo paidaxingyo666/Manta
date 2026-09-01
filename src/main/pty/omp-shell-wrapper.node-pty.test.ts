2022zhang125/orca-yunxiao

fix(terminal): complete OMP stale cwd recovery (#17154)

src/main/pty/omp-shell-wrapper.node-pty.test.ts
@@ -1,11 +1,13 @@
 import { spawnSync } from 'node:child_process'
 import {
   chmodSync,
+  existsSync,
   mkdtempSync,
   mkdirSync,
   readFileSync,
   realpathSync,
   rmSync,
+  symlinkSync,
   writeFileSync
 } from 'node:fs'
 import { tmpdir } from 'node:os'
@@ -334,18 +336,82 @@ exit 0
     const homeDir = join(tempDir, 'home')
     const binDir = join(tempDir, 'bin')
     const extensionDir = join(tempDir, 'extensions')
+    const logicalProjectLink = join(tempDir, 'logical-project')
     mkdirSync(projectDir, { recursive: true })
     mkdirSync(homeDir)
     mkdirSync(binDir)
     mkdirSync(extensionDir)
+    symlinkSync(projectDir, logicalProjectLink)
     const expectedProjectDir = realpathSync(projectDir)
+    const expectedWorkspaceDir = realpathSync(workspaceDir)
     const statusExtension = join(extensionDir, 'orca-agent-status.ts')
     writeFileSync(statusExtension, 'export default {}')
     writeFakeOmp(binDir)
 
     const unsetPwdCaptureFile = join(tempDir, 'unset-pwd-capture')
+    const deletedLinkCaptureFile = join(tempDir, 'deleted-link-capture')
     const staleCaptureFile = join(tempDir, 'stale-cwd-capture')
+    const skipCaptureFile = join(tempDir, 'skip-capture')
+    const staleUnsetCaptureFile = join(tempDir, 'stale-unset-capture')
+    const noLogicalCaptureFile = join(tempDir, 'no-logical-capture')
     const resultFile = join(tempDir, 'stale-cwd-result')
+    const scenarioFile = join(tempDir, 'stale-cwd-scenario')
+    writeFileSync(
+      scenarioFile,
+      `ORCA_CAPTURE_FILE="$ORCA_UNSET_PWD_CAPTURE_FILE"
+unset PWD
+omp
+__orca_test_unset_status=$?
+if [[ -z "\${PWD+x}" ]]; then
+  __orca_test_pwd_state=unset
+else
+  __orca_test_pwd_state=set
+fi
+builtin cd -- "$ORCA_LOGICAL_PROJECT_LINK"
+/bin/rm -- "$ORCA_LOGICAL_PROJECT_LINK"
+ORCA_CAPTURE_FILE="$ORCA_DELETED_LINK_CAPTURE_FILE"
+omp
+__orca_test_deleted_link_status=$?
+builtin cd -P -- "$ORCA_STALE_PROJECT_DIR"
+ORCA_CAPTURE_FILE="$ORCA_STALE_CAPTURE_FILE"
+/bin/rm -rf -- "$ORCA_STALE_PROJECT_DIR"
+/bin/mkdir -p -- "$ORCA_STALE_PROJECT_DIR"
+omp
+__orca_test_first_status=$?
+ORCA_CAPTURE_FILE="$ORCA_SKIP_CAPTURE_FILE"
+omp --version
+__orca_test_skip_status=$?
+if [[ "$PWD" -ef . ]]; then
+  __orca_test_parent_state=live
+else
+  __orca_test_parent_state=stale
+fi
+unset PWD
+ORCA_CAPTURE_FILE="$ORCA_STALE_UNSET_CAPTURE_FILE"
+omp
+__orca_test_stale_unset_status=$?
+unset ORCA_WORKTREE_PATH ORCA_ROOT_PATH
+ORCA_CAPTURE_FILE="$ORCA_NO_LOGICAL_CAPTURE_FILE"
+omp
+__orca_test_no_logical_status=$?
+PWD="$ORCA_STALE_PROJECT_DIR"
+/bin/rm -rf -- "$ORCA_STALE_PROJECT_DIR"
+omp
+__orca_test_missing_status=$?
+{
+  echo "UNSET=$__orca_test_unset_status"
+  echo "PWD=$__orca_test_pwd_state"
+  echo "LINK=$__orca_test_deleted_link_status"
+  echo "FIRST=$__orca_test_first_status"
+  echo "SKIP=$__orca_test_skip_status"
+  echo "PARENT=$__orca_test_parent_state"
+  echo "STALE_UNSET=$__orca_test_stale_unset_status"
+  echo "NO_LOGICAL=$__orca_test_no_logical_status"
+  echo "MISSING=$__orca_test_missing_status"
+} > "$ORCA_RESULT_FILE"
+exit 0
+`
+    )
     const output = await runInteractivePosixPty({
       shell,
       cwd: projectDir,
@@ -355,47 +421,30 @@ ${getPosixOmpShellWrapper()}`,
         INPUTRC: '/dev/null',
         PROMPT_COMMAND: '',
         ORCA_STALE_PROJECT_DIR: projectDir,
+        ORCA_LOGICAL_PROJECT_LINK: logicalProjectLink,
         ORCA_UNSET_PWD_CAPTURE_FILE: unsetPwdCaptureFile,
+        ORCA_DELETED_LINK_CAPTURE_FILE: deletedLinkCaptureFile,
         ORCA_STALE_CAPTURE_FILE: staleCaptureFile,
+        ORCA_SKIP_CAPTURE_FILE: skipCaptureFile,
+        ORCA_STALE_UNSET_CAPTURE_FILE: staleUnsetCaptureFile,
+        ORCA_NO_LOGICAL_CAPTURE_FILE: noLogicalCaptureFile,
         ORCA_WORKTREE_PATH: workspaceDir,
         HOME: homeDir,
         PATH: `${binDir}:/usr/bin:/bin:/usr/sbin:/sbin`,
         ORCA_OMP_STATUS_EXTENSION: statusExtension,
         ORCA_CAPTURE_FILE: staleCaptureFile,
         ORCA_RESULT_FILE: resultFile,
+        ORCA_SCENARIO_FILE: scenarioFile,
         ORCA_TEST_FAKE_OMP_EXIT_CODE: '23',
         TERM: 'xterm-256color'
       },
-      input: `ORCA_CAPTURE_FILE="$ORCA_UNSET_PWD_CAPTURE_FILE"
-unset PWD
-omp
-__orca_test_unset_status=$?
-if [[ -z "\${PWD+x}" ]]; then
-  __orca_test_pwd_state=unset
-else
-  __orca_test_pwd_state=set
-fi
-builtin cd -P -- "$ORCA_STALE_PROJECT_DIR"
-ORCA_CAPTURE_FILE="$ORCA_STALE_CAPTURE_FILE"
-/bin/rm -rf -- "$ORCA_STALE_PROJECT_DIR"
-/bin/mkdir -p -- "$ORCA_STALE_PROJECT_DIR"
-omp
-__orca_test_first_status=$?
-if [[ "$PWD" -ef . ]]; then
-  __orca_test_parent_state=live
-else
-  __orca_test_parent_state=stale
-fi
-/bin/rm -rf -- "$ORCA_STALE_PROJECT_DIR"
-omp
-__orca_test_missing_status=$?
-printf 'UNSET=%s\nPWD=%s\nFIRST=%s\nPARENT=%s\nMISSING=%s\n' "$__orca_test_unset_status" "$__orca_test_pwd_state" "$__orca_test_first_status" "$__orca_test_parent_state" "$__orca_test_missing_status" > "$ORCA_RESULT_FILE"
-exit 0
-`
+      input: 'source "$ORCA_SCENARIO_FILE"\n'
     })
 
     const unsetPwdCapture = readFileSync(unsetPwdCaptureFile, 'utf8')
     expect(unsetPwdCapture).toContain(`CWD=${expectedProjectDir}`)
+    const deletedLinkCapture = readFileSync(deletedLinkCaptureFile, 'utf8')
+    expect(deletedLinkCapture).toContain(`CWD=${expectedProjectDir}`)
     const staleCapture = readFileSync(staleCaptureFile, 'utf8')
     expect(staleCapture).toContain(`CWD=${expectedProjectDir}`)
     expect(staleCapture.split('\n').filter((line) => line.startsWith('ARG'))).toEqual([
@@ -403,9 +452,17 @@ exit 0
       `ARG2=${statusExtension}`
     ])
     expect(staleCapture).not.toContain('--cwd')
+    const skipCapture = readFileSync(skipCaptureFile, 'utf8')
+    expect(skipCapture).toContain(`CWD=${expectedProjectDir}`)
+    expect(skipCapture).toContain('ARG1=--version')
+    expect(skipCapture).not.toContain('--extension')
+    const staleUnsetCapture = readFileSync(staleUnsetCaptureFile, 'utf8')
+    expect(staleUnsetCapture).toContain(`CWD=${expectedWorkspaceDir}`)
+    expect(existsSync(noLogicalCaptureFile)).toBe(false)
     expect(readFileSync(resultFile, 'utf8')).toBe(
-      'UNSET=23\nPWD=unset\nFIRST=23\nPARENT=stale\nMISSING=1\n'
+      'UNSET=23\nPWD=unset\nLINK=23\nFIRST=23\nSKIP=23\nPARENT=stale\nSTALE_UNSET=23\nNO_LOGICAL=1\nMISSING=1\n'
     )
+    expect(output).toContain('no terminal working directory is available')
     expect(output).toContain('Orca: OMP cannot access the terminal working directory')
   }
 

src/main/pty/omp-shell-wrapper.ts
@@ -52,11 +52,14 @@ __orca_omp_should_skip_extension() {
   return 1
 }
 __orca_omp_cwd_is_usable() {
+  local __orca_physical_cwd
   [[ -x . ]] || return 1
-  if [[ -n "\${PWD:-}" ]]; then
-    [[ -d "\${PWD}" && "\${PWD}" -ef . ]]
+  if [[ -n "\${PWD:-}" && -d "\${PWD:-}" ]]; then
+    [[ "\${PWD}" -ef . ]]
   else
-    builtin pwd -P >/dev/null 2>&1
+    # Why compare the path: shell builtins can print a cached path for a deleted cwd.
+    __orca_physical_cwd="$(builtin pwd -P 2>/dev/null)" || return 1
+    [[ -d "$__orca_physical_cwd" && "$__orca_physical_cwd" -ef . ]]
   fi
 }
 __orca_omp_invoke() {
@@ -76,7 +79,7 @@ __orca_omp_invoke() {
 __orca_omp() {
   local __orca_use_extension=1
   __orca_omp_should_skip_extension "\${1:-}" && __orca_use_extension=0
-  if [[ $__orca_use_extension -eq 1 ]] && ! __orca_omp_cwd_is_usable; then
+  if ! __orca_omp_cwd_is_usable; then
     local __orca_logical_cwd="\${PWD:-\${ORCA_WORKTREE_PATH:-\${ORCA_ROOT_PATH:-}}}"
     # Why: a restored shell can retain the deleted directory inode after its path is recreated.
     (

paidaxingyo666/Manta

(cherry picked from commit 7655d20f04ffc11a6221c504652fb435c8e0d88d)

src/main/pty/omp-shell-wrapper.node-pty.test.ts
@@ -1,11 +1,13 @@
 import { spawnSync } from 'node:child_process'
 import {
   chmodSync,
+  existsSync,
   mkdtempSync,
   mkdirSync,
   readFileSync,
   realpathSync,
   rmSync,
+  symlinkSync,
   writeFileSync
 } from 'node:fs'
 import { tmpdir } from 'node:os'
@@ -334,18 +336,82 @@ exit 0
     const homeDir = join(tempDir, 'home')
     const binDir = join(tempDir, 'bin')
     const extensionDir = join(tempDir, 'extensions')
+    const logicalProjectLink = join(tempDir, 'logical-project')
     mkdirSync(projectDir, { recursive: true })
     mkdirSync(homeDir)
     mkdirSync(binDir)
     mkdirSync(extensionDir)
+    symlinkSync(projectDir, logicalProjectLink)
     const expectedProjectDir = realpathSync(projectDir)
+    const expectedWorkspaceDir = realpathSync(workspaceDir)
     const statusExtension = join(extensionDir, 'manta-agent-status.ts')
     writeFileSync(statusExtension, 'export default {}')
     writeFakeOmp(binDir)
 
     const unsetPwdCaptureFile = join(tempDir, 'unset-pwd-capture')
+    const deletedLinkCaptureFile = join(tempDir, 'deleted-link-capture')
     const staleCaptureFile = join(tempDir, 'stale-cwd-capture')
+    const skipCaptureFile = join(tempDir, 'skip-capture')
+    const staleUnsetCaptureFile = join(tempDir, 'stale-unset-capture')
+    const noLogicalCaptureFile = join(tempDir, 'no-logical-capture')
     const resultFile = join(tempDir, 'stale-cwd-result')
+    const scenarioFile = join(tempDir, 'stale-cwd-scenario')
+    writeFileSync(
+      scenarioFile,
+      `MANTA_CAPTURE_FILE="$MANTA_UNSET_PWD_CAPTURE_FILE"
+unset PWD
+omp
+__manta_test_unset_status=$?
+if [[ -z "\${PWD+x}" ]]; then
+  __manta_test_pwd_state=unset
+else
+  __manta_test_pwd_state=set
+fi
+builtin cd -- "$MANTA_LOGICAL_PROJECT_LINK"
+/bin/rm -- "$MANTA_LOGICAL_PROJECT_LINK"
+MANTA_CAPTURE_FILE="$MANTA_DELETED_LINK_CAPTURE_FILE"
+omp
+__manta_test_deleted_link_status=$?
+builtin cd -P -- "$MANTA_STALE_PROJECT_DIR"
+MANTA_CAPTURE_FILE="$MANTA_STALE_CAPTURE_FILE"
+/bin/rm -rf -- "$MANTA_STALE_PROJECT_DIR"
+/bin/mkdir -p -- "$MANTA_STALE_PROJECT_DIR"
+omp
+__manta_test_first_status=$?
+MANTA_CAPTURE_FILE="$MANTA_SKIP_CAPTURE_FILE"
+omp --version
+__manta_test_skip_status=$?
+if [[ "$PWD" -ef . ]]; then
+  __manta_test_parent_state=live
+else
+  __manta_test_parent_state=stale
+fi
+unset PWD
+MANTA_CAPTURE_FILE="$MANTA_STALE_UNSET_CAPTURE_FILE"
+omp
+__manta_test_stale_unset_status=$?
+unset MANTA_WORKTREE_PATH MANTA_ROOT_PATH
+MANTA_CAPTURE_FILE="$MANTA_NO_LOGICAL_CAPTURE_FILE"
+omp
+__manta_test_no_logical_status=$?
+PWD="$MANTA_STALE_PROJECT_DIR"
+/bin/rm -rf -- "$MANTA_STALE_PROJECT_DIR"
+omp
+__manta_test_missing_status=$?
+{
+  echo "UNSET=$__manta_test_unset_status"
+  echo "PWD=$__manta_test_pwd_state"
+  echo "LINK=$__manta_test_deleted_link_status"
+  echo "FIRST=$__manta_test_first_status"
+  echo "SKIP=$__manta_test_skip_status"
+  echo "PARENT=$__manta_test_parent_state"
+  echo "STALE_UNSET=$__manta_test_stale_unset_status"
+  echo "NO_LOGICAL=$__manta_test_no_logical_status"
+  echo "MISSING=$__manta_test_missing_status"
+} > "$MANTA_RESULT_FILE"
+exit 0
+`
+    )
     const output = await runInteractivePosixPty({
       shell,
       cwd: projectDir,
@@ -355,47 +421,30 @@ ${getPosixOmpShellWrapper()}`,
         INPUTRC: '/dev/null',
         PROMPT_COMMAND: '',
         MANTA_STALE_PROJECT_DIR: projectDir,
+        MANTA_LOGICAL_PROJECT_LINK: logicalProjectLink,
         MANTA_UNSET_PWD_CAPTURE_FILE: unsetPwdCaptureFile,
+        MANTA_DELETED_LINK_CAPTURE_FILE: deletedLinkCaptureFile,
         MANTA_STALE_CAPTURE_FILE: staleCaptureFile,
+        MANTA_SKIP_CAPTURE_FILE: skipCaptureFile,
+        MANTA_STALE_UNSET_CAPTURE_FILE: staleUnsetCaptureFile,
+        MANTA_NO_LOGICAL_CAPTURE_FILE: noLogicalCaptureFile,
         MANTA_WORKTREE_PATH: workspaceDir,
         HOME: homeDir,
         PATH: `${binDir}:/usr/bin:/bin:/usr/sbin:/sbin`,
         MANTA_OMP_STATUS_EXTENSION: statusExtension,
         MANTA_CAPTURE_FILE: staleCaptureFile,
         MANTA_RESULT_FILE: resultFile,
+        MANTA_SCENARIO_FILE: scenarioFile,
         MANTA_TEST_FAKE_OMP_EXIT_CODE: '23',
         TERM: 'xterm-256color'
       },
-      input: `MANTA_CAPTURE_FILE="$MANTA_UNSET_PWD_CAPTURE_FILE"
-unset PWD
-omp
-__manta_test_unset_status=$?
-if [[ -z "\${PWD+x}" ]]; then
-  __manta_test_pwd_state=unset
-else
-  __manta_test_pwd_state=set
-fi
-builtin cd -P -- "$MANTA_STALE_PROJECT_DIR"
-MANTA_CAPTURE_FILE="$MANTA_STALE_CAPTURE_FILE"
-/bin/rm -rf -- "$MANTA_STALE_PROJECT_DIR"
-/bin/mkdir -p -- "$MANTA_STALE_PROJECT_DIR"
-omp
-__manta_test_first_status=$?
-if [[ "$PWD" -ef . ]]; then
-  __manta_test_parent_state=live
-else
-  __manta_test_parent_state=stale
-fi
-/bin/rm -rf -- "$MANTA_STALE_PROJECT_DIR"
-omp
-__manta_test_missing_status=$?
-printf 'UNSET=%s\nPWD=%s\nFIRST=%s\nPARENT=%s\nMISSING=%s\n' "$__manta_test_unset_status" "$__manta_test_pwd_state" "$__manta_test_first_status" "$__manta_test_parent_state" "$__manta_test_missing_status" > "$MANTA_RESULT_FILE"
-exit 0
-`
+      input: 'source "$MANTA_SCENARIO_FILE"\n'
     })
 
     const unsetPwdCapture = readFileSync(unsetPwdCaptureFile, 'utf8')
     expect(unsetPwdCapture).toContain(`CWD=${expectedProjectDir}`)
+    const deletedLinkCapture = readFileSync(deletedLinkCaptureFile, 'utf8')
+    expect(deletedLinkCapture).toContain(`CWD=${expectedProjectDir}`)
     const staleCapture = readFileSync(staleCaptureFile, 'utf8')
     expect(staleCapture).toContain(`CWD=${expectedProjectDir}`)
     expect(staleCapture.split('\n').filter((line) => line.startsWith('ARG'))).toEqual([
@@ -403,9 +452,17 @@ exit 0
       `ARG2=${statusExtension}`
     ])
     expect(staleCapture).not.toContain('--cwd')
+    const skipCapture = readFileSync(skipCaptureFile, 'utf8')
+    expect(skipCapture).toContain(`CWD=${expectedProjectDir}`)
+    expect(skipCapture).toContain('ARG1=--version')
+    expect(skipCapture).not.toContain('--extension')
+    const staleUnsetCapture = readFileSync(staleUnsetCaptureFile, 'utf8')
+    expect(staleUnsetCapture).toContain(`CWD=${expectedWorkspaceDir}`)
+    expect(existsSync(noLogicalCaptureFile)).toBe(false)
     expect(readFileSync(resultFile, 'utf8')).toBe(
-      'UNSET=23\nPWD=unset\nFIRST=23\nPARENT=stale\nMISSING=1\n'
+      'UNSET=23\nPWD=unset\nLINK=23\nFIRST=23\nSKIP=23\nPARENT=stale\nSTALE_UNSET=23\nNO_LOGICAL=1\nMISSING=1\n'
     )
+    expect(output).toContain('no terminal working directory is available')
     expect(output).toContain('Manta: OMP cannot access the terminal working directory')
   }
 

src/main/pty/omp-shell-wrapper.ts
@@ -52,11 +52,14 @@ __manta_omp_should_skip_extension() {
   return 1
 }
 __manta_omp_cwd_is_usable() {
+  local __manta_physical_cwd
   [[ -x . ]] || return 1
-  if [[ -n "\${PWD:-}" ]]; then
-    [[ -d "\${PWD}" && "\${PWD}" -ef . ]]
+  if [[ -n "\${PWD:-}" && -d "\${PWD:-}" ]]; then
+    [[ "\${PWD}" -ef . ]]
   else
-    builtin pwd -P >/dev/null 2>&1
+    # Why compare the path: shell builtins can print a cached path for a deleted cwd.
+    __manta_physical_cwd="$(builtin pwd -P 2>/dev/null)" || return 1
+    [[ -d "$__manta_physical_cwd" && "$__manta_physical_cwd" -ef . ]]
   fi
 }
 __manta_omp_invoke() {
@@ -76,7 +79,7 @@ __manta_omp_invoke() {
 __manta_omp() {
   local __manta_use_extension=1
   __manta_omp_should_skip_extension "\${1:-}" && __manta_use_extension=0
-  if [[ $__manta_use_extension -eq 1 ]] && ! __manta_omp_cwd_is_usable; then
+  if ! __manta_omp_cwd_is_usable; then
     local __manta_logical_cwd="\${PWD:-\${MANTA_WORKTREE_PATH:-\${MANTA_ROOT_PATH:-}}}"
     # Why: a restored shell can retain the deleted directory inode after its path is recreated.
     (