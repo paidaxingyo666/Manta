2022zhang125/orca-yunxiao

fix(window): keep automated Electron launches out of the foreground (#17347)

config/scripts/run-codex-real-account-validation.mjs
@@ -520,7 +520,11 @@ async function main() {
 
     if (!options.dryRun) {
       const mainPath = buildAppIfNeeded(repoRoot, options.skipBuild)
-      app = await electron.launch({ args: [mainPath], env: launchEnv })
+      app = await electron.launch({
+        args: [mainPath],
+        // Why: a validation run must not pull the window over the developer's work.
+        env: { ...launchEnv, ORCA_BACKGROUND_LAUNCH: '1' }
+      })
       report.electronPaths = await app.evaluate(({ app: electronApp }) => ({
         home: electronApp.getPath('home'),
         userData: electronApp.getPath('userData'),

config/scripts/run-electron-vite-dev.mjs
@@ -624,6 +624,11 @@ const isHelpOrVersion = forwardedRaw.some((a) => a === '--help' || a === '-h' ||
 if (!isHelpOrVersion && process.env.ORCA_DEV_INSTANCE_LABEL) {
   console.error(`[orca-dev] Instance: ${process.env.ORCA_DEV_INSTANCE_LABEL}`)
 }
+// Why: automation launches this app while someone is working; announce that the
+// window will come up without taking the foreground so the mode is visible in logs.
+if (!isHelpOrVersion && process.env.ORCA_BACKGROUND_LAUNCH === '1') {
+  console.error('[orca-dev] Background launch: window shows without stealing focus')
+}
 let forwardedExtras = []
 if (!userPassedPort && !isHelpOrVersion) {
   const envPortRaw = process.env.REMOTE_DEBUGGING_PORT

src/main/window/foreground-activation-policy.test.ts
@@ -1,0 +1,124 @@
+import type { BrowserWindow } from 'electron'
+import { describe, expect, it, vi } from 'vitest'
+
+vi.mock('electron', () => ({ app: { dock: { hide: vi.fn() }, setActivationPolicy: vi.fn() } }))
+
+const {
+  applyBackgroundActivationPolicy,
+  isBackgroundLaunch,
+  isWindowlessLaunch,
+  showWindowWithoutStealingFocus
+} = await import('./foreground-activation-policy')
+
+function makeWindow(destroyed = false): BrowserWindow & {
+  show: ReturnType<typeof vi.fn>
+  showInactive: ReturnType<typeof vi.fn>
+} {
+  return {
+    isDestroyed: () => destroyed,
+    show: vi.fn(() => {}),
+    showInactive: vi.fn(() => {})
+  } as unknown as BrowserWindow & {
+    show: ReturnType<typeof vi.fn>
+    showInactive: ReturnType<typeof vi.fn>
+  }
+}
+
+describe('isBackgroundLaunch', () => {
+  it('covers headless and headful E2E plus opted-in dev launches', () => {
+    expect(isBackgroundLaunch({ ORCA_E2E_HEADLESS: '1' })).toBe(true)
+    expect(isBackgroundLaunch({ ORCA_E2E_HEADFUL: '1' })).toBe(true)
+    expect(isBackgroundLaunch({ ORCA_BACKGROUND_LAUNCH: '1' })).toBe(true)
+    expect(isBackgroundLaunch({})).toBe(false)
+  })
+
+  it('lets native-focus specs opt back into the foreground', () => {
+    expect(isBackgroundLaunch({ ORCA_E2E_HEADFUL: '1', ORCA_E2E_FOREGROUND: '1' })).toBe(false)
+    expect(isWindowlessLaunch({ ORCA_E2E_HEADLESS: '1', ORCA_E2E_FOREGROUND: '1' })).toBe(false)
+  })
+})
+
+describe('isWindowlessLaunch', () => {
+  it('is headless-only; a headful run still paints', () => {
+    expect(isWindowlessLaunch({ ORCA_E2E_HEADLESS: '1' })).toBe(true)
+    expect(isWindowlessLaunch({ ORCA_E2E_HEADLESS: '1', ORCA_E2E_HEADFUL: '1' })).toBe(false)
+    expect(isWindowlessLaunch({ ORCA_BACKGROUND_LAUNCH: '1' })).toBe(false)
+  })
+})
+
+describe('showWindowWithoutStealingFocus', () => {
+  it('keeps a headless window off screen', () => {
+    const window = makeWindow()
+    showWindowWithoutStealingFocus(window, { ORCA_E2E_HEADLESS: '1' })
+    expect(window.show).not.toHaveBeenCalled()
+    expect(window.showInactive).not.toHaveBeenCalled()
+  })
+
+  it('shows a background window without activating it', () => {
+    const window = makeWindow()
+    showWindowWithoutStealingFocus(window, { ORCA_BACKGROUND_LAUNCH: '1' })
+    expect(window.showInactive).toHaveBeenCalledOnce()
+    expect(window.show).not.toHaveBeenCalled()
+  })
+
+  it('shows normally for a real user launch', () => {
+    const window = makeWindow()
+    showWindowWithoutStealingFocus(window, {})
+    expect(window.show).toHaveBeenCalledOnce()
+    expect(window.showInactive).not.toHaveBeenCalled()
+  })
+
+  it('ignores a destroyed window', () => {
+    const window = makeWindow(true)
+    showWindowWithoutStealingFocus(window, {})
+    expect(window.show).not.toHaveBeenCalled()
+  })
+})
+
+describe('applyBackgroundActivationPolicy', () => {
+  function makeApp() {
+    return {
+      dock: { hide: vi.fn(() => {}) },
+      setActivationPolicy: vi.fn((_policy: 'accessory' | 'prohibited' | 'regular') => {})
+    }
+  }
+
+  it('drops the macOS Dock tile and menu bar for headless runs', () => {
+    const app = makeApp()
+    expect(
+      applyBackgroundActivationPolicy({
+        app,
+        env: { ORCA_E2E_HEADLESS: '1' },
+        platform: 'darwin'
+      })
+    ).toBe(true)
+    expect(app.dock.hide).toHaveBeenCalledOnce()
+    expect(app.setActivationPolicy).toHaveBeenCalledWith('accessory')
+  })
+
+  it('leaves a headful or user launch with its normal Dock presence', () => {
+    const headful = makeApp()
+    applyBackgroundActivationPolicy({
+      app: headful,
+      env: { ORCA_E2E_HEADLESS: '1', ORCA_E2E_HEADFUL: '1' },
+      platform: 'darwin'
+    })
+    expect(headful.setActivationPolicy).not.toHaveBeenCalled()
+
+    const user = makeApp()
+    applyBackgroundActivationPolicy({ app: user, env: {}, platform: 'darwin' })
+    expect(user.setActivationPolicy).not.toHaveBeenCalled()
+  })
+
+  it('is a no-op off macOS', () => {
+    const app = makeApp()
+    expect(
+      applyBackgroundActivationPolicy({
+        app,
+        env: { ORCA_E2E_HEADLESS: '1' },
+        platform: 'win32'
+      })
+    ).toBe(false)
+    expect(app.dock.hide).not.toHaveBeenCalled()
+  })
+})

src/main/window/foreground-activation-policy.ts
@@ -1,0 +1,83 @@
+import { app as electronApp, type BrowserWindow } from 'electron'
+
+/**
+ * Foreground policy for automated launches (E2E, benchmarks, agent-driven dev
+ * validation). These runs may use the machine, but must never take the OS
+ * foreground away from whatever the developer is doing.
+ *
+ * ORCA_BACKGROUND_LAUNCH=1 opts a normal launch in; ORCA_E2E_FOREGROUND=1 opts
+ * back out for the few specs whose subject *is* native focus (IME, key events).
+ */
+
+type ActivationPolicyApp = {
+  dock?: { hide: () => void }
+  setActivationPolicy: (policy: 'accessory' | 'prohibited' | 'regular') => void
+}
+
+/** Reads ORCA_BACKGROUND_LAUNCH, ORCA_E2E_FOREGROUND, ORCA_E2E_HEADLESS, ORCA_E2E_HEADFUL. */
+type PolicyEnv = Readonly<Record<string, string | undefined>>
+
+/** True when this process must not steal focus, raise windows, or activate the app. */
+export function isBackgroundLaunch(env: PolicyEnv = process.env): boolean {
+  if (env.ORCA_E2E_FOREGROUND === '1') {
+    return false
+  }
+  return (
+    env.ORCA_BACKGROUND_LAUNCH === '1' ||
+    env.ORCA_E2E_HEADLESS === '1' ||
+    env.ORCA_E2E_HEADFUL === '1'
+  )
+}
+
+/** True when no window should reach the screen at all (headless E2E; Playwright drives via CDP). */
+export function isWindowlessLaunch(env: PolicyEnv = process.env): boolean {
+  return isBackgroundLaunch(env) && env.ORCA_E2E_HEADLESS === '1' && env.ORCA_E2E_HEADFUL !== '1'
+}
+
+/**
+ * macOS: a windowless run still claims a Dock tile and the menu bar as it starts
+ * and exits. `accessory` drops both while leaving programmatic activation intact.
+ */
+export function applyBackgroundActivationPolicy(
+  options: {
+    app?: ActivationPolicyApp
+    env?: PolicyEnv
+    platform?: NodeJS.Platform
+    warn?: (message: string, error: unknown) => void
+  } = {}
+): boolean {
+  const platform = options.platform ?? process.platform
+  if (platform !== 'darwin' || !isWindowlessLaunch(options.env ?? process.env)) {
+    return false
+  }
+  try {
+    const app = options.app ?? electronApp
+    app.dock?.hide()
+    app.setActivationPolicy('accessory')
+    return true
+  } catch (error) {
+    options.warn?.('[window] Failed to apply background activation policy', error)
+    return false
+  }
+}
+
+/**
+ * Reveal a window without taking the foreground: hidden entirely when windowless,
+ * `showInactive()` (visible, not raised over the active app) in background launches.
+ */
+export function showWindowWithoutStealingFocus(
+  window: BrowserWindow,
+  env: PolicyEnv = process.env
+): void {
+  if (window.isDestroyed()) {
+    return
+  }
+  if (isWindowlessLaunch(env)) {
+    return
+  }
+  if (isBackgroundLaunch(env)) {
+    window.showInactive()
+    return
+  }
+  window.show()
+}

paidaxingyo666/Manta

config/scripts/run-codex-real-account-validation.mjs
@@ -520,7 +520,11 @@ async function main() {
 
     if (!options.dryRun) {
       const mainPath = buildAppIfNeeded(repoRoot, options.skipBuild)
-      app = await electron.launch({ args: [mainPath], env: launchEnv })
+      app = await electron.launch({
+        args: [mainPath],
+        // Why: a validation run must not pull the window over the developer's work.
+        env: { ...launchEnv, MANTA_BACKGROUND_LAUNCH: '1' }
+      })
       report.electronPaths = await app.evaluate(({ app: electronApp }) => ({
         home: electronApp.getPath('home'),
         userData: electronApp.getPath('userData'),

config/scripts/run-electron-vite-dev.mjs
@@ -624,6 +624,11 @@ const isHelpOrVersion = forwardedRaw.some((a) => a === '--help' || a === '-h' ||
 if (!isHelpOrVersion && process.env.MANTA_DEV_INSTANCE_LABEL) {
   console.error(`[manta-dev] Instance: ${process.env.MANTA_DEV_INSTANCE_LABEL}`)
 }
+// Why: automation launches this app while someone is working; announce that the
+// window will come up without taking the foreground so the mode is visible in logs.
+if (!isHelpOrVersion && process.env.MANTA_BACKGROUND_LAUNCH === '1') {
+  console.error('[manta-dev] Background launch: window shows without stealing focus')
+}
 let forwardedExtras = []
 if (!userPassedPort && !isHelpOrVersion) {
   const envPortRaw = process.env.REMOTE_DEBUGGING_PORT

src/main/window/foreground-activation-policy.test.ts
@@ -1,0 +1,124 @@
+import type { BrowserWindow } from 'electron'
+import { describe, expect, it, vi } from 'vitest'
+
+vi.mock('electron', () => ({ app: { dock: { hide: vi.fn() }, setActivationPolicy: vi.fn() } }))
+
+const {
+  applyBackgroundActivationPolicy,
+  isBackgroundLaunch,
+  isWindowlessLaunch,
+  showWindowWithoutStealingFocus
+} = await import('./foreground-activation-policy')
+
+function makeWindow(destroyed = false): BrowserWindow & {
+  show: ReturnType<typeof vi.fn>
+  showInactive: ReturnType<typeof vi.fn>
+} {
+  return {
+    isDestroyed: () => destroyed,
+    show: vi.fn(() => {}),
+    showInactive: vi.fn(() => {})
+  } as unknown as BrowserWindow & {
+    show: ReturnType<typeof vi.fn>
+    showInactive: ReturnType<typeof vi.fn>
+  }
+}
+
+describe('isBackgroundLaunch', () => {
+  it('covers headless and headful E2E plus opted-in dev launches', () => {
+    expect(isBackgroundLaunch({ MANTA_E2E_HEADLESS: '1' })).toBe(true)
+    expect(isBackgroundLaunch({ MANTA_E2E_HEADFUL: '1' })).toBe(true)
+    expect(isBackgroundLaunch({ MANTA_BACKGROUND_LAUNCH: '1' })).toBe(true)
+    expect(isBackgroundLaunch({})).toBe(false)
+  })
+
+  it('lets native-focus specs opt back into the foreground', () => {
+    expect(isBackgroundLaunch({ MANTA_E2E_HEADFUL: '1', MANTA_E2E_FOREGROUND: '1' })).toBe(false)
+    expect(isWindowlessLaunch({ MANTA_E2E_HEADLESS: '1', MANTA_E2E_FOREGROUND: '1' })).toBe(false)
+  })
+})
+
+describe('isWindowlessLaunch', () => {
+  it('is headless-only; a headful run still paints', () => {
+    expect(isWindowlessLaunch({ MANTA_E2E_HEADLESS: '1' })).toBe(true)
+    expect(isWindowlessLaunch({ MANTA_E2E_HEADLESS: '1', MANTA_E2E_HEADFUL: '1' })).toBe(false)
+    expect(isWindowlessLaunch({ MANTA_BACKGROUND_LAUNCH: '1' })).toBe(false)
+  })
+})
+
+describe('showWindowWithoutStealingFocus', () => {
+  it('keeps a headless window off screen', () => {
+    const window = makeWindow()
+    showWindowWithoutStealingFocus(window, { MANTA_E2E_HEADLESS: '1' })
+    expect(window.show).not.toHaveBeenCalled()
+    expect(window.showInactive).not.toHaveBeenCalled()
+  })
+
+  it('shows a background window without activating it', () => {
+    const window = makeWindow()
+    showWindowWithoutStealingFocus(window, { MANTA_BACKGROUND_LAUNCH: '1' })
+    expect(window.showInactive).toHaveBeenCalledOnce()
+    expect(window.show).not.toHaveBeenCalled()
+  })
+
+  it('shows normally for a real user launch', () => {
+    const window = makeWindow()
+    showWindowWithoutStealingFocus(window, {})
+    expect(window.show).toHaveBeenCalledOnce()
+    expect(window.showInactive).not.toHaveBeenCalled()
+  })
+
+  it('ignores a destroyed window', () => {
+    const window = makeWindow(true)
+    showWindowWithoutStealingFocus(window, {})
+    expect(window.show).not.toHaveBeenCalled()
+  })
+})
+
+describe('applyBackgroundActivationPolicy', () => {
+  function makeApp() {
+    return {
+      dock: { hide: vi.fn(() => {}) },
+      setActivationPolicy: vi.fn((_policy: 'accessory' | 'prohibited' | 'regular') => {})
+    }
+  }
+
+  it('drops the macOS Dock tile and menu bar for headless runs', () => {
+    const app = makeApp()
+    expect(
+      applyBackgroundActivationPolicy({
+        app,
+        env: { MANTA_E2E_HEADLESS: '1' },
+        platform: 'darwin'
+      })
+    ).toBe(true)
+    expect(app.dock.hide).toHaveBeenCalledOnce()
+    expect(app.setActivationPolicy).toHaveBeenCalledWith('accessory')
+  })
+
+  it('leaves a headful or user launch with its normal Dock presence', () => {
+    const headful = makeApp()
+    applyBackgroundActivationPolicy({
+      app: headful,
+      env: { MANTA_E2E_HEADLESS: '1', MANTA_E2E_HEADFUL: '1' },
+      platform: 'darwin'
+    })
+    expect(headful.setActivationPolicy).not.toHaveBeenCalled()
+
+    const user = makeApp()
+    applyBackgroundActivationPolicy({ app: user, env: {}, platform: 'darwin' })
+    expect(user.setActivationPolicy).not.toHaveBeenCalled()
+  })
+
+  it('is a no-op off macOS', () => {
+    const app = makeApp()
+    expect(
+      applyBackgroundActivationPolicy({
+        app,
+        env: { MANTA_E2E_HEADLESS: '1' },
+        platform: 'win32'
+      })
+    ).toBe(false)
+    expect(app.dock.hide).not.toHaveBeenCalled()
+  })
+})

src/main/window/foreground-activation-policy.ts
@@ -1,0 +1,83 @@
+import { app as electronApp, type BrowserWindow } from 'electron'
+
+/**
+ * Foreground policy for automated launches (E2E, benchmarks, agent-driven dev
+ * validation). These runs may use the machine, but must never take the OS
+ * foreground away from whatever the developer is doing.
+ *
+ * MANTA_BACKGROUND_LAUNCH=1 opts a normal launch in; MANTA_E2E_FOREGROUND=1 opts
+ * back out for the few specs whose subject *is* native focus (IME, key events).
+ */
+
+type ActivationPolicyApp = {
+  dock?: { hide: () => void }
+  setActivationPolicy: (policy: 'accessory' | 'prohibited' | 'regular') => void
+}
+
+/** Reads MANTA_BACKGROUND_LAUNCH, MANTA_E2E_FOREGROUND, MANTA_E2E_HEADLESS, MANTA_E2E_HEADFUL. */
+type PolicyEnv = Readonly<Record<string, string | undefined>>
+
+/** True when this process must not steal focus, raise windows, or activate the app. */
+export function isBackgroundLaunch(env: PolicyEnv = process.env): boolean {
+  if (env.MANTA_E2E_FOREGROUND === '1') {
+    return false
+  }
+  return (
+    env.MANTA_BACKGROUND_LAUNCH === '1' ||
+    env.MANTA_E2E_HEADLESS === '1' ||
+    env.MANTA_E2E_HEADFUL === '1'
+  )
+}
+
+/** True when no window should reach the screen at all (headless E2E; Playwright drives via CDP). */
+export function isWindowlessLaunch(env: PolicyEnv = process.env): boolean {
+  return isBackgroundLaunch(env) && env.MANTA_E2E_HEADLESS === '1' && env.MANTA_E2E_HEADFUL !== '1'
+}
+
+/**
+ * macOS: a windowless run still claims a Dock tile and the menu bar as it starts
+ * and exits. `accessory` drops both while leaving programmatic activation intact.
+ */
+export function applyBackgroundActivationPolicy(
+  options: {
+    app?: ActivationPolicyApp
+    env?: PolicyEnv
+    platform?: NodeJS.Platform
+    warn?: (message: string, error: unknown) => void
+  } = {}
+): boolean {
+  const platform = options.platform ?? process.platform
+  if (platform !== 'darwin' || !isWindowlessLaunch(options.env ?? process.env)) {
+    return false
+  }
+  try {
+    const app = options.app ?? electronApp
+    app.dock?.hide()
+    app.setActivationPolicy('accessory')
+    return true
+  } catch (error) {
+    options.warn?.('[window] Failed to apply background activation policy', error)
+    return false
+  }
+}
+
+/**
+ * Reveal a window without taking the foreground: hidden entirely when windowless,
+ * `showInactive()` (visible, not raised over the active app) in background launches.
+ */
+export function showWindowWithoutStealingFocus(
+  window: BrowserWindow,
+  env: PolicyEnv = process.env
+): void {
+  if (window.isDestroyed()) {
+    return
+  }
+  if (isWindowlessLaunch(env)) {
+    return
+  }
+  if (isBackgroundLaunch(env)) {
+    window.showInactive()
+    return
+  }
+  window.show()
+}