2022zhang125/orca-yunxiao

Open linked reviews in Orca browser (#17360)
* feat(sidebar): open linked reviews in Orca browser

* test(e2e): match paired window reveal assertion

* feat(sidebar): focus linked browser tabs

* ci: retry checks after cancelled rerun

src/renderer/src/components/sidebar/WorktreeCardMeta.interaction.test.tsx
@@ -116,7 +116,10 @@ describe('WorktreeCardDetailsHover interactions', () => {
     writeClipboardText.mockResolvedValue(undefined)
   })
 
-  function renderHover(onUnlinkReview = vi.fn()): ReturnType<typeof vi.fn> {
+  function renderHover(
+    onUnlinkReview = vi.fn(),
+    onOpenReviewInBrowser?: () => void
+  ): ReturnType<typeof vi.fn> {
     container = document.createElement('div')
     root = createRoot(container)
     act(() => {
@@ -130,6 +133,7 @@ describe('WorktreeCardDetailsHover interactions', () => {
           onEditComment={vi.fn()}
           onOpenReviewInOrca={vi.fn()}
           onUnlinkReview={onUnlinkReview}
+          onOpenReviewInBrowser={onOpenReviewInBrowser}
         >
           <span>Linked PR</span>
         </WorktreeCardDetailsHover>
@@ -288,6 +292,49 @@ describe('WorktreeCardDetailsHover interactions', () => {
     ).toBe('false')
   })
 
+  it('opens the review URL in Orca browser and leaves existing actions independent', () => {
+    const onOpenReviewInBrowser = vi.fn()
+    const onUnlinkReview = renderHover(vi.fn(), onOpenReviewInBrowser)
+
+    act(() => {
+      interactionMocks.onHoverOpenChange?.(true)
+      interactionMocks.onReviewMenuOpenChange?.(true)
+    })
+
+    const browserButton = Array.from(container.querySelectorAll('button')).find((button) =>
+      button.textContent?.includes('Open in Orca browser')
+    )
+
+    act(() => {
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    expect(onOpenReviewInBrowser).toHaveBeenCalledWith('https://github.com/acme/orca/pull/456')
+    expect(onUnlinkReview).not.toHaveBeenCalled()
+    expect(container.querySelector('[data-hover-open]')?.getAttribute('data-hover-open')).toBe(
+      'false'
+    )
+  })
+
+  it('preserves repeated-click behavior by forwarding each browser action', () => {
+    const onOpenReviewInBrowser = vi.fn()
+    renderHover(vi.fn(), onOpenReviewInBrowser)
+
+    act(() => {
+      interactionMocks.onReviewMenuOpenChange?.(true)
+    })
+    const browserButton = Array.from(container.querySelectorAll('button')).find((button) =>
+      button.textContent?.includes('Open in Orca browser')
+    )
+
+    act(() => {
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    expect(onOpenReviewInBrowser).toHaveBeenCalledTimes(2)
+  })
+
   it('reports clipboard failures without unlinking the review', async () => {
     writeClipboardText.mockRejectedValueOnce(new Error('clipboard unavailable'))
     const onUnlinkReview = renderHover()
@@ -309,5 +356,44 @@ describe('WorktreeCardDetailsHover interactions', () => {
     expect(writeClipboardText).toHaveBeenCalledWith('https://github.com/acme/orca/pull/456')
     expect(onUnlinkReview).not.toHaveBeenCalled()
     expect(toastMocks.error).toHaveBeenCalledWith('Failed to copy link')
+  })
+
+  it('passes a linked issue URL to the embedded-browser action', () => {
+    const onOpenIssueInBrowser = vi.fn()
+    container = document.createElement('div')
+    root = createRoot(container)
+    act(() => {
+      root.render(
+        <WorktreeCardDetailsHover
+          issue={{
+            number: 5518,
+            title: 'Agent monitor issue',
+            state: 'open',
+            url: 'https://github.com/acme/orca/issues/5518',
+            labels: []
+          }}
+          linearIssue={null}
+          review={null}
+          comment={null}
+          onOpenIssueInBrowser={onOpenIssueInBrowser}
+        >
+          <span>Linked issue</span>
+        </WorktreeCardDetailsHover>
+      )
+    })
+
+    act(() => {
+      interactionMocks.onHoverOpenChange?.(true)
+      interactionMocks.onReviewMenuOpenChange?.(true)
+    })
+    const browserButton = Array.from(container.querySelectorAll('button')).find((button) =>
+      button.textContent?.includes('Open in Orca browser')
+    )
+
+    act(() => {
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    expect(onOpenIssueInBrowser).toHaveBeenCalledWith('https://github.com/acme/orca/issues/5518')
   })
 })

src/renderer/src/components/sidebar/WorktreeCardMeta.test.tsx
@@ -130,6 +130,7 @@ describe('WorktreeCardDetailsHover', () => {
         onEditIssue={vi.fn()}
         onEditComment={vi.fn()}
         onOpenReviewInOrca={vi.fn()}
+        onOpenReviewInBrowser={vi.fn()}
         onUnlinkReview={vi.fn()}
       >
         <span>Linked PR</span>
@@ -143,10 +144,12 @@ describe('WorktreeCardDetailsHover', () => {
     expect(moreActionsIndex).toBeGreaterThan(-1)
     expect(markup).toContain('More PR actions')
     expect(markup).toContain('Copy link')
+    expect(markup).toContain('Open in Orca browser')
     expect(markup).toContain('Unlink PR')
     expect(moreActionsIndex).toBeLessThan(openInOrcaIndex)
     expect(openInOrcaIndex).toBeLessThan(viewOnGitHubIndex)
     expect(markup).not.toContain('aria-label="Unlink PR"')
+    expect(markup.indexOf('Open in Orca browser')).toBeLessThan(markup.indexOf('Copy link'))
     expect(markup.indexOf('Copy link')).toBeLessThan(markup.indexOf('Unlink PR'))
   })
 
@@ -166,6 +169,7 @@ describe('WorktreeCardDetailsHover', () => {
         onEditIssue={vi.fn()}
         onEditComment={vi.fn()}
         onOpenGitHubIssueInOrca={vi.fn()}
+        onOpenIssueInBrowser={vi.fn()}
       >
         <span>Linked issue</span>
       </WorktreeCardDetailsHover>
@@ -179,6 +183,8 @@ describe('WorktreeCardDetailsHover', () => {
 
     expect(moreActionsIndex).toBeGreaterThan(-1)
     expect(copyLinkIndex).toBeGreaterThan(-1)
+    expect(markup).toContain('Open in Orca browser')
+    expect(markup.indexOf('Open in Orca browser')).toBeLessThan(copyLinkIndex)
     expect(editIssueIndex).toBeGreaterThan(-1)
     expect(moreActionsIndex).toBeLessThan(editIssueIndex)
     expect(copyLinkIndex).toBeLessThan(editIssueIndex)
@@ -203,6 +209,7 @@ describe('WorktreeCardDetailsHover', () => {
         onEditIssue={vi.fn()}
         onEditComment={vi.fn()}
         onUnlinkReview={vi.fn()}
+        onOpenReviewInBrowser={vi.fn()}
       >
         <span>Linked MR</span>
       </WorktreeCardDetailsHover>
@@ -211,6 +218,53 @@ describe('WorktreeCardDetailsHover', () => {
     expect(markup).toContain('aria-label="More MR actions"')
     expect(markup).toContain('Unlink MR')
     expect(markup).toContain('View on GitLab')
+    expect(markup).toContain('Open in Orca browser')
+  })
+
+  it('hides the embedded-browser action when a linked review has no URL', () => {
+    const markup = renderToStaticMarkup(
+      <WorktreeCardDetailsHover
+        issue={null}
+        linearIssue={null}
+        review={{
+          provider: 'github',
+          number: 456,
+          title: 'Loading PR...',
+          state: 'open'
+        }}
+        comment={null}
+        onOpenReviewInBrowser={vi.fn()}
+      >
+        <span>Linked PR</span>
+      </WorktreeCardDetailsHover>
+    )
+
+    expect(markup).not.toContain('Open in Orca browser')
+  })
+
+  it('keeps the embedded-browser action provider-neutral for unsupported review URLs', () => {
+    const markup = renderToStaticMarkup(
+      <WorktreeCardDetailsHover
+        issue={null}
+        linearIssue={null}
+        review={{
+          provider: 'unsupported',
+          number: 12,
+          title: 'Review from an unsupported provider',
+          state: 'open',
+          url: 'https://code.example.test/reviews/12',
+          status: 'neutral',
+          updatedAt: '2026-05-17T00:00:00.000Z',
+          mergeable: 'UNKNOWN'
+        }}
+        comment={null}
+        onOpenReviewInBrowser={vi.fn()}
+      >
+        <span>Linked review</span>
+      </WorktreeCardDetailsHover>
+    )
+
+    expect(markup).toContain('Open in Orca browser')
   })
 
   it('displays Linear issue details with link', () => {

src/renderer/src/components/sidebar/WorktreeCardReviewDetailSection.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
-import { Copy, Ellipsis, ExternalLink, MonitorUp, Unlink } from 'lucide-react'
+import { Copy, Ellipsis, ExternalLink, Globe, MonitorUp, Unlink } from 'lucide-react'
 import { translate } from '@/i18n/i18n'
 import {
   WorktreeCardDetailSection,
@@ -24,6 +24,7 @@ type WorktreeCardReviewDetailSectionProps = {
   onReviewMenuOpenChange: (open: boolean) => void
   onOpenReviewInOrca?: (event: React.MouseEvent) => void
   onCopyReviewLink?: () => void
+  onOpenReviewInBrowser?: (url: string) => void
   onUnlinkReview?: () => void
   closeHover: () => void
 }
@@ -34,6 +35,7 @@ export function WorktreeCardReviewDetailSection({
   onReviewMenuOpenChange,
   onOpenReviewInOrca,
   onCopyReviewLink,
+  onOpenReviewInBrowser,
   onUnlinkReview,
   closeHover
 }: WorktreeCardReviewDetailSectionProps): React.JSX.Element | null {
@@ -78,7 +80,7 @@ export function WorktreeCardReviewDetailSection({
         )}
         actions={
           <>
-            {(onCopyReviewLink || onUnlinkReview) && (
+            {(onCopyReviewLink || onOpenReviewInBrowser || onUnlinkReview) && (
               <DropdownMenu
                 modal={false}
                 open={reviewMenuOpen}
@@ -95,6 +97,20 @@ export function WorktreeCardReviewDetailSection({
                   </Tooltip>
                 )}
                 <DropdownMenuContent align="end" className="w-40">
+                  {onOpenReviewInBrowser && (
+                    <DropdownMenuItem
+                      onSelect={() => {
+                        closeHover()
+                        onOpenReviewInBrowser(review.url!)
+                      }}
+                    >
+                      <Globe className="size-3.5" />
+                      {translate(
+                        'auto.components.sidebar.WorktreeCardMeta.openInOrcaBrowser',
+                        'Open in Orca browser'
+                      )}
+                    </DropdownMenuItem>
+                  )}
                   {onCopyReviewLink && (
                     <DropdownMenuItem
                       onSelect={() => {

src/renderer/src/lib/workspace-browser-tab-open.test.ts
@@ -104,6 +104,31 @@ describe('openWorkspaceBrowserTab', () => {
     expect(mocks.createRemote).not.toHaveBeenCalled()
   })
 
+  it('creates a client tab without activating it when requested', async () => {
+    const createBrowserTab = vi.fn()
+    const sshHost = toSshExecutionHostId('ssh-target')
+    mocks.state = {
+      ...ownerState(sshHost),
+      createBrowserTab,
+      defaultBrowserSessionProfileId: 'focused-profile',
+      defaultBrowserSessionProfileIdByHostId: { [sshHost]: 'ssh-profile' }
+    }
+
+    await openWorkspaceBrowserTab({
+      workspaceId: WORKSPACE_ID,
+      url: 'https://github.com/acme/orca/pull/456',
+      intent: { kind: 'url' },
+      focusOnCreate: false,
+      selectWorktree: false
+    })
+
+    expect(createBrowserTab).toHaveBeenCalledWith(
+      WORKSPACE_ID,
+      'https://github.com/acme/orca/pull/456',
+      expect.objectContaining({ activate: false })
+    )
+  })
+
   it('fails closed when the asserted SSH browser route is opted out or belongs to another host', async () => {
     const sshHost = toSshExecutionHostId('ssh-target')
     mocks.state = {
@@ -162,6 +187,32 @@ describe('openWorkspaceBrowserTab', () => {
       failureLogMode: 'operation-only'
     })
     expect(createBrowserTab).not.toHaveBeenCalled()
+  })
+
+  it('stages a runtime tab without selecting the worktree or focusing the browser', async () => {
+    mocks.state = {
+      ...ownerState(toRuntimeExecutionHostId('hub-a')),
+      ...browserCapableRuntime('hub-a'),
+      createBrowserTab: vi.fn(),
+      defaultBrowserSessionProfileId: 'client-profile',
+      defaultBrowserSessionProfileIdByHostId: {}
+    }
+
+    await openWorkspaceBrowserTab({
+      workspaceId: WORKSPACE_ID,
+      url: 'https://gitlab.com/acme/orca/-/merge_requests/77',
+      intent: { kind: 'url' },
+      focusOnCreate: false,
+      selectWorktree: false
+    })
+
+    expect(mocks.createRemote).toHaveBeenCalledWith(
+      expect.objectContaining({
+        focusOnCreate: false,
+        selectWorktree: false,
+        url: 'https://gitlab.com/acme/orca/-/merge_requests/77'
+      })
+    )
   })
 
   it('waits for host registration before reconciling an asserted runtime link', async () => {

paidaxingyo666/Manta

src/renderer/src/components/sidebar/WorktreeCardMeta.interaction.test.tsx
@@ -116,7 +116,10 @@ describe('WorktreeCardDetailsHover interactions', () => {
     writeClipboardText.mockResolvedValue(undefined)
   })
 
-  function renderHover(onUnlinkReview = vi.fn()): ReturnType<typeof vi.fn> {
+  function renderHover(
+    onUnlinkReview = vi.fn(),
+    onOpenReviewInBrowser?: () => void
+  ): ReturnType<typeof vi.fn> {
     container = document.createElement('div')
     root = createRoot(container)
     act(() => {
@@ -130,6 +133,7 @@ describe('WorktreeCardDetailsHover interactions', () => {
           onEditComment={vi.fn()}
           onOpenReviewInManta={vi.fn()}
           onUnlinkReview={onUnlinkReview}
+          onOpenReviewInBrowser={onOpenReviewInBrowser}
         >
           <span>Linked PR</span>
         </WorktreeCardDetailsHover>
@@ -288,6 +292,49 @@ describe('WorktreeCardDetailsHover interactions', () => {
     ).toBe('false')
   })
 
+  it('opens the review URL in Manta browser and leaves existing actions independent', () => {
+    const onOpenReviewInBrowser = vi.fn()
+    const onUnlinkReview = renderHover(vi.fn(), onOpenReviewInBrowser)
+
+    act(() => {
+      interactionMocks.onHoverOpenChange?.(true)
+      interactionMocks.onReviewMenuOpenChange?.(true)
+    })
+
+    const browserButton = Array.from(container.querySelectorAll('button')).find((button) =>
+      button.textContent?.includes('Open in Manta browser')
+    )
+
+    act(() => {
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    expect(onOpenReviewInBrowser).toHaveBeenCalledWith('https://github.com/acme/manta/pull/456')
+    expect(onUnlinkReview).not.toHaveBeenCalled()
+    expect(container.querySelector('[data-hover-open]')?.getAttribute('data-hover-open')).toBe(
+      'false'
+    )
+  })
+
+  it('preserves repeated-click behavior by forwarding each browser action', () => {
+    const onOpenReviewInBrowser = vi.fn()
+    renderHover(vi.fn(), onOpenReviewInBrowser)
+
+    act(() => {
+      interactionMocks.onReviewMenuOpenChange?.(true)
+    })
+    const browserButton = Array.from(container.querySelectorAll('button')).find((button) =>
+      button.textContent?.includes('Open in Manta browser')
+    )
+
+    act(() => {
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    expect(onOpenReviewInBrowser).toHaveBeenCalledTimes(2)
+  })
+
   it('reports clipboard failures without unlinking the review', async () => {
     writeClipboardText.mockRejectedValueOnce(new Error('clipboard unavailable'))
     const onUnlinkReview = renderHover()
@@ -309,5 +356,44 @@ describe('WorktreeCardDetailsHover interactions', () => {
     expect(writeClipboardText).toHaveBeenCalledWith('https://github.com/acme/manta/pull/456')
     expect(onUnlinkReview).not.toHaveBeenCalled()
     expect(toastMocks.error).toHaveBeenCalledWith('Failed to copy link')
+  })
+
+  it('passes a linked issue URL to the embedded-browser action', () => {
+    const onOpenIssueInBrowser = vi.fn()
+    container = document.createElement('div')
+    root = createRoot(container)
+    act(() => {
+      root.render(
+        <WorktreeCardDetailsHover
+          issue={{
+            number: 5518,
+            title: 'Agent monitor issue',
+            state: 'open',
+            url: 'https://github.com/acme/manta/issues/5518',
+            labels: []
+          }}
+          linearIssue={null}
+          review={null}
+          comment={null}
+          onOpenIssueInBrowser={onOpenIssueInBrowser}
+        >
+          <span>Linked issue</span>
+        </WorktreeCardDetailsHover>
+      )
+    })
+
+    act(() => {
+      interactionMocks.onHoverOpenChange?.(true)
+      interactionMocks.onReviewMenuOpenChange?.(true)
+    })
+    const browserButton = Array.from(container.querySelectorAll('button')).find((button) =>
+      button.textContent?.includes('Open in Manta browser')
+    )
+
+    act(() => {
+      browserButton?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    expect(onOpenIssueInBrowser).toHaveBeenCalledWith('https://github.com/acme/manta/issues/5518')
   })
 })

src/renderer/src/components/sidebar/WorktreeCardMeta.test.tsx
@@ -130,6 +130,7 @@ describe('WorktreeCardDetailsHover', () => {
         onEditIssue={vi.fn()}
         onEditComment={vi.fn()}
         onOpenReviewInManta={vi.fn()}
+        onOpenReviewInBrowser={vi.fn()}
         onUnlinkReview={vi.fn()}
       >
         <span>Linked PR</span>
@@ -143,10 +144,12 @@ describe('WorktreeCardDetailsHover', () => {
     expect(moreActionsIndex).toBeGreaterThan(-1)
     expect(markup).toContain('More PR actions')
     expect(markup).toContain('Copy link')
+    expect(markup).toContain('Open in Manta browser')
     expect(markup).toContain('Unlink PR')
     expect(moreActionsIndex).toBeLessThan(openInMantaIndex)
     expect(openInMantaIndex).toBeLessThan(viewOnGitHubIndex)
     expect(markup).not.toContain('aria-label="Unlink PR"')
+    expect(markup.indexOf('Open in Manta browser')).toBeLessThan(markup.indexOf('Copy link'))
     expect(markup.indexOf('Copy link')).toBeLessThan(markup.indexOf('Unlink PR'))
   })
 
@@ -166,6 +169,7 @@ describe('WorktreeCardDetailsHover', () => {
         onEditIssue={vi.fn()}
         onEditComment={vi.fn()}
         onOpenGitHubIssueInManta={vi.fn()}
+        onOpenIssueInBrowser={vi.fn()}
       >
         <span>Linked issue</span>
       </WorktreeCardDetailsHover>
@@ -179,6 +183,8 @@ describe('WorktreeCardDetailsHover', () => {
 
     expect(moreActionsIndex).toBeGreaterThan(-1)
     expect(copyLinkIndex).toBeGreaterThan(-1)
+    expect(markup).toContain('Open in Manta browser')
+    expect(markup.indexOf('Open in Manta browser')).toBeLessThan(copyLinkIndex)
     expect(editIssueIndex).toBeGreaterThan(-1)
     expect(moreActionsIndex).toBeLessThan(editIssueIndex)
     expect(copyLinkIndex).toBeLessThan(editIssueIndex)
@@ -203,6 +209,7 @@ describe('WorktreeCardDetailsHover', () => {
         onEditIssue={vi.fn()}
         onEditComment={vi.fn()}
         onUnlinkReview={vi.fn()}
+        onOpenReviewInBrowser={vi.fn()}
       >
         <span>Linked MR</span>
       </WorktreeCardDetailsHover>
@@ -211,6 +218,53 @@ describe('WorktreeCardDetailsHover', () => {
     expect(markup).toContain('aria-label="More MR actions"')
     expect(markup).toContain('Unlink MR')
     expect(markup).toContain('View on GitLab')
+    expect(markup).toContain('Open in Manta browser')
+  })
+
+  it('hides the embedded-browser action when a linked review has no URL', () => {
+    const markup = renderToStaticMarkup(
+      <WorktreeCardDetailsHover
+        issue={null}
+        linearIssue={null}
+        review={{
+          provider: 'github',
+          number: 456,
+          title: 'Loading PR...',
+          state: 'open'
+        }}
+        comment={null}
+        onOpenReviewInBrowser={vi.fn()}
+      >
+        <span>Linked PR</span>
+      </WorktreeCardDetailsHover>
+    )
+
+    expect(markup).not.toContain('Open in Manta browser')
+  })
+
+  it('keeps the embedded-browser action provider-neutral for unsupported review URLs', () => {
+    const markup = renderToStaticMarkup(
+      <WorktreeCardDetailsHover
+        issue={null}
+        linearIssue={null}
+        review={{
+          provider: 'unsupported',
+          number: 12,
+          title: 'Review from an unsupported provider',
+          state: 'open',
+          url: 'https://code.example.test/reviews/12',
+          status: 'neutral',
+          updatedAt: '2026-05-17T00:00:00.000Z',
+          mergeable: 'UNKNOWN'
+        }}
+        comment={null}
+        onOpenReviewInBrowser={vi.fn()}
+      >
+        <span>Linked review</span>
+      </WorktreeCardDetailsHover>
+    )
+
+    expect(markup).toContain('Open in Manta browser')
   })
 
   it('displays Linear issue details with link', () => {

src/renderer/src/components/sidebar/WorktreeCardReviewDetailSection.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
-import { Copy, Ellipsis, ExternalLink, MonitorUp, Unlink } from 'lucide-react'
+import { Copy, Ellipsis, ExternalLink, Globe, MonitorUp, Unlink } from 'lucide-react'
 import { translate } from '@/i18n/i18n'
 import {
   WorktreeCardDetailSection,
@@ -24,6 +24,7 @@ type WorktreeCardReviewDetailSectionProps = {
   onReviewMenuOpenChange: (open: boolean) => void
   onOpenReviewInManta?: (event: React.MouseEvent) => void
   onCopyReviewLink?: () => void
+  onOpenReviewInBrowser?: (url: string) => void
   onUnlinkReview?: () => void
   closeHover: () => void
 }
@@ -34,6 +35,7 @@ export function WorktreeCardReviewDetailSection({
   onReviewMenuOpenChange,
   onOpenReviewInManta,
   onCopyReviewLink,
+  onOpenReviewInBrowser,
   onUnlinkReview,
   closeHover
 }: WorktreeCardReviewDetailSectionProps): React.JSX.Element | null {
@@ -78,7 +80,7 @@ export function WorktreeCardReviewDetailSection({
         )}
         actions={
           <>
-            {(onCopyReviewLink || onUnlinkReview) && (
+            {(onCopyReviewLink || onOpenReviewInBrowser || onUnlinkReview) && (
               <DropdownMenu
                 modal={false}
                 open={reviewMenuOpen}
@@ -95,6 +97,20 @@ export function WorktreeCardReviewDetailSection({
                   </Tooltip>
                 )}
                 <DropdownMenuContent align="end" className="w-40">
+                  {onOpenReviewInBrowser && (
+                    <DropdownMenuItem
+                      onSelect={() => {
+                        closeHover()
+                        onOpenReviewInBrowser(review.url!)
+                      }}
+                    >
+                      <Globe className="size-3.5" />
+                      {translate(
+                        'auto.components.sidebar.WorktreeCardMeta.openInMantaBrowser',
+                        'Open in Manta browser'
+                      )}
+                    </DropdownMenuItem>
+                  )}
                   {onCopyReviewLink && (
                     <DropdownMenuItem
                       onSelect={() => {

src/renderer/src/lib/workspace-browser-tab-open.test.ts
@@ -104,6 +104,31 @@ describe('openWorkspaceBrowserTab', () => {
     expect(mocks.createRemote).not.toHaveBeenCalled()
   })
 
+  it('creates a client tab without activating it when requested', async () => {
+    const createBrowserTab = vi.fn()
+    const sshHost = toSshExecutionHostId('ssh-target')
+    mocks.state = {
+      ...ownerState(sshHost),
+      createBrowserTab,
+      defaultBrowserSessionProfileId: 'focused-profile',
+      defaultBrowserSessionProfileIdByHostId: { [sshHost]: 'ssh-profile' }
+    }
+
+    await openWorkspaceBrowserTab({
+      workspaceId: WORKSPACE_ID,
+      url: 'https://github.com/acme/manta/pull/456',
+      intent: { kind: 'url' },
+      focusOnCreate: false,
+      selectWorktree: false
+    })
+
+    expect(createBrowserTab).toHaveBeenCalledWith(
+      WORKSPACE_ID,
+      'https://github.com/acme/manta/pull/456',
+      expect.objectContaining({ activate: false })
+    )
+  })
+
   it('fails closed when the asserted SSH browser route is opted out or belongs to another host', async () => {
     const sshHost = toSshExecutionHostId('ssh-target')
     mocks.state = {
@@ -162,6 +187,32 @@ describe('openWorkspaceBrowserTab', () => {
       failureLogMode: 'operation-only'
     })
     expect(createBrowserTab).not.toHaveBeenCalled()
+  })
+
+  it('stages a runtime tab without selecting the worktree or focusing the browser', async () => {
+    mocks.state = {
+      ...ownerState(toRuntimeExecutionHostId('hub-a')),
+      ...browserCapableRuntime('hub-a'),
+      createBrowserTab: vi.fn(),
+      defaultBrowserSessionProfileId: 'client-profile',
+      defaultBrowserSessionProfileIdByHostId: {}
+    }
+
+    await openWorkspaceBrowserTab({
+      workspaceId: WORKSPACE_ID,
+      url: 'https://gitlab.com/acme/manta/-/merge_requests/77',
+      intent: { kind: 'url' },
+      focusOnCreate: false,
+      selectWorktree: false
+    })
+
+    expect(mocks.createRemote).toHaveBeenCalledWith(
+      expect.objectContaining({
+        focusOnCreate: false,
+        selectWorktree: false,
+        url: 'https://gitlab.com/acme/manta/-/merge_requests/77'
+      })
+    )
   })
 
   it('waits for host registration before reconciling an asserted runtime link', async () => {