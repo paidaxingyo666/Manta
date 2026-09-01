2022zhang125/orca-yunxiao

Split up combined-diff viewer into feature-organized modules (#17341)
* Reorganize combined-diff components into feature-organized structure

Splits flat combined-diff files into feature-focused subdirectories
(browse-files, load-sections, resolve-changes, review-controls,
scroll-viewport) to improve code organization and reduce clutter in
the editor directory. Groups related logic by concern for easier
navigation and maintenance.

* Split up combined-diff viewer into feature-organized modules

Decompose the 221-line monolithic CombinedDiffViewer into smaller, focused modules organized by feature: entry resolution, section loading, view state memory, file tree navigation, review controls, and scroll viewport handling. Main component now composes these hooks to orchestrate the combined-diff view.

* fix(combined-diff): prevent replayed preference writes

Move preference write outside state updater callback since React may
replay state updaters, causing multiple writes. Add sideBySide to
dependency array.

* fix(combined-diff): re-resolve sections by key to handle list rebuilds

The section list can rebuild while a write is pending (due to rebase, file changes, etc.); re-resolve by key instead of stale index to apply updates to the correct section.

- Convert skipped conflicts message to structured i18n plural forms
- Add oldPath field to git status signature for rename tracking

* Suppress react-doctor diagnostics in combined-diff feature

Add suppressions for react-doctor diagnostics that are necessary patterns
for the combined-diff implementation, configured in both the quality check
script and package.json.

src/renderer/src/components/editor/combined-diff/load-sections/use-combined-diff-section-revalidation.ts
@@ -1,0 +1,108 @@
+import React, { useEffect, useRef } from 'react'
+import type { OpenFile } from '@/store/slices/editor'
+import type { GitStatusEntry } from '../../../../../../shared/git-status-types'
+import type { DiffSection } from '../../diff-section-types'
+import {
+  ORCA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT,
+  type EditorPathMutationTarget
+} from '../../editor-autosave'
+import { buildCombinedGitStatusSignature } from '../resolve-changes/combined-diff-git-status-signature'
+import {
+  getCombinedDiffFileTreeSectionKey,
+  type CombinedDiffFileTreeMode
+} from '../resolve-changes/combined-diff-section-identity'
+import type { CombinedDiffSectionLoadRegistry } from './combined-diff-section-load-registry'
+
+// Why: git status and on-disk writes both revalidate loaded rows; one hook owns both watches so
+// they cannot disagree about which sections are eligible.
+export function useCombinedDiffSectionRevalidation({
+  file,
+  gitStatusEntries,
+  registry,
+  requestSectionReload,
+  sectionIndexByKeyRef,
+  sections,
+  shouldAutoReloadFromGitStatus,
+  treeMode
+}: {
+  file: OpenFile
+  gitStatusEntries: GitStatusEntry[]
+  registry: CombinedDiffSectionLoadRegistry
+  requestSectionReload: (index: number) => void
+  sectionIndexByKeyRef: React.RefObject<ReadonlyMap<string, number>>
+  sections: DiffSection[]
+  shouldAutoReloadFromGitStatus: boolean
+  treeMode: CombinedDiffFileTreeMode
+}): string {
+  const { loadedIndicesRef } = registry
+  const combinedGitStatusSignature = React.useMemo(() => {
+    if (!shouldAutoReloadFromGitStatus) {
+      return ''
+    }
+    return buildCombinedGitStatusSignature(sections, gitStatusEntries)
+  }, [gitStatusEntries, sections, shouldAutoReloadFromGitStatus])
+  const prevCombinedGitStatusSignatureRef = useRef<string | null>(null)
+
+  useEffect(() => {
+    if (!shouldAutoReloadFromGitStatus) {
+      prevCombinedGitStatusSignatureRef.current = null
+      return
+    }
+    if (prevCombinedGitStatusSignatureRef.current === null) {
+      prevCombinedGitStatusSignatureRef.current = combinedGitStatusSignature
+      return
+    }
+    if (prevCombinedGitStatusSignatureRef.current === combinedGitStatusSignature) {
+      return
+    }
+    prevCombinedGitStatusSignatureRef.current = combinedGitStatusSignature
+    for (const index of loadedIndicesRef.current) {
+      requestSectionReload(index)
+    }
+  }, [
+    combinedGitStatusSignature,
+    loadedIndicesRef,
+    requestSectionReload,
+    shouldAutoReloadFromGitStatus
+  ])
+
+  useEffect(() => {
+    if (treeMode !== 'all' && treeMode !== 'uncommitted') {
+      return
+    }
+    const handler = (event: Event): void => {
+      const detail = (event as CustomEvent<EditorPathMutationTarget>).detail
+      if (!detail || detail.worktreeId !== file.worktreeId) {
+        return
+      }
+      const hasRuntimeOwnerFilter = Object.hasOwn(detail, 'runtimeEnvironmentId')
+      const targetRuntimeOwner = detail.runtimeEnvironmentId?.trim() || null
+      const fileRuntimeOwner = file.runtimeEnvironmentId?.trim() || null
+      if (hasRuntimeOwnerFilter && targetRuntimeOwner !== fileRuntimeOwner) {
+        return
+      }
+      for (const area of ['unstaged', 'staged', 'untracked'] as const) {
+        const key = getCombinedDiffFileTreeSectionKey('uncommitted', {
+          path: detail.relativePath,
+          status: 'modified',
+          area
+        })
+        const index = sectionIndexByKeyRef.current.get(key)
+        if (index !== undefined) {
+          requestSectionReload(index)
+        }
+      }
+    }
+    window.addEventListener(ORCA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT, handler as EventListener)
+    return () =>
+      window.removeEventListener(ORCA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT, handler as EventListener)
+  }, [
+    file.runtimeEnvironmentId,
+    file.worktreeId,
+    requestSectionReload,
+    sectionIndexByKeyRef,
+    treeMode
+  ])
+
+  return combinedGitStatusSignature
+}

src/renderer/src/components/editor/combined-diff/remember-view/combined-diff-view-memory.ts
@@ -1,0 +1,45 @@
+import type { VirtualizedScrollAnchor } from '@/hooks/useVirtualizedScrollAnchor'
+import type { DiffSection } from '../../diff-section-types'
+import {
+  ORCA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT,
+  type EditorPathMutationTarget
+} from '../../editor-autosave'
+
+export type CachedCombinedDiffViewState = {
+  entrySignature: string
+  gitStatusSignature: string
+  sections: DiffSection[]
+  sectionHeights: Record<number, number>
+  loadedIndices: number[]
+  scrollTop: number
+  sideBySide: boolean
+}
+
+export const combinedDiffViewStateCache = new Map<string, CachedCombinedDiffViewState>()
+export const combinedDiffScrollTopCache = new Map<string, number>()
+export const combinedDiffScrollAnchorCache = new Map<string, VirtualizedScrollAnchor>()
+
+// Why: session-scoped toolbar choices outlive the unmount, so they are module state rather than component state.
+export const combinedDiffViewPreferences: {
+  collapsed: boolean | null
+  sideBySide: boolean | null
+  fileTreeCollapsed: boolean | null
+} = { collapsed: null, sideBySide: null, fileTreeCollapsed: null }
+
+function invalidateCombinedDiffCachesForRelativePath(relativePath: string): void {
+  for (const [key, cached] of combinedDiffViewStateCache.entries()) {
+    if (cached.sections.some((section) => section.path === relativePath)) {
+      combinedDiffViewStateCache.delete(key)
+    }
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.addEventListener(ORCA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT, (event) => {
+    const detail = (event as CustomEvent<EditorPathMutationTarget>).detail
+    if (detail?.relativePath) {
+      // Why: inactive combined-diff tabs are unmounted, so only a module-level cache bust stops a remount replaying stale bodies.
+      invalidateCombinedDiffCachesForRelativePath(detail.relativePath)
+    }
+  })
+}

paidaxingyo666/Manta

src/renderer/src/components/editor/combined-diff/load-sections/use-combined-diff-section-revalidation.ts
@@ -1,0 +1,108 @@
+import React, { useEffect, useRef } from 'react'
+import type { OpenFile } from '@/store/slices/editor'
+import type { GitStatusEntry } from '../../../../../../shared/git-status-types'
+import type { DiffSection } from '../../diff-section-types'
+import {
+  MANTA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT,
+  type EditorPathMutationTarget
+} from '../../editor-autosave'
+import { buildCombinedGitStatusSignature } from '../resolve-changes/combined-diff-git-status-signature'
+import {
+  getCombinedDiffFileTreeSectionKey,
+  type CombinedDiffFileTreeMode
+} from '../resolve-changes/combined-diff-section-identity'
+import type { CombinedDiffSectionLoadRegistry } from './combined-diff-section-load-registry'
+
+// Why: git status and on-disk writes both revalidate loaded rows; one hook owns both watches so
+// they cannot disagree about which sections are eligible.
+export function useCombinedDiffSectionRevalidation({
+  file,
+  gitStatusEntries,
+  registry,
+  requestSectionReload,
+  sectionIndexByKeyRef,
+  sections,
+  shouldAutoReloadFromGitStatus,
+  treeMode
+}: {
+  file: OpenFile
+  gitStatusEntries: GitStatusEntry[]
+  registry: CombinedDiffSectionLoadRegistry
+  requestSectionReload: (index: number) => void
+  sectionIndexByKeyRef: React.RefObject<ReadonlyMap<string, number>>
+  sections: DiffSection[]
+  shouldAutoReloadFromGitStatus: boolean
+  treeMode: CombinedDiffFileTreeMode
+}): string {
+  const { loadedIndicesRef } = registry
+  const combinedGitStatusSignature = React.useMemo(() => {
+    if (!shouldAutoReloadFromGitStatus) {
+      return ''
+    }
+    return buildCombinedGitStatusSignature(sections, gitStatusEntries)
+  }, [gitStatusEntries, sections, shouldAutoReloadFromGitStatus])
+  const prevCombinedGitStatusSignatureRef = useRef<string | null>(null)
+
+  useEffect(() => {
+    if (!shouldAutoReloadFromGitStatus) {
+      prevCombinedGitStatusSignatureRef.current = null
+      return
+    }
+    if (prevCombinedGitStatusSignatureRef.current === null) {
+      prevCombinedGitStatusSignatureRef.current = combinedGitStatusSignature
+      return
+    }
+    if (prevCombinedGitStatusSignatureRef.current === combinedGitStatusSignature) {
+      return
+    }
+    prevCombinedGitStatusSignatureRef.current = combinedGitStatusSignature
+    for (const index of loadedIndicesRef.current) {
+      requestSectionReload(index)
+    }
+  }, [
+    combinedGitStatusSignature,
+    loadedIndicesRef,
+    requestSectionReload,
+    shouldAutoReloadFromGitStatus
+  ])
+
+  useEffect(() => {
+    if (treeMode !== 'all' && treeMode !== 'uncommitted') {
+      return
+    }
+    const handler = (event: Event): void => {
+      const detail = (event as CustomEvent<EditorPathMutationTarget>).detail
+      if (!detail || detail.worktreeId !== file.worktreeId) {
+        return
+      }
+      const hasRuntimeOwnerFilter = Object.hasOwn(detail, 'runtimeEnvironmentId')
+      const targetRuntimeOwner = detail.runtimeEnvironmentId?.trim() || null
+      const fileRuntimeOwner = file.runtimeEnvironmentId?.trim() || null
+      if (hasRuntimeOwnerFilter && targetRuntimeOwner !== fileRuntimeOwner) {
+        return
+      }
+      for (const area of ['unstaged', 'staged', 'untracked'] as const) {
+        const key = getCombinedDiffFileTreeSectionKey('uncommitted', {
+          path: detail.relativePath,
+          status: 'modified',
+          area
+        })
+        const index = sectionIndexByKeyRef.current.get(key)
+        if (index !== undefined) {
+          requestSectionReload(index)
+        }
+      }
+    }
+    window.addEventListener(MANTA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT, handler as EventListener)
+    return () =>
+      window.removeEventListener(MANTA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT, handler as EventListener)
+  }, [
+    file.runtimeEnvironmentId,
+    file.worktreeId,
+    requestSectionReload,
+    sectionIndexByKeyRef,
+    treeMode
+  ])
+
+  return combinedGitStatusSignature
+}

src/renderer/src/components/editor/combined-diff/remember-view/combined-diff-view-memory.ts
@@ -1,0 +1,45 @@
+import type { VirtualizedScrollAnchor } from '@/hooks/useVirtualizedScrollAnchor'
+import type { DiffSection } from '../../diff-section-types'
+import {
+  MANTA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT,
+  type EditorPathMutationTarget
+} from '../../editor-autosave'
+
+export type CachedCombinedDiffViewState = {
+  entrySignature: string
+  gitStatusSignature: string
+  sections: DiffSection[]
+  sectionHeights: Record<number, number>
+  loadedIndices: number[]
+  scrollTop: number
+  sideBySide: boolean
+}
+
+export const combinedDiffViewStateCache = new Map<string, CachedCombinedDiffViewState>()
+export const combinedDiffScrollTopCache = new Map<string, number>()
+export const combinedDiffScrollAnchorCache = new Map<string, VirtualizedScrollAnchor>()
+
+// Why: session-scoped toolbar choices outlive the unmount, so they are module state rather than component state.
+export const combinedDiffViewPreferences: {
+  collapsed: boolean | null
+  sideBySide: boolean | null
+  fileTreeCollapsed: boolean | null
+} = { collapsed: null, sideBySide: null, fileTreeCollapsed: null }
+
+function invalidateCombinedDiffCachesForRelativePath(relativePath: string): void {
+  for (const [key, cached] of combinedDiffViewStateCache.entries()) {
+    if (cached.sections.some((section) => section.path === relativePath)) {
+      combinedDiffViewStateCache.delete(key)
+    }
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.addEventListener(MANTA_EDITOR_EXTERNAL_FILE_CHANGE_EVENT, (event) => {
+    const detail = (event as CustomEvent<EditorPathMutationTarget>).detail
+    if (detail?.relativePath) {
+      // Why: inactive combined-diff tabs are unmounted, so only a module-level cache bust stops a remount replaying stale bodies.
+      invalidateCombinedDiffCachesForRelativePath(detail.relativePath)
+    }
+  })
+}